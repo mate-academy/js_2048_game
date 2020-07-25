26576638a72ef29b710e20943311a07a12382a68
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-07-25 15:45:28 +0300

    Working version of the game. Enjoy!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,38 @@ const startButton = controls.querySelector('.start');
 const gameField = container.querySelector('.game-field');
 const messages = container.querySelector('.message-container');
 const startMessage = messages.querySelector('.message-start');
-// const loseMessage = messages.querySelector('.message-lose');
+const loseMessage = messages.querySelector('.message-lose');
 const winMessage = messages.querySelector('.message-win');
 const scoreValue = container.querySelector('.game-score');
-let arrField = [
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-];
+const winNumber = 2048;
+let totalScore = 0;
+let arrField = [];
+const FIELD_SIZE = {
+  rows: 4,
+  columns: 4,
+};
+const ARROWS = {
+  'ArrowUp': 'ArrowUp',
+  'ArrowRight': 'ArrowRight',
+  'ArrowDown': 'ArrowDown',
+  'ArrowLeft': 'ArrowLeft',
+};
+
+// Create array field
+
+function createArrField(arrRows, arrColumns) {
+  arrField = new Array(arrRows);
+
+  for (let row = 0; row < arrRows; row++) {
+    arrField[row] = new Array(arrColumns);
+
+    for (let column = 0; column < arrColumns; column++) {
+      arrField[row][column] = 0;
+    }
+  }
+}
+
+createArrField(FIELD_SIZE.rows, FIELD_SIZE.columns);
 
 // Create restart button
 
@@ -34,37 +57,40 @@ function createTwoNewItem() {
   newRandomCell();
 }
 
-// Change start button to restart
+// Start game & change start button to restart
 
 startButton.addEventListener('click', event => {
   startButton.remove();
   controls.append(createRestartButton());
   startMessage.classList.add('hidden');
   createTwoNewItem();
-  calculateScore();
-  loopArray(buildGameField);
+  processField(buildGameField);
+  addKeysListener();
 });
 
-// Reset game field
+// Restart game & reset game field
 
 restartButton.addEventListener('click', event => {
-  arrField = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  resetScore();
+  createArrField(FIELD_SIZE.rows, FIELD_SIZE.columns);
   createTwoNewItem();
-  calculateScore();
-  loopArray(buildGameField);
+  processField(buildGameField);
+  addKeysListener();
 });
 
 // Win message
 
 function checkWin(number) {
-  if (number === 2048) {
+  if (number === winNumber) {
     winMessage.classList.remove('hidden');
+    removeKeysListener();
   }
+}
+
+// Lose message
+
+function lose() {
+  loseMessage.classList.remove('hidden');
 }
 
 // Build game field
@@ -85,42 +111,58 @@ function buildGameField(row, column) {
 // 10% random
 
 function randomValue() {
-  if (Math.random() < 0.1) {
+  if (Math.random() <= 0.1) {
     return 4;
   } else {
     return 2;
   }
 }
 
-// Random position
+// Reset score
 
-function randomPosition() {
-  return {
-    row: Math.floor(Math.random() * 4),
-    column: Math.floor(Math.random() * 4),
-  };
+function resetScore() {
+  totalScore = 0;
+  scoreValue.textContent = totalScore;
 }
 
 // Calculate score
 
-function calculateScore() {
-  let totalScore = 0;
+function calculateScore(cellValue) {
+  totalScore += cellValue;
+  scoreValue.textContent = totalScore;
+}
 
-  for (const row of arrField) {
-    for (const column of row) {
-      totalScore += column;
+// Random position
+
+function randomPosition() {
+  const freeArr = [];
+
+  for (let row = 0; row < arrField.length; row++) {
+    for (let column = 0; column < arrField[row].length; column++) {
+      if (arrField[row][column] === 0) {
+        freeArr.push([row, column]);
+      }
     }
   }
 
-  scoreValue.textContent = totalScore;
+  return checkFreeCell(freeArr);
+}
+
+// Check free cell
+
+function checkFreeCell(array) {
+  if (array.length > 0) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 }
 
 // New random cell
 
 function newRandomCell() {
-  const { row, column } = randomPosition();
+  const [ row, column ] = randomPosition();
+  const isCellEmpty = arrField[row][column] === 0;
 
-  if (arrField[row][column] === 0) {
+  if (isCellEmpty) {
     arrField[row][column] = randomValue();
   } else {
     return newRandomCell();
@@ -139,116 +181,238 @@ function moveElement(row, column, newRow, newColumn) {
 function sumElement(row, column, newRow, newColumn) {
   arrField[newRow][newColumn] *= 2;
   arrField[row][column] = 0;
-
+  calculateScore(arrField[newRow][newColumn]);
   checkWin(arrField[newRow][newColumn]);
 }
 
 // Loop array
 
-function loopArray(action) {
+function processField(callback) {
   for (let row = 0; row < arrField.length; row++) {
     for (let column = 0; column < arrField[row].length; column++) {
-      action(row, column);
+      callback(row, column);
     }
   }
 }
 
 // Move elements up
 
-function moveUp(row, column) {
-  if (arrField[row][column] > 0 && row - 1 >= 0) {
-    if (arrField[row - 1][column] === 0) {
-      moveElement(row, column, row - 1, column);
+function moveUp() {
+  for (let row = arrField.length - 1; row >= 1; row--) {
+    for (let column = 0; column < arrField[row].length; column++) {
+      if (arrField[row][column] > 0 && row - 1 >= 0) {
+        if (arrField[row - 1][column] === 0) {
+          moveElement(row, column, row - 1, column);
 
-      return moveUp(row - 1, column);
-    }
+          return moveUp();
+        }
 
-    if (arrField[row][column] === arrField[row - 1][column]
-      && row - 1 >= 0) {
-      sumElement(row, column, row - 1, column);
+        if (arrField[row][column] === arrField[row - 1][column]
+          && row - 1 >= 0) {
+          sumElement(row, column, row - 1, column);
+        }
+      }
     }
   }
 }
 
 // Move elements down
 
-function moveDown(row, column) {
-  if (arrField[row][column] > 0 && row + 1 < arrField.length) {
-    if (arrField[row + 1][column] === 0) {
-      moveElement(row, column, row + 1, column);
+function moveDown() {
+  for (let row = 0; row < arrField.length - 1; row++) {
+    for (let column = 0; column < arrField[row].length; column++) {
+      if (arrField[row][column] > 0 && row + 1 < arrField.length) {
+        if (arrField[row + 1][column] === 0) {
+          moveElement(row, column, row + 1, column);
 
-      return moveDown(row + 1, column);
-    }
+          return moveDown();
+        }
 
-    if (arrField[row][column] === arrField[row + 1][column]
-      && row + 1 < arrField.length) {
-      sumElement(row, column, row + 1, column);
+        if (arrField[row][column] === arrField[row + 1][column]
+          && row + 1 < arrField.length) {
+          sumElement(row, column, row + 1, column);
+        }
+      }
     }
   }
 }
 
 // Move elements left
 
-function moveLeft(row, column) {
-  if (arrField[row][column] > 0 && column - 1 >= 0) {
-    if (arrField[row][column - 1] === 0) {
-      moveElement(row, column, row, column - 1);
+function moveLeft() {
+  for (let row = 0; row < arrField.length; row++) {
+    for (let column = arrField[row].length - 1; column >= 1; column--) {
+      if (arrField[row][column] > 0 && column - 1 >= 0) {
+        if (arrField[row][column - 1] === 0) {
+          moveElement(row, column, row, column - 1);
 
-      return moveLeft(row, column - 1);
-    }
+          return moveLeft();
+        }
 
-    if (arrField[row][column] === arrField[row][column - 1]
-      && column - 1 >= 0) {
-      sumElement(row, column, row, column - 1);
+        if (arrField[row][column] === arrField[row][column - 1]
+          && column - 1 >= 0) {
+          sumElement(row, column, row, column - 1);
+        }
+      }
     }
   }
 }
 
 // Move elements right
 
-function moveRight(row, column) {
-  if (arrField[row][column] > 0 && column + 1 < arrField[row].length) {
-    if (arrField[row][column + 1] === 0) {
-      moveElement(row, column, row, column + 1);
+function moveRight() {
+  for (let row = 0; row < arrField.length; row++) {
+    for (let column = 0; column < arrField[row].length - 1; column++) {
+      if (arrField[row][column] > 0 && column + 1 < arrField[row].length) {
+        if (arrField[row][column + 1] === 0) {
+          moveElement(row, column, row, column + 1);
 
-      return moveRight(row, column + 1);
-    }
+          return moveRight();
+        }
 
-    if (arrField[row][column] === arrField[row][column + 1]
-      && column + 1 < arrField[row].length) {
-      sumElement(row, column, row, column + 1);
+        if (arrField[row][column] === arrField[row][column + 1]
+          && column + 1 < arrField[row].length) {
+          sumElement(row, column, row, column + 1);
+        }
+      }
     }
   }
 }
 
+// Check up
+
+function checkUp() {
+  for (let row = arrField.length - 1; row >= 1; row--) {
+    for (let column = 0; column < arrField[row].length; column++) {
+      if (arrField[row][column] > 0
+        && (arrField[row][column] === arrField[row - 1][column]
+          || arrField[row - 1][column] === 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Check down
+
+function checkDown() {
+  for (let row = 0; row < arrField.length - 1; row++) {
+    for (let column = 0; column < arrField[row].length; column++) {
+      if (arrField[row][column] > 0
+        && (arrField[row][column] === arrField[row + 1][column]
+          || arrField[row + 1][column] === 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Check Left
+
+function checkLeft() {
+  for (let row = 0; row < arrField.length; row++) {
+    for (let column = arrField[row].length - 1; column >= 1; column--) {
+      if (arrField[row][column] > 0
+        && (arrField[row][column] === arrField[row][column - 1]
+          || arrField[row][column - 1] === 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Check right
+
+function checkRight() {
+  for (let row = 0; row < arrField.length; row++) {
+    for (let column = 0; column < arrField[row].length - 1; column++) {
+      if (arrField[row][column] > 0
+        && (arrField[row][column] === arrField[row][column + 1]
+          || arrField[row][column + 1] === 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Check lose
+
+function checkLose() {
+  if (checkUp() || checkDown() || checkLeft() || checkRight()) {
+    return true;
+  }
+  removeKeysListener();
+  lose();
+}
+
 // Press key
 
-document.addEventListener('keydown', event => {
-  if (event.key === 'ArrowUp' && controls.querySelector('.start') === null) {
-    loopArray(moveUp);
-    newRandomCell();
-    loopArray(buildGameField);
-    calculateScore();
+const arrUp = function(event) {
+  if (controls.querySelector('.start') === null) {
+    if (event.key === ARROWS.ArrowUp && checkUp()) {
+      moveUp();
+      newRandomCell();
+      processField(buildGameField);
+      checkLose();
+    }
   }
+};
 
-  if (event.key === 'ArrowDown' && controls.querySelector('.start') === null) {
-    loopArray(moveDown);
-    newRandomCell();
-    loopArray(buildGameField);
-    calculateScore();
+const arrDown = function(event) {
+  if (controls.querySelector('.start') === null) {
+    if (event.key === ARROWS.ArrowDown && checkDown()) {
+      moveDown();
+      newRandomCell();
+      processField(buildGameField);
+      checkLose();
+    }
   }
+};
 
-  if (event.key === 'ArrowLeft' && controls.querySelector('.start') === null) {
-    loopArray(moveLeft);
-    newRandomCell();
-    loopArray(buildGameField);
-    calculateScore();
+const arrLeft = function(event) {
+  if (controls.querySelector('.start') === null) {
+    if (event.key === ARROWS.ArrowLeft && checkLeft()) {
+      moveLeft();
+      newRandomCell();
+      processField(buildGameField);
+      checkLose();
+    }
   }
+};
 
-  if (event.key === 'ArrowRight' && controls.querySelector('.start') === null) {
-    loopArray(moveRight);
-    newRandomCell();
-    loopArray(buildGameField);
-    calculateScore();
+const arrRight = function(event) {
+  if (controls.querySelector('.start') === null) {
+    if (event.key === ARROWS.ArrowRight && checkRight()) {
+      moveRight();
+      newRandomCell();
+      processField(buildGameField);
+      checkLose();
+    }
   }
-});
+};
+
+// Add keys listener
+
+function addKeysListener() {
+  document.addEventListener('keydown', arrUp);
+  document.addEventListener('keydown', arrDown);
+  document.addEventListener('keydown', arrLeft);
+  document.addEventListener('keydown', arrRight);
+}
+
+// Remove keys listener
+
+function removeKeysListener() {
+  document.removeEventListener('keydown', arrUp);
+  document.removeEventListener('keydown', arrDown);
+  document.removeEventListener('keydown', arrLeft);
+  document.removeEventListener('keydown', arrRight);
+}
