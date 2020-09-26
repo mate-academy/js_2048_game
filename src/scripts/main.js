6955b0152fe050de20308efe794342c679a5e396
kshvetsova <katshvetsova@gmail.com>
2020-09-26 22:32:52 +0300

    fixed Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,239 +4,248 @@ const button = document.querySelector('button');
 const messageStart = document.querySelector('.message-start');
 const messageLose = document.querySelector('.message-lose');
 const messageWin = document.querySelector('.message-win');
-const score = document.querySelector('.game-score');
-const cell = document.querySelectorAll('td');
+const scoreBoard = document.querySelector('.game-score');
+const tableCells = document.querySelectorAll('td');
+const tableSize = 4;
+let score = 0;
+let scoreBuffer = [];
+const keyCodeDic = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+};
+let gameData = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
 
-function random() {
-  const rand = Math.floor(Math.random() * cell.length);
+function checkTableFull() {
+  for (let i = 0; i < tableSize; i++) {
+    if (gameData[i].includes(0)) {
+      return false;
+    }
+  }
+}
 
-  if (cell[rand].innerHTML === '') {
-    cell[rand].innerHTML = (Math.random() <= 0.1) ? 4 : 2;
-    checkForLose();
-    design();
+function randomGenerator() {
+  while (!checkTableFull()) {
+    const row = Math.floor(Math.random() * 4);
+    const col = Math.floor(Math.random() * 4);
+
+    if (gameData[row][col] === 0) {
+      gameData[row][col] = (Math.random() <= 0.1) ? 4 : 2;
+      break;
+    }
+  }
+}
+
+function merge(data) {
+  const arr = [];
+  let alpha = data.shift();
+
+  if (!alpha) {
+    return;
+  }
+
+  for (let i = 0; i < tableSize; i++) {
+    const beta = data.shift();
+
+    if (!beta) {
+      if (alpha) {
+        arr.push(alpha);
+      }
+      break;
+    } else if (alpha === beta) {
+      arr.push(alpha + beta);
+      scoreBuffer.push(alpha + beta);
+      alpha = null;
+    } else if (alpha !== beta) {
+      if (alpha) {
+        arr.push(alpha);
+      }
+      alpha = beta;
+    }
+  }
+
+  return arr.slice();
+}
+
+function makeMove(direction) {
+  const gameDataNext = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  switch (direction) {
+    case keyCodeDic.left:
+      for (let i = 0; i < tableSize; i++) {
+        const shifted = merge(gameData[i].filter((val) => val > 0));
+
+        if (shifted) {
+          for (let j = 0; j < shifted.length; j++) {
+            gameDataNext[i][j] = shifted[j];
+          }
+        }
+      }
+      break;
+    case keyCodeDic.right:
+      for (let i = 0; i < tableSize; i++) {
+        const shifted = merge(gameData[i].filter((val) => val > 0).reverse());
+
+        if (shifted) {
+          for (let j = 0; j < shifted.length; j++) {
+            gameDataNext[i][tableSize - 1 - j] = shifted[j];
+          }
+        }
+      }
+      break;
+    case keyCodeDic.up:
+      for (let j = 0; j < tableSize; j++) {
+        const arr = [];
+
+        for (let i = 0; i < tableSize; i++) {
+          arr.push(gameData[i][j]);
+        }
+
+        const shifted = merge(arr.filter((val) => val > 0));
+
+        if (shifted) {
+          for (let i = 0; i < shifted.length; i++) {
+            gameDataNext[i][j] = shifted[i];
+          }
+        }
+      }
+      break;
+    case keyCodeDic.down:
+      for (let j = 0; j < tableSize; j++) {
+        const arr = [];
+
+        for (let i = 0; i < tableSize; i++) {
+          arr.push(gameData[tableSize - 1 - i][j]);
+        }
+
+        const shifted = merge(arr.filter((val) => val > 0));
+
+        if (shifted) {
+          for (let i = 0; i < shifted.length; i++) {
+            gameDataNext[tableSize - 1 - i][j] = shifted[i];
+          }
+        }
+      }
+      break;
+    default:
+      break;
+  }
+
+  return gameDataNext;
+}
+
+function gameoverCheck() {
+  if (`${gameData}` !== `${makeMove(keyCodeDic.left)}`
+    || `${gameData}` !== `${makeMove(keyCodeDic.right)}`
+    || `${gameData}` !== `${makeMove(keyCodeDic.up)}`
+    || `${gameData}` !== `${makeMove(keyCodeDic.down)}`) {
+    return true;
+  }
+}
+
+function addScore() {
+  if (scoreBuffer.length > 0) {
+    score += scoreBuffer.reduce((acc, cur) => acc + cur);
+    scoreBoard.textContent = !score ? 0 : score;
+  }
+}
+
+function action(direction) {
+  scoreBuffer = [];
+
+  const gameDataNext = makeMove(direction);
+
+  if (`${gameData}` === `${gameDataNext}`) {
+    return false;
+  }
+
+  gameData = gameDataNext;
+
+  return true;
+}
+
+function endGame(result) {
+  if (result) {
+    messageWin.classList.remove('hidden');
   } else {
-    random();
+    messageLose.classList.remove('hidden');
   }
+  document.removeEventListener('keydown', start);
 }
 
-function design() {
-  for (let i = 0; i < cell.length; i++) {
-    cell[i].className = 'field-cell';
-
-    switch (cell[i].innerHTML) {
-      case '': cell[i].className = 'field-cell';
-        break;
-      case '2': cell[i].classList.add('field-cell--2');
-        break;
-      case '4': cell[i].classList.add('field-cell--4');
-        break;
-      case '8': cell[i].classList.add('field-cell--8');
-        break;
-      case '16': cell[i].classList.add('field-cell--16');
-        break;
-      case '32': cell[i].classList.add('field-cell--32');
-        break;
-      case '64': cell[i].classList.add('field-cell--64');
-        break;
-      case '128': cell[i].classList.add('field-cell--128');
-        break;
-      case '256': cell[i].classList.add('field-cell--256');
-        break;
-      case '512': cell[i].classList.add('field-cell--512');
-        break;
-      case '1024': cell[i].classList.add('field-cell--1024');
-        break;
-      case '2048': cell[i].classList.add('field-cell--2048');
-        break;
-    }
-  }
-}
-
-const board = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]];
-
-function moveUp() {
-  for (let j = 0; j < 4; j++) {
-    for (let i = 1; i < 4; i++) {
-      if (cell[board[i][j]].innerHTML) {
-        let row = i;
-
-        while (row > 0) {
-          if (cell[board[row - 1][j]].innerHTML === '') {
-            cell[board[row - 1][j]].innerHTML = cell[board[row][j]].innerHTML;
-            cell[board[row][j]].innerHTML = '';
-            row--;
-          } else if (cell[board[row - 1][j]].innerHTML
-            === cell[board[row][j]].innerHTML) {
-            cell[board[row - 1][j]].innerHTML *= 2;
-            cell[board[row][j]].innerHTML = '';
-
-            score.innerHTML = +score.innerHTML
-            + (+cell[board[row - 1][j]].innerHTML);
-            checkForWin();
-            break;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-
-function moveDown() {
-  for (let j = 0; j < 4; j++) {
-    for (let i = 2; i >= 0; i--) {
-      if (cell[board[i][j]].innerHTML) {
-        let row = i;
-
-        while (row + 1 < 4) {
-          if (cell[board[row + 1][j]].innerHTML === '') {
-            cell[board[row + 1][j]].innerHTML = cell[board[row][j]].innerHTML;
-            cell[board[row][j]].innerHTML = '';
-            row++;
-          } else if (cell[board[row + 1][j]].innerHTML
-            === cell[board[row][j]].innerHTML) {
-            cell[board[row + 1][j]].innerHTML *= 2;
-            cell[board[row][j]].innerHTML = '';
-
-            score.innerHTML = +score.innerHTML
-            + (+cell[board[row + 1][j]].innerHTML);
-            checkForWin();
-            break;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-
-function moveRight() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 2; j >= 0; j--) {
-      if (cell[board[i][j]].innerHTML) {
-        let col = j;
-
-        while (col + 1 < 4) {
-          if (cell[board[i][col + 1]].innerHTML === '') {
-            cell[board[i][col + 1]].innerHTML = cell[board[i][col]].innerHTML;
-            cell[board[i][col]].innerHTML = '';
-            col++;
-          } else if (cell[board[i][col]].innerHTML
-            === cell[board[i][col + 1]].innerHTML) {
-            cell[board[i][col + 1]].innerHTML *= 2;
-            cell[board[i][col]].innerHTML = '';
-
-            score.innerHTML = +score.innerHTML
-            + (+cell[board[i][col + 1]].innerHTML);
-            checkForWin();
-            break;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-
-function moveLeft() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 1; j < 4; j++) {
-      if (cell[board[i][j]].innerHTML) {
-        let col = j;
-
-        while (col - 1 >= 0) {
-          if (cell[board[i][col - 1]].innerHTML === '') {
-            cell[board[i][col - 1]].innerHTML = cell[board[i][col]].innerHTML;
-            cell[board[i][col]].innerHTML = '';
-            col--;
-          } else if (cell[board[i][col]].innerHTML
-            === cell[board[i][col - 1]].innerHTML) {
-            cell[board[i][col - 1]].innerHTML *= 2;
-            cell[board[i][col]].innerHTML = '';
-
-            score.innerHTML = +score.innerHTML
-            + (+cell[board[i][col - 1]].innerHTML);
-            checkForWin();
-            break;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-
-function startGame(event) {
-  const codes = event.keyCode;
-
-  switch (codes) {
-    case 38:
-      moveUp();
-      random();
-      design();
-      break;
-    case 40:
-      moveDown();
-      random();
-      design();
-      break;
-    case 37:
-      moveLeft();
-      random();
-      design();
-      break;
-    case 39:
-      moveRight();
-      random();
-      design();
-      break;
-  }
-}
-
-button.addEventListener('click', () => {
-  messageStart.hidden = true;
-  button.innerHTML = 'Restart';
-  button.classList.add('restart');
+function init() {
   button.classList.remove('start');
+  button.textContent = 'Restart';
+  button.classList.add('restart');
+  messageStart.classList.add('hidden');
   messageWin.classList.add('hidden');
   messageLose.classList.add('hidden');
-  score.innerHTML = '0';
+  scoreBoard.textContent = '0';
+  score = 0;
   cleanTable();
-  random();
-  random();
-  document.addEventListener('keydown', startGame);
-});
+  randomGenerator();
+  randomGenerator();
+  draw();
+  document.addEventListener('keydown', start);
+}
 
 function cleanTable() {
-  [...cell].map(el => {
+  [...tableCells].map(el => {
+    el.classList.remove(`field-cell--${el.textContent}`);
     el.textContent = '';
+  });
+
+  gameData.map(row => row.map((_, ind) => {
+    row[ind] = 0;
+  }));
+}
+
+function draw() {
+  const gameDataUnpacked = [];
+
+  gameData.forEach((rows) => {
+    rows.forEach((cells) => {
+      gameDataUnpacked.push(cells);
+    });
+  });
+
+  tableCells.forEach((cells) => {
+    const text = gameDataUnpacked.shift();
+
+    cells.textContent = !text ? null : text;
+    cells.className = 'field-cell';
+    cells.classList.add(`field-cell--${cells.textContent}`);
   });
 }
 
-function checkForWin() {
-  for (let i = 0; i < cell.length; i++) {
-    if (+cell[i].innerHTML === 2048) {
-      messageWin.classList.remove('hidden');
-      document.removeEventListener('keydown', startGame);
-    }
-  }
-}
-
-function checkForLose() {
-  let count = 0;
-
-  for (let i = 0; i < cell.length; i++) {
-    if (cell[i].innerHTML === '') {
-      count++;
-    }
+function start(e) {
+  if (e.keyCode < 36 || e.keyCode > 41) {
+    return;
   }
 
-  if (count === 0) {
-    messageLose.classList.remove('hidden');
-    document.removeEventListener('keydown', startGame);
+  if (action(e.keyCode)) {
+    if (scoreBuffer.includes(2048)) {
+      endGame(true);
+    }
+    addScore();
+    randomGenerator();
+
+    if (!checkTableFull() && !gameoverCheck()) {
+      endGame(false);
+    }
+    scoreBuffer = [];
+    draw();
   }
 };
+button.addEventListener('click', init);
