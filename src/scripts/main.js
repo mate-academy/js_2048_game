60ecfe2547921a459fe392077226bd7b239a5c4d
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-11-13 20:13:12 +0200

    New Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,32 @@
 'use strict';
 
-const tbody = document.querySelector('tbody');
-let allCell = tbody.querySelectorAll('td');
+const tbody = document.querySelector('table').tBodies[0].children;
+const allCell = document.querySelectorAll('td');
 const startGame = document.querySelector('.start');
 const blockScore = document.querySelector('.game-score');
 const youWin = document.querySelector('.message-win');
 const youLose = document.querySelector('.message-lose');
 const messStart = document.querySelector('.message-start');
-let score = 0;
 const width = 4;
-let allEmptyCell = [...allCell].filter(item => item.classList.length < 2);
-let randomCell = 0;
-let countEmptyCell = 0;
+let score = 0;
+let allEmptyCell = [];
+let countBusyCell = false;
 let oldArray = [];
+let randomCell = 0;
+let matrix = [
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+];
+
+const rotateMatrix = arrMatrix => {
+  matrix = arrMatrix.map((row, i) =>
+    row.map((val, j) => arrMatrix[arrMatrix.length - 1 - j][i])
+  );
+
+  return matrix;
+};
 
 function generateRandomInteger(min, max) {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -24,286 +38,131 @@ function generateRandomInteger(min, max) {
   return Math.round(rand);
 }
 
+function render() {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let k = 0; k < matrix[y].length; k++) {
+      tbody[y].children[k].innerHTML = matrix[y][k];
+    }
+  }
+  addOrRemoveClass();
+}
+
 function generateNumInCell() {
   const newDiv = document.createElement('div');
 
-  allCell = tbody.querySelectorAll('td');
-  allEmptyCell = [...allCell].filter(item => item.classList.length < 2);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '') {
+        allEmptyCell.push({
+          x: i,
+          y: j,
+        });
+      }
+    }
+  }
 
   if (youLoseNow() && allEmptyCell.length === 0) {
     youLose.classList.remove('hidden');
   }
 
-  if (countEmptyCell) {
+  if (countBusyCell) {
     return;
   }
 
   randomCell = generateRandomInteger(0, allEmptyCell.length - 1);
-  newDiv.textContent = Math.random() >= 0.1 ? 2 : 4;
 
-  allEmptyCell[randomCell].append(newDiv);
+  matrix[allEmptyCell[randomCell].x][allEmptyCell[randomCell].y]
+  = Math.random() >= 0.9 ? 4 : 2;
+
+  newDiv.textContent
+  = matrix[allEmptyCell[randomCell].x][allEmptyCell[randomCell].y];
+
+  tbody[allEmptyCell[randomCell].x]
+    .children[allEmptyCell[randomCell].y].innerHTML
+  = matrix[allEmptyCell[randomCell].x][allEmptyCell[randomCell].y];
 
   if (newDiv.textContent === '4') {
-    allEmptyCell[randomCell].classList.add('field-cell--4');
+    tbody[allEmptyCell[randomCell].x]
+      .children[allEmptyCell[randomCell].y].className = 'field-cell';
+
+    tbody[allEmptyCell[randomCell].x]
+      .children[allEmptyCell[randomCell].y].classList.add('field-cell--4');
   } else {
-    allEmptyCell[randomCell].classList.add('field-cell--2');
+    tbody[allEmptyCell[randomCell].x]
+      .children[allEmptyCell[randomCell].y].className = 'field-cell';
+
+    tbody[allEmptyCell[randomCell].x]
+      .children[allEmptyCell[randomCell].y].classList.add('field-cell--2');
   }
 
-  allCell = tbody.querySelectorAll('td');
+  allEmptyCell = [];
   oldArray = [];
   allCell.forEach(item => oldArray.push(item.innerText));
 };
 
 function combineRowRight() {
-  for (let i = tbody.children.length - 1; i >= 0; i--) {
-    const row = tbody.children[i].children;
+  clickArrowRight();
 
-    for (let j = row.length - 1; j >= 0; j--) {
-      if (row[j - 1] === undefined) {
+  for (let y = matrix.length - 1; y >= 0; y--) {
+    for (let k = matrix[y].length - 1; k >= 0; k--) {
+      if (matrix[y][k - 1] === undefined) {
         break;
       }
 
-      if (row[j].innerText === row[j - 1].innerText
-        && row[j].innerText !== '') {
-        const totalValue = parseInt(row[j].innerText)
-        + parseInt(row[j - 1].innerText);
+      if (matrix[y][k] === matrix[y][k - 1]
+        && matrix[y][k] !== '') {
+        const totalValue = parseInt(matrix[y][k])
+        + parseInt(matrix[y][k - 1]);
 
         score += totalValue;
-
         blockScore.innerText = score;
 
         isYouWin();
 
-        row[j].innerText = totalValue;
-        row[j].className = 'field-cell';
-        row[j - 1].innerText = '';
-        row[j - 1].className = 'field-cell';
+        matrix[y][k] = totalValue;
+        matrix[y][k - 1] = '';
       } else {
         continue;
       }
     }
   }
-}
-
-function combineRowLeft() {
-  for (let i = 0; i < tbody.children.length; i++) {
-    const row = tbody.children[i].children;
-
-    for (let j = 0; j <= row.length; j++) {
-      if (row[j + 1] === undefined) {
-        break;
-      }
-
-      if (row[j].innerText === row[j + 1].innerText
-        && row[j].innerText !== '') {
-        const totalValue = parseInt(row[j].innerText)
-        + parseInt(row[j + 1].innerText);
-
-        score += totalValue;
-
-        blockScore.innerText = score;
-
-        isYouWin();
-
-        row[j].innerText = totalValue;
-        row[j].className = 'field-cell';
-        row[j + 1].innerText = '';
-        row[j + 1].className = 'field-cell';
-      } else {
-        continue;
-      }
-    }
-  }
-}
-
-function combineColumnUp() {
-  for (let i = 0; i < allCell.length - 4; i++) {
-    if (allCell[i].innerText === allCell[i + width].innerText
-      && allCell[i].innerText !== '') {
-      const totalValue = parseInt(allCell[i].innerText)
-      + parseInt(allCell[i + width].innerText);
-
-      score += totalValue;
-
-      blockScore.innerText = score;
-
-      isYouWin();
-
-      allCell[i].innerText = totalValue;
-      allCell[i].className = 'field-cell';
-      allCell[i + width].innerText = '';
-      allCell[i + width].className = 'field-cell';
-    } else {
-      continue;
-    }
-  }
-}
-
-function combineColumnDown() {
-  for (let i = allCell.length - 1; i > 3; i--) {
-    if (allCell[i].innerText === allCell[i - width].innerText
-      && allCell[i].innerText !== '') {
-      const totalValue = parseInt(allCell[i].innerText)
-      + parseInt(allCell[i - width].innerText);
-
-      score += totalValue;
-
-      blockScore.innerText = score;
-
-      isYouWin();
-
-      allCell[i].innerText = totalValue;
-      allCell[i].className = 'field-cell';
-      allCell[i - width].innerText = '';
-      allCell[i - width].className = 'field-cell';
-    } else {
-      continue;
-    }
-  }
+  clickArrowRight();
 }
 
 function clickArrowRight() {
-  for (let i = 0; i < allCell.length; i++) {
-    if (i % 4 === 0) {
-      const totalOne = allCell[i].innerText;
-      const totalTwo = allCell[i + 1].innerText;
-      const totalThree = allCell[i + 2].innerText;
-      const totalFour = allCell[i + 3].innerText;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (j % 4 === 0) {
+        const totalOne = matrix[i][j];
+        const totalTwo = matrix[i][j + 1];
+        const totalThree = matrix[i][j + 2];
+        const totalFour = matrix[i][j + 3];
 
-      const Row = [totalOne, totalTwo, totalThree, totalFour];
+        const Row = [totalOne, totalTwo, totalThree, totalFour];
 
-      const filteredRow = Row.filter(item => item);
-      const missing = width - filteredRow.length;
-      const empty = new Array(missing).fill('');
+        const filteredRow = Row.filter(item => item);
+        const missing = width - filteredRow.length;
+        const empty = new Array(missing).fill('');
 
-      const newRow = empty.concat(filteredRow);
+        const newRow = empty.concat(filteredRow);
 
-      allCell[i].innerText = newRow[0];
-      allCell[i + 1].innerText = newRow[1];
-      allCell[i + 2].innerText = newRow[2];
-      allCell[i + 3].innerText = newRow[3];
+        matrix[i][j] = newRow[0];
+        matrix[i][j + 1] = newRow[1];
+        matrix[i][j + 2] = newRow[2];
+        matrix[i][j + 3] = newRow[3];
+      }
     }
   }
-  addOrRemoveClass();
-}
-
-function clickArrowLeft() {
-  for (let i = 0; i < allCell.length; i++) {
-    if (i % 4 === 0) {
-      const totalOne = allCell[i].innerText;
-      const totalTwo = allCell[i + 1].innerText;
-      const totalThree = allCell[i + 2].innerText;
-      const totalFour = allCell[i + 3].innerText;
-
-      const Row = [parseInt(totalOne), parseInt(totalTwo),
-        parseInt(totalThree), parseInt(totalFour)];
-
-      const filteredRow = Row.filter(item => item);
-      const missing = 4 - filteredRow.length;
-      const empty = new Array(missing).fill('');
-
-      const newRow = filteredRow.concat(empty);
-
-      allCell[i].innerText = newRow[0];
-      allCell[i + 1].innerText = newRow[1];
-      allCell[i + 2].innerText = newRow[2];
-      allCell[i + 3].innerText = newRow[3];
-    }
-  }
-  addOrRemoveClass();
-}
-
-function clickArrowUp() {
-  for (let i = 0; i < 4; i++) {
-    const totalOne = allCell[i].innerText;
-    const totalTwo = allCell[i + width].innerText;
-    const totalThree = allCell[i + (width * 2)].innerText;
-    const totalFour = allCell[i + (width * 3)].innerText;
-
-    const column = [parseInt(totalOne), parseInt(totalTwo),
-      parseInt(totalThree), parseInt(totalFour)];
-
-    const filteredColumn = column.filter(item => item);
-    const missingColumn = width - filteredColumn.length;
-    const createEmptyCell = new Array(missingColumn).fill('');
-
-    const newColumn = filteredColumn.concat(createEmptyCell);
-
-    allCell[i].innerText = newColumn[0];
-    allCell[i + width].innerText = newColumn[1];
-    allCell[i + (width * 2)].innerText = newColumn[2];
-    allCell[i + (width * 3)].innerText = newColumn[3];
-  }
-
-  addOrRemoveClass();
-}
-
-function clickArrowDown() {
-  for (let i = 0; i < 4; i++) {
-    const totalOne = allCell[i].innerText;
-    const totalTwo = allCell[i + width].innerText;
-    const totalThree = allCell[i + (width * 2)].innerText;
-    const totalFour = allCell[i + (width * 3)].innerText;
-
-    const column = [parseInt(totalOne), parseInt(totalTwo),
-      parseInt(totalThree), parseInt(totalFour)];
-
-    const filteredColumn = column.filter(item => item);
-    const missingColumn = width - filteredColumn.length;
-    const createEmptyCell = new Array(missingColumn).fill('');
-
-    const newColumn = createEmptyCell.concat(filteredColumn);
-
-    allCell[i].innerText = newColumn[0];
-    allCell[i + width].innerText = newColumn[1];
-    allCell[i + (width * 2)].innerText = newColumn[2];
-    allCell[i + (width * 3)].innerText = newColumn[3];
-  }
-  addOrRemoveClass();
 }
 
 function addOrRemoveClass() {
   for (let k = 0; k < allCell.length; k++) {
+    // console.log(allCell)
     allCell[k].className = 'field-cell';
 
-    switch (allCell[k].innerText) {
-      case '':
-        allCell[k].className = 'field-cell';
-        break;
-      case '2':
-        allCell[k].classList.add('field-cell--2');
-        break;
-      case '4':
-        allCell[k].classList.add('field-cell--4');
-        break;
-      case '8':
-        allCell[k].classList.add('field-cell--8');
-        break;
-      case '16':
-        allCell[k].classList.add('field-cell--16');
-        break;
-      case '32':
-        allCell[k].classList.add('field-cell--32');
-        break;
-      case '64':
-        allCell[k].classList.add('field-cell--64');
-        break;
-      case '128':
-        allCell[k].classList.add('field-cell--128');
-        break;
-      case '256':
-        allCell[k].classList.add('field-cell--256');
-        break;
-      case '512':
-        allCell[k].classList.add('field-cell--512');
-        break;
-      case '1024':
-        allCell[k].classList.add('field-cell--1024');
-        break;
-      case '2048':
-        allCell[k].classList.add('field-cell--2048');
-        break;
-    }
+    allCell[k] === ''
+      ? allCell[k].className = 'field-cell'
+      : allCell[k].classList.add(`field-cell--${allCell[k].innerText}`);
   }
 }
 
@@ -314,13 +173,11 @@ startGame.addEventListener('click', () => {
   startGame.classList.add('restart');
   youLose.classList.add('hidden');
   youWin.classList.add('hidden');
-
   score = 0;
   blockScore.innerText = score;
-
   cleanTable();
-
-  countEmptyCell = false;
+  render();
+  countBusyCell = false;
   generateNumInCell();
   generateNumInCell();
 });
@@ -338,27 +195,28 @@ function lookChanged() {
 }
 
 function cleanTable() {
-  allCell = [...allCell].map(item => {
-    item.innerText = '';
-    item.className = 'field-cell';
-  });
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix[i][j] = '';
+    }
+  }
 }
 
 function youLoseNow() {
-  for (let i = 0; i <= tbody.children.length - 1;) {
-    const row = tbody.children[i].children;
+  for (let i = 0; i <= matrix.length - 1;) {
+    const row = matrix[i];
 
     for (let j = 0; j <= row.length - 1;) {
-      if (tbody.children[i + 1] === undefined) {
-        if (row[j].innerText === row[j + 1].innerText) {
+      if (matrix[i + 1] === undefined) {
+        if (row[j] === row[j + 1]) {
           return false;
         }
       }
 
       if (i === 3) {
-        if (row[j].innerText === row[j + 1].innerText
-          || row[j + 1].innerText === row[j + 2].innerText
-          || row[j + 2].innerText === row[j + 3].innerText) {
+        if (row[j] === row[j + 1]
+          || row[j + 1] === row[j + 2]
+          || row[j + 2] === row[j + 3]) {
           return false;
         }
 
@@ -366,16 +224,16 @@ function youLoseNow() {
       }
 
       if (j === 3) {
-        if (row[j].innerText === tbody.children[i + 1].children[j].innerText
-          || row[j].innerText === tbody.children[i + 1].children[j].innerText) {
+        if (row[j] === matrix[i + 1][j]
+          || row[j] === matrix[i + 1][j]) {
           return false;
         }
         j++;
         continue;
       }
 
-      if (row[j].innerText === row[j + 1].innerText
-        || row[j].innerText === tbody.children[i + 1].children[j].innerText) {
+      if (row[j] === row[j + 1]
+        || row[j] === matrix[i + 1][j]) {
         return false;
       }
 
@@ -388,9 +246,11 @@ function youLoseNow() {
 }
 
 function isYouWin() {
-  for (let k = 0; k < allCell.length; k++) {
-    if (+allCell[k].innerText >= 2048) {
-      youWin.classList.remove('hidden');
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (+matrix[i][j] >= 2048) {
+        youWin.classList.remove('hidden');
+      }
     }
   }
 }
@@ -415,33 +275,41 @@ document.addEventListener('keydown', (event) => {
 });
 
 function keyRight() {
-  clickArrowRight();
   combineRowRight();
-  clickArrowRight();
-  countEmptyCell = lookChanged();
+  render();
+  countBusyCell = lookChanged();
   generateNumInCell();
 }
 
 function keyLeft() {
-  clickArrowLeft();
-  combineRowLeft();
-  clickArrowLeft();
-  countEmptyCell = lookChanged();
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  combineRowRight();
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  render();
+  countBusyCell = lookChanged();
   generateNumInCell();
 }
 
 function keyUp() {
-  clickArrowUp();
-  combineColumnUp();
-  clickArrowUp();
-  countEmptyCell = lookChanged();
+  rotateMatrix(matrix);
+  combineRowRight();
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  render();
+  countBusyCell = lookChanged();
   generateNumInCell();
 }
 
 function keyDown() {
-  clickArrowDown();
-  combineColumnDown();
-  clickArrowDown();
-  countEmptyCell = lookChanged();
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  rotateMatrix(matrix);
+  combineRowRight();
+  rotateMatrix(matrix);
+  render();
+  countBusyCell = lookChanged();
   generateNumInCell();
 }
