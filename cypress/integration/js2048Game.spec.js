096b7690b83890baf8ab27713417b2bc8567379a
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-20 16:36:46 +0200

    js_2048_game: add tests

---
+++ cypress/integration/js2048Game.spec.js
@@ -0,0 +1,58 @@
+'use strict';
+
+  describe('2048 game', () => {
+    beforeEach('Open site', () => {
+      cy.visit('https://kshvetsova.github.io/js_2048_game/');
+    });
+
+    it('should show the score', () => {
+      cy.get('button').click();
+      cy.get('body').type('{rightArrow}');
+      cy.get('body').type('{upArrow}');
+      cy.get('body').type('{rightArrow}');
+      cy.contains('Score').should('not.have.value', 0);
+    });
+
+    it('should show the score', () => {
+      cy.get('button').click();
+      cy.get('body').type('{leftArrow}');
+      cy.get('body').type('{upArrow}');
+      cy.get('body').type('{leftArrow}');
+      cy.contains('Score').should('not.have.value', 0);
+    });
+
+    it('should show the score', () => {
+      cy.get('button').click();
+      cy.get('body').type('{rightArrow}');
+      cy.get('body').type('{downArrow}');
+      cy.get('body').type('{rightArrow}');
+      cy.contains('Score').should('not.have.value', 0);
+    });
+
+    it('should show the score', () => {
+      cy.get('button').click();
+      cy.get('body').type('{leftArrow}');
+      cy.get('body').type('{downArrow}');
+      cy.get('body').type('{leftArrow}');
+      cy.contains('Score').should('not.have.value', 0);
+    });
+
+    it('should reset the score', () => {
+      cy.get('button').click();
+      cy.get('body').type('{leftArrow}');
+      cy.get('body').type('{downArrow}');
+      cy.get('body').type('{leftArrow}');
+      cy.get('[class="game-score"]').should('not.have.value', '0');
+      cy.get('button').click();
+      cy.get('[class="game-score"]').should('have.value', '');
+    });
+
+    it('should show message wh', () => {
+      cy.get('button').click();
+      for (let n = 0; n < 120; n++) {
+        cy.get('body').type('{rightArrow}');
+        cy.get('body').type('{upArrow}');
+      }
+      cy.get('body').contains('lose');
+    });
+  });
--- cypress/integration/page.spec.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// TODO: edit it
-describe('Page', () => {
-  it('should be visitable', () => {
-    cy.visit('/');
-  });
-});
