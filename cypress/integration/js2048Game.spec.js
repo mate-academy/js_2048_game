1bd106af587ae57dade514a8c5e7496715963bfe
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-30 17:44:35 +0200

    fix

--- cypress/integration/js2048Game.spec.js
+++ cypress/integration/js2048Game.spec.js
@@ -1,58 +1,76 @@
 'use strict';
 
-  describe('2048 game', () => {
-    beforeEach('Open site', () => {
-      cy.visit('/');
-    });
-
-    it('should show the score', () => {
-      cy.get('button').click();
-      cy.get('body').type('{rightArrow}');
-      cy.get('body').type('{upArrow}');
-      cy.get('body').type('{rightArrow}');
-      cy.contains('Score').should('not.have.value', 0);
-    });
-
-    it('should show the score', () => {
-      cy.get('button').click();
-      cy.get('body').type('{leftArrow}');
-      cy.get('body').type('{upArrow}');
-      cy.get('body').type('{leftArrow}');
-      cy.contains('Score').should('not.have.value', 0);
-    });
-
-    it('should show the score', () => {
-      cy.get('button').click();
-      cy.get('body').type('{rightArrow}');
-      cy.get('body').type('{downArrow}');
-      cy.get('body').type('{rightArrow}');
-      cy.contains('Score').should('not.have.value', 0);
-    });
-
-    it('should show the score', () => {
-      cy.get('button').click();
-      cy.get('body').type('{leftArrow}');
-      cy.get('body').type('{downArrow}');
-      cy.get('body').type('{leftArrow}');
-      cy.contains('Score').should('not.have.value', 0);
-    });
-
-    it('should reset the score', () => {
-      cy.get('button').click();
-      cy.get('body').type('{leftArrow}');
-      cy.get('body').type('{downArrow}');
-      cy.get('body').type('{leftArrow}');
-      cy.get('[class="game-score"]').should('not.have.value', '0');
-      cy.get('button').click();
-      cy.get('[class="game-score"]').should('have.value', '');
-    });
-
-    it('should show message wh', () => {
-      cy.get('button').click();
-      for (let n = 0; n < 120; n++) {
-        cy.get('body').type('{rightArrow}');
-        cy.get('body').type('{upArrow}');
-      }
-      cy.get('body').contains('lose');
-    });
+describe('2048 game', () => {
+  beforeEach(() => {
+    cy.visit('/');
   });
+
+  it('should show the initial score', () => {
+    cy.get('.button.start').click();
+    cy.get('.game-score').should('have.value', '');
+  });
+
+  it('should show the score', () => {
+    cy.get('.button.start').click();
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{upArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{upArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{upArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  });
+
+  it('should show the score', () => {
+    cy.get('.button.start').click();
+    cy.get('body').type('{rightArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{rightArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{rightArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{rightArrow}');
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  });
+
+  it('should show the score', () => {
+    cy.get('.button.start').click();
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  });
+
+  it('should reset the score', () => {
+    cy.get('.button.start').click();
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('body').type('{downArrow}');
+    cy.get('body').type('{leftArrow}');
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.button.restart').click();
+    cy.get('.game-score').should('have.value', '');
+  });
+
+  it('should show message in case of the loss', () => {
+    cy.get('.button.start').click();
+
+    for (let n = 0; n < 100; n++) {
+      cy.get('body').type('{rightArrow}');
+      cy.get('body').type('{downArrow}');
+      cy.get('body').type('{leftArrow}');
+      cy.get('body').type('{upArrow}');
+    }
+
+    cy.contains('You lose! Restart the game?')
+      .should('not.have.class', 'hidden');
+  });
+});
