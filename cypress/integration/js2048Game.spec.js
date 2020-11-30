cea31f25b0f051dcbfe76dc063df1a616efda36f
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-30 19:08:21 +0200

    fix

--- cypress/integration/js2048Game.spec.js
+++ cypress/integration/js2048Game.spec.js
@@ -1,4 +1,15 @@
+/// <reference types="cypress" />
 'use strict';
+
+Cypress.Commands.add('shuffleBoxes', (arrow1, arrow2, times) => {
+  cy.get('.button.start').click();
+
+  for (let n = 0; n < times; n++) {
+    cy.get('body').type(arrow1);
+    cy.get('body').type(arrow2);
+  }
+  cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+});
 
 describe('2048 game', () => {
   beforeEach(() => {
@@ -10,52 +21,24 @@ describe('2048 game', () => {
     cy.get('.game-score').should('have.value', '');
   });
 
-  it('should show the score', () => {
-    cy.get('.button.start').click();
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{upArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{upArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{upArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  it.only('should show the score', () => {
+    cy.shuffleBoxes('{leftArrow}', '{upArrow}', 3);
   });
 
-  it('should show the score', () => {
-    cy.get('.button.start').click();
-    cy.get('body').type('{rightArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{rightArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{rightArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{rightArrow}');
-    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  it.only('should show the score', () => {
+    cy.shuffleBoxes('{rightArrow}', '{upArrow}', 3);
   });
 
-  it('should show the score', () => {
-    cy.get('.button.start').click();
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+  it.only('should show the score', () => {
+    cy.shuffleBoxes('{rightArrow}', '{downArrow}', 3);
+  });
+
+  it.only('should show the score', () => {
+    cy.shuffleBoxes('{leftArrow}', '{downArrow}', 3);
   });
 
   it('should reset the score', () => {
-    cy.get('.button.start').click();
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('body').type('{downArrow}');
-    cy.get('body').type('{leftArrow}');
-    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.shuffleBoxes('{leftArrow}', '{downArrow}', 3);
     cy.get('.button.restart').click();
     cy.get('.game-score').should('have.value', '');
   });
