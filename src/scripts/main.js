62b7e4664ff2de114f5bf80b09d4a2f9a9909088
Inna <inna_temis@ukr.net>
2021-10-26 22:57:28 +0300

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 'use strict';
 // write your code here
 
-const boardCell = [...document.querySelectorAll('.field-cell')];
+const boardGame = document.querySelector('.game-field');
 const btnStart = document.querySelector('.start');
 const startMessage = document.querySelector('.message-start');
 const winMessage = document.querySelector('.message-win');
@@ -10,10 +10,22 @@ const loseMessage = document.querySelector('.message-lose');
 const scoreBoard = document.querySelector('.game-score');
 const controls = document.querySelector('.controls');
 let score = 0;
-const win = '2048';
-const grid = [];
+const win = 2048;
 const width = 4;
-let randomNumber = 0;
+
+function matrix() {
+  return [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]
+}
+
+let grid = matrix();
+
+// create button restart
+
 const btnRestart = document.createElement('button');
 
 function createRestartButton() {
@@ -29,17 +41,16 @@ function getStart() {
   btnStart.remove();
   controls.append(createRestartButton());
   startMessage.classList.add('hidden');
-  document.addEventListener('keyup', control);
-
-  boardCell.map((el) => {
-    el.className = 'field-cell';
-  });
+  getRandomNumber();
+  getRandomNumber();
   createBoard();
-  getSumOfNumber();
+
+  document.addEventListener('keyup', control);
 }
 btnRestart.addEventListener('click', getReset);
 
 function getReset() {
+  grid = matrix();
   getStart();
   scoreBoard.innerText = 0;
   score = 0;
@@ -49,130 +60,185 @@ function getReset() {
 
 // create board game
 function createBoard() {
-  for (let i = 0; i < boardCell.length; i++) {
-    boardCell[i].innerText = 0;
-    grid.push(boardCell[i]);
-  }
-  getRandomNumber();
-  getRandomNumber();
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      const cell = boardGame.rows[i].cells[j];
+
+      cell.classList = ('field-cell');
+      cell.textContent = `${grid[i][j]}`;
+
+      if (grid[i][j] > 0) {
+        cell.classList.add(`field-cell--${grid[i][j]}`);
+        cell.textContent = `${grid[i][j]}`;
+      } else {
+        cell.textContent = '';
+      }
+    }
+  };
 }
 
 // generate random number
 function getRandomNumber() {
-  randomNumber = Math.floor(Math.random() * grid.length);
+  const firstIndex = Math.floor(Math.random() * width);
+  const secondIndex = Math.floor(Math.random() * width);
 
-  if (grid[randomNumber].innerText === '0') {
-    grid[randomNumber].innerText = Math.random() <= 0.9 ? 2 : 4;
-    getColor(grid[randomNumber]);
+  if (grid[firstIndex][secondIndex] === 0) {
+    grid[firstIndex][secondIndex] = Math.random() <= 0.9 ? 2 : 4;
     checkLose();
   } else {
     getRandomNumber();
   }
+  
 }
+
 // move
+function sliding(row) {
+  let filterRows = row.filter(num => num > 0);
+  const emptyCell = width - filterRows.length;
+  const zeroesArr = Array(emptyCell).fill(0);
 
-function moveHorizontal(move) {
-  let newRow = [];
+  filterRows = [...zeroesArr, ...filterRows];
 
-  for (let i = 0; i < width * width; i += width) {
-    let filterRows = [];
-
-    for (let j = 0; j < width; j++) {
-      filterRows.push(Number(grid[i + j].innerText));
-    }
-    filterRows = filterRows.filter(num => num > 0);
-
-    const emptyCell = width - filterRows.length;
-    const zeroesArr = Array(emptyCell).fill(0);
-
-    if (!move) {
-      newRow = [...zeroesArr, ...filterRows];
-    } else {
-      newRow = [...filterRows, ...zeroesArr];
-    }
-
-    for (let k = 0; k < newRow.length; k++) {
-      grid[i + k].innerText = newRow[k];
-      getColor(grid[i + k]);
-    }
-  }
+  return filterRows;
 }
 
-function moveVertical(move) {
-  let newRow = [];
+function copyGrid(arrGrid) {
+  const extraGrid = matrix();
 
   for (let i = 0; i < width; i++) {
-    let filteredColumn = [];
+    for (let j = 0; j < width; j++) {
 
-    for (let j = 0; j < width * width; j += width) {
-      filteredColumn.push(Number(grid[i + j].innerText));
+      extraGrid[i][j] = arrGrid[i][j];
     }
-    filteredColumn = filteredColumn.filter(num => num > 0);
+  }
 
-    const emptyCell = width - filteredColumn.length;
-    const zeroesArr = Array(emptyCell).fill(0);
+  return extraGrid;
+}
 
-    if (!move) {
-      newRow = [...zeroesArr, ...filteredColumn];
-    } else {
-      newRow = [...filteredColumn, ...zeroesArr];
+function compare(a, b) {
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      if (a[i][j] !== b[i][j]) {
+        return true;
+      }
     }
+  }
 
-    for (let k = 0; k < newRow.length; k++) {
-      grid[i + (width * k)].innerText = newRow[k];
-      getColor(grid[i + (width * k)]);
-    }
+  return false;
+}
+
+function getMove(direction) {
+  let flip = false;
+  let rotate = false;
+
+  switch (direction) {
+    case 'ArrowRight':
+      break;
+    case 'ArrowLeft':
+      grid = flipGrid(grid);
+      flip = true;
+      break;
+    case 'ArrowDown':
+      grid = rotateGrid(grid, 1);
+      rotate = true;
+      break;
+    case 'ArrowUp':
+      grid = rotateGrid(grid, 1);
+      grid = flipGrid(grid);
+      flip = true;
+      rotate = true;
+      break;
+    default:
+      break;
+  }
+
+  const past = copyGrid(grid);
+
+  for (let i = 0; i < width; i++) {
+    grid[i] = sliding(grid[i]);
+    getSumOfNumber(grid[i]);
+    grid[i] = sliding(grid[i]);
+}
+
+  const changed = compare(past, grid);
+
+  if (flip) {
+    grid = flipGrid(grid);
+}
+
+  if (rotate) {
+    grid = rotateGrid(grid, -1);
+}
+
+  if (changed) {
+    getRandomNumber();
   }
 }
-// the sum of two the same numbers
 
-function getSumOfNumber(len, index) {
-  let sum = 0;
+function flipGrid() {
+  grid.map(arr => arr.reverse());
 
-  for (let i = 0; i < len; i++) {
-    if (Number(grid[i].innerText) === Number(grid[i + index].innerText)) {
-      sum = Number(grid[i].innerText) + Number(grid[i + index].innerText);
-      grid[i].innerText = sum;
-      grid[i + index].innerText = 0;
-      score += sum;
+  return grid;
+}
+
+function rotateGrid(gridArr, direction) {
+  const newGrid = matrix();
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      if (direction === 1) {
+        newGrid[i][j] = gridArr[j][i];
+      } else {
+        newGrid[j][i] = gridArr[i][j];
+      }
     }
-    scoreBoard.innerText = score;
   }
+
+  return newGrid;
+}
+
+// // the sum of two the same numbers
+function getSumOfNumber(row) {
+  for (let i = width - 1; i >= 1; i--) { 
+    if (row[i] === row[i - 1]) {
+      row[i] = row[i] + row[i - 1];
+      row[i - 1] = 0;
+    }
+    score += row[i];
+  }
+
+  scoreBoard.innerText = score;
+
   checkOnWin();
+
+  return row;
 }
 
 function control(e) {
   switch (e.key) {
     case 'ArrowLeft':
-      moveHorizontal(1);
-      getSumOfNumber(grid.length - 1, 1);
-      moveHorizontal(1);
-      getRandomNumber();
+      getMove('ArrowLeft');
+      createBoard();
       break;
     case 'ArrowUp':
-      moveVertical(1);
-      getSumOfNumber(grid.length - width, width);
-      moveVertical(1);
-      getRandomNumber();
+      getMove('ArrowUp');
+      createBoard();
       break;
     case 'ArrowRight':
-      moveHorizontal();
-      getSumOfNumber(grid.length - 1, 1);
-      moveHorizontal();
-      getRandomNumber();
+      getMove('ArrowRight');
+      createBoard();
       break;
     case 'ArrowDown':
-      moveVertical();
-      getSumOfNumber(grid.length - width, width);
-      moveVertical();
-      getRandomNumber();
+      getMove('ArrowDown');
+      createBoard();
       break;
-  }
+}
 }
 
 function checkOnWin() {
-  grid.map((el) => {
-    if (el.innerText === win) {
+  grid.map((el, i) => {
+    if (el[i] === win) {
       winMessage.classList.remove('hidden');
       btnStart.classList.remove('restart');
       btnStart.classList.add('start');
@@ -184,9 +250,11 @@ function checkOnWin() {
 function checkLose() {
   let count = 0;
 
-  for (let i = 0; i < grid.length; i++) {
-    if (grid[i].innerText === '0') {
-      count++;
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      if (grid[i][j] === 0) {
+        count++;
+      }
     }
   }
 
@@ -195,9 +263,4 @@ function checkLose() {
     winMessage.classList.add('hidden');
     document.removeEventListener('keyup', control);
   }
-};
-
-function getColor(item) {
-  item.className = `field-cell
-  field-cell--${Number(item.innerText)}`;
 };
