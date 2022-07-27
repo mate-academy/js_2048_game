048523137a4166223b33c5e1b07af7baf6be3f1e
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-07-27 23:46:16 +0300

    v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,51 +1,51 @@
 'use strict';
 
 const square = 4;
+const cellSize = 75;
+const gapSize = 10;
+let score = 0;
+const values = [[0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]];
 
 const root = document.querySelector('.container');
 const gameBoard = document.createElement('div');
-const gameCells = Array(square);
 
 gameBoard.className = 'game-field';
 root.firstElementChild.after(gameBoard);
 
-for (let i = 0; i < square * square; i++) {
-  const cell = document.createElement('div');
-
-  cell.className = 'field-cell';
-  gameBoard.append(cell);
-}
-
 for (let i = 0; i < square; i++) {
-  gameCells[i] = [...gameBoard.children].slice(i * square, (i + 1) * square);
+  for (let j = 0; j < square; j++) {
+    const cell = document.createElement('div');
+
+    cell.className = 'field-cell';
+    cell.id = `${i + '-' + j}`;
+    gameBoard.append(cell);
+  }
 }
 
-const cellSize = parseInt(getComputedStyle(gameBoard.firstElementChild).width);
-
-root.style.width = square * cellSize + (square - 1) * 10 + 'px';
+root.style.width = square * cellSize + (square - 1) * gapSize + 'px';
 
 function generate() {
   const probability = Math.random();
-  const randomNumber1 = Math.floor(Math.random() * square);
-  const randomNumber2 = Math.floor(Math.random() * square);
-  const cell = document.createElement('div');
-
-  let value = 2;
-  let colorClass = 'cell--2';
+  const rowIndex = Math.floor(Math.random() * square);
+  const columnIndex = Math.floor(Math.random() * square);
+  let newValue = 2;
 
   if (probability <= 0.1) {
-    value = 4;
-    colorClass = 'cell--4';
+    newValue = 4;
   }
 
-  cell.innerHTML = value;
-  cell.classList.add('cell');
-  cell.classList.add(colorClass);
-  cell.classList.add('cell-new');
-  cell.style.animationDuration = '0.3s';
+  // cell.classList.add('cell-new');
+  // cell.style.animationDuration = '0.3s';
 
-  if (gameCells[randomNumber1][randomNumber2].innerHTML === '') {
-    gameCells[randomNumber1][randomNumber2].append(cell);
+  if (values[rowIndex][columnIndex] === 0) {
+    values[rowIndex][columnIndex] = newValue;
+
+    const cell = document.getElementById(`${rowIndex + '-' + columnIndex}`);
+
+    updateBoard(cell, newValue);
   } else {
     generate();
   }
@@ -54,99 +54,100 @@ function generate() {
 generate();
 generate();
 
-function rowMove(direction) {
-  let grid = gameCells;
-  let sign = '';
-  let isGenerate = false;
+function updateBoard(cell, value) {
+  cell.innerText = value === 0 ? '' : value;
+  cell.className = '';
+  cell.classList.add('field-cell');
+  cell.classList.add(`field-cell--${value}`);
+}
 
-  if (direction === 'left') {
-    grid = gameCells.map(row => [...row].reverse());
-    sign = '-';
-  }
+function slide(row) {
+  let newRow = row.filter(num => num !== 0);
 
-  for (let rowIndex = 0; rowIndex < square; rowIndex++) {
-    const cells = grid[rowIndex].map(field => field.firstElementChild);
-    const values = cells.map(cell => {
-      if (cell !== null) {
-        return cell.innerHTML;
-      }
+  for (let i = 0; i < newRow.length - 1; i++) {
+    if (newRow[i] === newRow[i + 1]) {
+      newRow[i] *= 2;
+      newRow[i + 1] = 0;
 
-      return '';
-    });
-
-    let prevCount = 0;
-
-    for (let i = square - 2; i >= 0; i--) {
-      if (values[i] === '') {
-        continue;
-      }
-
-      let count = prevCount;
-      let isSimilar = false;
-      let canBeSimilar = true;
-
-      for (let j = i + 1; j < square; j++) {
-        if (values[j] === '') {
-          count++;
-        } else if (values[j] === values[i] && canBeSimilar) {
-          count++;
-          prevCount++;
-          isSimilar = true;
-          break;
-        } else {
-          canBeSimilar = false;
-        }
-      }
-
-      if (count > 0) {
-        isGenerate = true;
-
-        const translateSize = count * (cellSize + 10) + 'px';
-
-        cells[i].style.transform = `translate(${sign + translateSize})`;
-        cells[i].style.transition = 'transform 0.2s';
-        cells[i].classList.remove('cell-new');
-
-        setTimeout(() => {
-          cells[i].style.transform = '';
-          cells[i].style.transition = '';
-
-          grid[rowIndex][i + count].append(cells[i]);
-
-          if (isSimilar && canBeSimilar) {
-            const fieldCell = grid[rowIndex][i + count];
-            const value = fieldCell.firstElementChild.innerHTML * 2;
-
-            fieldCell.firstElementChild.remove();
-            fieldCell.firstElementChild.innerHTML = value;
-
-            fieldCell.firstElementChild.classList
-              .remove(`cell--${value / 2}`);
-            fieldCell.firstElementChild.classList.add(`cell--${value}`);
-            fieldCell.firstElementChild.classList.add('cell-new');
-            fieldCell.firstElementChild.style.animationDuration = '0.1s';
-          }
-        }, 200);
-      }
+      score += newRow[i];
     }
   }
+  newRow = newRow.filter(num => num !== 0);
 
-  if (isGenerate) {
-    setTimeout(() => {
-      generate();
-    }, 300);
+  while (newRow.length < square) {
+    newRow.push(0);
+  }
+
+  return newRow;
+}
+
+function moveRow(direction) {
+  for (let rowIndex = 0; rowIndex < square; rowIndex++) {
+    let row = values[rowIndex];
+
+    if (direction === 'right') {
+      row.reverse();
+    }
+
+    row = slide(row);
+
+    if (direction === 'right') {
+      row.reverse();
+    }
+
+    values[rowIndex] = row;
+
+    for (let columnIndex = 0; columnIndex < square; columnIndex++) {
+      const cell = document.getElementById(`${rowIndex + '-' + columnIndex}`);
+      const value = values[rowIndex][columnIndex];
+
+      updateBoard(cell, value);
+    }
+  }
+}
+
+function moveColumn(direction) {
+  for (let columnIndex = 0; columnIndex < square; columnIndex++) {
+    let row = [values[0][columnIndex], values[1][columnIndex],
+      values[2][columnIndex], values[3][columnIndex]];
+
+    if (direction === 'down') {
+      row.reverse();
+    }
+
+    row = slide(row);
+
+    if (direction === 'down') {
+      row.reverse();
+    }
+
+    for (let rowIndex = 0; rowIndex < square; rowIndex++) {
+      values[rowIndex][columnIndex] = row[rowIndex];
+
+      const cell = document.getElementById(`${rowIndex + '-' + columnIndex}`);
+      const value = values[rowIndex][columnIndex];
+
+      updateBoard(cell, value);
+    }
   }
 }
 
 document.addEventListener('keyup', (e) => {
-  setTimeout(() => {
-    switch (e.key) {
-      case 'ArrowLeft':
-        rowMove('left');
-        break;
-      case 'ArrowRight':
-        rowMove('right');
-        break;
-    }
-  }, 100);
+  switch (e.key) {
+    case 'ArrowLeft':
+      moveRow('left');
+      break;
+    case 'ArrowRight':
+      moveRow('right');
+      break;
+    case 'ArrowUp':
+      moveColumn('up');
+      break;
+    case 'ArrowDown':
+      moveColumn('down');
+      break;
+  }
+
+  generate();
+  alert(score);
 });
