0921cae0ca42ba7ea94cb9ecd77a46f895fb7e3a
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-18 20:46:08 +0300

    Implemented basic Cell class

---
+++ src/scripts/classes/Cell.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class Cell {
+  constructor(element, value) {
+    this._element = element;
+    this._value = value || 0;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(number) {
+    this._value = number;
+    this._element.innerText = number || ''; // ignore zero
+
+    this._element.className = this._element.className
+      .replace(/( ?field-cell--\d+)/, '');
+    this._element.classList.add(`field-cell--${number}`);
+  }
+}
+
+module.exports = Cell;
