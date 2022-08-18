b2d8b3674f80153de173fc0682e0ded4a4c7865b
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-18 20:48:16 +0300

    Added test game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
+/* eslint-disable */
 'use strict';
 
-// write your code here
+const Game2048 = require('./classes/Game2048');
+const Cell = require('./classes/Cell');
+
+const testGame = new Game2048('.app', 8);
+
+testGame._initGame();
+
+console.log(testGame._field);
