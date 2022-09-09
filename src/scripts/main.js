6ea6c749c78014364d37b027facf130e8c2ca9e5
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-09-09 13:20:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,18 +7,20 @@ const startMessage = document.querySelector('.message-start');
 
 button.addEventListener('click', e => {
   const cells = document.querySelectorAll('.field-cell');
+  const gameScore = document.querySelector('.game-score');
 
   if (e.target.matches('.start')) {
     e.target.innerHTML = 'Restart';
     e.target.classList.remove('start');
     e.target.classList.add('restart');
-    startMessage.hidden = true;
+    startMessage.classList.add('hidden');
   }
 
   if (e.target.matches('.restart')) {
     [...cells].forEach(cell => {
       cell.removeAttribute('data-number');
     });
+    gameScore.innerHTML = '0';
   }
 
   addStartCells(cells);
@@ -30,7 +32,6 @@ function probability(n) {
 }
 
 function addStartCells(cells) {
-  // const cells = document.querySelectorAll('.field-cell');
   let firstCell = Math.floor(Math.random() * 16);
   let secondCell = Math.floor(Math.random() * 16);
 
@@ -54,98 +55,161 @@ function addStartCells(cells) {
   updateCells();
 }
 
+function availibleMoves() {
+  let result = false;
+  const rows = document.querySelectorAll('.field-row');
+  const columns = [
+    document.querySelectorAll('.field-row :first-child'),
+    document.querySelectorAll('.field-row :nth-child(2)'),
+    document.querySelectorAll('.field-row :nth-child(3)'),
+    document.querySelectorAll('.field-row :last-child'),
+  ];
+
+  [...rows].forEach(row => {
+    const cells = document.querySelectorAll('.field-cell');
+
+    for (let i = 0; i < cells.length - 1; i++) {
+      if (cells[i].dataset.number === cells[i + 1].dataset.number) {
+        result = true;
+
+        return;
+      }
+    }
+  });
+
+  columns.forEach(column => {
+    for (let i = 0; i < column.length - 1; i++) {
+      if (column[i].dataset.number === column[i + 1].dataset.number) {
+        result = true;
+
+        return;
+      }
+    }
+  });
+
+  return result;
+}
+
 document.addEventListener('keydown', e => {
-  if (!startMessage.hidden) {
+  if (!startMessage.classList.contains('hidden')) {
     return;
   }
 
-  console.log(e.key);
+  const cells = document.querySelectorAll('.field-cell');
+  const gameScore = document.querySelector('.game-score');
+  let score = +gameScore.innerHTML;
+  let scoreIncrease;
+
+  [...cells].forEach(cell => {
+    cell.classList.remove(`field-cell--${cell.innerHTML}`);
+  });
 
   if (e.key === 'ArrowRight') {
-    right();
+    scoreIncrease = right();
   }
 
   if (e.key === 'ArrowLeft') {
-    left();
+    scoreIncrease = left();
   }
 
   if (e.key === 'ArrowUp') {
-    let columns = [
-      document.querySelectorAll('.field-row :first-child[data-number]'),
-      document.querySelectorAll('.field-row :nth-child(2)[data-number]'),
-      document.querySelectorAll('.field-row :nth-child(3)[data-number]'),
-      document.querySelectorAll('.field-row :last-child[data-number]'),
-    ];
-
-    [...columns].forEach(column => {
-      for (let i = 1; i < column.length; i++) {
-        const current = column[i];
-        const prev = column[i - 1];
-  
-        if (current.dataset.number === prev.dataset.number) {
-          prev.dataset.number = `${+current.dataset.number * 2}`;
-          current.removeAttribute('data-number');
-        }
-      }
-    });
-
-    let rows = document.querySelectorAll('.field-row');
-
-    rows = [...rows];
-    columns = [...columns];
-
-    for (let i = 0; i < 4; i++) {
-      const rowCells = rows[i].querySelectorAll('.field-cell');
-
-      for (let cellNum = 0; cellNum < 4; cellNum++) {
-        const column = columns[cellNum];
-
-        if (column[i] && rowCells[cellNum] !== column[i] && column[i].dataset.number) {
-          rowCells[cellNum].dataset.number = column[i].dataset.number;
-          column[i].removeAttribute('data-number');
-        }
-      }
-    }
+    scoreIncrease = moveUp();
   }
 
+  if (e.key === 'ArrowDown') {
+    scoreIncrease = moveDown();
+  }
+
+  score += scoreIncrease;
+  gameScore.innerHTML = '' + score;
+
+  deleteEmptySpaces(e.key);
   updateCells();
   addCell();
 });
 
+function moveDown() {
+  let score = 0;
+  const columns = [
+    document.querySelectorAll('.field-row :first-child[data-number]'),
+    document.querySelectorAll('.field-row :nth-child(2)[data-number]'),
+    document.querySelectorAll('.field-row :nth-child(3)[data-number]'),
+    document.querySelectorAll('.field-row :last-child[data-number]'),
+  ];
+
+  [...columns].forEach(column => {
+    for (let i = column.length - 1; i > 0; i--) {
+      const current = column[i];
+      const prev = column[i - 1];
+
+      if (current.dataset.number === prev.dataset.number) {
+        current.dataset.number = `${+prev.dataset.number * 2}`;
+        score += +prev.dataset.number * 2;
+        prev.removeAttribute('data-number');
+      }
+    }
+  });
+
+  return score;
+}
+
+function moveUp() {
+  let score = 0;
+  const columns = [
+    document.querySelectorAll('.field-row :first-child[data-number]'),
+    document.querySelectorAll('.field-row :nth-child(2)[data-number]'),
+    document.querySelectorAll('.field-row :nth-child(3)[data-number]'),
+    document.querySelectorAll('.field-row :last-child[data-number]'),
+  ];
+
+  [...columns].forEach(column => {
+    for (let i = 1; i < column.length; i++) {
+      const current = column[i];
+      const prev = column[i - 1];
+
+      if (current.dataset.number === prev.dataset.number) {
+        prev.dataset.number = `${+current.dataset.number * 2}`;
+        score += +current.dataset.number * 2;
+        current.removeAttribute('data-number');
+      }
+    }
+  });
+
+  return score;
+}
+
 function right() {
+  let score = 0;
   const rows = document.querySelectorAll('.field-row');
 
   [...rows].forEach(row => {
     const cells = row.querySelectorAll('.field-cell');
-    const cellsWithNumber = [...cells].filter(cell => cell.hasAttribute('data-number'));
-
-    // [...cells].filter(cell => cell.hasAttribute('data-number'))
-    //   .forEach(cell => row.append(cell));
+    const cellsWithNumber = [...cells]
+      .filter(cell => cell.hasAttribute('data-number'));
 
     for (let i = cellsWithNumber.length - 1; i > 0; i--) {
       const current = cellsWithNumber[i];
       const prev = cellsWithNumber[i - 1];
 
-      console.log(row);
-
       if (current.dataset.number === prev.dataset.number) {
         current.dataset.number = `${+prev.dataset.number * 2}`;
+        score += +prev.dataset.number * 2;
         prev.removeAttribute('data-number');
       }
-
-      console.log(row, cellsWithNumber);
     }
-
-    cellsWithNumber.forEach(cell => row.append(cell));
   });
+
+  return score;
 }
 
 function left() {
+  let score = 0;
   const rows = document.querySelectorAll('.field-row');
 
   [...rows].forEach(row => {
     const cells = row.querySelectorAll('.field-cell');
-    const cellsWithNumber = [...cells].filter(cell => cell.hasAttribute('data-number'));
+    const cellsWithNumber = [...cells]
+      .filter(cell => cell.hasAttribute('data-number'));
 
     // [...cells].filter(cell => cell.hasAttribute('data-number'))
     //   .forEach(cell => row.append(cell));
@@ -154,36 +218,23 @@ function left() {
       const current = cellsWithNumber[i];
       const prev = cellsWithNumber[i - 1];
 
-      console.log(row);
-
       if (current.dataset.number === prev.dataset.number) {
         prev.dataset.number = `${+current.dataset.number * 2}`;
+        score += +current.dataset.number * 2;
         current.removeAttribute('data-number');
       }
-
-      console.log(row, cellsWithNumber);
     }
-
-    [...cells].filter(cell => !cell.hasAttribute('data-number')).forEach(cell => row.append(cell));
   });
-}
 
-function up() {
-  // const rows = document.querySelectorAll('.field-row');
-
-  // for (let i = 1; i < rows.length; i++) {
-  //   const currentRow = rows[i].querySelectorAll('.field-cell[data-number]');
-  //   const previousRow = rows[i - 1].querySelectorAll('.field-cell[data-number]');
-
-  //   console.log(currentRow, previousRow);
-
-  //   // for (let )
-
-  // };
+  return score;
 }
 
 function updateCells() {
   const cells = document.querySelectorAll('.field-cell');
+  const winMessage = document.querySelector('.message-win');
+  const messageLose = document.querySelector('.message-lose');
+  const emptyCells = [...cells]
+    .filter(cell => !cell.hasAttribute('data-number'));
 
   [...cells].forEach(cell => {
     if (cell.hasAttribute('data-number')) {
@@ -195,13 +246,102 @@ function updateCells() {
       cell.className = 'field-cell';
     }
   });
+
+  if ([...cells].some(cell => cell.dataset.number === '2048')) {
+    winMessage.classList.remove('hidden');
+  }
+
+  if (emptyCells.length === 0) {
+    const hasAvailibleMoves = availibleMoves();
+
+    if (!hasAvailibleMoves) {
+      messageLose.classList.remove('hidden');
+
+      return;
+    };
+  }
 }
 
 function addCell() {
-  const emptyCells = [...document.querySelectorAll('.field-cell')].filter(cell => !cell.hasAttribute('data-number'));
+  const emptyCells = [...document.querySelectorAll('.field-cell')]
+    .filter(cell => !cell.hasAttribute('data-number'));
   const randomCell = Math.floor(Math.random() * emptyCells.length);
 
   emptyCells[randomCell].dataset.number = probability(0.1) ? '4' : '2';
-  emptyCells[randomCell].classList.add(`field-cell--${emptyCells[randomCell].dataset.number}`);
+
+  emptyCells[randomCell].classList
+    .add(`field-cell--${emptyCells[randomCell].dataset.number}`);
   emptyCells[randomCell].innerHTML = `${emptyCells[randomCell].dataset.number}`;
 }
+
+function deleteEmptySpaces(direction) {
+  const rows = document.querySelectorAll('.field-row');
+  const columns = [
+    document.querySelectorAll('.field-row :first-child'),
+    document.querySelectorAll('.field-row :nth-child(2)'),
+    document.querySelectorAll('.field-row :nth-child(3)'),
+    document.querySelectorAll('.field-row :last-child'),
+  ];
+
+  [...rows].forEach(row => {
+    const cells = row.querySelectorAll('.field-cell');
+    const emptyCells = [...cells]
+      .filter(cell => !cell.hasAttribute('data-number'));
+
+    emptyCells.forEach(cell => {
+      if (direction === 'ArrowRight') {
+        row.prepend(cell);
+      }
+
+      if (direction === 'ArrowLeft') {
+        row.append(cell);
+      }
+    });
+  });
+
+  if (direction === 'ArrowUp') {
+    [...columns].forEach(column => {
+      for (let i = 0; i < column.length - 1; i++) {
+        const currentCell = column[i];
+
+        if (!currentCell.hasAttribute('data-number')) {
+          for (let n = i + 1; n < column.length; n++) {
+            if (column[n].hasAttribute('data-number')) {
+              currentCell.dataset.number = column[n].dataset.number;
+              column[n].removeAttribute('data-number');
+
+              break;
+            }
+          }
+        }
+      }
+    });
+  }
+
+  if (direction === 'ArrowDown') {
+    [...columns].forEach(column => {
+      for (let i = column.length - 1; i > 0; i--) {
+        const currentCell = column[i];
+
+        if (!currentCell.hasAttribute('data-number')) {
+          for (let n = i - 1; n >= 0; n--) {
+            if (column[n].hasAttribute('data-number')) {
+              currentCell.dataset.number = column[n].dataset.number;
+              column[n].removeAttribute('data-number');
+
+              break;
+            }
+          }
+        }
+      }
+    });
+  }
+}
+
+window.addEventListener('unhandledrejection', (e) => {
+  e.preventDefault();
+
+  const messageLose = document.querySelector('.message-lose');
+
+  messageLose.classList.remove('hidden');
+});
