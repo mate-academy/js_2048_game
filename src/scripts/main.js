27d47543050516511af971f20dc6f87fdcd0cf1e
Vitalii-Heiko <papus@ukr.net.com>
2022-09-28 12:48:08 +0300

    first commit, not finished yet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,6 @@ const messageWin = document.querySelector('.message-win');
 const messageLose = document.querySelector('.message-lose');
 const gameScore = document.querySelector('.game-score');
 const rows = document.querySelectorAll('.field-row');
-const rowCells = 4;
-const cells = 4;
 let score = 0;
 let gameField = [
   [0, 0, 0, 0],
@@ -18,8 +16,8 @@ let gameField = [
 ];
 
 function setGameField() {
-  for (let row = 0; row < rowCells; row++) {
-    for (let cell = 0; cell < cells; cell++) {
+  for (let row = 0; row < 4; row++) {
+    for (let cell = 0; cell < 4; cell++) {
       rows[row].children[cell].textContent = gameField[row][cell] || '';
 
       rows[row].children[cell].className
@@ -31,12 +29,12 @@ function setGameField() {
     }
   }
 
-  // checkGameOver();
+  return gameField;
 };
 
 function hasEmptyCell() {
-  for (let row = 0; row < rowCells; row++) {
-    for (let cell = 0; cell < cells; cell++) {
+  for (let row = 0; row < 4; row++) {
+    for (let cell = 0; cell < 4; cell++) {
       if (gameField[row][cell] === 0) {
         return true;
       }
@@ -53,20 +51,100 @@ function setTwoOrFour() {
     return;
   }
 
-  const row = Math.floor(Math.random() * rowCells);
-  const cell = Math.floor(Math.random() * cells);
+  const row = Math.floor(Math.random() * 4);
+  const cell = Math.floor(Math.random() * 4);
 
   (gameField[row][cell] === 0)
-    ? gameField[row][cell] = (Math.floor(Math.random() * 100) > 10 ? 2 : 4)
+    ? gameField[row][cell] = Math.floor(Math.random() >= 0.9 ? 4 : 2)
     : setTwoOrFour();
 
   setGameField();
 }
 
-// function slideUp() {
-// for (let row = 0, row < rowCells, row++){
-// }
-// };
+function filteringZero(row) {
+  return row.filter(num => num !== 0);
+}
+
+function slide(row) {
+  let filteredRow = filteringZero(row);
+
+  for (let i = 0; i < filteredRow.length - 1; i++) {
+    if (filteredRow[i] === filteredRow[i + 1]) {
+      filteredRow[i] *= 2;
+      filteredRow[i + 1] = 0;
+      score += filteredRow[i];
+    }
+  }
+  filteredRow = filteringZero(filteredRow);
+
+  while (filteredRow.length < 4) {
+    filteredRow.push(0);
+  };
+
+  return filteredRow;
+}
+
+function slideLeft() {
+  for (let r = 0; r < gameField.length; r++) {
+    let row = gameField[r];
+
+    row = slide(row);
+    gameField[r] = row;
+  }
+
+  setGameField();
+}
+
+function slideRight() {
+  for (let r = 0; r < gameField.length; r++) {
+    let row = gameField[r].reverse();
+
+    row = slide(row);
+    gameField[r] = row.reverse();
+  }
+
+  setGameField();
+}
+
+function slideUp() {
+  for (let c = 0; c < gameField.length; c++) {
+    let column = [
+      gameField[0][c],
+      gameField[1][c],
+      gameField[2][c],
+      gameField[3][c],
+    ];
+
+    column = slide(column);
+
+    for (let r = 0; r < gameField.length; r++) {
+      gameField[r][c] = column[r];
+    }
+  }
+
+  setGameField();
+}
+
+function slideDown() {
+  for (let c = 0; c < gameField.length; c++) {
+    let column = [
+      gameField[0][c],
+      gameField[1][c],
+      gameField[2][c],
+      gameField[3][c],
+    ];
+
+    column.reverse();
+    column = slide(column);
+    column.reverse();
+
+    for (let r = 0; r < gameField.length; r++) {
+      gameField[r][c] = column[r];
+    }
+  }
+
+  setGameField();
+}
 
 button.addEventListener('click', () => {
   if (button.textContent === 'Start') {
@@ -85,30 +163,36 @@ button.addEventListener('click', () => {
       [0, 0, 0, 0],
     ];
     score = 0;
+    gameScore.textContent = score;
     messageLose.classList.add('hidden');
 
     setTwoOrFour();
     setTwoOrFour();
   };
+  score = 0;
 });
 
 document.addEventListener('keyup', (e) => {
   switch (e.key) {
     case 'ArrowUp':
+      slideUp();
       setTwoOrFour();
 
       break;
     case 'ArrowDown':
+      slideDown();
       setTwoOrFour();
 
       break;
 
     case 'ArrowRight':
+      slideRight();
       setTwoOrFour();
 
       break;
 
     case 'ArrowLeft':
+      slideLeft();
       setTwoOrFour();
 
       break;
