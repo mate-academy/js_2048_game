3e0d45bd11eefa86076797fd5ed266026dda7946
Andriy Andrieiev <proph7000@gmail.com>
2022-10-07 16:21:03 +0300

    Fixes bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -253,27 +253,30 @@ function addNumbersIntoGameField(field, clone) {
 
 function moveRight(row) {
   let lastFullCell = null;
+  let isMultiplied = false;
 
   for (let i = row.length - 1; i >= 0; i--) {
-    if (row[i] === row[lastFullCell]) {
+    if (row[i] === row[lastFullCell] && !isMultiplied) {
       row[lastFullCell] *= 2;
       score += row[lastFullCell];
       gameScore.innerText = score;
+      isMultiplied = true;
 
       if (score > bestScore) {
         bestScore = score;
         gameBestScore.innerText = bestScore;
       }
+
       row[i] = 0;
     }
 
     if (
       row[i]
-      && (row[i] !== row[lastFullCell])
       && lastFullCell !== null
       && !row[lastFullCell - 1]
     ) {
       row[lastFullCell - 1] = row[i];
+      isMultiplied = false;
       row[i] = 0;
       lastFullCell--;
     }
