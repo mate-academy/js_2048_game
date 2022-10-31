0ea5b3b32f564c7c44d193241b666da771620ee9
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-31 22:23:12 +0100

    swiping added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,6 +36,60 @@ function handleInput(e) {
       break;
   }
 
+  mergeAndAddRandom();
+}
+
+// ------------- for swiping ----------- //
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches; // browser API || jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  const xUp = evt.touches[0].clientX;
+  const yUp = evt.touches[0].clientY;
+
+  const xDiff = xDown - xUp;
+  const yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      moveLeft(grid.cellsByRow);
+    } else {
+      moveRight(grid.cellsByRow.map(row => [...row].reverse()));
+    }
+  } else {
+    if (yDiff > 0) {
+      moveUp(grid.cellsByColumn);
+    } else {
+      moveDown(grid.cellsByColumn.map(column => [...column].reverse()));
+    }
+  }
+
+  mergeAndAddRandom();
+  /* reset values */
+  xDown = null;
+  yDown = null;
+};
+
+function mergeAndAddRandom() {
   grid.cells.forEach(cell => {
     cell.mergeTiles();
   });
