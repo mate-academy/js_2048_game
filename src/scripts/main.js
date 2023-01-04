9058c86828dc878030acaf0e17a72e285bb18f07
Vladyslava Buzova <dev.vladyslava@gmail.com>
2023-01-05 01:04:46 +0800

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+// const gameField = document.querySelector('.game-field');
+// const scores = document.querySelector('.game-score');
+// const resultMessageWin = document.querySelector('.message-win');
+// const resultMessageLose = document.querySelector('.message-lose');
+// const messageStart = document.querySelector('.message-start');
+// const start = document.querySelector('.start');
+// const width = 4;
+// const squares = [];
+// let score = 0;
+
+// const square = document.createElement('div');
+
+// square.classList.add('field-cell');
