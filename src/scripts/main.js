2aeddcd02822cccc3ce1f321d1aa779425170956
Taras Baran <baranprivat22@gmail.com>
2023-02-08 16:10:26 +0200

    reduced code, and removed setTimeout about addCell

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,24 +11,21 @@ const game = {
   moved: false,
 };
 
-const gameFieldBody = document.querySelector('.game-field tbody');
-const controls = document.querySelector('.controls');
-const button = controls.querySelector('.start');
-const gameScore = controls.querySelector('.game-score');
-const messageContainer = document.querySelector('.message-container');
+const messageGame = {
+  win: 'Winner! Congrats! You did it!',
+  lose: 'You lose! Restart the game?',
+  start: 'Press "Start" to begin game. Good luck!',
+};
+
+const gameFieldCells = document.querySelector('.game-field tbody');
+const messagePlace = document.querySelector('.message-container');
+const button = document.querySelector('.start');
+const gameScore = document.querySelector('.game-score');
 
 render();
 
-function clearData() {
-  game.matrix = game.matrix.map(col => col.map(() => 0));
-  game.score = 0;
-  game.moved = false;
-
-  render();
-}
-
 function render() {
-  gameFieldBody.innerHTML = '';
+  gameFieldCells.innerHTML = '';
 
   game.matrix.forEach(col => {
     const tr = document.createElement('tr');
@@ -49,16 +46,34 @@ function render() {
       tr.append(td);
     });
 
-    gameFieldBody.append(tr);
+    gameFieldCells.append(tr);
   });
 
-  renderMessage('message-start', 'Press "Start" to begin game. Good luck!');
+  gameScore.textContent = game.score;
+
+  renderMessage('message-start', messageGame.start);
 
   const isWin = isGameWin();
 
   if (!isWin) {
     isGameLose();
   }
+}
+
+function renderMessage(classMessage, message) {
+  messagePlace.innerHTML = `
+    <p class="message ${classMessage}">
+      ${message}
+    </p>
+  `;
+}
+
+function clearData() {
+  game.matrix = game.matrix.map(col => col.map(() => 0));
+  game.score = 0;
+  game.moved = false;
+
+  render();
 }
 
 function addRandomCell() {
@@ -81,169 +96,37 @@ function addRandomCell() {
   game.matrix[randomCol][randomRow] = randomCellNum;
 }
 
-function checkEmptyTail(col, row, currCol, currRow) {
-  if (currCol !== col || currRow !== row) {
-    game.matrix[currCol][currRow] = game.matrix[col][row];
-    game.matrix[col][row] = 0;
-
-    game.moved = true;
-  }
-}
-
-function searchEmptyTails(move) {
-  if (move === 'up') {
-    for (let col = 1; col < 4; col++) {
-      for (let row = 0; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        let currCol = col;
-
-        while (currCol > 0 && game.matrix[currCol - 1][row] === 0) {
-          currCol--;
-        }
-
-        checkEmptyTail(col, row, currCol, row);
-      }
-    }
-  } else if (move === 'down') {
-    for (let col = 2; col >= 0; col--) {
-      for (let row = 0; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        let currCol = col;
-
-        while (currCol < 3 && game.matrix[currCol + 1][row] === 0) {
-          currCol++;
-        }
-
-        checkEmptyTail(col, row, currCol, row);
-      }
-    }
-  } else if (move === 'left') {
-    for (let col = 0; col < 4; col++) {
-      for (let row = 1; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        let currRow = row;
-
-        while (currRow > 0 && game.matrix[col][currRow - 1] === 0) {
-          currRow--;
-        }
-
-        checkEmptyTail(col, row, col, currRow);
-      }
-    }
-  } else if (move === 'right') {
-    for (let col = 0; col < 4; col++) {
-      for (let row = 2; row >= 0; row--) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        let currRow = row;
-
-        while (currRow < 3 && game.matrix[col][currRow + 1] === 0) {
-          currRow++;
-        }
-
-        checkEmptyTail(col, row, col, currRow);
-      }
-    }
-  }
-}
-
-function checkSameTail(col, row, anotherCol, anotherRow) {
-  if (game.matrix[anotherCol][anotherRow] === game.matrix[col][row]) {
-    game.matrix[anotherCol][anotherRow] *= 2;
-    game.score += game.matrix[anotherCol][anotherRow];
-    game.matrix[col][row] = 0;
-
-    game.moved = true;
-  }
-}
-
-function searchSameTails(move) {
-  if (move === 'up') {
-    for (let col = 1; col < 4; col++) {
-      for (let row = 0; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        checkSameTail(col, row, col - 1, row);
-      }
-    }
-  } else if (move === 'down') {
-    for (let col = 2; col >= 0; col--) {
-      for (let row = 0; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        checkSameTail(col, row, col + 1, row);
-      }
-    }
-  } else if (move === 'left') {
-    for (let col = 0; col < 4; col++) {
-      for (let row = 1; row < 4; row++) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        checkSameTail(col, row, col, row - 1);
-      }
-    }
-  } else if (move === 'right') {
-    for (let col = 0; col < 4; col++) {
-      for (let row = 2; row >= 0; row--) {
-        if (game.matrix[col][row] === 0) {
-          continue;
-        }
-
-        checkSameTail(col, row, col, row + 1);
-      }
-    }
-  }
-}
-
 function isGameLose() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const current = game.matrix[i][j];
+  for (let col = 0; col < 4; col++) {
+    for (let row = 0; row < 4; row++) {
+      const current = game.matrix[col][row];
 
       if (current === 0) {
         return;
       }
 
-      if (i !== game.matrix.length - 1 && current === game.matrix[i + 1][j]) {
+      if (col !== 3 && current === game.matrix[col + 1][row]) {
         return;
       }
 
-      if (j !== game.matrix[i].length - 1
-        && current === game.matrix[i][j + 1]) {
+      if (row !== 3 && current === game.matrix[col][row + 1]) {
         return;
       }
     }
   }
 
-  renderMessage('message-lose', 'You lose! Restart the game?');
+  renderMessage('message-lose', messageGame.lose);
 
   window.removeEventListener('keydown', handlerKey);
 }
 
 function isGameWin() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const current = game.matrix[i][j];
+  for (let col = 0; col < 4; col++) {
+    for (let row = 0; row < 4; row++) {
+      const current = game.matrix[col][row];
 
       if (current === 2048) {
-        renderMessage('message-win', 'Winner! Congrats! You did it!');
+        renderMessage('message-win', messageGame.win);
 
         window.removeEventListener('keydown', handlerKey);
 
@@ -251,14 +134,6 @@ function isGameWin() {
       }
     }
   }
-}
-
-function renderMessage(classMessage, message) {
-  messageContainer.innerHTML = `
-    <p class="message ${classMessage}">
-      ${message}
-    </p>
-  `;
 }
 
 function startGame() {
@@ -306,26 +181,130 @@ function handlerKey(e) {
   render();
 
   if (game.moved) {
-    setTimeout(() => {
-      gameScore.textContent = game.score;
-
-      addRandomCell();
-
-      render();
-    }, 240);
+    addRandomCell();
+    render();
 
     game.moved = false;
   }
 }
 
-controls.addEventListener('click', e => {
-  const { target } = e;
+let startCol = 0;
+let startRow = 0;
 
-  if (target.closest('.button')) {
-    startGame();
+let actionX = 0;
+let actionY = 0;
 
-    button.classList.remove('start');
-    button.classList.add('restart');
-    button.textContent = 'Restart';
+function setValue(move) {
+  startCol = 0;
+  startRow = 0;
+
+  actionX = 0;
+  actionY = 0;
+
+  switch (move) {
+    case 'up':
+      startCol = 1;
+
+      actionX = -1;
+
+      break;
+    case 'down':
+      startCol = 2;
+
+      actionX = 1;
+
+      break;
+    case 'left':
+      startRow = 1;
+
+      actionY = -1;
+
+      break;
+    case 'right':
+      startRow = 2;
+
+      actionY = 1;
+
+      break;
   }
+}
+
+function searchEmptyTails(move) {
+  setValue(move);
+
+  for (
+    let col = startCol;
+    col >= 0 && col < 4;
+    col += (move === 'down' ? -1 : 1)
+  ) {
+    for (
+      let row = startRow;
+      row >= 0 && row < 4;
+      row += (move === 'right' ? -1 : 1)
+    ) {
+      if (game.matrix[col][row] === 0) {
+        continue;
+      }
+
+      let currCol = col;
+      let currRow = row;
+
+      while (
+        (move === 'up' ? currCol > 0
+          : move === 'down' ? currCol < 3
+            : move === 'left' ? currRow > 0
+              : currRow < 3
+        ) && game.matrix[currCol + actionX][currRow + actionY] === 0
+      ) {
+        currCol += actionX;
+        currRow += actionY;
+      }
+
+      if (currCol !== col || currRow !== row) {
+        game.matrix[currCol][currRow] = game.matrix[col][row];
+        game.matrix[col][row] = 0;
+
+        game.moved = true;
+      }
+    }
+  }
+}
+
+function searchSameTails(move) {
+  setValue(move);
+
+  for (
+    let col = startCol;
+    col >= 0 && col < 4;
+    col += (move === 'down' ? -1 : 1)
+  ) {
+    for (
+      let row = startRow;
+      row >= 0 && row < 4;
+      row += (move === 'right' ? -1 : 1)
+    ) {
+      const actionCol = col + actionX;
+      const actionRow = row + actionY;
+
+      if (game.matrix[col][row] === 0) {
+        continue;
+      }
+
+      if (game.matrix[actionCol][actionRow] === game.matrix[col][row]) {
+        game.matrix[actionCol][actionRow] *= 2;
+        game.score += game.matrix[actionCol][actionRow];
+        game.matrix[col][row] = 0;
+
+        game.moved = true;
+      }
+    }
+  }
+}
+
+button.addEventListener('click', () => {
+  startGame();
+
+  button.classList.remove('start');
+  button.classList.add('restart');
+  button.textContent = 'Restart';
 });
