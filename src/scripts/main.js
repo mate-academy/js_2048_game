0290ea022b4cf4743d0f2ad321c39d979f535fbb
Kate <ekaterina2002aleks@gmail.com>
2023-02-12 23:12:43 +0200

    Problem with multiply startings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 'use strict';
 
-const board = [
+let board = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0],
@@ -14,9 +15,15 @@ const board = [
 //   [2, 0, 0, 0],
 // ];
 
-console.table(board);
+// console.table(board);
 
 let probabilityCount = 0;
+
+const body = document.querySelector('body');
+const fieldRowAll = body.querySelectorAll('.field-row');
+const start = body.querySelector('.start');
+const scoreDisplay = body.querySelector('.game-score');
+let score = 0;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -73,6 +80,7 @@ function moveRight() {
       if (board[i][j - 1] === board[i][j]) {
         board[i][j - 1] = 0;
         board[i][j] = board[i][j] * 2;
+        score += board[i][j];
       }
     }
   }
@@ -95,6 +103,7 @@ function moveLeft() {
       if (board[i][j - 1] === board[i][j]) {
         board[i][j - 1] = board[i][j - 1] * 2;
         board[i][j] = 0;
+        score += board[i][j - 1];
       }
     }
   }
@@ -121,6 +130,7 @@ function moveUp() {
       if (newColumn[j - 1] === newColumn[j]) {
         newColumn[j - 1] = newColumn[j - 1] * 2;
         newColumn[j] = 0;
+        score += newColumn[j - 1];
       }
     }
 
@@ -149,6 +159,7 @@ function moveDown() {
       if (newColumn[j - 1] === newColumn[j]) {
         newColumn[j - 1] = 0;
         newColumn[j] = newColumn[j] * 2;
+        score += newColumn[j];
       }
     }
 
@@ -173,9 +184,6 @@ function changeAdditionalClassCell(element, newAddClass) {
 }
 
 function display() {
-  const body = document.querySelector('body');
-  const fieldRowAll = body.querySelectorAll('.field-row');
-
   for (let i = 0; i < board.length; i++) {
     const cells = fieldRowAll[i].querySelectorAll('.field-cell');
 
@@ -192,307 +200,73 @@ function display() {
       }
     }
   }
+
+  scoreDisplay.textContent = score;
 }
 
-document.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'ArrowRight':
-      moveRight();
-      display();
-      newCell();
-      display();
-      break;
+function startGame() {
+  board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  probabilityCount = 0;
 
-    case 'ArrowLeft':
-      moveLeft();
-      display();
-      newCell();
-      display();
-      break;
+  newCell();
+  newCell();
+  display();
+}
 
-    case 'ArrowUp':
-      moveUp();
-      display();
-      newCell();
-      display();
-      break;
+start.addEventListener('click', ourEvent => {
+  console.log('NEW GAME');
+  console.table(board);
+  startGame();
 
-    case 'ArrowDown':
-      moveDown();
-      display();
-      newCell();
-      display();
-      break;
-
-    default:
-      break;
+  if (ourEvent.target.matches('.restart')) {
+    start.textContent = 'Start';
+    start.classList.remove('restart');
+    start.classList.add('start');
   }
+
+  document.addEventListener('keydown', e => {
+    if (ourEvent.target.matches('.start')) {
+      start.textContent = 'Restart';
+      start.classList.remove('start');
+      start.classList.add('restart');
+    }
+
+    switch (e.key) {
+      case 'ArrowRight':
+        moveRight();
+        display();
+        newCell();
+        display();
+        break;
+
+      case 'ArrowLeft':
+        moveLeft();
+        display();
+        newCell();
+        display();
+        break;
+
+      case 'ArrowUp':
+        moveUp();
+        display();
+        newCell();
+        display();
+        break;
+
+      case 'ArrowDown':
+        moveDown();
+        display();
+        newCell();
+        display();
+        break;
+
+      default:
+        break;
+    }
+  });
 });
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-// const body = document.querySelector('body');
-// const fieldRowAll = body.querySelectorAll('.field-row');
-// const start = body.querySelector('.start');
-// let probabilityCount = 0;
-// const board = [
-//   [0, 0, 0, 0],
-//   [0, 0, 0, 0],
-//   [0, 0, 0, 0],
-//   [0, 0, 0, 0],
-// ];
-
-// function changeAdditionalClassCell(element, newAddClass) {
-//   for (let i = 2; i <= 2048; i = i * 2) {
-//     element.classList.remove('field-cell--' + i);
-//   }
-
-//   if (newAddClass) {
-//     element.classList.add(newAddClass);
-//   }
-// }
-
-// function newCell() {
-//   let cellNew = getRandomInt(16);
-//   let count = 0;
-
-//   for (const row of fieldRowAll) {
-//     const cells = row.querySelectorAll('.field-cell');
-
-//     for (const cell of cells) {
-//       if (count === cellNew && !cell.textContent) {
-//         if (probabilityCount < 10) {
-//           cell.textContent = 2;
-//           cell.classList.add('field-cell--2');
-//           probabilityCount++;
-//         } else {
-//           cell.textContent = 4;
-//           cell.classList.add('field-cell--4');
-//           probabilityCount = 0;
-//         }
-//       } else if (count === cellNew && cell.textContent) {
-//         cellNew = getRandomInt(16);
-//       }
-//       count++;
-//     }
-//   }
-// };
-
-// function moveLeft() {
-//   for (let i = 0; i < fieldRowAll.length; i++) {
-//     const cells = fieldRowAll[i].querySelectorAll('.field-cell');
-
-//     for (let j = 0; j < cells.length; j++) {
-//       if (cells[i].textContent) {
-//         board[i][j] = parseInt(cells[i].textContent);
-//       }
-//     }
-//   }
-
-//   for (let i = 0; i < board.length; i++) {
-//     const newRow = [];
-
-//     for (let j = 0; j < board[i].length; j++) {
-//       if (board[i][j] !== 0) {
-//         newRow.push(board[i][j]);
-//       }
-//     }
-
-//     for (let j = 0; j < newRow.length; j++) {
-//       board[i][j] = newRow[j];
-//     }
-
-//     for (let j = newRow.length; j < board[i].length; j++) {
-//       board[i][j] = 0;
-//     }
-//   }
-
-//   for (let i = 0; i < board.length; i++) {
-//     for (let j = 1; j < board[i].length; j++) {
-//       if (board[i][j - 1] === board[i][j]) {
-//         board[i][j - 1] = board[i][j - 1] * 2;
-//         board[i][j] = 0;
-//       }
-//     }
-//   }
-
-//   for (let i = 0; i < board.length; i++) {
-//     const newRow = [];
-
-//     for (let j = 0; j < board[i].length; j++) {
-//       if (board[i][j] !== 0) {
-//         newRow.push(board[i][j]);
-//       }
-//     }
-
-//     for (let j = 0; j < newRow.length; j++) {
-//       board[i][j] = newRow[j];
-//     }
-
-//     for (let j = newRow.length; j < board[i].length; j++) {
-//       board[i][j] = 0;
-//     }
-//   }
-
-//   for (let i = 0; i < board.length; i++) {
-//     const cells = fieldRowAll[i].querySelectorAll('.field-cell');
-
-//     for (let j = 0; j < board[i].length; j++) {
-//       if (board[i][j] !== 0) {
-//         cells[j].textContent = board[i][j];
-
-//         const addClass = 'field-cell--' + board[i][j];
-
-//         changeAdditionalClassCell(cells[j], addClass);
-//       } else {
-//         cells[j].textContent = '';
-//         changeAdditionalClassCell(cells[j]);
-//       }
-//     }
-//   }
-
-//   newCell();
-// }
-
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-
-// start.addEventListener('click', ourEvent => {
-//   if (start.classList.contains('start')) {
-//     start.textContent = 'Restart';
-//     newCell();
-//     newCell();
-//   }
-
-//   start.classList.remove('start');
-//   start.classList.add('restart');
-
-//   document.addEventListener('keydown', e => {
-//     switch (e.key) {
-//       case 'ArrowRight':
-//         break;
-
-//       case 'ArrowLeft':
-//         moveLeft();
-//         break;
-
-//       case 'ArrowUp':
-//         break;
-
-//       case 'ArrowDown':
-//         break;
-
-//       default:
-//         break;
-//     }
-//   });
-// });
