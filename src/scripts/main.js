40aba7eb08759a19ce59d0f61151f21344c31aed
Vlad <bojko.vladislav16@gmail.com>
2023-03-24 17:47:20 +0200

    release

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,215 +1,227 @@
-'use strict';
+"use strict";
 
-// * initializing vars
-
+// * initializing variables
 let board;
-const cells = document.querySelectorAll('.field-cell');
-const startButton = document.querySelector('.button.start');
-const gameField = document.querySelector('tbody');
-const messageStart = document.querySelector('.message.message-start');
-const messageWin = document.querySelector('.message.message-win');
-const messageLose = document.querySelector('.message.message-lose');
-const initialClassName = 'field-cell';
+const gameField = document.querySelector("tbody");
+const startButton = document.querySelector(".button.start");
+const startMessage = document.querySelector(".message.message-start");
+const winMessage = document.querySelector(".message.message-win");
+const loseMessage = document.querySelector(".message.message-lose");
+const initialClassName = "field-cell";
+const score = document.querySelector(".game-score");
+let scoreCounter = 0;
+let nowhereToGo = false;
 
-go();
+startGame();
 
-// * starting game
-function go() {
-  startButton.removeEventListener('click', startGame);
-  startButton.addEventListener('click', startGame);
+// * calls handleClick on startButton click
+function startGame() {
+  startButton.removeEventListener("click", handleClick);
+  startButton.addEventListener("click", handleClick);
 }
 
-// * main semantic of logic
-
-function startGame() {
-  if (startButton.innerText === 'Start') {
+// * main function that starts a game and calls all helper functions
+function handleClick() {
+  if (startButton.innerText === "Start") {
     clearBoard();
-    startButton.innerText = 'Restart';
-    startButton.classList.add('restart');
-    hideMessage(messageStart);
     setRandomTile();
     setRandomTile();
+    startButton.innerText = "Restart";
+    startButton.classList.add("restart");
+    hideMessage(startMessage);
   } else {
     restartGame();
   }
 
-  window.addEventListener('keyup', moveListener);
-  checkForWin();
-  checkForLose(); // should be modified
+  window.addEventListener("keyup", moveListener);
   updateCells();
 }
 
+const getTiles = (arr) => arr.filter((val) => val !== 0);
+
+// * on Arrow clicks cells move
 function moveListener(e) {
+  const originalBoard = JSON.stringify(board);
+
   switch (e.key) {
-    case 'ArrowRight':
-      move('right');
+    case "ArrowLeft":
+      moveLeft();
       break;
 
-    case 'ArrowLeft':
-      move('left');
+    case "ArrowRight":
+      moveRight();
       break;
 
-    case 'ArrowUp':
-      move('up');
+    case "ArrowUp":
+      moveUp();
       break;
 
-    case 'ArrowDown':
-      move('down');
+    case "ArrowDown":
+      moveDown();
       break;
   }
 
+  // * setting random tile if there was a movement
+  if (JSON.stringify(board) !== originalBoard) {
+    nowhereToGo = false;
+    setRandomTile();
+  } else {
+    nowhereToGo = true;
+  }
+
+  checkForWin();
+  checkForLose();
   updateCells();
 }
 
-function move(direction) {
-  let noFreeSpace = false;
+// * helper function for movement which checks merging as well
+function move(row) {
+  row = getTiles(row);
 
-  switch (direction) {
-    case 'right':
-      board.forEach((boardRow) => {
-        let numberOfEmptyCells = 0;
-
-        for (let i = boardRow.length; i >= 0; i--) {
-          if (boardRow[i] === 0) {
-            numberOfEmptyCells++;
-            continue;
-          }
-
-          if (numberOfEmptyCells > 0) {
-            boardRow[i + numberOfEmptyCells] = boardRow[i];
-            boardRow[i] = 0;
-            noFreeSpace = true;
-          }
-        }
-      });
-
-      break;
-
-    case 'left':
-      board.forEach((boardRow) => {
-        let numberOfEmptyCells = 0;
-
-        for (let i = 0; i < boardRow.length; i++) {
-          if (boardRow[i] === 0) {
-            numberOfEmptyCells++;
-            continue;
-          }
-
-          if (numberOfEmptyCells > 0) {
-            boardRow[i - numberOfEmptyCells] = boardRow[i];
-            boardRow[i] = 0;
-            noFreeSpace = true;
-          }
-        }
-      });
-
-      break;
-
-    case 'up':
-      board.forEach((boardRow, boardRowIndex) => {
-        for (let i = 0; i < boardRow.length; i++) {
-          if (boardRow[i] > 0) {
-            let targetRow = boardRowIndex;
-
-            for (let j = boardRowIndex - 1; j >= 0; j--) {
-              if (board[j][i] !== 0) {
-                break;
-              }
-
-              targetRow = j;
-            }
-
-            if (targetRow !== boardRowIndex) {
-              board[targetRow][i] = boardRow[i];
-              boardRow[i] = 0;
-              noFreeSpace = true;
-            }
-          }
-        }
-      });
-
-      break;
-
-    case 'down':
-      board.forEach((boardRow, boardRowIndex) => {
-        for (let i = 0; i < boardRow.length; i++) {
-          if (boardRow[i] > 0) {
-            let targetRow = boardRowIndex;
-
-            for (let j = boardRowIndex + 1; j < board.length; j++) {
-              if (board[j][i] !== 0) {
-                continue;
-              }
-
-              targetRow = j;
-            }
-
-            if (targetRow !== boardRowIndex) {
-              board[targetRow][i] = boardRow[i];
-              boardRow[i] = 0;
-              noFreeSpace = true;
-            }
-          }
-        }
-      });
-
-      break;
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] === row[i + 1]) {
+      row[i] *= 2;
+      row[i + 1] = 0;
+      scoreCounter += row[i];
+      score.innerText = scoreCounter;
+    }
   }
 
-  if (noFreeSpace) {
-    setRandomTile();
+  row = getTiles(row);
+
+  while (row.length < board[0].length) {
+    row.push(0);
+  }
+
+  return row;
+}
+
+// * func that provides ability to move cells to the right
+function moveRight() {
+  for (let r = 0; r < board.length; r++) {
+    const row = board[r];
+
+    row.reverse();
+    board[r] = move(row);
+    board[r].reverse();
+  }
+}
+
+// * func that provides ability to move cells to the left
+function moveLeft() {
+  for (let r = 0; r < board.length; r++) {
+    let row = board[r];
+
+    row = move(row);
+    board[r] = row;
+  }
+}
+
+// * func that provides ability to move cells to the top
+function moveUp() {
+  for (let c = 0; c < board[0].length; c++) {
+    let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+
+    row = move(row);
+    board[0][c] = row[0];
+    board[1][c] = row[1];
+    board[2][c] = row[2];
+    board[3][c] = row[3];
+  }
+}
+
+// * func that provides ability to move cells to the bottom
+function moveDown() {
+  for (let c = 0; c < board[0].length; c++) {
+    let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+
+    row.reverse();
+    row = move(row);
+    row.reverse();
+    board[0][c] = row[0];
+    board[1][c] = row[1];
+    board[2][c] = row[2];
+    board[3][c] = row[3];
   }
 }
 
 // * restart button modification
 function restartGame() {
-  const confirmed = window.confirm('Are you sure you want to restart game?');
+  const confirmed = window.confirm("Are you sure you want to restart game?");
 
   if (!confirmed) {
     return;
   }
 
-  hideMessage(messageWin);
-  hideMessage(messageLose);
-  showMessage(messageStart);
+  hideMessage(winMessage);
+  hideMessage(loseMessage);
+  showMessage(startMessage);
   clearBoard();
+  score.innerText = 0;
+  scoreCounter = 0;
 
-  startButton.innerText = 'Start';
-  startButton.classList.remove('restart');
+  startButton.innerText = "Start";
+  startButton.classList.remove("restart");
 
-  go();
+  startGame();
 }
 
 // * checking if we have a winner
 function checkForWin() {
-  const isWin = [...cells].some((cell) => cell.innerText === '2048');
-
-  if (isWin) {
-    showMessage(messageWin);
-  }
+  board.forEach((row) => {
+    if (row.some((cell) => cell === 2048)) {
+      showMessage(winMessage);
+      window.removeEventListener("keyup", moveListener);
+    }
+  });
 }
 
 // * checking if we have a loser
 function checkForLose() {
-  const isLose = [...cells].every((cell) => cell.innerText !== '');
+  let hasLost = true;
 
-  if (isLose) {
-    showMessage(messageLose);
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      if (board[r][c] === 0) {
+        hasLost = false;
+        break;
+      }
+
+      if (r < board.length - 1 && board[r][c] === board[r + 1][c]) {
+        hasLost = false;
+        break;
+      }
+
+      if (c < board[r].length - 1 && board[r][c] === board[r][c + 1]) {
+        hasLost = false;
+        break;
+      }
+    }
+
+    if (!hasLost) {
+      break;
+    }
+  }
+
+  if (hasLost) {
+    showMessage(loseMessage);
+    window.removeEventListener("keyup", moveListener);
   }
 }
 
 // * hiding message func
 function hideMessage(message) {
-  if (!message.classList.contains('hidden')) {
-    message.classList.add('hidden');
+  if (!message.classList.contains("hidden")) {
+    message.classList.add("hidden");
   }
 }
 
 // * showing message func
 function showMessage(message) {
-  return message.classList.remove('hidden');
+  return message.classList.remove("hidden");
 }
 
+// * clearing board
 function clearBoard() {
   board = [
     [0, 0, 0, 0],
@@ -219,40 +231,49 @@ function clearBoard() {
   ];
 }
 
-// * update Tile value and class
-
+// * update Tile's value and class
 function updateCells() {
   board.forEach((boardRow, boardRowIndex) => {
     boardRow.forEach((boardCellValue, boardCellIndex) => {
       const cell = gameField.rows[boardRowIndex].cells[boardCellIndex];
 
-      cell.innerText = '';
+      cell.innerText = "";
       cell.className = initialClassName;
 
       if (boardCellValue > 0 && boardCellValue <= 2048) {
         cell.innerText = boardCellValue;
         cell.classList.add(`${initialClassName}--${boardCellValue}`);
+
+        if (!nowhereToGo) {
+          cell.classList.add(`${initialClassName}--merge-animation`);
+
+          setTimeout(() => {
+            cell.classList.remove(`${initialClassName}--merge-animation`);
+          }, 150);
+        }
       }
     });
   });
 }
 
+// * func to set 2 or 4 in random position in the board
 function setRandomTile() {
   const numberOfRows = 4;
   const numberOfCells = 4;
 
   const getRandomRow = Math.floor(Math.random() * numberOfRows);
-  const getRandomCell = Math.floor(Math.random() * numberOfCells);
+  const getRandomCol = Math.floor(Math.random() * numberOfCells);
 
-  if (board[getRandomRow][getRandomCell] !== 0) {
+  if (board[getRandomRow][getRandomCol] !== 0) {
     setRandomTile();
 
     return;
   }
 
-  board[getRandomRow][getRandomCell] = getTwoOrFour();
+  board[getRandomRow][getRandomCol] = getTwoOrFour();
 }
 
+// * getting random number (2 or 4)
 function getTwoOrFour() {
   const randomNum = Math.random();
 
