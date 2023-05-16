012eb0649bac96437d5bf0b9cbd20eac6260fc5a
Іван Вигінний <ivan665303@gmail.com>
2023-05-16 22:39:35 +0300

    Added apperance, restart and updating cells

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Replace `<your_account>` with your Github username in the link
-    - [DEMO LINK](https://<your_account>.github.io/js_2048_game/)
+    - [DEMO LINK](https://ivxvi.github.io/js_2048_game/)
 2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
     - Run `npm run test` command to test your code;
     - Run `npm run test:only -- -n` to run fast test ignoring linter;
@@ -40,6 +40,6 @@ Hints:
 - Use `start`, `restart` classes for the main button for different styles.
 - Use `field-cell--%cell_value%` class like additional class, don't replace the main class.
 
-You can change the HTM/CSS layout if you need it.
+You can change the HTML/CSS layout if you need it.
 
 ![Preview](./src/images/reference.png)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,101 @@
 'use strict';
 
-// write your code here
+const gameScore = document.querySelector('.game-score');
+const gameField = document.querySelector('.game-field');
+const startButton = document.querySelector('.button');
+const loseMessage = document.querySelector('.message-lose');
+const winMessage = document.querySelector('.message-win');
+const startMessage = document.querySelector('.message-start');
+
+const gameBoard = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
+const rows = 4;
+const cells = 4;
+let score = 0;
+
+function appendDigit() {
+  return Math.round(Math.random()) < 0.1 ? 4 : 2;
+}
+
+function getRandomIndex() {
+  return Math.floor(Math.random() * 4);
+}
+
+function resetBoard() {
+  for (let row = 0; row < rows; row++) {
+    for (let cell = 0; cell < cells; cell++) {
+      gameBoard[row][cell] = 0;
+    }
+  }
+}
+
+startButton.addEventListener('click', (buttonEvent) => {
+  resetBoard();
+  startMessage.classList.add('hidden');
+  winMessage.classList.add('hidden');
+  loseMessage.classList.add('hidden');
+
+  startButton.classList.remove('start');
+  buttonEvent.target.classList.add('restart');
+  startButton.innerHTML = 'Restart';
+
+  document.addEventListener('keyup', () => {});
+
+  appendTile();
+  appendTile();
+
+  renderBoard();
+});
+
+function hasEmptyTile() {
+  return gameBoard.some(row => row.some(cell => cell === 0));
+}
+
+function appendTile() {
+  if (hasEmptyTile()) {
+    let rowIndex, cellIndex;
+
+    do {
+      rowIndex = getRandomIndex();
+      cellIndex = getRandomIndex();
+    } while (gameBoard[rowIndex][cellIndex] !== 0);
+
+    gameBoard[rowIndex][cellIndex] = appendDigit();
+  } else {
+    return null;
+  }
+}
+
+function updateTile(tile, cellValue) {
+  tile.innerHTML = '';
+  tile.className = '';
+  tile.classList.add('field-cell');
+
+  if (cellValue > 0) {
+    tile.classList.add(`field-cell--${cellValue}`);
+    tile.innerHTML = cellValue;
+    score += cellValue;
+  }
+
+  updateScore();
+};
+
+function updateScore() {
+  gameScore.innerHTML = score;
+}
+
+function renderBoard() {
+  for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < cells; cellIndex++) {
+      const tile = gameField.rows[rowIndex].cells[cellIndex];
+      const cellValue = gameBoard[rowIndex][cellIndex];
+
+      updateTile(tile, cellValue);
+    }
+  }
+}
