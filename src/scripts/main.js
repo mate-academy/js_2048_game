a222ea304ce462df85c0606c088af25b1eeb93d5
Іван Вигінний <ivan665303@gmail.com>
2023-05-17 22:13:13 +0300

    Added movement and win check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,25 +17,28 @@ const gameBoard = [
 const rows = 4;
 const cells = 4;
 let score = 0;
+let moveScore = 0;
 
-function appendDigit() {
-  return Math.round(Math.random()) < 0.1 ? 4 : 2;
+function getRandomTileValue() {
+  return Math.round(Math.random()) < 0.9 ? 2 : 4;
 }
 
 function getRandomIndex() {
   return Math.floor(Math.random() * 4);
 }
 
+function isWinner() {
+  return gameBoard.some(row => row.some(cell => cell === 2048));
+}
+
 function resetBoard() {
-  for (let row = 0; row < rows; row++) {
-    for (let cell = 0; cell < cells; cell++) {
-      gameBoard[row][cell] = 0;
-    }
-  }
+  gameBoard.forEach(row => row.fill(0));
 }
 
 startButton.addEventListener('click', (buttonEvent) => {
   resetBoard();
+  score = 0;
+
   startMessage.classList.add('hidden');
   winMessage.classList.add('hidden');
   loseMessage.classList.add('hidden');
@@ -44,7 +47,7 @@ startButton.addEventListener('click', (buttonEvent) => {
   buttonEvent.target.classList.add('restart');
   startButton.innerHTML = 'Restart';
 
-  document.addEventListener('keyup', () => {});
+  document.addEventListener('keyup', keyHandler);
 
   appendTile();
   appendTile();
@@ -65,7 +68,7 @@ function appendTile() {
       cellIndex = getRandomIndex();
     } while (gameBoard[rowIndex][cellIndex] !== 0);
 
-    gameBoard[rowIndex][cellIndex] = appendDigit();
+    gameBoard[rowIndex][cellIndex] = getRandomTileValue();
   } else {
     return null;
   }
@@ -79,7 +82,7 @@ function updateTile(tile, cellValue) {
   if (cellValue > 0) {
     tile.classList.add(`field-cell--${cellValue}`);
     tile.innerHTML = cellValue;
-    score += cellValue;
+    moveScore += cellValue;
   }
 
   updateScore();
@@ -99,3 +102,137 @@ function renderBoard() {
     }
   }
 }
+
+function move(row) {
+  const noZerosRow = row.filter(cell => cell !== 0);
+  const mergedRow = [];
+  let mergedValue = 0;
+
+  for (let i = 0; i < noZerosRow.length; i++) {
+    if (noZerosRow[i] === noZerosRow[i + 1]) {
+      mergedRow.push(noZerosRow[i] * 2);
+      mergedValue += noZerosRow[i] * 2;
+      i++;
+    } else {
+      mergedRow.push(noZerosRow[i]);
+    }
+  }
+
+  while (mergedRow.length < cells) {
+    mergedRow.push(0);
+  }
+
+  score += mergedValue;
+
+  return mergedRow;
+}
+
+function moveLeft() {
+  isWinner();
+  moveScore = 0;
+
+  for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    const newRow = gameBoard[rowIndex];
+
+    gameBoard[rowIndex] = move(newRow);
+  }
+
+  appendTile();
+  score += moveScore;
+
+  updateScore();
+  renderBoard();
+}
+
+function moveRight() {
+  isWinner();
+  moveScore = 0;
+
+  for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    const newRow = gameBoard[rowIndex];
+
+    newRow.reverse();
+
+    gameBoard[rowIndex] = move(newRow).reverse();
+  }
+
+  appendTile();
+  score += moveScore;
+
+  updateScore();
+  renderBoard();
+};
+
+function moveUp() {
+  isWinner();
+  moveScore = 0;
+
+  for (let columnIndex = 0; columnIndex < 4; columnIndex++) {
+    let newColumn = [
+      gameBoard[0][columnIndex],
+      gameBoard[1][columnIndex],
+      gameBoard[2][columnIndex],
+      gameBoard[3][columnIndex],
+    ];
+
+    newColumn = move(newColumn);
+
+    gameBoard[0][columnIndex] = newColumn[0];
+    gameBoard[1][columnIndex] = newColumn[1];
+    gameBoard[2][columnIndex] = newColumn[2];
+    gameBoard[3][columnIndex] = newColumn[3];
+  }
+
+  appendTile();
+  score += moveScore;
+
+  updateScore();
+  renderBoard();
+};
+
+function moveDown() {
+  isWinner();
+  moveScore = 0;
+
+  for (let columnIndex = 0; columnIndex < 4; columnIndex++) {
+    let newColumn = [
+      gameBoard[3][columnIndex],
+      gameBoard[2][columnIndex],
+      gameBoard[1][columnIndex],
+      gameBoard[0][columnIndex],
+    ];
+
+    newColumn = move(newColumn);
+
+    gameBoard[3][columnIndex] = newColumn[0];
+    gameBoard[2][columnIndex] = newColumn[1];
+    gameBoard[1][columnIndex] = newColumn[2];
+    gameBoard[0][columnIndex] = newColumn[3];
+  }
+
+  appendTile();
+  score += moveScore;
+
+  updateScore();
+  renderBoard();
+};
+
+function keyHandler(keyEvent) {
+  switch (keyEvent.code) {
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+
+    case 'ArrowRight':
+      moveRight();
+      break;
+
+    case 'ArrowUp':
+      moveUp();
+      break;
+
+    case 'ArrowDown':
+      moveDown();
+      break;
+  }
+};
