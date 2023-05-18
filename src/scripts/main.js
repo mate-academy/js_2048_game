4fdae2dd57b062d3b6aa574c886fd0e2ea06f02d
Vladyslav-Fedorishchev <fedorischev23@gmail.com>
2023-05-18 22:22:54 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 'use strict';
 
 const startButton = document.querySelector('.button');
@@ -42,24 +41,31 @@ startButton.addEventListener('click', () => {
     fillRandomCell();
     render();
   } else if (startButton.classList.contains('restart')) {
-    if (checkGameOver()) {
+    if (!checkGameOver()) {
+      scoreOfTheGame = 0;
+
+      gameMarkup.map(row => row.fill(0));
+
+      fillRandomCell();
+      fillRandomCell();
+      render();
+    } else {
       startButton.classList.remove('restart');
       startButton.classList.add('start');
       startButton.textContent = 'Start';
+
+      theGameStarted = false;
 
       for (const message of allMessages) {
         message.classList.add('hidden');
       }
 
+      gameMarkup.map(row => row.fill(0));
+
       startMessage.classList.remove('hidden');
+
+      render();
     }
-    scoreOfTheGame = 0;
-
-    gameMarkup.map(row => row.fill(0));
-
-    fillRandomCell();
-    fillRandomCell();
-    render();
   }
 });
 
@@ -73,17 +79,15 @@ const render = () => {
 };
 
 const handleRowRender = (gameRow, markupRow) => {
-  for (let i = 0; i < gameRow.length; i++) {
-    for (let j = 0; j < markupRow.length; j++) {
-      if (markupRow[j] !== 0) {
-        gameRow[j].textContent = markupRow[j];
-        gameRow[j].className = `field-cell field-cell--${markupRow[j]}`;
-      } else {
-        gameRow[j].textContent = '';
-        gameRow[j].className = 'field-cell';
-      }
+  [...gameRow].forEach((cell, index) => {
+    if (markupRow[index] !== 0) {
+      cell.textContent = markupRow[index];
+      cell.className = `field-cell field-cell--${markupRow[index]}`;
+    } else {
+      cell.textContent = '';
+      cell.className = 'field-cell';
     }
-  }
+  });
 };
 
 const fillRandomCell = () => {
@@ -142,7 +146,8 @@ const moveUp = () => {
           currentRow--;
         }
 
-        if (currentRow > 0 && gameMarkup[currentRow - 1][col] === gameMarkup[currentRow][col]) {
+        if (currentRow > 0
+           && gameMarkup[currentRow - 1][col] === gameMarkup[currentRow][col]) {
           gameMarkup[currentRow - 1][col] *= 2;
           scoreOfTheGame += gameMarkup[currentRow - 1][col];
           gameMarkup[currentRow][col] = 0;
@@ -164,7 +169,8 @@ const moveDown = () => {
           currentRow++;
         }
 
-        if (currentRow < 3 && gameMarkup[currentRow + 1][col] === gameMarkup[currentRow][col]) {
+        if (currentRow < 3
+           && gameMarkup[currentRow + 1][col] === gameMarkup[currentRow][col]) {
           gameMarkup[currentRow + 1][col] *= 2;
           scoreOfTheGame += gameMarkup[currentRow + 1][col];
           gameMarkup[currentRow][col] = 0;
@@ -186,7 +192,8 @@ const moveLeft = () => {
           currentCol--;
         }
 
-        if (currentCol > 0 && gameMarkup[row][currentCol - 1] === gameMarkup[row][currentCol]) {
+        if (currentCol > 0
+          && gameMarkup[row][currentCol - 1] === gameMarkup[row][currentCol]) {
           gameMarkup[row][currentCol - 1] *= 2;
           scoreOfTheGame += gameMarkup[row][currentCol - 1];
           gameMarkup[row][currentCol] = 0;
@@ -208,7 +215,8 @@ const moveRight = () => {
           currentCol++;
         }
 
-        if (currentCol < 3 && gameMarkup[row][currentCol + 1] === gameMarkup[row][currentCol]) {
+        if (currentCol < 3
+          && gameMarkup[row][currentCol + 1] === gameMarkup[row][currentCol]) {
           gameMarkup[row][currentCol + 1] *= 2;
           scoreOfTheGame += gameMarkup[row][currentCol + 1];
           gameMarkup[row][currentCol] = 0;
@@ -222,36 +230,18 @@ const checkGameOver = () => {
   if (!gameMarkup.some(row => row.includes(0))) {
     let gameOver = true;
 
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 3; col++) {
-        if (gameMarkup[row][col] === gameMarkup[row][col + 1]) {
+    gameMarkup.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        if (colIndex < 3 && cell === gameMarkup[rowIndex][colIndex + 1]) {
           gameOver = false;
-          break;
-        }
-      }
-
-      if (!gameOver) {
-        break;
-      }
-    }
-
-    if (gameOver) {
-      for (let col = 0; col < 4; col++) {
-        for (let row = 0; row < 3; row++) {
-          if (gameMarkup[row][col] === gameMarkup[row + 1][col]) {
-            gameOver = false;
-            break;
-          }
         }
 
-        if (!gameOver) {
-          break;
+        if (rowIndex < 3 && cell === gameMarkup[rowIndex + 1][colIndex]) {
+          gameOver = false;
         }
-      }
-    }
+      });
+    });
 
-    if (gameOver) {
-      return true;
-    }
+    return gameOver;
   }
 };
