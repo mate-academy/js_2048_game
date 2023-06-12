14109f979dbe78cded1d643753989139abbc1eaa
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-06-12 17:08:25 +0300

    check swipe

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,6 +37,7 @@ button.addEventListener('click', () => {
   });
 
   addCell();
+  addCell();
   render();
   button.blur();
 });
@@ -65,6 +66,7 @@ document.addEventListener('keyup', e => {
 
     case 'ArrowDown':
       moveDown();
+      // moves(e.code);
       break;
 
     default:
@@ -232,3 +234,118 @@ function moveDown() {
     tableGame[3][i] = row[3];
   }
 }
+
+// function moves(key) {
+//   for (let i = 0; i < tableGame.length; i++) {
+//     let row = tableGame[i];
+
+//     if (key === 'ArrowUp' || key === 'ArrowDown') {
+//       row = [
+//         tableGame[0][i], tableGame[1][i], tableGame[2][i], tableGame[3][i],
+//       ];
+//     }
+
+//     if (key === 'ArrowRight' || key === 'ArrowDown') {
+//       row.reverse();
+//     }
+
+//     row = move(row); // можна навіть прописати код функції move(row) тут
+
+//     if (key === 'ArrowRight' || key === 'ArrowDown') {
+//       row.reverse();
+//     }
+
+//     if (key === 'ArrowLeft' || key === 'ArrowRight') {
+//       tableGame[i] = row;
+//     } else {
+//       tableGame[0][i] = row[0];
+//       tableGame[1][i] = row[1];
+//       tableGame[2][i] = row[2];
+//       tableGame[3][i] = row[3];
+//     }
+//   }
+// }
+
+// for swipe
+// const eStart = 'pointerdown';
+// const eMove = 'pointermove';
+// const eEnd = 'pointerup';
+const eStart = 'touchstart';
+const eMove = 'touchmove';
+const eEnd = 'touchend';
+
+let startX, startY, endX, endY;
+
+document.addEventListener(eStart, e => {
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+document.addEventListener(eMove, e => {
+  endX = e.clientX - startX;
+  endY = e.clientY - startY;
+});
+
+document.addEventListener(eEnd, e => {
+  const copyTableGame = JSON.parse(JSON.stringify(tableGame));
+
+  if (Math.abs(endX) > Math.abs(endY)) {
+    if (endX < 0) {
+      moveLeft();
+    } else {
+      moveRight();
+    }
+  } else {
+    if (endY < 0) {
+      moveUp();
+    } else {
+      moveDown();
+    }
+  }
+
+  if (JSON.stringify(copyTableGame) === JSON.stringify(tableGame)) {
+    return;
+  }
+
+  addCell();
+  render();
+
+  if ([...cells].some(cell => cell.classList.contains('field-cell--2048'))) {
+    messageWin.classList.remove('hidden');
+  }
+
+  let endH = true;
+  let endV = true;
+
+  for (let r = 0; r < tableGame.length; r++) {
+    for (let c = 0; c < tableGame.length - 1; c++) {
+      if (tableGame[r][c] === tableGame[r][c + 1]) {
+        endH = false;
+
+        return;
+      }
+    }
+  }
+
+  for (let r = 0; r < tableGame.length; r++) {
+    const row = [
+      tableGame[0][r], tableGame[1][r], tableGame[2][r], tableGame[3][r],
+    ];
+
+    for (let c = 0; c < row.length - 1; c++) {
+      if (row[c] === row[c + 1]) {
+        endV = false;
+
+        return;
+      }
+    }
+  }
+
+  if (
+    tableGame.every(row => row.every(cell => cell !== 0))
+    && endV === true
+    && endH === true
+  ) {
+    messageLose.classList.remove('hidden');
+  }
+});
