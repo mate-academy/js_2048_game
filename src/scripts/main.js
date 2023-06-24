68e08240f8fdfee2cfb69d58c9ac27802b7d133e
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-06-24 10:18:09 +0300

    new functional was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,6 +16,9 @@ let accessToMoveDown;
 let accessToMoveRight;
 let accessToMoveLeft;
 let scoreNum;
+let startX;
+let startY;
+// Directions Object
 const Directions = {
   ArrowUp: 'ArrowUp',
   ArrowDown: 'ArrowDown',
@@ -23,6 +26,7 @@ const Directions = {
   ArrowRight: 'ArrowRight',
 };
 
+// cells creator
 function createNewCell() {
   const cellNum = Math.floor(Math.random() * freeCells.length);
 
@@ -34,6 +38,7 @@ function createNewCell() {
   freeCells.splice(cellNum, 1);
 }
 
+//  checker for free cells
 function cellsChecker() {
   freeCells = [];
   occupiedCells = [];
@@ -45,6 +50,304 @@ function cellsChecker() {
       occupiedCells.push(cell);
     }
   });
+}
+
+// checker for access
+function accessChecker() {
+  return !accessToMoveUp && !accessToMoveDown
+  && !accessToMoveRight && !accessToMoveLeft;
+}
+
+function handleTouchStart(e) {
+  startX = e.pageX || e.touches[0].pageX;
+  startY = e.pageY || e.touches[0].pageY;
+}
+
+function handleTouchMove(e) {
+  if (!startX || !startY) {
+    return;
+  }
+
+  if (cellArr.length === occupiedCells.length
+    && accessChecker()) {
+    accessToMoveUp = false;
+    accessToMoveDown = false;
+    accessToMoveRight = false;
+    accessToMoveLeft = false;
+
+    loseMessage.classList.remove('hidden');
+
+    return;
+  }
+
+  const currentX = e.pageX || e.touches[0].pageX;
+  const currentY = e.pageY || e.touches[0].pageY;
+
+  const deltaX = startX - currentX;
+  const deltaY = startY - currentY;
+
+  // Set the minimum swipe distance for it to be considered a swipe
+  const minimumSwipeDistance = 5;
+
+  // Check if the swipe is horizontal or vertical based on the distance
+  if (Math.abs(deltaX) > minimumSwipeDistance) {
+    if (deltaX > 0) {
+      // Swipe left
+      moveCellsLeft();
+    } else {
+      // Swipe right
+      moveCellsRight();
+    }
+  } else if (Math.abs(deltaY) > minimumSwipeDistance) {
+    if (deltaY > 0) {
+      // Swipe up
+      moveCellsUp();
+    } else {
+      // Swipe down
+      moveCellsDown();
+    }
+  }
+
+  // Reset start position
+  startX = null;
+  startY = null;
+}
+
+function moveCellsUp() {
+  let accessNum = 0;
+
+  occupiedCells.forEach(cell => {
+    let cellNum = cellArr.indexOf(cell);
+    let doublCount = 0;
+
+    while (cellNum >= 0) {
+      cellNum -= 4;
+
+      if (cellNum < 0) {
+        break;
+      } else if (cellArr[cellNum].textContent !== '') {
+        if (cellArr[cellNum].textContent
+          === cellArr[cellNum + 4].textContent
+          && doublCount === 0) {
+          const celltext = cellArr[cellNum + 4].textContent;
+
+          cellArr[cellNum].classList.add(
+            `field-cell--${Number(celltext) * 2}`);
+          cellArr[cellNum].innerText = `${Number(celltext) * 2}`;
+
+          cellArr[cellNum].classList.remove(
+            `field-cell--${celltext}`);
+          cellArr[cellNum + 4].classList.remove(`field-cell--${celltext}`);
+          cellArr[cellNum + 4].innerText = '';
+          scoreNum += Number(celltext) * 2;
+          gameScore.innerText = `${scoreNum}`;
+          accessNum = 1;
+          doublCount++;
+        } else {
+          break;
+        }
+      } else {
+        const celltext = cellArr[cellNum + 4].textContent;
+
+        accessNum = 1;
+        cellArr[cellNum].innerText = celltext;
+
+        cellArr[cellNum].classList.add(
+          `field-cell--${celltext}`);
+        cellArr[cellNum + 4].classList.remove(`field-cell--${celltext}`);
+        cellArr[cellNum + 4].innerText = '';
+      }
+    }
+  });
+
+  cellsChecker();
+
+  if (!accessNum) {
+    accessToMoveUp = false;
+  } else {
+    createNewCell();
+    accessToMoveDown = true;
+    accessToMoveRight = true;
+    accessToMoveLeft = true;
+  }
+}
+
+function moveCellsDown() {
+  const rvrsdOccpdCells = [ ...occupiedCells ].reverse();
+  let accessNum = 0;
+
+  rvrsdOccpdCells.forEach(cell => {
+    let cellNum = cellArr.indexOf(cell);
+    let doubleCount = 0;
+
+    while (cellNum <= 16) {
+      cellNum += 4;
+
+      if (cellNum >= 16) {
+        break;
+      } else if (cellArr[cellNum].textContent !== '') {
+        if (cellArr[cellNum].textContent
+          === cellArr[cellNum - 4].textContent
+          && doubleCount === 0) {
+          const celltext = cellArr[cellNum - 4].textContent;
+
+          cellArr[cellNum].classList.add(
+            `field-cell--${Number(celltext) * 2}`);
+          cellArr[cellNum].innerText = `${Number(celltext) * 2}`;
+
+          cellArr[cellNum].classList.remove(
+            `field-cell--${celltext}`);
+          cellArr[cellNum - 4].classList.remove(`field-cell--${celltext}`);
+          cellArr[cellNum - 4].innerText = '';
+          scoreNum += Number(celltext) * 2;
+          gameScore.innerText = `${scoreNum}`;
+          accessNum = 1;
+          doubleCount++;
+        } else {
+          break;
+        }
+      } else {
+        const celltext = cellArr[cellNum - 4].textContent;
+
+        accessNum = 1;
+        cellArr[cellNum].innerText = celltext;
+
+        cellArr[cellNum].classList.add(
+          `field-cell--${celltext}`);
+        cellArr[cellNum - 4].classList.remove(`field-cell--${celltext}`);
+        cellArr[cellNum - 4].innerText = '';
+      }
+    }
+  });
+  cellsChecker();
+
+  if (!accessNum) {
+    accessToMoveDown = false;
+  } else {
+    createNewCell();
+    accessToMoveUp = true;
+    accessToMoveRight = true;
+    accessToMoveLeft = true;
+  }
+}
+
+function moveCellsRight() {
+  const rvrsdOccpdCells = [ ...occupiedCells ].reverse();
+  let accessNum = 0;
+
+  rvrsdOccpdCells.forEach(cell => {
+    const parentRow = [ ...cell.parentElement.children ].reverse();
+    let cellNum = parentRow.indexOf(cell);
+    let doubleCount = 0;
+
+    while (cellNum > 0) {
+      cellNum -= 1;
+
+      if (cellNum < 0) {
+        break;
+      } else if (parentRow[cellNum].textContent !== '') {
+        if (parentRow[cellNum].textContent
+            === parentRow[cellNum + 1].textContent
+            && doubleCount === 0) {
+          const celltext = parentRow[cellNum + 1].textContent;
+
+          parentRow[cellNum].classList.add(
+            `field-cell--${Number(celltext) * 2}`);
+          parentRow[cellNum].innerText = `${Number(celltext) * 2}`;
+
+          parentRow[cellNum].classList.remove(
+            `field-cell--${celltext}`);
+          parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
+          parentRow[cellNum + 1].innerText = '';
+          scoreNum += Number(celltext) * 2;
+          gameScore.innerText = `${scoreNum}`;
+          accessNum = 1;
+          doubleCount++;
+        }
+      } else {
+        const celltext = parentRow[cellNum + 1].textContent;
+
+        parentRow[cellNum].innerText = celltext;
+
+        parentRow[cellNum].classList.add(
+          `field-cell--${celltext}`);
+        parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
+        parentRow[cellNum + 1].innerText = '';
+
+        accessNum = 1;
+      }
+    }
+  });
+
+  cellsChecker();
+
+  if (!accessNum) {
+    accessToMoveRight = false;
+  } else {
+    createNewCell();
+    accessToMoveUp = true;
+    accessToMoveDown = true;
+    accessToMoveLeft = true;
+  }
+}
+
+function moveCellsLeft() {
+  let accessNum = 0;
+
+  occupiedCells.forEach(cell => {
+    const parentRow = [ ...cell.parentElement.children ];
+    let cellNum = parentRow.indexOf(cell);
+    let doubleCount = 0;
+
+    while (cellNum > 0) {
+      cellNum -= 1;
+
+      if (cellNum < 0) {
+        break;
+      } else if (parentRow[cellNum].textContent !== '') {
+        if (parentRow[cellNum].textContent
+            === parentRow[cellNum + 1].textContent
+            && doubleCount === 0) {
+          const celltext = parentRow[cellNum + 1].textContent;
+
+          parentRow[cellNum].classList.add(
+            `field-cell--${Number(celltext) * 2}`);
+          parentRow[cellNum].innerText = `${Number(celltext) * 2}`;
+
+          parentRow[cellNum].classList.remove(
+            `field-cell--${celltext}`);
+          parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
+          parentRow[cellNum + 1].innerText = '';
+          scoreNum += Number(celltext) * 2;
+          gameScore.innerText = `${scoreNum}`;
+          accessNum = 1;
+          doubleCount++;
+        }
+      } else {
+        const celltext = parentRow[cellNum + 1].textContent;
+
+        parentRow[cellNum].innerText = celltext;
+
+        parentRow[cellNum].classList.add(
+          `field-cell--${celltext}`);
+        parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
+        parentRow[cellNum + 1].innerText = '';
+
+        accessNum = 1;
+      }
+    }
+  });
+
+  cellsChecker();
+
+  if (!accessNum) {
+    accessToMoveLeft = false;
+  } else {
+    createNewCell();
+    accessToMoveUp = true;
+    accessToMoveDown = true;
+    accessToMoveRight = true;
+  }
 }
 
 startButton.addEventListener('click', () => {
@@ -91,8 +394,7 @@ document.addEventListener('keydown', (e) => {
   });
 
   if (cellArr.length === occupiedCells.length
-    && !accessToMoveUp && !accessToMoveDown
-    && !accessToMoveRight && !accessToMoveLeft) {
+    && accessChecker()) {
     accessToMoveUp = false;
     accessToMoveDown = false;
     accessToMoveRight = false;
@@ -102,239 +404,24 @@ document.addEventListener('keydown', (e) => {
   }
 
   if (e.key === Directions.ArrowUp && accessToMoveUp) {
-    let accessNum = 0;
-
-    occupiedCells.forEach(cell => {
-      let cellNum = cellArr.indexOf(cell);
-      let doublCount = 0;
-
-      while (cellNum >= 0) {
-        cellNum -= 4;
-
-        if (cellNum < 0) {
-          break;
-        } else if (cellArr[cellNum].textContent !== '') {
-          if (cellArr[cellNum].textContent
-            === cellArr[cellNum + 4].textContent
-            && doublCount === 0) {
-            const celltext = cellArr[cellNum + 4].textContent;
-
-            cellArr[cellNum].classList.add(
-              `field-cell--${Number(celltext) * 2}`);
-            cellArr[cellNum].innerText = `${Number(celltext) * 2}`;
-
-            cellArr[cellNum].classList.remove(
-              `field-cell--${celltext}`);
-            cellArr[cellNum + 4].classList.remove(`field-cell--${celltext}`);
-            cellArr[cellNum + 4].innerText = '';
-            scoreNum += Number(celltext) * 2;
-            gameScore.innerText = `${scoreNum}`;
-            accessNum = 1;
-            doublCount++;
-          } else {
-            break;
-          }
-        } else {
-          const celltext = cellArr[cellNum + 4].textContent;
-
-          accessNum = 1;
-          cellArr[cellNum].innerText = celltext;
-
-          cellArr[cellNum].classList.add(
-            `field-cell--${celltext}`);
-          cellArr[cellNum + 4].classList.remove(`field-cell--${celltext}`);
-          cellArr[cellNum + 4].innerText = '';
-        }
-      }
-    });
-
-    cellsChecker();
-
-    if (accessNum === 0) {
-      accessToMoveUp = false;
-    } else {
-      createNewCell();
-      accessToMoveDown = true;
-      accessToMoveRight = true;
-      accessToMoveLeft = true;
-    }
+    moveCellsUp();
   }
 
   if (e.key === Directions.ArrowDown && accessToMoveDown) {
-    const rvrsdOccpdCells = [ ...occupiedCells ].reverse();
-    let accessNum = 0;
-
-    rvrsdOccpdCells.forEach(cell => {
-      let cellNum = cellArr.indexOf(cell);
-      let doubleCount = 0;
-
-      while (cellNum <= 16) {
-        cellNum += 4;
-
-        if (cellNum >= 16) {
-          break;
-        } else if (cellArr[cellNum].textContent !== '') {
-          if (cellArr[cellNum].textContent
-            === cellArr[cellNum - 4].textContent
-            && doubleCount === 0) {
-            const celltext = cellArr[cellNum - 4].textContent;
-
-            cellArr[cellNum].classList.add(
-              `field-cell--${Number(celltext) * 2}`);
-            cellArr[cellNum].innerText = `${Number(celltext) * 2}`;
-
-            cellArr[cellNum].classList.remove(
-              `field-cell--${celltext}`);
-            cellArr[cellNum - 4].classList.remove(`field-cell--${celltext}`);
-            cellArr[cellNum - 4].innerText = '';
-            scoreNum += Number(celltext) * 2;
-            gameScore.innerText = `${scoreNum}`;
-            accessNum = 1;
-            doubleCount++;
-          } else {
-            break;
-          }
-        } else {
-          const celltext = cellArr[cellNum - 4].textContent;
-
-          accessNum = 1;
-          cellArr[cellNum].innerText = celltext;
-
-          cellArr[cellNum].classList.add(
-            `field-cell--${celltext}`);
-          cellArr[cellNum - 4].classList.remove(`field-cell--${celltext}`);
-          cellArr[cellNum - 4].innerText = '';
-        }
-      }
-    });
-    cellsChecker();
-
-    if (accessNum === 0) {
-      accessToMoveDown = false;
-    } else {
-      createNewCell();
-      accessToMoveUp = true;
-      accessToMoveRight = true;
-      accessToMoveLeft = true;
-    }
+    moveCellsDown();
   }
 
   if (e.key === Directions.ArrowRight && accessToMoveRight) {
-    const rvrsdOccpdCells = [ ...occupiedCells ].reverse();
-    let accessNum = 0;
-
-    rvrsdOccpdCells.forEach(cell => {
-      const parentRow = [ ...cell.parentElement.children ].reverse();
-      let cellNum = parentRow.indexOf(cell);
-      let doubleCount = 0;
-
-      while (cellNum > 0) {
-        cellNum -= 1;
-
-        if (cellNum < 0) {
-          break;
-        } else if (parentRow[cellNum].textContent !== '') {
-          if (parentRow[cellNum].textContent
-            === parentRow[cellNum + 1].textContent
-            && doubleCount === 0) {
-            const celltext = parentRow[cellNum + 1].textContent;
-
-            parentRow[cellNum].classList.add(
-              `field-cell--${Number(celltext) * 2}`);
-            parentRow[cellNum].innerText = `${Number(celltext) * 2}`;
-
-            parentRow[cellNum].classList.remove(
-              `field-cell--${celltext}`);
-            parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
-            parentRow[cellNum + 1].innerText = '';
-            scoreNum += Number(celltext) * 2;
-            gameScore.innerText = `${scoreNum}`;
-            accessNum = 1;
-            doubleCount++;
-          }
-        } else {
-          const celltext = parentRow[cellNum + 1].textContent;
-
-          parentRow[cellNum].innerText = celltext;
-
-          parentRow[cellNum].classList.add(
-            `field-cell--${celltext}`);
-          parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
-          parentRow[cellNum + 1].innerText = '';
-
-          accessNum = 1;
-        }
-      }
-    });
-
-    cellsChecker();
-
-    if (accessNum === 0) {
-      accessToMoveRight = false;
-    } else {
-      createNewCell();
-      accessToMoveUp = true;
-      accessToMoveDown = true;
-      accessToMoveLeft = true;
-    }
+    moveCellsRight();
   }
 
   if (e.key === Directions.ArrowLeft && accessToMoveLeft) {
-    let accessNum = 0;
-
-    occupiedCells.forEach(cell => {
-      const parentRow = [ ...cell.parentElement.children ];
-      let cellNum = parentRow.indexOf(cell);
-      let doubleCount = 0;
-
-      while (cellNum > 0) {
-        cellNum -= 1;
-
-        if (cellNum < 0) {
-          break;
-        } else if (parentRow[cellNum].textContent !== '') {
-          if (parentRow[cellNum].textContent
-            === parentRow[cellNum + 1].textContent
-            && doubleCount === 0) {
-            const celltext = parentRow[cellNum + 1].textContent;
-
-            parentRow[cellNum].classList.add(
-              `field-cell--${Number(celltext) * 2}`);
-            parentRow[cellNum].innerText = `${Number(celltext) * 2}`;
-
-            parentRow[cellNum].classList.remove(
-              `field-cell--${celltext}`);
-            parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
-            parentRow[cellNum + 1].innerText = '';
-            scoreNum += Number(celltext) * 2;
-            gameScore.innerText = `${scoreNum}`;
-            accessNum = 1;
-            doubleCount++;
-          }
-        } else {
-          const celltext = parentRow[cellNum + 1].textContent;
-
-          parentRow[cellNum].innerText = celltext;
-
-          parentRow[cellNum].classList.add(
-            `field-cell--${celltext}`);
-          parentRow[cellNum + 1].classList.remove(`field-cell--${celltext}`);
-          parentRow[cellNum + 1].innerText = '';
-
-          accessNum = 1;
-        }
-      }
-    });
-
-    cellsChecker();
-
-    if (accessNum === 0) {
-      accessToMoveLeft = false;
-    } else {
-      createNewCell();
-      accessToMoveUp = true;
-      accessToMoveDown = true;
-      accessToMoveRight = true;
-    }
+    moveCellsLeft();
   }
 });
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('mousedown', handleTouchStart, false);
+
+document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('mousemove', handleTouchMove, false);
