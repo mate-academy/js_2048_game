445dbcb166fe1381a6b7b02212903fca2a90c512
Antonina Klishch <tonya91t@gmail.com>
2023-06-27 19:18:39 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -90,51 +90,15 @@ function hasEmptyCell() {
 };
 
 document.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
   if (ev.target.closest('.start')) {
     messageStart.classList.add(`hidden`);
     buttonStart.textContent = 'Restart';
     buttonStart.classList.add(`restart`);
     buttonStart.classList.remove(`start`);
     setGame();
-
-    document.addEventListener('keydown', (e) => {
-      switch (e.key) {
-        case 'ArrowRight':
-          moveRight();
-          addNumber();
-          break;
-        case 'ArrowLeft':
-          moveLeft();
-          addNumber();
-          break;
-        case 'ArrowDown':
-          moveDown();
-          addNumber();
-          break;
-        case 'ArrowUp':
-          moveUp();
-          addNumber();
-          break;
-        default:
-          return;
-      }
-
-      [...cells].forEach(cell => {
-        if (cell.textContent === '2048') {
-          messageWin.classList.remove(`hidden`);
-
-          setTimeout(() => {
-            document.location.reload();
-          }, 2000);
-        }
-      });
-
-      if (!hasEmptyCell() && !hasAdjacentSameNumbers(valueBoard)) {
-        messageLose.classList.remove(`hidden`);
-      }
-
-      document.querySelector('.game-score').textContent = score;
-    });
+    document.addEventListener('keydown', playGame);
   } else if (ev.target.closest('.restart')) {
     setGame();
     score = 0;
@@ -155,17 +119,20 @@ function hasAdjacentSameNumbers(data) {
   return false;
 }
 
-function filterZero(y) {
-  const newRow = y.filter(num => num !== 0);
+function filterZero(data) {
+  const newRow = data.filter(num => num !== 0);
 
   return newRow;
 }
 
 function move(data) {
+  let flag = false;
   let row = filterZero(data);
+  const rowStart = data;
 
   for (let i = 0; i < row.length - 1; i++) {
     if (row[i] === row[i + 1]) {
+      flag = true;
       row[i] *= 2;
       row[i + 1] = 0;
       score += row[i];
@@ -178,14 +145,28 @@ function move(data) {
     row.push(0);
   }
 
-  return row;
+  if (rowStart.toString() !== row.toString()) {
+    flag = true;
+  }
+
+  return [row, flag];
+}
+
+function hasActions(actions) {
+  if (actions.includes(true)) {
+    addNumber();
+  }
 }
 
 function moveLeft() {
+  const actions = [];
+  let flag;
+
   for (let r = 0; r < rows; r++) {
     let row = valueBoard[r];
 
-    row = move(row);
+    [row, flag] = move(row);
+    actions.push(flag);
     valueBoard[r] = row;
 
     for (let c = 0; c < columns; c++) {
@@ -194,14 +175,22 @@ function moveLeft() {
       updateCell(board[r][c], number);
     }
   }
+
+  hasActions(actions);
+  checkLose();
+  checkWin();
 };
 
 function moveRight() {
+  const actions = [];
+  let flag;
+
   for (let r = 0; r < rows; r++) {
     let row = valueBoard[r];
 
-    row.reverse(); ;
-    row = move(row);
+    row.reverse();
+    [row, flag] = move(row);
+    actions.push(flag);
     valueBoard[r] = row.reverse(); ;
 
     for (let c = 0; c < columns; c++) {
@@ -210,14 +199,21 @@ function moveRight() {
       updateCell(board[r][c], number);
     }
   }
+  hasActions(actions);
+  checkLose();
+  checkWin();
 };
 
 function moveUp() {
+  const actions = [];
+  let flag;
+
   for (let c = 0; c < columns; c++) {
     let column = [valueBoard[0][c], valueBoard[1][c],
       valueBoard[2][c], valueBoard[3][c]];
 
-    column = move(column);
+    [column, flag] = move(column);
+    actions.push(flag);
 
     for (let r = 0; r < columns; r++) {
       valueBoard[r][c] = column[r];
@@ -227,14 +223,23 @@ function moveUp() {
       updateCell(board[r][c], number);
     }
   }
+
+  hasActions(actions);
+  checkLose();
+  checkWin();
 }
 
 function moveDown() {
+  const actions = [];
+  let flag;
+
   for (let c = 0; c < columns; c++) {
     let column = [valueBoard[3][c], valueBoard[2][c],
       valueBoard[1][c], valueBoard[0][c]];
 
-    column = move(column);
+    [column, flag] = move(column);
+    actions.push(flag);
+
     column.reverse();
 
     for (let r = 0; r < columns; r++) {
@@ -245,4 +250,47 @@ function moveDown() {
       updateCell(board[r][c], number);
     }
   }
+  hasActions(actions);
+  checkLose();
+  checkWin();
+}
+
+function checkWin() {
+  [...cells].forEach(cell => {
+    if (cell.textContent === '2048') {
+      messageWin.classList.remove(`hidden`);
+      document.removeEventListener('keydown', playGame);
+
+      setTimeout(() => {
+        document.location.reload();
+      }, 5000);
+    }
+  });
+}
+
+function checkLose() {
+  if (!hasEmptyCell() && !hasAdjacentSameNumbers(valueBoard)) {
+    messageLose.classList.remove(`hidden`);
+  }
+}
+
+function playGame(e) {
+  e.preventDefault();
+
+  switch (e.key) {
+    case 'ArrowRight':
+      moveRight();
+      break;
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break;
+    case 'ArrowUp':
+      moveUp();
+      break;
+  }
+
+  document.querySelector('.game-score').textContent = score;
 }
