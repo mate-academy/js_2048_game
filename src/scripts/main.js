d34f5b9c7bccb00880d27343db3054e47f94d738
Alla Serhiienko <allasergienko706@gmail.com>
2023-07-25 01:37:45 +0200

    movement was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const button = document.querySelector('.button');
 const messageStart = document.querySelector('.message-start');
 const messageRules = document.querySelector('.message-rules');
+// const cells = [...document.querySelectorAll('.field-cell')];
 const cells = document.querySelectorAll('.field-cell');
 
 let emptyCells = [];
@@ -10,21 +11,15 @@ let filledCells = [];
 
 const arrows = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
 
-document.addEventListener('keydown', event => {
-  // зробити, щоб реагувало лише на стрілки-клавіші
-  // if (!arrows.includes(event.key)) {
-  //   return;
-  // }
+document.addEventListener('keydown', keydownEvent => {
+  const key = keydownEvent.key;
 
-  if (arrows.includes(event.key)) {
-    handleArrowKeyAction(event.key);
+  if (arrows.includes(key)) {
+    handleArrowKeyAction(key);
 
     if (button.classList.contains('start')
       && !messageRules.classList.contains('hidden')) {
-      button.classList.remove('start');
-      button.classList.add('restart');
-      button.textContent = 'Restart';
-      messageRules.classList.add('hidden');
+      setGameRestart();
     }
   }
 });
@@ -54,27 +49,221 @@ function startGame() {
 
 function restartGame() {
   clearField();
-  button.classList.remove('restart');
-  button.classList.add('start');
-  button.textContent = 'Start';
-  messageStart.classList.remove('hidden');
+  setGameStart();
 }
 
 function addNewTile() {
-  const randomCell = getRandomCell();
+  const randomCellIndex = getRandomCellIndex();
   const randomNumber = getRandomNumber();
+  const randomCell = emptyCells[randomCellIndex];
 
-  emptyCells[randomCell].classList.add(`field-cell--${randomNumber}`);
-  emptyCells[randomCell].textContent = `${randomNumber}`;
+  randomCell.classList.add(`field-cell--${randomNumber}`);
+  randomCell.textContent = `${randomNumber}`;
 
   updateCellLists();
 }
 
 function handleArrowKeyAction(key) {
-  //
+  switch (key) {
+    case 'ArrowUp':
+      for (let i = 0; i < filledCells.length; i++) {
+        const cell = filledCells[i];
+        const cellIndex = [...cells].indexOf(cell);
+        let emptyCell = filledCells[i];
+
+        for (let j = cellIndex - 4; j >= 0; j -= 4) {
+          if (emptyCells.includes([...cells][j])) {
+            emptyCell = [...cells][j];
+          } else {
+            break;
+          }
+        }
+
+        // if ([4, 5, 6, 7].includes(cellIndex)) {
+        //   for (let j = cellIndex - 4; j >= 0; j -= 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        // if ([8, 9, 10, 11].includes(cellIndex)) {
+        //   for (let j = cellIndex - 4; j >= 0; j -= 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        // if ([12, 13, 14, 15].includes(cellIndex)) {
+        //   for (let j = cellIndex - 4; j >= 0; j -= 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        if (emptyCell !== cell) {
+          emptyCell.classList.add(cell.classList[1]);
+          emptyCell.textContent = cell.textContent;
+          clearCell([...cells][cellIndex]);
+        }
+      }
+      // updateCellLists();
+      break;
+
+    case 'ArrowDown':
+      for (let i = filledCells.length - 1; i >= 0; i--) {
+        const cell = filledCells[i];
+        const cellIndex = [...cells].indexOf(cell);
+        let emptyCell = filledCells[i];
+
+        for (let j = cellIndex + 4; j <= 15; j += 4) {
+          if (emptyCells.includes([...cells][j])) {
+            emptyCell = [...cells][j];
+          } else {
+            break;
+          }
+        }
+
+        // if ([8, 9, 10, 11].includes(cellIndex)) {
+        //   for (let j = cellIndex + 4; j <= 15; j += 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        // if ([4, 5, 6, 7].includes(cellIndex)) {
+        //   for (let j = cellIndex + 4; j <= 15; j += 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        // if ([0, 1, 2, 3].includes(cellIndex)) {
+        //   for (let j = cellIndex + 4; j <= 15; j += 4) {
+        //     if (emptyCells.includes([...cells][j])) {
+        //       emptyCell = [...cells][j];
+        //     } else {
+        //       break;
+        //     }
+        //   }
+        // }
+
+        if (emptyCell !== cell) {
+          emptyCell.classList.add(cell.classList[1]);
+          emptyCell.textContent = cell.textContent;
+          clearCell([...cells][cellIndex]);
+        }
+      }
+      // updateCellLists();
+      break;
+
+    case 'ArrowLeft':
+      for (let i = 0; i < filledCells.length; i++) {
+        const cell = filledCells[i];
+        const cellIndex = [...cells].indexOf(cell);
+        let emptyCell = filledCells[i];
+
+        if ([1, 5, 9, 13].includes(cellIndex)) {
+          for (let j = cellIndex - 1; j > cellIndex - 2; j--) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if ([2, 6, 10, 14].includes(cellIndex)) {
+          for (let j = cellIndex - 1; j > cellIndex - 3; j--) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if ([3, 7, 11, 15].includes(cellIndex)) {
+          for (let j = cellIndex - 1; j > cellIndex - 4; j--) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if (emptyCell !== cell) {
+          emptyCell.classList.add(cell.classList[1]);
+          emptyCell.textContent = cell.textContent;
+          clearCell([...cells][cellIndex]);
+        }
+      }
+      // updateCellLists();
+      break;
+
+    case 'ArrowRight':
+      for (let i = filledCells.length - 1; i >= 0; i--) {
+        const cell = filledCells[i];
+        const cellIndex = [...cells].indexOf(cell);
+        let emptyCell = filledCells[i];
+
+        if ([2, 6, 10, 14].includes(cellIndex)) {
+          for (let j = cellIndex + 1; j < cellIndex + 2; j++) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if ([1, 5, 9, 13].includes(cellIndex)) {
+          for (let j = cellIndex + 1; j < cellIndex + 3; j++) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if ([0, 4, 8, 12].includes(cellIndex)) {
+          for (let j = cellIndex + 1; j < cellIndex + 4; j++) {
+            if (emptyCells.includes([...cells][j])) {
+              emptyCell = [...cells][j];
+            } else {
+              break;
+            }
+          }
+        }
+
+        if (emptyCell !== cell) {
+          emptyCell.classList.add(cell.classList[1]);
+          emptyCell.textContent = cell.textContent;
+          clearCell([...cells][cellIndex]);
+        }
+      }
+      // updateCellLists();
+      break;
+  }
 }
 
-function getRandomCell() {
+function getRandomCellIndex() {
   return Math.floor(Math.random() * emptyCells.length);
 }
 
@@ -84,14 +273,38 @@ function getRandomNumber() {
 
 function clearField() {
   filledCells.forEach(item => {
-    item.classList.remove(`${item.classList[1]}`);
+    const cssClass = item.classList[1];
+
+    item.classList.remove(cssClass);
     item.textContent = '';
   });
 
   updateCellLists();
 }
 
+function clearCell(cell) {
+  const cssClass = cell.classList[1];
+
+  cell.classList.remove(cssClass);
+  cell.textContent = '';
+  updateCellLists();
+}
+
 function updateCellLists() {
   emptyCells = [...cells].filter(item => item.classList.length === 1);
   filledCells = [...cells].filter(item => item.classList.length > 1);
+}
+
+function setGameStart() {
+  button.classList.remove('restart');
+  button.classList.add('start');
+  button.textContent = 'Start';
+  messageStart.classList.remove('hidden');
+}
+
+function setGameRestart() {
+  button.classList.remove('start');
+  button.classList.add('restart');
+  button.textContent = 'Restart';
+  messageRules.classList.add('hidden');
 }
