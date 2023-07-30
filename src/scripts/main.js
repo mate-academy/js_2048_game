a1887f701ee71c64d1a49017f03e71b599ed99ad
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-07-30 16:35:08 +0300

    finish game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,139 @@ const table = [
   [0, 0, 0, 0],
 ];
 
+function up(paramArr, index) {
+  const arr = paramArr.filter(item => item !== 0);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const current = arr[i];
+    const next = arr[i + 1];
+
+    if (current === next) {
+      arr[i] = current * 2;
+      scoreValue += arr[i];
+      updateScore();
+      arr[i + 1] = 0;
+    }
+  }
+
+  const mergedArr = arr.filter(item => item !== 0);
+
+  table[0][index] = mergedArr[0] || 0;
+  table[1][index] = mergedArr[1] || 0;
+  table[2][index] = mergedArr[2] || 0;
+  table[3][index] = mergedArr[3] || 0;
+}
+
+function down(paramArr, index) {
+  const arr = paramArr.filter(item => item !== 0);
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const current = arr[i];
+    const prev = arr[i - 1];
+
+    if (current === prev) {
+      arr[i] = current * 2;
+      scoreValue += arr[i];
+      updateScore();
+      arr[i - 1] = 0;
+    }
+  }
+
+  const mergedArr = arr.filter(item => item !== 0);
+
+  while (mergedArr.length < 4) {
+    mergedArr.unshift(0);
+  }
+
+  table[0][index] = mergedArr[0] || 0;
+  table[1][index] = mergedArr[1] || 0;
+  table[2][index] = mergedArr[2] || 0;
+  table[3][index] = mergedArr[3] || 0;
+}
+
+const winMessage = document.querySelector('.message-win');
+const loseMessage = document.querySelector('.message-lose');
+const startMessage = document.querySelector('.message-start');
+
+function messageWin() {
+  winMessage.classList = ['message message-win'];
+  loseMessage.classList = ['message message-lose hidden'];
+  startMessage.classList = ['message message-start hidden'];
+}
+
+function messageStart() {
+  winMessage.classList = ['message message-win hidden'];
+  loseMessage.classList = ['message message-lose hidden'];
+  startMessage.classList = ['message message-start'];
+}
+
+function messageLose() {
+  winMessage.classList = ['message message-win hidden'];
+  loseMessage.classList = ['message message-lose'];
+  startMessage.classList = ['message message-start hidden'];
+}
+
+function noEmptyCell() {
+  for (const row of table) {
+    for (const cell of row) {
+      if (cell === 0) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function noCellToMerge() {
+  for (const row of table) {
+    for (let i = 0; i < row.length - 1; i++) {
+      const current = row[i];
+      const next = row[i + 1];
+
+      if (current === next) {
+        return false;
+      }
+    }
+  }
+
+  for (let j = 0; j < 3; j++) {
+    for (let k = 0; k < 3; k++) {
+      const current = table[k][j];
+      const cellNextRow = table[k + 1][j];
+
+      if (current === cellNextRow) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function loseCheck() {
+  if (noEmptyCell() && noCellToMerge()) {
+    messageLose();
+  }
+}
+
+function updateScore() {
+  score.textContent = scoreValue;
+
+  for (const row of table) {
+    for (const item of row) {
+      if (item === 2048) {
+        messageWin();
+      }
+    }
+  }
+}
+
+let scoreValue = 0;
+const score = document.querySelector('.game-score');
+
+score.textContent = scoreValue;
+
 const fields = document.querySelectorAll('.field-cell');
 const button = document.querySelector('.button');
 
@@ -18,8 +151,9 @@ function updateGameFields() {
 
     fields[i].textContent = tableValue;
 
+    fields[i].classList = ['field-cell'];
+
     if (tableValue !== 0) {
-      fields[i].classList = ['field-cell'];
       fields[i].classList.add(`field-cell--${tableValue}`);
     }
   }
@@ -28,8 +162,6 @@ function updateGameFields() {
 updateGameFields();
 
 function appearingOneRandomCell() {
-  updateGameFields();
-
   const fieldIndex = getRandonCell();
   const rowIndex = Math.floor(fieldIndex / 4);
   const colIndex = fieldIndex - (rowIndex * 4);
@@ -41,21 +173,17 @@ function appearingOneRandomCell() {
   } else {
     table[rowIndex][colIndex] = 2;
   }
-
-  updateGameFields();
 }
 
 function restart() {
-  updateGameFields();
+  table[0] = [0, 0, 0, 0];
+  table[1] = [0, 0, 0, 0];
+  table[2] = [0, 0, 0, 0];
+  table[3] = [0, 0, 0, 0];
 
-  for (let i = 0; i < fields.length; i++) {
-    const rowIndex = Math.floor(i / 4);
-    const colIndex = i - (rowIndex * 4);
-
-    table[rowIndex][colIndex] = 0;
-  }
-
-  updateGameFields();
+  scoreValue = 0;
+  updateScore();
+  messageStart();
 }
 
 button.addEventListener('click', () => {
@@ -65,64 +193,211 @@ button.addEventListener('click', () => {
     button.textContent = 'restart';
 
     appearingOneRandomCell();
+    updateGameFields();
     appearingOneRandomCell();
+    updateGameFields();
+    // console.log(table);
   } else {
     button.classList.remove('restart');
     button.classList.add('start');
-    button.textContent = 'start';
+    button.textContent = 'Start';
 
     restart();
+    updateGameFields();
+    // console.log(table);
   }
 });
 
 function getRandonCell() {
-  updateGameFields();
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-  let randomIndex = Math.floor(Math.random() * 15);
-  const indexRow = Math.floor(randomIndex / 4);
-  const indexCol = randomIndex - (indexRow * 4);
+  for (let i = 15; i >= 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
 
-  while (
-    table[indexRow][indexCol] !== 0
-  ) {
-    randomIndex = Math.floor(Math.random() * 15);
+    const value = arr[randomIndex];
+
+    const indexRow = Math.floor(value / 4);
+    const indexCol = value - (indexRow * 4);
+
+    if (table[indexRow][indexCol] === 0) {
+      return value;
+    } else {
+      arr.splice(randomIndex, 1);
+    }
+  }
+}
+
+function left(paramArr) {
+  const arr = paramArr.filter(item => item !== 0);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const current = arr[i];
+    const next = arr[i + 1];
+
+    if (current === next) {
+      arr[i] = current * 2;
+      scoreValue += arr[i];
+      updateScore();
+      arr[i + 1] = 0;
+    }
   }
 
-  return randomIndex;
+  const mergedArr = arr.filter(item => item !== 0);
+
+  paramArr[0] = mergedArr[0] || 0;
+  paramArr[1] = mergedArr[1] || 0;
+  paramArr[2] = mergedArr[2] || 0;
+  paramArr[3] = mergedArr[3] || 0;
+}
+
+function right(paramArr) {
+  const arr = paramArr.filter(item => item !== 0);
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const current = arr[i];
+    const prev = arr[i - 1];
+
+    if (current === prev) {
+      arr[i] = current * 2;
+      scoreValue += arr[i];
+      updateScore();
+      arr[i - 1] = 0;
+    }
+  }
+
+  const mergedArr = arr.filter(item => item !== 0);
+
+  while (mergedArr.length < 4) {
+    mergedArr.unshift(0);
+  }
+
+  paramArr[0] = mergedArr[0] || 0;
+  paramArr[1] = mergedArr[1] || 0;
+  paramArr[2] = mergedArr[2] || 0;
+  paramArr[3] = mergedArr[3] || 0;
 }
 
 function moveLeft() {
-  // console.log('left');
-  // console.log(table);
-  appearingOneRandomCell();
+  const json = JSON.stringify(table);
+  const copyTable = JSON.parse(json);
+
+  for (const row of table) {
+    left(row);
+  }
+
+  for (let i = 0; i < 16; i++) {
+    const rowIndex = Math.floor(i / 4);
+    const colIndex = i - (rowIndex * 4);
+
+    const tableValue = table[rowIndex][colIndex];
+    const copyTableValue = copyTable[rowIndex][colIndex];
+
+    if (tableValue !== copyTableValue) {
+      appearingOneRandomCell();
+      updateGameFields();
+
+      break;
+    }
+  }
 }
 
 function moveRight() {
-  // console.log('right');
+  const json = JSON.stringify(table);
+  const copyTable = JSON.parse(json);
+
+  for (const row of table) {
+    right(row);
+  }
+
+  for (let i = 0; i < 16; i++) {
+    const rowIndex = Math.floor(i / 4);
+    const colIndex = i - (rowIndex * 4);
+
+    const tableValue = table[rowIndex][colIndex];
+    const copyTableValue = copyTable[rowIndex][colIndex];
+
+    if (tableValue !== copyTableValue) {
+      appearingOneRandomCell();
+      updateGameFields();
+
+      break;
+    }
+  }
 }
 
 function moveUp() {
-  // console.log('up');
+  const json = JSON.stringify(table);
+  const copyTable = JSON.parse(json);
+
+  for (let j = 0; j < 4; j++) {
+    up([table[0][j], table[1][j], table[2][j], table[3][j]], j);
+  }
+
+  for (let i = 0; i < 16; i++) {
+    const rowIndex = Math.floor(i / 4);
+    const colIndex = i - (rowIndex * 4);
+
+    const tableValue = table[rowIndex][colIndex];
+    const copyTableValue = copyTable[rowIndex][colIndex];
+
+    if (tableValue !== copyTableValue) {
+      appearingOneRandomCell();
+      updateGameFields();
+
+      break;
+    }
+  }
 }
 
 function moveDown() {
-  // console.log('down');
+  const json = JSON.stringify(table);
+  const copyTable = JSON.parse(json);
+
+  for (let j = 0; j < 4; j++) {
+    down([table[0][j], table[1][j], table[2][j], table[3][j]], j);
+  }
+
+  for (let i = 0; i < 16; i++) {
+    const rowIndex = Math.floor(i / 4);
+    const colIndex = i - (rowIndex * 4);
+
+    const tableValue = table[rowIndex][colIndex];
+    const copyTableValue = copyTable[rowIndex][colIndex];
+
+    if (tableValue !== copyTableValue) {
+      appearingOneRandomCell();
+      updateGameFields();
+
+      break;
+    }
+  }
 }
 
-addEventListener('keydown', (eventParam) => {
-  if (eventParam.code === 'ArrowLeft') {
+document.addEventListener('keydown', eventParam => {
+  if (eventParam.key === 'ArrowLeft') {
     moveLeft();
+    // console.log('left');
+    loseCheck();
   }
 
-  if (eventParam.code === 'ArrowRight') {
+  if (eventParam.key === 'ArrowRight') {
     moveRight();
+    // console.log('right');
+    loseCheck();
   }
 
-  if (eventParam.code === 'ArrowUp') {
+  if (eventParam.key === 'ArrowUp') {
     moveUp();
+    // console.log('up');
+    loseCheck();
   }
 
-  if (eventParam.code === 'ArrowDown') {
+  if (eventParam.key === 'ArrowDown') {
     moveDown();
+    // console.log('down');
+    loseCheck();
   }
 });
+
+// document.addEventListener('keydown', event => console.log(event.key));
+restart();
