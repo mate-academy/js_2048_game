1a92237b1f519a5184f228af31a82cae94422567
Orchibald <orestkozak17@gmail.com>
2023-08-05 14:17:18 +0200

    big changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,487 +1,233 @@
 'use strict';
 
-const buttonToPlay = document.querySelector('.button');
-const startMessage = document.querySelector('.message_start');
-const winMessage = document.querySelector('.message_win');
-const loseMessage = document.querySelector('.message_lose');
-const scoreCounter = document.querySelector('.game_score');
+const button = document.querySelector('button');
+const messageStart = document.querySelector('.message_start');
+const messageWin = document.querySelector('.message_win');
+const messageLose = document.querySelector('.message_lose');
+const gameScore = document.querySelector('.game_score');
+const fieldRows = document.querySelectorAll('.field_row');
 
-const tableRows = document.querySelector('tbody').rows;
-
-const fieldCells = document.querySelectorAll('td');
-
-const matrix = createMatrix();
+const size = 4;
 let score = 0;
+let isWin = false;
+let newGameField;
+let gameField = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
 
-buttonToPlay.addEventListener('click', () => {
-  startMessage.classList.add('hidden');
-  loseMessage.classList.add('hidden');
-  winMessage.classList.add('hidden');
+button.addEventListener('click', e => {
+  document.addEventListener('keydown', move);
 
-  score = 0;
-
-  if (buttonToPlay.classList.contains('start')) {
-    spawnStartNumber();
-    buttonToPlay.classList.remove('start');
-    buttonToPlay.classList.add('restart');
-    buttonToPlay.innerHTML = 'Restart';
+  if (button.classList.contains('start')) {
+    button.classList.replace('start', 'restart');
+    button.innerText = 'Restart';
+    messageStart.classList.add('hidden');
+  } else {
+    isWin = false;
+    reset();
   }
 
-  if (buttonToPlay.classList.contains('restart')) {
-    iterateMatrix(element => restoreCell(element));
-    spawnStartNumber();
-  }
+  addNumber();
+  addNumber();
+  render();
 });
 
-callMove();
+function move(e) {
+  newGameField = [...gameField];
 
-function gameResult() {
-  iterateMatrix((element) => {
-    if (parseInt(element.innerHTML) === 2048) {
-      winMessage.classList.remove('hidden');
+  const leftArrow = 'ArrowLeft';
+  const rightArrow = 'ArrowRight';
+  const diwnArrow = 'ArrowDown';
+  const upArrow = 'ArrowUp';
+
+  switch (e.key) {
+    case leftArrow:
+      left(newGameField);
+      break;
+
+    case rightArrow:
+      right(newGameField);
+      break;
+
+    case diwnArrow:
+      down(newGameField);
+      break;
+
+    case upArrow:
+      up(newGameField);
+      break;
+
+    default:
+      return;
+  }
+
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      if (newGameField[row][col] !== gameField[row][col]) {
+        gameField = newGameField;
+        addNumber();
+        render();
+      }
     }
+  }
+
+  if (isWin) {
+    messageWin.classList.remove('hidden');
+  }
+
+  if (!isPosibleToMove()) {
+    messageLose.classList.remove('hidden');
+    document.removeEventListener('keydown', move);
+  }
+}
+
+function reset() {
+  score = 0;
+
+  gameField = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  if (!messageLose.classList.contains('hidden')) {
+    messageLose.classList.add('hidden');
+  }
+
+  if (!messageWin.classList.contains('hidden')) {
+    messageWin.classList.add('hidden');
+  }
+};
+
+function addNumber() {
+  const [randomY, randomX] = findEmptyCell();
+
+  gameField[randomY][randomX] = Math.random() < 0.9 ? 2 : 4;
+};
+
+function findEmptyCell() {
+  const emptyCells = [];
+
+  gameField.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      if (cell === 0) {
+        emptyCells.push([rowIndex, columnIndex]);
+      }
+    });
   });
 
-  if (!gameLost()) {
-    loseMessage.classList.remove('hidden');
-  }
+  return emptyCells[Math.floor(Math.random() * emptyCells.length)];
 }
 
-function createMatrix() {
-  let rowIndex = 0;
-  let colIndex = 0;
+function render() {
+  gameField.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      const elem = fieldRows[rowIndex].children[colIndex];
 
-  const cols = 4;
-  const array = [];
+      if (cell === 0) {
+        elem.textContent = '';
+        elem.className = 'field_cell';
+      } else {
+        elem.textContent = cell;
+        elem.className = `field_cell field_cell--${cell}`;
+      }
+    });
+  });
 
-  for (let i = 0; i < fieldCells.length; i++) {
-    if (!array[rowIndex]) {
-      array[rowIndex] = [];
-    }
+  gameScore.textContent = score;
+};
 
-    array[rowIndex].push(fieldCells[i]);
-
-    colIndex++;
-
-    if (colIndex === cols) {
-      rowIndex++;
-      colIndex = 0;
-    }
-  }
-
-  return array;
-}
-
-function spawnStartNumber() {
-  for (let i = 0; i < 2; i++) {
-    spawnNewNumber(2);
-  }
-
-  paintCell();
-}
-
-function spawnNewNumber(numberToSpawn) {
-  if (!checkForEmpty()) {
+function left() {
+  if (!checkRows()) {
     return;
   }
 
-  const randomValue = Math.random() < 0.9 ? 2 : 4;
+  newGameField = newGameField.map(row => {
+    const newRow = row.filter(cell => cell !== 0);
 
-  while (true) {
-    const row = Math.floor(Math.random() * 4);
-    const col = Math.floor(Math.random() * 4);
+    newRow.forEach((cell, index) => {
+      if (cell === newRow[index + 1]) {
+        newRow[index] *= 2;
+        newRow.splice(index + 1, 1);
+        score += newRow[index];
 
-    if (matrix[row][col].innerHTML === '') {
-      matrix[row][col].innerHTML = randomValue;
-      break;
-    }
+        if (newRow[index] === 2048) {
+          isWin = true;
+        }
+      }
+    });
 
-    updateGame();
-  }
-}
-
-function paintCell() {
-  iterateMatrix((element) => {
-    const cellValue = element.innerHTML;
-
-    if (cellValue !== '') {
-      element.classList = `field_cell field_cell--${cellValue}`;
-    } else {
-      element.classList = 'field_cell';
-    }
+    return newRow.concat(Array(size - newRow.length).fill(0));
   });
 }
 
-function compareCells(element1, element2) {
-  for (let num = 2; num <= 2048; num *= 2) {
-    if (parseInt(element1.innerHTML) === num
-    && parseInt(element2.innerHTML) === num) {
-      return true;
-    }
-  }
-}
-
-function restoreCell(arrElement) {
-  arrElement.innerHTML = '';
-  arrElement.classList.remove(`field_cell--${arrElement.innerHTML}`);
-}
-
-function iterateMatrix(callback) {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      callback(matrix[i][j]);
-    }
-  }
-}
-
-function updateCell(cell, num) {
-  cell.innerText = '';
-  cell.classList.value = '';
-  cell.classList.add('field_cell');
-
-  if (num > 0) {
-    cell.innerText = num.toString();
-    cell.classList.add(`field_cell--${num.toString()}`);
-  }
-}
-
-function updateGame() {
-  for (let r = 0; r < 4; r++) {
-    for (let c = 0; c < 4; c++) {
-      const currentCell = tableRows[r].cells[c];
-      const num = parseInt(matrix[r][c].innerHTML);
-
-      updateCell(currentCell, num);
-    }
+function right() {
+  if (!checkRows()) {
+    return;
   }
 
-  scoreCounter.innerText = score.toString();
+  reverseRows();
+  left();
+  reverseRows();
 }
 
-function callMove() {
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') {
-      moveUp();
+function reverseRows() {
+  newGameField.forEach(row => row.reverse());
+}
+
+function down() {
+  transposeGameField();
+  right();
+  transposeGameField();
+}
+
+function up() {
+  transposeGameField();
+  left();
+  transposeGameField();
+}
+
+function transposeGameField() {
+  newGameField = newGameField[0]
+    .map((_, colIndex) => newGameField.map(row => row[colIndex]));
+}
+
+function isPosibleToMove() {
+  if (checkRows()) {
+    return true;
+  }
+
+  transposeGameField();
+
+  return checkColumns();
+}
+
+function checkRows() {
+  let hasDuplicates = false;
+
+  newGameField.forEach((row) => {
+    if (row.some((cell, index) => cell === 0 || cell === row[index + 1])) {
+      hasDuplicates = true;
     }
-
-    if (e.key === 'ArrowDown') {
-      moveDown();
-    }
-
-    if (e.key === 'ArrowRight') {
-      moveRight();
-    }
-
-    if (e.key === 'ArrowLeft') {
-      moveLeft();
-    }
-
-    gameResult();
-
-    scoreCounter.innerText = score.toString();
-
-    paintCell();
   });
+
+  return hasDuplicates;
 }
 
-function forMoves(startIndex, endIndex, startFunction, moveFunction) {
-  const pastValues = createMatrixValueCopy();
+function checkColumns() {
+  let hasDuplicates = false;
 
-  for (let i = startIndex; i !== endIndex; i = moveFunction(i)) {
-    let merged = false;
-
-    for (let j = startFunction(i); j !== endIndex; j = moveFunction(j)) {
-      if (matrix[i][j].innerHTML !== '') {
-        let k = moveFunction(j);
-
-        while (k !== endIndex && matrix[i][k].innerHTML === '') {
-          matrix[i][k].innerHTML = matrix[i][k - moveFunction(1)].innerHTML;
-          restoreCell(matrix[i][k - moveFunction(1)]);
-          k = moveFunction(k);
-        }
-
-        while (k !== endIndex) {
-          if (!merged
-            && compareCells(matrix[i][k], matrix[i][k - moveFunction(1)])) {
-            matrix[i][k].innerHTML = `${parseInt(matrix[i][k].innerHTML) * 2}`;
-            matrix[i][k - moveFunction(1)].innerHTML = '';
-            restoreCell(matrix[i][k - moveFunction(1)]);
-
-            score += parseInt(matrix[i][k].innerHTML);
-
-            // Встановлюємо прапорець "merged" на true
-            merged = true;
-          } else {
-            merged = false; // Скидаємо прапорець, якщо злиття не відбулось
-          }
-          k = moveFunction(k);
-        }
+  newGameField.forEach((row) => {
+    row.forEach((cell, index) => {
+      if (cell === row[index + 1]) {
+        hasDuplicates = true;
       }
-    }
-  }
+    });
+  });
 
-  const newValues = createMatrixValueCopy();
-
-  if (isChanged(pastValues, newValues)) {
-    spawnNewNumber(requiredNumber());
-  }
-
-  updateGame();
-}
-
-function moveRight() {
-  forMoves(0, matrix.length, i => i, j => j - 1);
-}
-
-function moveLeft() {
-  forMoves(0, matrix.length, i => i, j => j + 1);
-}
-
-function moveUp() {
-  forMoves(0, matrix[0].length, j => j, i => i + 1);
-}
-
-function moveDown() {
-  forMoves(matrix.length - 1, -1, j => j, i => i - 1);
-}
-
-// function moveRight() {
-//   const pastValues = createMatrixValueCopy();
-
-//   for (let i = 0; i < matrix.length; i++) {
-//     let merged = false;
-
-//     for (let j = matrix[i].length - 2; j >= 0; j--) {
-//       if (matrix[i][j].innerHTML !== '') {
-//         let k = j + 1;
-
-//         while (k < matrix[i].length && matrix[i][k].innerHTML === '') {
-//           matrix[i][k].innerHTML = matrix[i][k - 1].innerHTML;
-//           restoreCell(matrix[i][k - 1]);
-//           k++;
-//         }
-
-//         while (k < matrix[i].length) {
-//           if (!merged && compareCells(matrix[i][k], matrix[i][k - 1])) {
-//             matrix[i][k].innerHTML = `${parseInt(matrix[i][k].innerHTML) * 2}`;
-//             matrix[i][k - 1].innerHTML = '';
-//             restoreCell(matrix[i][k - 1]);
-
-//             score += parseInt(matrix[i][k].innerHTML);
-
-//             // Встановлюємо прапорець "merged" на t
-//             merged = true;
-//           } else {
-//             merged = false; // Скидаємо прапорець, якщо злиття не відбулось
-//           }
-//           k++;
-//         }
-//       }
-//     }
-//   }
-
-//   const newValues = createMatrixValueCopy();
-
-//   if (isChanged(pastValues, newValues)) {
-//     spawnNewNumber(requiredNumber());
-//   }
-
-//   updateGame();
-// }
-
-// function moveLeft() {
-//   const pastValues = createMatrixValueCopy();
-
-//   for (let i = 0; i < matrix.length; i++) {
-//     let merged = false;
-
-//     for (let j = 1; j < matrix[i].length; j++) {
-//       if (matrix[i][j].innerHTML !== '') {
-//         let k = j - 1;
-
-//         while (k >= 0 && matrix[i][k].innerHTML === '') {
-//           matrix[i][k].innerHTML = matrix[i][k + 1].innerHTML;
-//           restoreCell(matrix[i][k + 1]);
-//           k--;
-//         }
-
-//         while (k >= 0) {
-//           if (!merged && compareCells(matrix[i][k], matrix[i][k + 1])) {
-//             matrix[i][k].innerHTML = `${parseInt(matrix[i][k].innerHTML) * 2}`;
-//             matrix[i][k + 1].innerHTML = '';
-//             restoreCell(matrix[i][k + 1]);
-
-//             score += parseInt(matrix[i][k].innerHTML);
-
-//             // Встановлюємо прапорець "merged" на true
-//             merged = true;
-//           } else {
-//             merged = false; // Скидаємо прапорець, якщо злиття не відбулось
-//           }
-//           k--;
-//         }
-//       }
-//     }
-//   }
-
-//   const newValues = createMatrixValueCopy();
-
-//   if (isChanged(pastValues, newValues)) {
-//     spawnNewNumber(requiredNumber());
-//   }
-
-//   updateGame();
-// }
-
-// function moveUp() {
-//   const pastValues = createMatrixValueCopy();
-
-//   for (let j = 0; j < matrix[0].length; j++) {
-//     let merged = false;
-
-//     for (let i = 1; i < matrix.length; i++) {
-//       if (matrix[i][j].innerHTML !== '') {
-//         let k = i - 1;
-
-//         while (k >= 0 && matrix[k][j].innerHTML === '') {
-//           matrix[k][j].innerHTML = matrix[k + 1][j].innerHTML;
-//           restoreCell(matrix[k + 1][j]);
-//           k--;
-//         }
-
-//         while (k >= 0) {
-//           if (!merged && compareCells(matrix[k][j], matrix[k + 1][j])) {
-//             matrix[k][j].innerHTML = `${parseInt(matrix[k][j].innerHTML) * 2}`;
-//             matrix[k + 1][j].innerHTML = '';
-//             restoreCell(matrix[k + 1][j]);
-
-//             score += parseInt(matrix[k][j].innerHTML);
-
-//             // Встановлюємо прапорець "merged" на true
-//             merged = true;
-//           } else {
-//             merged = false; // Скидаємо прапорець, якщо злиття не відбулось
-//           }
-//           k--;
-//         }
-//       }
-//     }
-//   }
-
-//   const newValues = createMatrixValueCopy();
-
-//   if (isChanged(pastValues, newValues)) {
-//     spawnNewNumber(requiredNumber());
-//   }
-
-//   updateGame();
-// }
-
-// function moveDown() {
-//   const pastValues = createMatrixValueCopy();
-
-//   for (let j = 0; j < matrix[0].length; j++) {
-//     let merged = false;
-
-//     for (let i = matrix.length - 2; i >= 0; i--) {
-//       if (matrix[i][j].innerHTML !== '') {
-//         let k = i + 1;
-
-//         while (k < matrix.length && matrix[k][j].innerHTML === '') {
-//           matrix[k][j].innerHTML = matrix[k - 1][j].innerHTML;
-//           restoreCell(matrix[k - 1][j]);
-//           k++;
-//         }
-
-//         while (k < matrix.length) {
-//           if (!merged && compareCells(matrix[k][j], matrix[k - 1][j])) {
-//             matrix[k][j].innerHTML = `${parseInt(matrix[k][j].innerHTML) * 2}`;
-//             matrix[k - 1][j].innerHTML = '';
-//             restoreCell(matrix[k - 1][j]);
-
-//             score += parseInt(matrix[k][j].innerHTML);
-
-//             // Встановлюємо прапорець "merged" на true
-//             merged = true;
-//           } else {
-//             merged = false; // Скидаємо прапорець, якщо злиття не відбулось
-//           }
-//           k++;
-//         }
-//       }
-//     }
-//   }
-
-//   const newValues = createMatrixValueCopy();
-
-//   if (isChanged(pastValues, newValues)) {
-//     spawnNewNumber(requiredNumber());
-//   }
-
-//   updateGame();
-// }
-
-function isChanged(matrixA, matrixB) {
-  for (let i = 0; i < matrixA.length; i++) {
-    for (let j = 0; j < matrixA[i].length; j++) {
-      if (matrixA[i][j] !== matrixB[i][j]) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-function createMatrixValueCopy() {
-  const matrixValueCopy
-  = Array(matrix.length).fill(null).map(() => Array(matrix[0].length));
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      matrixValueCopy[i][j] = matrix[i][j].innerHTML;
-    }
-  }
-
-  return matrixValueCopy;
-}
-
-function gameLost() {
-  let check = false;
-  const copyMatrixValue = createMatrixValueCopy();
-
-  for (let r = 0; r < 3; r++) {
-    for (let c = 0; c < 4; c++) {
-      if (copyMatrixValue[r][c] === copyMatrixValue[r + 1][c]
-        || copyMatrixValue[r][c] === copyMatrixValue[r][c + 1]) {
-        check = true;
-      }
-    }
-  }
-
-  if (!check && !checkForEmpty()) {
-    return false;
-  }
-
-  return true;
-}
-
-function checkForEmpty() {
-  const newValues = createMatrixValueCopy();
-
-  for (let r = 0; r < 4; r++) {
-    for (let c = 0; c < 4; c++) {
-      if (newValues[r][c] === '') {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-function requiredNumber() {
-  const requiredNumberIn = Math.random() < 0.9 ? 2 : 4;
-
-  return requiredNumberIn;
+  return hasDuplicates;
 }
