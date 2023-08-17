522a5894ff4150a91aa546fc1d5f2c87416b0052
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-08-17 16:55:32 +0200

    add task solution 170523

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,225 +1,200 @@
 'use strict';
 
-// write your code here
-class Cell {
-  constructor(cellEl, cellClass, x, y) {
-    this.$el = cellEl;
+const columns = [[], [], [], []];
+const rows = [...document.querySelectorAll('.field-row')];
+const cells = document.querySelectorAll('.field-cell');
+const cellsInRow = rows.map((row) => [...row.querySelectorAll('.field-cell')]);
+const startCellVelues = [2, 2, 2, 2, 2, 2, 2, 2, 2, 4];
 
-    this.x = x;
-    this.y = y;
-    this.cellClass = cellClass;
-    this.value = null;
-    this.mergeValue = null;
-  }
+const startButton = document.querySelector('.start');
+const startMessage = document.querySelector('.message-start');
+const loseMessage = document.querySelector('.message-lose');
+const winMessage = document.querySelector('.message-win');
+const gameScore = document.querySelector('.game-score');
 
-  setValue(value, mergeValue = null) {
-    this.value = value;
-    this.mergeValue = mergeValue;
-    this.$el.innerText = this.value;
-    this.$el.className = this.cellClass;
-
-    if (this.value !== null) {
-      this.$el.classList.add(`${this.cellClass}--${this.value}`);
-    }
-  }
-
-  canMerge(newCell) {
-    return !this.value || (!this.mergeValue && this.value === newCell.value);
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 4; j++) {
+    columns[j].push(rows[i].children[j]);
   }
 }
 
-class Game {
-  constructor() {
-    this.messageClass = 'message';
-    this.messageHiddenClass = 'hidden';
-    this.cellClass = 'field-cell';
-    this.state = 'start';
-    this.cells = [];
+let clearCells = [...cells];
+let started = false;
+let rotated = false;
 
-    this.$score = document.querySelector('.game-score');
-    this.$toggleBtn = document.querySelector('.button');
-    this.$messages = document.querySelectorAll(`.${this.messageClass}`);
-    this.$rows = document.querySelectorAll('.field-row');
+function randomCellFill() {
+  if (!clearCells.length) {
+    return;
   }
 
-  init() {
-    this.initCells();
-    this.initEvents();
-  }
-  initCells() {
-    this.$rows.forEach((row, rowIndex) => {
-      const rowCells = row.querySelectorAll(`.${this.cellClass}`);
+  const randomIndex = (someArr) => Math.floor(Math.random() * someArr.length);
+  const randomCell = clearCells[randomIndex(clearCells)];
+  const randomValue = startCellVelues[randomIndex(startCellVelues)];
 
-      rowCells.forEach((cell, cellIndex) => {
-        this.cells.push(new Cell(cell, this.cellClass, cellIndex, rowIndex));
-      });
-    });
-  }
-  initEvents() {
-    this.$toggleBtn.addEventListener('click', e => {
-      e.preventDefault();
+  randomCell.classList = (`field-cell field-cell--${randomValue}`);
+  randomCell.innerText = randomValue;
 
-      this.restart();
-    });
-    document.addEventListener('keydown', this.handleKeyInput.bind(this));
-  }
-  groupCellsByColumn() {
-    return this.cells.reduce((group, cell) => {
-      group[cell.x] = group[cell.x] || [];
-      group[cell.x][cell.y] = cell;
+  clearCells.splice(clearCells.indexOf(randomCell), 1);
+}
 
-      return group;
-    }, []);
-  }
-  groupCellsByRow() {
-    return this.cells.reduce((group, cell) => {
-      group[cell.y] = group[cell.y] || [];
-      group[cell.y][cell.x] = cell;
+const start = () => {
+  startMessage.style = 'display: none;';
+  startButton.classList = 'button restart';
+  startButton.innerText = 'Restart';
 
-      return group;
-    }, []);
-  }
-  getRandomEmptyCell() {
-    const emptyCells = this.cells.filter(cell => !cell.value);
-    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+  randomCellFill();
+  randomCellFill();
+  started = true;
+};
 
-    return emptyCells[randomIndex];
-  }
-  changeState(state = null) {
-    this.state = state;
+const restart = () => {
+  loseMessage.classList.add('hidden');
+  winMessage.classList.add('hidden');
+  clearCells = [...cells];
+  gameScore.innerText = 0;
 
-    this.$messages.forEach(message => {
-      if (message.classList.contains(`${this.messageClass}-${this.state}`)) {
-        message.classList.remove(this.messageHiddenClass);
-      } else {
-        message.classList.add(this.messageHiddenClass);
+  cells.forEach(cell => {
+    cell.innerText = '';
+    cell.className = 'field-cell';
+  });
+
+  randomCellFill();
+  randomCellFill();
+};
+
+function isMergePossible() {
+  const wholeField = [...cellsInRow, ...columns];
+
+  for (const line of wholeField) {
+    for (let i = 0; i < 3; i++) {
+      if (line[i].innerText === line[i + 1].innerText) {
+        return true;
       }
-    });
-
-    if (this.state !== 'start') {
-      this.$toggleBtn.classList.remove('start');
-      this.$toggleBtn.classList.add('restart');
-      this.$toggleBtn.innerText = 'Restart';
     }
-  }
-  changeScore(value = null) {
-    const current = Number(this.$score.innerText);
-
-    this.$score.innerText = value ? current + value : 0;
-  }
-  clearGrid() {
-    this.cells.forEach(cell => cell.setValue(null));
-    this.changeScore();
-  }
-  canMoveGroup(groupCells) {
-    return groupCells.some(group => {
-      return group.some((cell, index) => {
-        if (index === 0) {
-          return false;
-        }
-
-        if (!cell.value) {
-          return false;
-        }
-
-        const targetCell = group[index - 1];
-
-        return targetCell.canMerge(cell);
-      });
-    });
-  }
-  canMove() {
-    return [
-      this.groupCellsByColumn(),
-      this.groupCellsByColumn().map(col => col.reverse()),
-      this.groupCellsByRow(),
-      this.groupCellsByRow().map(col => col.reverse()),
-    ].some(this.canMoveGroup);
-  }
-  mergeCells() {
-    this.cells.forEach(cell => {
-      if (cell.mergeValue) {
-        const newValue = cell.value + cell.mergeValue;
-
-        cell.setValue(newValue);
-        cell.mergeValue = null;
-
-        this.changeScore(newValue);
-
-        if (newValue >= 2048) {
-          this.changeState('win');
-        }
-      }
-    });
-  }
-  moveCells(groupCells) {
-    if (this.state === 'start') {
-      return this.restart();
-    } else if (['win', 'lose'].includes(this.state)) {
-      return;
-    }
-
-    if (!this.canMoveGroup(groupCells)) {
-      return;
-    }
-
-    groupCells.forEach(group => {
-      for (let i = 1; i < group.length; i++) {
-        if (!group[i].value) {
-          continue;
-        }
-
-        const cellWithValue = group[i];
-        let targetCell;
-        let j = i - 1;
-
-        while (j >= 0 && group[j].canMerge(cellWithValue)) {
-          targetCell = group[j];
-          j--;
-        }
-
-        if (!targetCell) {
-          continue;
-        }
-
-        targetCell.setValue(cellWithValue.value, targetCell.value);
-        cellWithValue.setValue(null);
-      }
-    });
-
-    this.mergeCells();
-    this.getRandomEmptyCell().setValue(Math.random() > 0.5 ? 2 : 4);
-
-    if (!this.canMove()) {
-      this.changeState('lose');
-    }
-  }
-  handleKeyInput(e) {
-    switch (e.key) {
-      case 'ArrowUp':
-        this.moveCells(this.groupCellsByColumn());
-        break;
-      case 'ArrowDown':
-        this.moveCells(this.groupCellsByColumn().map(col => col.reverse()));
-        break;
-      case 'ArrowLeft':
-        this.moveCells(this.groupCellsByRow());
-        break;
-      case 'ArrowRight':
-        this.moveCells(this.groupCellsByRow().map(col => col.reverse()));
-        break;
-      default:
-        break;
-    }
-  }
-  restart() {
-    this.clearGrid();
-    this.changeState();
-    this.getRandomEmptyCell().setValue(Math.random() > 0.5 ? 2 : 4);
-    this.getRandomEmptyCell().setValue(Math.random() > 0.5 ? 2 : 4);
   }
 }
 
-const game = new Game();
+function upArrow() {
+  for (const column of columns) {
+    rotateCells([...column].reverse());
+  }
+}
 
-game.init();
+function downArrow() {
+  for (const column of columns) {
+    rotateCells(column);
+  }
+}
+
+function rightArrow() {
+  for (const row of cellsInRow) {
+    rotateCells(row);
+  }
+}
+
+function leftArrow() {
+  for (const row of cellsInRow) {
+    rotateCells([...row].reverse());
+  }
+}
+
+function arrowMove(direction) {
+  if (!clearCells.length && !isMergePossible()) {
+    loseMessage.classList.remove('hidden');
+  }
+
+  switch (direction) {
+    case 'ArrowUp':
+      upArrow();
+      break;
+    case 'ArrowDown':
+      downArrow();
+      break;
+    case 'ArrowRight':
+      rightArrow();
+      break;
+    case 'ArrowLeft':
+      leftArrow();
+      break;
+  }
+
+  cells.forEach(cell => {
+    cell.removeAttribute('data-blocked');
+  });
+}
+
+function deleteElement(element) {
+  element.innerText = '';
+  element.className = 'field-cell';
+  clearCells.push(element);
+}
+
+function rotateElement(curr, prev) {
+  curr.innerText = prev.innerText;
+  curr.className = prev.className;
+  clearCells.splice(clearCells.indexOf(curr), 1);
+  deleteElement(prev);
+}
+
+function cellMerge(curr, prev) {
+  const value = curr.innerText * 2;
+
+  curr.innerText = value;
+  curr.className = `field-cell field-cell--${value}`;
+  gameScore.innerText = +gameScore.innerText + value;
+  deleteElement(prev);
+
+  curr.dataset.blocked = true;
+  prev.dataset.blocked = true;
+
+  if (value === 2048) {
+    winMessage.classList.remove('hidden');
+  }
+}
+
+function rotateCells(line) {
+  for (let i = 3; i > 0; i--) {
+    const moveAllowed = clearCells.includes(line[i])
+      && !clearCells.includes(line[i - 1]);
+
+    const mergeAllowed = line[i].innerText === line[i - 1].innerText
+      && line[i].innerText.length
+      && !line[i - 1].dataset.blocked;
+
+    if ((mergeAllowed || moveAllowed) && !rotated) {
+      rotated = true;
+    }
+
+    if (mergeAllowed) {
+      cellMerge(line[i], line[i - 1]);
+      rotateCells(line);
+    }
+
+    if (moveAllowed) {
+      rotateElement(line[i], line[i - 1]);
+      rotateCells(line);
+    }
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  const arrowDirections = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+  if (arrowDirections.includes(e.key)) {
+    e.preventDefault();
+    arrowMove(e.key);
+
+    if (rotated) {
+      randomCellFill();
+      rotated = false;
+    }
+  }
+});
+
+startButton.addEventListener('click', () => {
+  if (!started) {
+    start();
+  } else {
+    restart();
+  }
+});
