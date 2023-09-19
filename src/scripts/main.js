4068e8195dff59e1d862bc14dbfdc6ab9e60eb18
stunnerboyone <stunnerboy691@gmail.com>
2023-09-19 22:53:32 +0300

    fist try)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,142 +1,271 @@
 'use strict';
 
-// write your code here
+// Constants
 const button = document.querySelector('.button');
-const messageWin = document.querySelector('.message-win');
-const messageLose = document.querySelector('.message-lose');
-const messageStart = document.querySelector('.message-start');
-const fieldRows = document.querySelectorAll('.field-row');
-const gameScore = document.querySelector('.game-score');
+const startMessage = document.querySelector('.message-start');
+const scoreText = document.querySelector('.game-score');
+const field = document.querySelector('.game-field');
+const winMessage = document.querySelector('.message-win');
+const loseMessage = document.querySelector('.message-lose');
 
-const rowLength = 4;
+let board;
 let score = 0;
-let field;
+const cellsInRow = 4;
 
-function startGame() {
-  field = [
+const filterZero = (row) => {
+  return row.filter(num => num !== 0);
+};
+
+const compareFields = (prevBoard, boards) => {
+  return JSON.stringify(prevBoard) !== JSON.stringify(boards);
+};
+
+const slide = (row) => {
+  const newRow = filterZero(row);
+
+  for (let i = 0; i < newRow.length - 1; i++) {
+    if (newRow[i] === newRow[i + 1]) {
+      newRow[i] *= 2;
+      newRow.splice(i + 1, 1);
+      score += newRow[i];
+    }
+  }
+
+  while (newRow.length < cellsInRow) {
+    newRow.push(0);
+  }
+
+  return newRow;
+};
+
+const hasEmptyCell = () => {
+  for (let r = 0; r < cellsInRow; r++) {
+    for (let c = 0; c < cellsInRow; c++) {
+      if (board[r][c] === 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+const renderField = () => {
+  for (let r = 0; r < cellsInRow; r++) {
+    for (let c = 0; c < cellsInRow; c++) {
+      field.rows[r].cells[c].className = '';
+
+      field.rows[r].cells[c].classList.add(
+        'field-cell',
+        `field-cell--${board[r][c]}`
+      );
+      field.rows[r].cells[c].textContent = board[r][c] || '';
+    }
+  }
+};
+
+const spawnRandomCell = () => {
+  if (!hasEmptyCell()) {
+    return;
+  }
+
+  let found = false;
+
+  while (!found) {
+    const r = Math.floor(Math.random() * cellsInRow);
+    const c = Math.floor(Math.random() * cellsInRow);
+
+    if (board[r][c] === 0) {
+      board[r][c] = 2;
+      renderField();
+
+      found = true;
+    }
+  }
+};
+
+const transponseField = (currentField) => {
+  let transponsedBoard = currentField;
+
+  transponsedBoard = transponsedBoard[0].map(
+    (_, colIndex) => transponsedBoard.map(
+      row => row[colIndex]
+    )
+  );
+
+  return transponsedBoard;
+};
+
+const moveLeft = () => {
+  const prevBoard = JSON.parse(JSON.stringify(board));
+
+  for (let r = 0; r < cellsInRow; r++) {
+    let row = board[r];
+
+    row = slide(row);
+    board[r] = row;
+  };
+
+  if (compareFields(prevBoard, board)) {
+    spawnRandomCell();
+  }
+
+  renderField();
+};
+
+const moveRight = () => {
+  const prevBoard = JSON.parse(JSON.stringify(board));
+
+  for (let r = 0; r < cellsInRow; r++) {
+    let row = board[r].reverse();
+
+    row = slide(row);
+    board[r] = row.reverse();
+  };
+
+  if (compareFields(prevBoard, board)) {
+    spawnRandomCell();
+  }
+
+  renderField();
+};
+
+const moveUp = () => {
+  const prevBoard = JSON.parse(JSON.stringify(board));
+  const newField = transponseField(board);
+
+  for (let r = 0; r < cellsInRow; r++) {
+    let row = newField[r];
+
+    row = slide(row);
+    newField[r] = row;
+  };
+
+  board = transponseField(newField);
+
+  if (compareFields(prevBoard, board)) {
+    spawnRandomCell();
+  }
+
+  renderField();
+};
+
+const moveDown = () => {
+  const prevBoard = JSON.parse(JSON.stringify(board));
+  const newField = transponseField(board);
+
+  for (let r = 0; r < cellsInRow; r++) {
+    let row = newField[r].reverse();
+
+    row = slide(row);
+    newField[r] = row.reverse();
+  };
+
+  board = transponseField(newField);
+
+  if (compareFields(prevBoard, board)) {
+    spawnRandomCell();
+  }
+
+  renderField();
+};
+
+const winGame = () => {
+  for (let i = 0; i < board[0].length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j] === 2048) {
+        return true;
+      }
+    }
+  }
+};
+
+const checkFields = (fieldToCheck) => {
+  for (const row of fieldToCheck) {
+    const duplicates = row.filter((item, index) => row.indexOf(item) !== index);
+
+    if (duplicates.length !== 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const gameOver = (desk) => {
+  if (hasEmptyCell()) {
+    return false;
+  }
+
+  const newDesk = transponseField(desk);
+
+  if (checkFields(desk) || checkFields(newDesk)) {
+    return false;
+  };
+
+  return true;
+};
+
+button.addEventListener('click', () => {
+  if (button.classList.contains('start')) {
+    button.classList.remove('start');
+    button.classList.add('restart');
+    button.textContent = 'Restart';
+    startMessage.classList.add('hidden');
+  } else {
+    winMessage.classList.add('hidden');
+    loseMessage.classList.add('hidden');
+  };
+
+  board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ];
 
-  addRandomBlock();
-  addRandomBlock();
+  score = 0;
+  scoreText.textContent = score;
 
-  renderField();
-}
-
-function renderField() {
-  field.forEach((row, rowIndex) => {
-    row.forEach((cell, colIndex) => {
-      const block = fieldRows[rowIndex].children[colIndex];
-
-      if (cell === 0) {
-        block.textContent = '';
-        block.className = 'field-cell';
-      } else {
-        block.textContent = cell;
-        block.className = `field-cell field-cell--${cell}`;
-      }
-    });
-  });
-
-  gameScore.textContent = score;
-}
-
-function canSlide() {
-  let canBlockSlide = false;
-
-  field.forEach((row) => {
-    if (row.some((block, index) => block === 0 || block === row[index + 1])) {
-      canBlockSlide = true;
-    }
-  });
-
-  return canBlockSlide;
-}
-
-const addRandomBlock = () => {
-  while (canSlide()) {
-    const randomRow = Math.floor(Math.random() * rowLength);
-    const randomCol = Math.floor(Math.random() * rowLength);
-
-    if (field[randomRow][randomCol] === 0) {
-      field[randomRow][randomCol] = Math.random() >= 0.8 ? 4 : 2;
-      break;
-    }
-  }
-};
-
-function moveBlocks() {
-  if (!canSlide()) {
-    return;
-  }
-
-  field = field.map(row => {
-    const newRow = row.filter(block => block !== 0);
-
-    newRow.forEach((block, index) => {
-      if (block === newRow[index + 1]) {
-        newRow[index] *= 2;
-        newRow.splice(index + 1, 1);
-        score += newRow[index];
-      }
-    });
-
-    return newRow.concat(Array(rowLength - newRow.length).fill(0));
-  });
-}
-
-function reverseRows() {
-  field.forEach(row => row.reverse());
-}
-
-function transponseField() {
-  field = field[0].map((block, colIndex) => field.map(row => row[colIndex]));
-}
-
-document.addEventListener('keyup', (e) => {
-  e.preventDefault();
-
-  switch (e.code) {
-    case 'ArrowUp':
-      transponseField();
-      moveBlocks();
-      transponseField();
-      addRandomBlock();
-      renderField();
-      break;
-    case 'ArrowDown':
-      transponseField();
-      reverseRows();
-      moveBlocks();
-      reverseRows();
-      transponseField();
-      addRandomBlock();
-      renderField();
-      break;
-    case 'ArrowLeft':
-      moveBlocks();
-      addRandomBlock();
-      renderField();
-      break;
-    case 'ArrowRight':
-      reverseRows();
-      moveBlocks();
-      reverseRows();
-      addRandomBlock();
-      renderField();
-      break;
-  }
+  spawnRandomCell();
+  spawnRandomCell();
 });
 
-button.addEventListener('click', (element) => {
-  if (button.classList.contains('start')) {
-    button.classList.replace('start', 'restart');
-    button.innerText = 'Restart';
-    messageStart.classList.add('hidden');
+document.addEventListener('keydown', (e) => {
+  if (gameOver(board)) {
+    loseMessage.classList.remove('hidden');
   }
 
-  startGame();
+  if (winGame()) {
+    winMessage.classList.remove('hidden');
+
+    board = [
+      [2, 0, 4, 8],
+      [2, 0, 4, 8],
+      [2, 0, 4, 8],
+      [2, 0, 4, 8],
+    ];
+
+    renderField();
+  } else {
+    switch (e.key) {
+      case 'ArrowLeft':
+        moveLeft();
+        break;
+
+      case 'ArrowRight':
+        moveRight();
+        break;
+
+      case 'ArrowUp':
+        moveUp();
+        break;
+
+      case 'ArrowDown':
+        moveDown();
+        break;
+    }
+  }
+
+  scoreText.textContent = score;
 });
