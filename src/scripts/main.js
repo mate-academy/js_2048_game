4db71ea7782eee9a27f6d31ee5e152e2b1b91a4f
Kriss Kozak <misskrissretro@gmail.com>
2023-09-21 11:45:46 +0300

    2048

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,65 @@
 'use strict';
 
-// write your code here
+const gameField = [];
+const size = 4;
+const score = 0;
+
+function initGame() {
+  for (let i = 0; i < size; i++) {
+    gameField[i] = [];
+
+    for (let j = 0; j < size; j++) {
+      gameField[i][j] = 0;
+    }
+  }
+  addRandomTile();
+  addRandomTile();
+  renderGameField();
+}
+
+function addRandomTile() {
+  const availableCells = [];
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (gameField[i][j] === 0) {
+        availableCells.push({
+          x: i, y: j,
+        });
+      }
+    }
+  }
+
+  if (!availableCells.length) {
+    return;
+  }
+
+  const randomCell = availableCells(Math.floor(Math.random()
+    * availableCells.length));
+
+  gameField[randomCell.x][randomCell.y] = Math.random() < 0.9
+    ? 2
+    : 4;
+}
+
+function renderGameField() {
+  const gameFieldEl = document.querySelector('.game-field');
+  const scoreEl = document.querySelector('.game-score');
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const cell = gameFieldEl.rows[i].cells[j];
+      const value = gameField[i][j];
+
+      cell.textContent = value || '';
+      cell.className = 'field-cell';
+
+      if (value) {
+        cell.classList.add('field-cell--' + value);
+      }
+    }
+  }
+  scoreEl.textContent = score;
+}
+
+document.querySelector('.start').addEventListener('click', initGame);
