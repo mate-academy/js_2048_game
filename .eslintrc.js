8f5b253520e74f8ee450235abeb5a021c8dbaa3e
Taras Shevchenko <shevchenkotv91@gmail.com>
2023-10-05 10:12:54 +0300

    create game 2048

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  }
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
-'use strict';
+import { Move } from './move';
 
-// write your code here
+class Game extends Move {
+  constructor() {
+    super();
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleKeydown = this.handleKeydown.bind(this);
+  }
+
+  init() {
+    this.setInitData();
+    this.buttonEl.addEventListener('click', this.handleButtonClick);
+  }
+
+  handleKeydown({ key }) {
+    switch (key) {
+      case 'ArrowDown':
+        this.goDown();
+        break;
+
+      case 'ArrowLeft':
+        this.goLeft();
+        break;
+
+      case 'ArrowUp':
+        this.goUp();
+        break;
+
+      case 'ArrowRight':
+        this.goRight();
+        break;
+
+      default:
+        return;
+    }
+
+    if (!this.isGameOver) {
+      this.setRandomNumbers();
+      this.updateCells();
+      this.updateScore();
+    }
+
+    this.checkIfGameIsOver();
+  }
+
+  addKeydownListener() {
+    document.addEventListener('keydown', this.handleKeydown);
+  }
+
+  removeKeydownListener() {
+    document.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleButtonClick() {
+    this.isGameStart = !this.isGameStart;
+
+    if (this.isGameStart) {
+      this.startGame();
+    } else {
+      this.restartGame();
+    }
+
+    this.updateCells();
+  }
+}
+
+const newGame = new Game();
+
+newGame.init();
