b8939f416251b30b951b36993633107c7490d88b
Danylo Artomov <daniilart01@gmail.com>
2023-10-14 21:01:39 +0300

    animation and appearence impl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,11 @@
-// import { Tile } from './tile.js';
-
-const cellsMatrix = fillCellsMatrixWithCells();
-let valuesMatrix = fillValuesMatrix();
+import { Tile } from './tile.js';
 
 const button = document.querySelector('.button');
-// const score = document.querySelector('.game-score');
-// const messageWin = document.querySelector('.message-win');
+const score = document.querySelector('.game-score');
+const messageWin = document.querySelector('.message-win');
 const messageLose = document.querySelector('.message-lose');
 const messageStart = document.querySelector('.message-start');
-// const gameField = document.querySelector('.game-field');
+const gameField = document.querySelector('.game-field');
 
 function keydownFunction(e) {
   if (['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(e.key)) {
@@ -21,28 +18,12 @@ function keydownFunction(e) {
     }
   }
 
-  let isCellMoved = false;
-
   switch (e.key) {
     case 'ArrowLeft':
-
       for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-          if (j !== 0) {
-            if (valuesMatrix[i][j - 1] === 0 && valuesMatrix[i][j] !== 0) {
-              valuesMatrix[i][j - 1] = valuesMatrix[i][j];
-              valuesMatrix[i][j] = 0;
+        for (let j = 1; j < 4; j++) {
 
-              swapCellValues(cellsMatrix[i][j], cellsMatrix[i][j - 1]);
-              isCellMoved = true;
-              j = 0;
-            }
-          }
         }
-      }
-
-      if (isCellMoved) {
-        pushNewCell();
       }
 
       break;
@@ -64,126 +45,11 @@ function buttonClickFunction() {
 
     messageStart.classList.remove('hidden');
   }
-  clearField();
-  valuesMatrix = fillValuesMatrix();
 
-  let coordinates = getRandomPosition();
+  Tile.clearField();
 
-  const firstValue = getCellValue();
-  const secondValue = getCellValue();
-
-  const firstIndexY = coordinates[0];
-  const firstIndexX = coordinates[1];
-  let secondIndexX;
-  let secondIndexY;
-
-  do {
-    coordinates = getRandomPosition();
-
-    secondIndexY = coordinates[0];
-    secondIndexX = coordinates[1];
-  } while (firstIndexX === secondIndexX && firstIndexY === secondIndexY);
-
-  cellsMatrix[firstIndexY][firstIndexX].classList
-    .add(`field-cell--${+firstValue}`);
-  cellsMatrix[firstIndexY][firstIndexX].textContent = firstValue;
-  valuesMatrix[firstIndexY][firstIndexX] = +firstValue;
-
-  cellsMatrix[secondIndexY][secondIndexX].classList
-    .add(`field-cell--${+secondValue}`);
-  cellsMatrix[secondIndexY][secondIndexX].textContent = secondValue;
-  valuesMatrix[secondIndexY][secondIndexX] = +secondValue;
-}
-
-function clearField() {
-  cellsMatrix.forEach((row) => {
-    row.forEach((cell) => {
-      cell.removeAttribute('class');
-      cell.setAttribute('class', 'field-cell');
-      cell.textContent = '';
-    });
-  });
-}
-
-function getRandomPosition() {
-  return [Math.floor(Math.random() * 4), Math.floor(Math.random() * 4)];
-}
-
-function getCellValue() {
-  return Math.floor(Math.random() * 10) === 0 ? '4' : '2';
-}
-
-function fillCellsMatrixWithCells() {
-  const cellsList = document.querySelectorAll('.field-cell');
-
-  const cellsInARow = 4;
-  const cellsCount = cellsList.length;
-  const cellsMatrixToReturn = [];
-
-  for (let i = 0; i < cellsCount; i += cellsInARow) {
-    cellsMatrixToReturn[i / cellsInARow] = [cellsList[i], cellsList[i + 1],
-      cellsList[i + 2], cellsList[i + 3]];
-  }
-
-  return cellsMatrixToReturn;
-}
-
-function fillValuesMatrix() {
-  const matrix = [[], [], [], []];
-
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      matrix[i][j] = 0;
-    }
-  }
-
-  return matrix;
-}
-
-function swapCellValues(from, to) {
-  const valueToSwap = from.textContent;
-  const classToSwap = `field-cell--${valueToSwap}`;
-
-  from.classList.remove(classToSwap);
-  from.textContent = '';
-
-  to.classList.add(classToSwap);
-  to.textContent = valueToSwap;
-}
-
-function pushNewCell() {
-  const val = getCellValue();
-  let coordinates;
-
-  if (!checkEmptyFields()) {
-    messageLose.classList.remove('hidden');
-
-    return null;
-  }
-
-  do {
-    coordinates = getRandomPosition();
-  } while (valuesMatrix[coordinates[0]][coordinates[1]] !== 0);
-
-  const posY = coordinates[0];
-  const posX = coordinates[1];
-
-  valuesMatrix[posY][posX] = val;
-
-  cellsMatrix[posY][posX].classList.add(`field-cell--${val}`);
-  cellsMatrix[posY][posX].textContent = val;
-}
-
-function checkEmptyFields() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      if (valuesMatrix[i][j] === 0) {
-        return true;
-      }
-    }
-  }
-
-  return false;
+  Tile.initNewTile();
+  Tile.initNewTile();
 }
 
 window.onload = () => {
--- src/scripts/tile.js
+++ src/scripts/tile.js
@@ -1,9 +1,86 @@
 export class Tile {
   constructor(gameField) {
     this.tile = document.createElement('div');
-    this.tile.classList.add('tile');
     this.value = Math.floor(Math.random() * 10) === 0 ? 4 : 2;
+    this.tile.classList.add('tile');
+    this.tile.classList.add(`field-cell--${this.value}`);
     this.tile.textContent = this.value;
     gameField.append(this.tile);
+
+    const coordinates = Tile.getRandomEmptyPosition();
+
+    this.tile.style.setProperty('--index-x', coordinates[0]);
+    this.tile.style.setProperty('--index-y', coordinates[1]);
+
+    Tile.tilesMatrix[coordinates[1]][coordinates[0]] = this;
+  }
+
+  moveTileTo(x, y) {
+    this.tile.style.setProperty('--index-x', x);
+    this.tile.style.setProperty('--index-y', y);
+  }
+
+  mergeTiles() {
+    this.tile.classList.remove(`field-cell--${this.value}`);
+    this.value *= 2;
+    this.tile.classList.add(`field-cell--${this.value}`);
+    this.tile.textContent = this.value;
+  }
+
+  static initNewTile() {
+    return new Tile(document.querySelector('.game-field'));
+  }
+
+  static initTilesMatrix() {
+    const arr = [];
+
+    for (let i = 0; i < Tile.TILES_COUNT; i++) {
+      arr[i] = [];
+
+      for (let j = 0; j < Tile.TILES_COUNT; j++) {
+        arr[i][j] = null;
+      }
+    }
+
+    return arr;
+  }
+
+  static clearField() {
+    Tile.tilesMatrix.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile !== null) {
+          tile.tile.remove();
+        }
+      });
+    });
+
+    Tile.tilesMatrix = Tile.initTilesMatrix();
+  }
+
+  static isFieldFull() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (Tile.tilesMatrix[i][j] === 0) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  static getRandomEmptyPosition() {
+    let coordinates;
+
+    do {
+      coordinates = [Math.floor(Math.random() * Tile.TILES_COUNT),
+        Math.floor(Math.random() * Tile.TILES_COUNT)];
+    }
+    while (Tile.tilesMatrix[coordinates[1]][coordinates[0]] !== null);
+
+    return coordinates;
   }
 }
+
+Tile.TILES_COUNT = 4;
+Tile.tilesMatrix = Tile.initTilesMatrix();
