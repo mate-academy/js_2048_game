d343a554e6a037067b1c927aea7e7424671b27a9
Danylo Artomov <daniilart01@gmail.com>
2023-10-15 23:10:58 +0300

    v1

--- src/scripts/tile.js
+++ src/scripts/tile.js
@@ -9,22 +9,51 @@ export class Tile {
 
     const coordinates = Tile.getRandomEmptyPosition();
 
-    this.tile.style.setProperty('--index-x', coordinates[0]);
-    this.tile.style.setProperty('--index-y', coordinates[1]);
+    this.x = coordinates[0];
+    this.y = coordinates[1];
+
+    this.tile.style.setProperty('--index-x', this.x);
+    this.tile.style.setProperty('--index-y', this.y);
 
     Tile.tilesMatrix[coordinates[1]][coordinates[0]] = this;
+
+    if (Tile.isFieldFull()) {
+      Tile.checkGameEnd();
+    }
   }
 
   moveTileTo(x, y) {
     this.tile.style.setProperty('--index-x', x);
     this.tile.style.setProperty('--index-y', y);
+
+    Tile.tilesMatrix[y][x] = this;
+    Tile.tilesMatrix[this.y][this.x] = null;
+
+    this.x = x;
+    this.y = y;
   }
 
-  mergeTiles() {
+  mergeTiles(tileToRemove) {
     this.tile.classList.remove(`field-cell--${this.value}`);
     this.value *= 2;
     this.tile.classList.add(`field-cell--${this.value}`);
     this.tile.textContent = this.value;
+
+    Tile.tilesMatrix[tileToRemove.y][tileToRemove.x] = null;
+    this.tile.style.setProperty('z-index', 2);
+    tileToRemove.tile.style.setProperty('--index-x', this.x);
+    tileToRemove.tile.style.setProperty('--index-y', this.y);
+
+    setTimeout(() => {
+      tileToRemove.tile.remove();
+      this.tile.style.removeProperty('z-index', 2);
+    }, 200);
+
+    if (this.value >= 2048) {
+      document.querySelector('.message-win').classList.remove('hidden');
+    }
+
+    return this.value;
   }
 
   static initNewTile() {
@@ -60,13 +89,13 @@ export class Tile {
   static isFieldFull() {
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 4; j++) {
-        if (Tile.tilesMatrix[i][j] === 0) {
-          return true;
+        if (Tile.tilesMatrix[i][j] === null) {
+          return false;
         }
       }
     }
 
-    return false;
+    return true;
   }
 
   static getRandomEmptyPosition() {
@@ -79,6 +108,50 @@ export class Tile {
     while (Tile.tilesMatrix[coordinates[1]][coordinates[0]] !== null);
 
     return coordinates;
+  }
+
+  static checkGameEnd() {
+    let mergeAvailable = false;
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        if (Tile.tilesMatrix[i][j - 1].value
+          === Tile.tilesMatrix[i][j].value) {
+          mergeAvailable = true;
+        }
+      }
+    }
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 2; j >= 0; j--) {
+        if (Tile.tilesMatrix[i][j + 1].value
+          === Tile.tilesMatrix[i][j].value) {
+          mergeAvailable = true;
+        }
+      }
+    }
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        if (Tile.tilesMatrix[j - 1][i].value
+          === Tile.tilesMatrix[j][i].value) {
+          mergeAvailable = true;
+        }
+      }
+    }
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 2; j >= 0; j--) {
+        if (Tile.tilesMatrix[j + 1][i].value
+          === Tile.tilesMatrix[j][i].value) {
+          mergeAvailable = true;
+        }
+      }
+    }
+
+    if (!mergeAvailable) {
+      document.querySelector('.message-lose').classList.remove('hidden');
+    }
   }
 }
 
