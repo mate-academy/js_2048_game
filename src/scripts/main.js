126fed8eb5e7e35acf91607d5144ebbf3395c3eb
SerhiiUnhurian <sergeyunguryan322@gmail.com>
2023-10-16 20:29:09 +0300

    need help

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,39 @@
 'use strict';
 
-const Grid = require('./Grid'); // я не можу тут використовувати імпорт
-const Tile = require('./Tile');
+import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
-const gameField = document.getElementsByClassName('game-field');
+const gameBoard = document.getElementById('game-board');
 
-const grid = new Grid(gameField);
+const grid = new Grid(gameBoard);
 
-grid.randomEmptyCell().tile = new Tile(gameField);
-grid.randomEmptyCell().tile = new Tile(gameField);
+console.log(grid.randomEmptyCell())
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+
+function setupInput() {
+  window.addEventListener('keydown', handleInput, {once: true})
+}
+
+function handleInput(e) {
+  switch(e.key) {
+    case 'ArrowUp':
+      moveUp()
+      break
+
+    case 'ArrowDown':
+      moveDown()
+      break
+
+    case 'ArrowLeft':
+      moveLeft()
+      break
+
+    case 'ArrowRight':
+      moveRight()
+      break
+
+    default:
+      break
+  }
+}
