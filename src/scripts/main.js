a1165cc83b2ccb30f541c674afb3149cde63ffc9
Viktoriia Soloviova <ivicki.vs@gmail.com>
2023-10-19 19:16:10 +0300

    problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,79 @@
 'use strict';
 
-// write your code here
+// const GAME_SCORE = document.getElementById('score');
+const BT_START = document.getElementById('start');
+const MS_START = document.getElementById('message_start');
+// const MS_LOSE = document.getElementById('lose');
+// const MS_WIN = document.getElementById('win');
+const FIELD = document.getElementById('field');
+const CELLS = document.getElementsByClassName('field-cell');
+const collectionOfNumbers = [2, 2, 2, 2, 2, 2, 2, 2, 2, 4];
+let index = Math.floor(Math.random() * collectionOfNumbers.length);
+
+BT_START.addEventListener('click', () => {
+  if (BT_START.classList.contains('start')) {
+    BT_START.classList.remove('start');
+    BT_START.classList.add('restart');
+    BT_START.innerHTML = 'Restart';
+    MS_START.innerHTML = 'Press "Restart" to begin new game.';
+
+    const block1 = CELLS[Math.floor(Math.random() * CELLS.length)];
+    const block2 = CELLS[Math.floor(Math.random() * CELLS.length)];
+
+    index = Math.floor(Math.random() * collectionOfNumbers.length);
+    block1.classList.add(`field-cell--${collectionOfNumbers[index]}`);
+
+    index = Math.floor(Math.random() * collectionOfNumbers.length);
+    block1.innerHTML = `${collectionOfNumbers[index]}`;
+    block2.classList.add(`field-cell--${collectionOfNumbers[index]}`);
+    block2.innerHTML = `${collectionOfNumbers[index]}`;
+  } else if (BT_START.classList.contains('restart')) {
+    BT_START.classList.remove('restart');
+    BT_START.classList.add('start');
+    BT_START.innerHTML = 'Start';
+    MS_START.innerHTML = 'Press "Start" to begin game. Good luck!';
+
+    for (const element of CELLS) {
+      for (const num of [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]) {
+        element.classList.remove(`field-cell--${num}`);
+        element.classList.add('field-cell');
+        element.innerHTML = '';
+      }
+    }
+  }
+});
+
+let row;
+let cow;
+let nextCell;
+
+document.addEventListener('keydown', () => {
+  // якщо є місце, якщо нерівні значення клітинок, то зміщуємо
+  // об'єднати
+  // якщо ні об'єднати, ні змістити - програш
+
+  for (const cell of CELLS) {
+    row = cell.getAttribute('id').split('-')[0];
+    cow = cell.getAttribute('id').split('-')[1];
+    nextCell = document.getElementById(`${+row + 1}-${+cow}`);
+
+    const num = cell.textContent;
+    // const nextNum = nextCell.textContent;
+
+    if (row < FIELD.rows.length
+      && cell.classList.contains(`field-cell--${num}`)) {
+      // console.log(nextCell);
+      nextCell.classList.add(`field-cell--${num}`);
+      nextCell.innerHTML = `${num}`;
+      cell.classList.remove(`field-cell--${num}`);
+      cell.innerHTML = '';
+    }
+    // else if (cell.classList.contains(`field-cell--${num}`)
+    // && num === +nextCell.outerText
+    // && nextCell.outerText) {
+    //   nextCell.innerHTML = +nextCell.textContent * 2;
+    //   cell.classList.remove(`field-cell${num}`);
+    //   cell.innerHTML = '';
+    // }
+  }
+});
