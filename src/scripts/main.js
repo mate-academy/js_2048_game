2a512c29ea619d6a9d426d8e114e76b9ea0f7405
Asya Yeromina <asyayeromina@gmail.com>
2023-11-21 13:12:11 +0100

    find elements on the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+// Step1: Identify elements
+// ===================================
+const GAME_FIELD = document.querySelector('.game-field');
+const MESSAGES = document.querySelectorAll('.message');
+const START_BUTTON = document.querySelector('.button');
+const SCORE_NUMBER = document.querySelector('.game-score');
+
+// Step2: Setting game after page load
+// ===================================
+
+// Step3: Start game (on clicking the start button)
+// ===================================
+
+// Step4: Movements (on keyup)
+// ===================================
+
+// Step5: add messages to functions
+// ===================================
