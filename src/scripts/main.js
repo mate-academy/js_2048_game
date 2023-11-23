7d18790e41b110aa88125e3d3ae2bbb667479dc6
Asya Yeromina <asyayeromina@gmail.com>
2023-11-23 19:07:30 +0100

    add updateField func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,40 +4,56 @@
 // ===================================
 const GAME_FIELD = document.querySelector('.game-field');
 const FIELD_ROWS = document.querySelectorAll('.field-row');
+const FIELD_CELLS = Array.from(FIELD_ROWS).map(row => row.querySelectorAll('.field-cell'));
+// const MESSAGES = document.querySelectorAll('.message');
 const MESSAGE_START = document.querySelector('.message-start');
-const MESSAGE_LOSE = document.querySelector('.message-lose');
-const MESSAGE_WIN = document.querySelector('.message-win');
+// const MESSAGE_LOSE = document.querySelector('.message-lose');
+// const MESSAGE_WIN = document.querySelector('.message-win');
 const START_BUTTON = document.querySelector('.button');
 const SCORE_NUMBER = document.querySelector('.game-score');
+let gameMatrix;
+
+// console.log(FIELD_CELLS);
 
 // Step2: Start game (on clicking the start button)
-
 START_BUTTON.addEventListener('click', startGame);
 document.addEventListener('keyup', makeMove);
 
 function startGame() {
-  const gameMatrix = [
-    [0, 2, 0, 0],
+  gameMatrix = [
+    [2, 2, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 4, 0],
-    [0, 0, 0, 0],
+    [0, 64, 0, 0],
   ];
 
   SCORE_NUMBER.innerText = '0';
   START_BUTTON.classList.add('restart');
   START_BUTTON.innerText = 'Restart';
   MESSAGE_START.classList.add('hidden');
-  addTile();
-  addTile();
-  updateMatrix();
+  addNewNumber();
+  addNewNumber();
+  updateField();
 }
 
-function updateMatrix() {
-  // renew classes/innerText according to the cell value use class  field-cell--%cell_value%
-  
+function updateField() {
+  for (let row = 0; row < gameMatrix.length; row++) {
+    for (let col = 0; col < gameMatrix[row].length; col++) {
+      const num = gameMatrix[row][col];
+
+      FIELD_CELLS[row][col].innerText = num === 0 ? '' : `${num}`;
+      FIELD_CELLS[row][col].className = 'field-cell';
+      // rewriting all classes each time
+      // ? refactor
+
+      if (num !== 0) {
+        FIELD_CELLS[row][col].classList.add(`field-cell--${num}`);
+      }
+    }
+  }
 }
 
-function addTile() {
+function addNewNumber() {
   // adds one tile to random place:
   // create 2 or 4 el
   // choose place for new el
@@ -54,3 +70,7 @@ function makeMove() {
 
 // Step4: add messages to functions
 // ===================================
+
+//
+// Add animations to CSS
+//
