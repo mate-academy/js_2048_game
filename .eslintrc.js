e23aace3ce5de69ae23d1633f07636bc7ec8448a
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-12-01 20:04:52 +0200

    write solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+
+  parseOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+  }
 };
---
+++ src/scripts/tile.js
@@ -0,0 +1,45 @@
+export class Tile {
+  constructor(gridElement) {
+    this.tileElement = document.createElement('div');
+    this.tileElement.classList.add('tile');
+    this.setValue(Math.random() > 0.1 ? 2 : 4);
+    gridElement.append(this.tileElement);
+  }
+
+  setXY(x, y) {
+    this.x = x;
+    this.y = y;
+    this.tileElement.style.setProperty('--x', x);
+    this.tileElement.style.setProperty('--y', y);
+  }
+
+  setValue(value) {
+    const bgLightness = 100 - Math.log2(value) * 9;
+
+    this.value = value;
+    this.tileElement.textContent = value;
+    this.tileElement.style.setProperty('--bg-lightness', `${bgLightness}%`);
+
+    this.tileElement.style.setProperty(
+      '--text-lightness', `${bgLightness < 50 ? 90 : 10}%`
+    );
+  }
+
+  removeFromDOM() {
+    this.tileElement.remove();
+  }
+
+  waitForTransitionEnd() {
+    return new Promise(resolve => {
+      this.tileElement
+        .addEventListener('transitionend', resolve, { once: true });
+    });
+  }
+
+  waitForAnimationEnd() {
+    return new Promise(resolve => {
+      this.tileElement
+        .addEventListener('animationend', resolve, { once: true });
+    });
+  }
+}
