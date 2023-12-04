ac04341b9948ce61d1ad48efcf4fc5586f8be381
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-12-04 18:50:30 +0200

    work with npm deploy problem, save in package.json main to default...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
-
-  rules: {
-    strict: ['error', 'global'],
-  },
-
-  parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
-    sourceType: 'module',
-  },
 };
