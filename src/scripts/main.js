ded453f1b6a1fbd97f9ca89c2af52fbd4c7ad143
Olesia Semen <semenolesa01@gmail.com>
2023-12-25 11:28:28 +0200

    Test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,8 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     if (emptyCells.length > 0) {
-      const { row, col } = emptyCells[Math.floor(Math.random() 
-      * emptyCells.length)];
+      const { row, col } = emptyCells[Math.floor(Math.random()* emptyCells.length)];
       board[row][col] = Math.random() < 0.9 ? 2 : 4;
     }
   }
@@ -142,24 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     return true;
   }
-
-  function handleKeyPress(event) {
-    const direction = event.key.replace('Arrow', '').toLowerCase();
-    if (['up', 'down', 'left', 'right'].includes(direction)) {
-      const moved = move(direction);
-      if (moved) {
-        addRandomNumber();
-        updateBoard();
-        if (isGameWon()) {
-          winMessage.classList.remove('hidden');
-        }
-        if (isGameOver()) {
-          gameOverMessage.classList.remove('hidden');
-        }
-      }
-    }
-  }
-
+  
   function restartGame() {
     startMessage.classList.add('hidden');
     gameOverMessage.classList.add('hidden');
