0996ac5059072e429f0e77202add7320b417ee97
Oleksandr <d43z61p0@gmail.com>
2024-01-13 19:41:28 +0200

    fix eslint errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-let initialField = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+'use strict';
+
+const initialField = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let upDatefield = replaceRandomZero(initialField);
 let scoreNumber = 0;
 const cells = document.getElementsByClassName('field-cell');
@@ -14,7 +16,8 @@ buttonStart.addEventListener('click', () => {
   startText[0].style.display = 'none';
   buttonStart.innerHTML = 'restart';
   buttonStart.style.backgroundColor = 'pink';
-  document.addEventListener('keydown', function(event) {
+
+  document.addEventListener('keydown', function() {
     switch (event.key) {
       case 'ArrowLeft':
         upDatefield = keyLeft(upDatefield);
@@ -25,27 +28,26 @@ buttonStart.addEventListener('click', () => {
 
       case 'ArrowRight':
         upDatefield = keyRight(upDatefield);
-        upDatefield = replaceRandomZero(upDatefield)
-        initial(upDatefield)
+        upDatefield = replaceRandomZero(upDatefield);
+        initial(upDatefield);
         checker();
         break;
 
       case 'ArrowUp':
         upDatefield = keyUp(upDatefield);
-        upDatefield = replaceRandomZero(upDatefield)
-        initial(upDatefield)
+        upDatefield = replaceRandomZero(upDatefield);
+        initial(upDatefield);
         checker();
         break;
 
       case 'ArrowDown':
         upDatefield = keyDown(upDatefield);
-        upDatefield = replaceRandomZero(upDatefield)
-        initial(upDatefield)
+        upDatefield = replaceRandomZero(upDatefield);
+        initial(upDatefield);
         checker();
         break;
 
       default:
-        return;
     }
   });
 });
@@ -56,44 +58,48 @@ function initial(arr) {
   }
 }
 
-
 // PRESS LEFT <-----------------------------------
 // Split the flat array into rows
 function keyLeft(field) {
-  let rows = [];
-  for (let i = 0; i < field.length; i += 4) {
-    rows.push(field.slice(i, i + 4));
+  let matrix = field;
+  const rows = [];
+
+  for (let i = 0; i < matrix.length; i += 4) {
+    rows.push(matrix.slice(i, i + 4));
   }
 
   rows.map((item, index) => {
     rows[index] = moveLeft(item);
   });
 
-  field = [].concat(...rows);
+  matrix = [].concat(...rows);
+
   // Now `field` is updated based on the manipulated rows
   function moveLeft(row) {
     // remove 0's
-    row = removeZero(row);
+    let tempRow = row;
+
+    tempRow = removeZero(row);
 
     // move & sum rows near cells
-    for (let i = 0; i < row.length - 1; i++) {
-      if (row[i + 1] === row[i]) {
-        scoreNumber += row[i] * 2;
-        [row[i], row[i + 1]] = [row[i] * 2, 0];
+    for (let i = 0; i < tempRow.length - 1; i++) {
+      if (tempRow[i + 1] === tempRow[i]) {
+        scoreNumber += tempRow[i] * 2;
+        [tempRow[i], tempRow[i + 1]] = [tempRow[i] * 2, 0];
       }
     }
-    row = removeZero(row);
+    tempRow = removeZero(tempRow);
 
-    row = fillArrayWithZero(row);
+    tempRow = fillArrayWithZero(tempRow);
 
-    return row;
+    return tempRow;
   }
 
-  if (JSON.stringify(upDatefield) === JSON.stringify(field)) {
+  if (JSON.stringify(upDatefield) === JSON.stringify(matrix)) {
     return upDatefield;
   }
 
-  upDatefield = field;
+  upDatefield = matrix;
   scoreText[0].innerHTML = scoreNumber;
   // checker();
 
@@ -104,9 +110,11 @@ function keyLeft(field) {
 
 function keyRight(field) {
   // Split the flat array into rows
-  let rows = [];
-  for (let i = 0; i < field.length; i += 4) {
-    rows.push(field.slice(i, i + 4));
+  let tempField = field;
+  const rows = [];
+
+  for (let i = 0; i < tempField.length; i += 4) {
+    rows.push(tempField.slice(i, i + 4));
   }
 
   rows.map((item, index) => {
@@ -114,34 +122,35 @@ function keyRight(field) {
     rows[index] = moveRight(item);
   });
 
-  field = [].concat(...rows);
+  tempField = [].concat(...rows);
 
   function moveRight(row) {
+    let tempRow = row;
 
-    row = removeZero(row);
+    tempRow = removeZero(tempRow);
 
     // move & sum rows near cells
-    for (let i = 0; i < row.length - 1; i++) {
-      if (row[i + 1] === row[i]) {
-        scoreNumber += row[i] * 2;
-        [row[i], row[i + 1]] = [row[i] * 2, 0];
+    for (let i = 0; i < tempRow.length - 1; i++) {
+      if (tempRow[i + 1] === tempRow[i]) {
+        scoreNumber += tempRow[i] * 2;
+        [tempRow[i], tempRow[i + 1]] = [tempRow[i] * 2, 0];
       }
     }
 
     // remove 0's
-    row = removeZero(row);
+    tempRow = removeZero(tempRow);
 
     // fill 0's
-    row = fillArrayWithZero(row);
+    tempRow = fillArrayWithZero(tempRow);
 
-    return row.reverse();
+    return tempRow.reverse();
   }
 
-  if (JSON.stringify(upDatefield) === JSON.stringify(field)) {
+  if (JSON.stringify(upDatefield) === JSON.stringify(tempField)) {
     return upDatefield;
   }
 
-  upDatefield = field;
+  upDatefield = tempField;
   scoreText[0].innerHTML = scoreNumber;
   // checker();
 
@@ -151,10 +160,12 @@ function keyRight(field) {
 // PRESS UP
 function keyUp(arr) {
   let columns = Array.from({ length: 4 }, (_, colIndex) =>
-    arr.filter((_, rowIndex) => rowIndex % 4 === colIndex)
+    arr.filter((__, rowIndex) => rowIndex % 4 === colIndex),
   );
+
   columns = [].concat(...columns);
   columns = keyLeft(columns);
+
   const numCols = columns.length / 4;
   const transposedArray = [];
 
@@ -166,14 +177,15 @@ function keyUp(arr) {
 
   upDatefield = transposedArray;
   scoreText[0].innerHTML = scoreNumber;
+
   // checker();
   return upDatefield;
 }
 
-//press down
+// press down
 function keyDown(arr) {
   let columns = Array.from({ length: 4 }, (_, colIndex) =>
-    arr.filter((_, rowIndex) => rowIndex % 4 === colIndex)
+    arr.filter((__, rowIndex) => rowIndex % 4 === colIndex),
   );
 
   columns = [].concat(...columns);
@@ -190,6 +202,7 @@ function keyDown(arr) {
   }
 
   upDatefield = transposedArray;
+
   return transposedArray;
 }
 
@@ -202,7 +215,8 @@ function fillArrayWithZero(arr) {
   let res = arr;
 
   res.length = 4;
-  res = Array.from(res, (item) => (item ? item : 0));
+  res = Array.from(res, (item) => (item || 0));
+
   return res;
 }
 
@@ -211,6 +225,7 @@ function replaceRandomZero(array) {
     if (value === 0) {
       indices.push(index);
     }
+
     return indices;
   }, []);
 
@@ -218,33 +233,40 @@ function replaceRandomZero(array) {
     return array;
   }
 
-  const randomIndex = zeroIndices[Math.floor(Math.random() * zeroIndices.length)];
+  const partCondition = Math.floor(Math.random() * zeroIndices.length);
+  const randomIndex = zeroIndices[partCondition];
 
   const replacementValue = Math.random() < 0.9 ? 2 : 4;
 
-  return array.map((value, index) => (index === randomIndex ? replacementValue : value));
+  return array.map((value, index) => {
+    return (index === randomIndex ? replacementValue : value);
+  });
   // return array;
 }
 
 function checker() {
-  let origin = upDatefield;
-  let left = keyLeft(origin);
-  let right = keyRight(origin);
-  let down = keyDown(origin);
-  let up = keyUp(origin);
+  const original = upDatefield;
+  const left = keyLeft(original);
+  const right = keyRight(original);
+  const down = keyDown(original);
+  const up = keyUp(original);
 
   if (upDatefield.includes(2048)) {
-    let win =  document.getElementsByClassName('message-win')[0];
+    const win = document.getElementsByClassName('message-win')[0];
+
     win.style.display = 'block';
-  } else if (JSON.stringify(origin) === JSON.stringify(left)
-    && JSON.stringify(origin) === JSON.stringify(right)
-    && JSON.stringify(origin) === JSON.stringify(down)
-    && JSON.stringify(origin) === JSON.stringify(up)) {
-      let lose =  document.getElementsByClassName('message-lose')[0];
-      upDatefield = origin;
-      lose.style.display = 'block';
-      return true;
+  } else if (JSON.stringify(original) === JSON.stringify(left)
+    && JSON.stringify(original) === JSON.stringify(right)
+    && JSON.stringify(original) === JSON.stringify(down)
+    && JSON.stringify(original) === JSON.stringify(up)) {
+    const lose = document.getElementsByClassName('message-lose')[0];
+
+    upDatefield = original;
+    lose.style.display = 'block';
+
+    return true;
   }
-  upDatefield = origin;
+  upDatefield = original;
+
   return false;
 }
