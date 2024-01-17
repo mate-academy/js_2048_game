10958f852e04f47ff9247e69f6c28506fa1292c8
Daryna Hnidash <melissa998877@gmail.com>
2024-01-17 17:31:12 +0200

    button and messages added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,54 @@
+/* eslint-disable no-console */
 'use strict';
 
 const gameFieldElement = document.querySelector('.game-field');
 const gameScore = document.querySelector('.game-score');
+const startBtn = document.querySelector('.button');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
 
 const rows = 4;
 const columns = 4;
 let score = 0;
 
-// const gameField = [
-//   [2, 2, 2, 0],
-//   [4, 4, 2, 0],
-//   [8, 8, 2, 0],
-//   [2, 2, 2, 2],
-// ];
-
-const gameField = [
+let gameField = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
 
-window.onload = function() {
+let gameStarted = false;
+
+startBtn.addEventListener('click', () => {
+  if (gameStarted) {
+    while (gameFieldElement.firstChild) {
+      gameFieldElement.removeChild(gameFieldElement.firstChild);
+    }
+
+    gameField = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    messageWin.classList.add('hidden');
+    messageLose.classList.add('hidden');
+  }
+
+  messageStart.classList.add('hidden');
   setGame();
-  addTwo();
-  addTwo();
-};
+  addTile();
+  addTile();
+  gameStarted = true;
+
+  startBtn.classList.remove('start');
+  startBtn.classList.add('restart');
+  startBtn.textContent = 'Restart';
+  score = 0;
+});
 
 const setGame = () => {
   for (let r = 0; r < rows; r++) {
@@ -54,7 +77,19 @@ function hasEmptyTile() {
   return false;
 }
 
-function addTwo() {
+function isWon() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      if (gameField[r][c] === 2048) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function addTile() {
   if (!hasEmptyTile()) {
     return;
   }
@@ -64,14 +99,15 @@ function addTwo() {
   while (!found) {
     const r = Math.floor(Math.random() * rows);
     const c = Math.floor(Math.random() * columns);
+    const num = Math.random() > 0.1 ? 2 : 4;
 
     if (gameField[r][c] === 0) {
-      gameField[r][c] = 2;
+      gameField[r][c] = num;
 
       const tile = document.getElementById(r.toString() + '-' + c.toString());
 
-      tile.innerText = '2';
-      tile.classList.add('field-cell--2');
+      tile.innerText = num.toString();
+      tile.classList.add('field-cell--' + num);
       found = true;
     }
   }
@@ -91,24 +127,52 @@ function updateCell(cell, num) {
 document.addEventListener('keyup', (e) => {
   if (e.code === 'ArrowLeft') {
     slideLeft();
-    addTwo();
   }
 
   if (e.code === 'ArrowRight') {
     slideRight();
-    addTwo();
   }
 
   if (e.code === 'ArrowUp') {
     slideUp();
-    addTwo();
   }
 
   if (e.code === 'ArrowDown') {
     slideDown();
-    addTwo();
+  }
+
+  if (isWon()) {
+    messageWin.classList.remove('hidden');
+  }
+
+  if (isLost()) {
+    messageLose.classList.remove('hidden');
   }
 });
+
+function isLost() {
+  if (hasEmptyTile()) {
+    return false;
+  }
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 1; c++) {
+      if (gameField[r][c] === gameField[r][c + 1]) {
+        return false;
+      }
+    }
+  }
+
+  for (let r = 0; r < rows - 1; r++) {
+    for (let c = 0; c < columns; c++) {
+      if (gameField[r][c] === gameField[r + 1][c]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
 
 function updateGameField() {
   for (let r = 0; r < rows; r++) {
@@ -142,56 +206,87 @@ function slide(row) {
 }
 
 function slideLeft() {
+  const fieldCopy = [...gameField];
+
+  console.log(gameField);
+  console.log(fieldCopy);
+
   for (let r = 0; r < rows; r++) {
-    let row = gameField[r];
+    let row = fieldCopy[r];
 
     row = slide(row);
-
-    gameField[r] = row;
-
-    for (let c = 0; c < columns; c++) {
-      const cell = document.getElementById(r.toString() + '-' + c.toString());
-      const num = gameField[r][c];
-
-      updateCell(cell, num);
-    }
+    fieldCopy[r] = row;
   };
+
+  if (fieldCopy.toString() !== gameField.toString()) {
+    gameField = fieldCopy;
+
+    updateGameField();
+    addTile();
+  }
+
+  if (!hasEmptyTile) {
+    messageLose.classList.remove('hidden');
+  }
 };
 
 function slideRight() {
+  const fieldCopy = [...gameField];
+
   for (let r = 0; r < rows; r++) {
-    let row = gameField[r];
+    let row = fieldCopy[r];
 
     row = row.reverse();
     row = slide(row);
-    gameField[r] = row.reverse();
 
-    for (let c = 0; c < columns; c++) {
-      const cell = document.getElementById(r.toString() + '-' + c.toString());
-      const num = gameField[r][c];
+    const newRow = row.reverse();
 
-      updateCell(cell, num);
-    }
+    fieldCopy[r] = newRow;
   };
+
+  const reversedField = [];
+
+  for (const row of gameField) {
+    const newRow = row.reverse();
+
+    reversedField.push(newRow);
+  }
+
+  if (fieldCopy.toString() !== reversedField.toString()) {
+    gameField = fieldCopy;
+
+    updateGameField();
+    addTile();
+  }
 };
 
 function slideUp() {
+  const fieldCopy = [...gameField];
+
+  console.log(gameField);
+  console.log(fieldCopy);
+
   for (let c = 0; c < rows; c++) {
     let row = [
-      gameField[0][c],
-      gameField[1][c],
-      gameField[2][c],
-      gameField[3][c],
+      fieldCopy[0][c],
+      fieldCopy[1][c],
+      fieldCopy[2][c],
+      fieldCopy[3][c],
     ];
 
     row = slide(row);
 
     for (let index = 0; index < rows; index++) {
-      gameField[index][c] = row[index];
+      fieldCopy[index][c] = row[index];
     }
   };
 
-  updateGameField();
+  if (fieldCopy.toString() !== gameField.toString()) {
+    gameField = fieldCopy;
+
+    updateGameField();
+    addTile();
+  }
 };
 
 function slideDown() {
@@ -213,35 +308,3 @@ function slideDown() {
 
   updateGameField();
 };
-
-// function check(a) {
-//   for (let q = 1; q < a.length; ++q) {
-//     if (a[q] > 0 && a[q] === a[q - 1]) {
-//       return true;
-//     }
-//   }
-
-//   return false;
-// }
-
-// function isCanSlide() {
-//   for (let r = 0; r < rows; r++) {
-//     const row = gameField[r].filter(num => num !== 0);
-
-//     if (check(row)) {
-//       return true;
-//     }
-//   }
-
-//   return false;
-// }
-
-// function hasSpaceLeft() {
-//   for (let r = 0; r < rows; r++) {
-//     if (gameField[r][0] === 0) {
-//       return true;
-//     }
-//   }
-
-//   return false;
-// }
