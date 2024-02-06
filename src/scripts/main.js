c3d299270d5b7baaf9d3b03cb066f2ef6cac261a
Nadinnnka <tymoshyk02@gmail.com>
2024-02-06 18:44:18 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,347 +1,304 @@
 'use strict';
 
-class Game {
-  constructor(initialState) {
-    this.board = initialState || this.initializeBoard();
-    this.score = 0;
-    this.hasWon = false;
-    this.movedDuringKeyPress = false;
-    this.messageStart = document.querySelector('.message-start');
-    this.messageLose = document.querySelector('.message-lose');
-    this.messageWin = document.querySelector('.message-win');
-  }
+let board;
+let score = 0;
+const rows = 4;
+const columns = 4;
+let hasWon = false;
+const messageStart = document.querySelector('.message-start');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
 
-  initializeBoard() {
-    return Array.from({ length: 4 }, () => Array(4).fill(0));
-  }
+function initializeBoard() {
+  return Array.from({ length: rows }, () => Array(columns).fill(0));
+}
 
-  createTile(r, c) {
-    const tile = document.createElement('div');
+function createTile(r, c) {
+  const tile = document.createElement('div');
 
-    tile.id = r + '-' + c;
-    document.getElementById('board').append(tile);
+  tile.id = r + '-' + c;
+  document.getElementById('board').append(tile);
 
-    return tile;
-  }
+  return tile;
+}
 
-  setGame() {
-    this.board = this.initializeBoard();
+function setGame() {
+  board = initializeBoard();
 
-    for (let r = 0; r < 4; r++) {
-      for (let c = 0; c < 4; c++) {
-        const tile = this.createTile(r, c);
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      const tile = createTile(r, c);
 
-        this.updateTile(tile, this.board[r][c]);
-      }
+      updateTile(tile, board[r][c]);
     }
-  }
-
-  restartGame() {
-    this.board = this.initializeBoard();
-    this.score = 0;
-
-    for (let r = 0; r < 4; r++) {
-      for (let c = 0; c < 4; c++) {
-        const tile = document.getElementById(r + '-' + c);
-
-        this.updateTile(tile, this.board[r][c]);
-      }
-    }
-
-    this.setTwo();
-    this.setTwo();
-
-    document.getElementById('score').innerText = this.score;
-
-    this.messageLose.style.display = 'none';
-  }
-
-  startGame() {
-    this.restartGame();
-    this.messageStart.style.display = 'none';
-    document.getElementById('startButton').textContent = 'Restart';
-
-    const buttonReset = document.querySelector('.start');
-
-    buttonReset.classList.remove('start');
-    buttonReset.classList.add('restart');
-  }
-
-  updateTile(tile, num) {
-    tile.innerText = '';
-    tile.className = 'tile';
-
-    if (num > 0) {
-      tile.innerText = num.toString();
-
-      if (num === 2048) {
-        this.hasWon = true;
-      }
-
-      if (num <= 4096) {
-        tile.classList.add('x' + num.toString());
-      } else {
-        tile.classList.add('x8192');
-      }
-    }
-  }
-
-  isGameOver() {
-    return !this.hasEmptyTile() && !this.canAddNumbers();
-  }
-
-  canAddNumbers() {
-    for (let r = 0; r < 4; r++) {
-      for (let c = 0; c < 4; c++) {
-        // eslint-disable-next-line max-len
-        if ((c < 3 && this.board[r][c] === this.board[r][c + 1]) || (r < 3 && this.board[r][c] === this.board[r + 1][c])) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  filterZero(row) {
-    return row.filter(num => num !== 0);
-  }
-
-  slide(rowSlide) {
-    let row = rowSlide;
-
-    row = this.filterZero(row);
-
-    for (let i = 0; i < row.length - 1; i++) {
-      if (row[i] === row[i + 1]) {
-        row[i] *= 2;
-        row[i + 1] = 0;
-        this.score += row[i];
-      }
-    }
-
-    row = this.filterZero(row);
-
-    while (row.length < 4) {
-      row.push(0);
-    }
-
-    return row;
-  }
-
-  slideLeft() {
-    let moved = false;
-
-    for (let r = 0; r < 4; r++) {
-      let row = this.board[r];
-
-      const originalRow = [...row];
-
-      row = this.slide(row);
-      this.board[r] = row;
-
-      if (!this.arraysEqual(originalRow, row)) {
-        moved = true;
-      }
-
-      for (let c = 0; c < 4; c++) {
-        const tile = document.getElementById(r + '-' + c);
-        const num = this.board[r][c];
-
-        this.updateTile(tile, num);
-      }
-    }
-
-    return moved;
-  }
-
-  slideRight() {
-    let moved = false;
-
-    for (let r = 0; r < 4; r++) {
-      let row = this.board[r];
-
-      row.reverse();
-
-      const originalRow = [...row];
-
-      row = this.slide(row);
-      this.board[r] = row.reverse();
-
-      if (!this.arraysEqual(originalRow, row)) {
-        moved = true;
-      }
-
-      for (let c = 0; c < 4; c++) {
-        const tile = document.getElementById(r + '-' + c);
-        const num = this.board[r][c];
-
-        this.updateTile(tile, num);
-      }
-    }
-
-    return moved;
-  }
-
-  slideUp() {
-    let moved = false;
-
-    for (let c = 0; c < 4; c++) {
-      // eslint-disable-next-line max-len
-      let row = [this.board[0][c], this.board[1][c], this.board[2][c], this.board[3][c]];
-
-      const originalRow = [...row];
-
-      row = this.slide(row);
-
-      if (!this.arraysEqual(originalRow, row)) {
-        moved = true;
-      }
-
-      for (let r = 0; r < 4; r++) {
-        this.board[r][c] = row[r];
-
-        const tile = document.getElementById(r + '-' + c);
-        const num = this.board[r][c];
-
-        this.updateTile(tile, num);
-      }
-    }
-
-    return moved;
-  }
-
-  slideDown() {
-    let moved = false;
-
-    for (let c = 0; c < 4; c++) {
-      // eslint-disable-next-line max-len
-      let row = [this.board[0][c], this.board[1][c], this.board[2][c], this.board[3][c]];
-
-      row.reverse();
-
-      const originalRow = [...row];
-
-      row = this.slide(row);
-      row.reverse();
-
-      if (!this.arraysEqual(originalRow, row)) {
-        moved = true;
-      }
-
-      for (let r = 0; r < 4; r++) {
-        this.board[r][c] = row[r];
-
-        const tile = document.getElementById(r + '-' + c);
-        const num = this.board[r][c];
-
-        this.updateTile(tile, num);
-      }
-    }
-
-    return moved;
-  }
-
-  setTwo() {
-    if (!this.hasEmptyTile()) {
-      return;
-    }
-
-    let found = false;
-
-    while (!found) {
-      const r = Math.floor(Math.random() * 4);
-      const c = Math.floor(Math.random() * 4);
-
-      if (this.board[r][c] === 0) {
-        this.board[r][c] = 2;
-
-        const tile = document.getElementById(r + '-' + c);
-
-        tile.innerText = '2';
-        tile.classList.add('x2');
-        found = true;
-      }
-    }
-  }
-
-  hasEmptyTile() {
-    for (let r = 0; r < 4; r++) {
-      for (let c = 0; c < 4; c++) {
-        if (this.board[r][c] === 0) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  arraysEqual(arr1, arr2) {
-    return JSON.stringify(arr1) === JSON.stringify(arr2);
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const game = new Game();
+function restartGame() {
+  board = initializeBoard();
+  score = 0;
 
-  game.setGame();
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      const tile = document.getElementById(r + '-' + c);
+
+      updateTile(tile, board[r][c]);
+    }
+  }
+
+  setTwo();
+  setTwo();
+
+  document.getElementById('score').innerText = score;
+
+  messageLose.style.display = 'none';
+}
+
+function startGame() {
+  restartGame();
+  messageStart.style.display = 'none';
+  document.getElementById('startButton').textContent = 'Restart';
+
+  const buttonReset = document.querySelector('.start');
+
+  buttonReset.classList.remove('start');
+  buttonReset.classList.add('restart');
+}
+
+function updateTile(tile, num) {
+  tile.innerText = '';
+  tile.className = 'tile';
+
+  if (num > 0) {
+    tile.innerText = num.toString();
+
+    if (num === 2048) {
+      hasWon = true;
+    }
+
+    if (num <= 4096) {
+      tile.classList.add('x' + num.toString());
+    } else {
+      tile.classList.add('x8192');
+    }
+  }
+}
+
+function isGameOver() {
+  return !hasEmptyTile() && !canAddNumbers();
+}
+
+function canAddNumbers() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      if ((c < columns - 1 && board[r][c] === board[r][c + 1])
+        || (r < rows - 1 && board[r][c] === board[r + 1][c])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function filterZero(row) {
+  return row.filter(num => num !== 0);
+}
+
+function slide(rowSlide) {
+  let row = rowSlide;
+
+  row = filterZero(row);
+
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] === row[i + 1]) {
+      row[i] *= 2;
+      row[i + 1] = 0;
+      score += row[i];
+    }
+  }
+
+  row = filterZero(row);
+
+  while (row.length < columns) {
+    row.push(0);
+  }
+
+  return row;
+}
+
+function slideLeft() {
+  const originalBoard = JSON.stringify(board);
+
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+
+    row = slide(row);
+    board[r] = row;
+
+    for (let c = 0; c < columns; c++) {
+      const tile = document.getElementById(r + '-' + c);
+      const num = board[r][c];
+
+      updateTile(tile, num);
+    }
+  }
+
+  if (JSON.stringify(board) !== originalBoard) {
+    setTwo();
+  }
+}
+
+function slideRight() {
+  const originalBoard = JSON.stringify(board);
+
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+
+    row.reverse();
+    row = slide(row);
+    board[r] = row.reverse();
+
+    for (let c = 0; c < columns; c++) {
+      const tile = document.getElementById(r + '-' + c);
+      const num = board[r][c];
+
+      updateTile(tile, num);
+    }
+  }
+
+  if (JSON.stringify(board) !== originalBoard) {
+    setTwo();
+  }
+}
+
+function slideUp() {
+  const originalBoard = JSON.stringify(board);
+
+  for (let c = 0; c < columns; c++) {
+    let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+
+    row = slide(row);
+
+    for (let r = 0; r < rows; r++) {
+      board[r][c] = row[r];
+
+      const tile = document.getElementById(r + '-' + c);
+      const num = board[r][c];
+
+      updateTile(tile, num);
+    }
+  }
+
+  if (JSON.stringify(board) !== originalBoard) {
+    setTwo();
+  }
+}
+
+function slideDown() {
+  const originalBoard = JSON.stringify(board);
+
+  for (let c = 0; c < columns; c++) {
+    let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+
+    row.reverse();
+    row = slide(row);
+    row.reverse();
+
+    for (let r = 0; r < rows; r++) {
+      board[r][c] = row[r];
+
+      const tile = document.getElementById(r + '-' + c);
+      const num = board[r][c];
+
+      updateTile(tile, num);
+    }
+  }
+
+  if (JSON.stringify(board) !== originalBoard) {
+    setTwo();
+  }
+}
+
+function setTwo() {
+  if (!hasEmptyTile()) {
+    return;
+  }
+
+  let found = false;
+
+  while (!found) {
+    const r = Math.floor(Math.random() * rows);
+    const c = Math.floor(Math.random() * columns);
+
+    if (board[r][c] === 0) {
+      board[r][c] = 2;
+
+      const tile = document.getElementById(r + '-' + c);
+
+      tile.innerText = '2';
+      tile.classList.add('x2');
+      found = true;
+    }
+  }
+}
+
+function hasEmptyTile() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      if (board[r][c] === 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setGame();
 
   const button = document.getElementById('startButton');
 
   button.textContent = 'Start';
+  button.addEventListener('click', startGame);
+});
 
-  button.addEventListener('click', function() {
-    game.startGame();
-  });
-
-  document.addEventListener('keyup', (e) => {
-    game.movedDuringKeyPress = false; // Reset the variable on each keypress
-
-    if (e.code === 'ArrowLeft' && game.slideLeft()) {
-      game.movedDuringKeyPress = true;
-    } else if (e.code === 'ArrowRight' && game.slideRight()) {
-      game.movedDuringKeyPress = true;
-    } else if (e.code === 'ArrowUp' && game.slideUp()) {
-      game.movedDuringKeyPress = true;
-    } else if (e.code === 'ArrowDown' && game.slideDown()) {
-      game.movedDuringKeyPress = true;
-    }
-
-    if (game.movedDuringKeyPress) {
-      game.setTwo();
-    }
-
-    if (game.isGameOver()) {
-      game.messageLose.classList.add('text-centered');
-
-      setTimeout(() => {
-        game.messageLose.style.display = 'block';
-        game.messageLose.classList.remove('text-centered');
-        game.messageLose.classList.add('text-hidden');
-      }, 1000);
-
-      setTimeout(() => {
-        // eslint-disable-next-line max-len
-        game.messageStart.textContent = 'Press "Restart" to begin the game. Good luck!';
-        game.messageStart.style.display = 'block';
-      }, 2000);
-    }
-
-    if (game.hasWon) {
-      game.messageWin.style.opacity = '1';
-    }
-    document.getElementById('score').innerText = game.score;
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  function toggleRule() {
-    const rule = document.getElementById('rule');
-
-    rule.classList.toggle('rule-visible');
+document.addEventListener('keyup', (e) => {
+  if (e.code === 'ArrowLeft') {
+    slideLeft();
+  } else if (e.code === 'ArrowRight') {
+    slideRight();
+  } else if (e.code === 'ArrowUp') {
+    slideUp();
+  } else if (e.code === 'ArrowDown') {
+    slideDown();
   }
 
-  const ruleLink = document.querySelector('.rule__link');
+  if (isGameOver()) {
+    messageLose.classList.add('text-centered');
 
-  ruleLink.addEventListener('click', toggleRule);
+    setTimeout(() => {
+      messageLose.style.display = 'block';
+      messageLose.classList.remove('text-centered');
+      messageLose.classList.add('text-hidden');
+    }, 1000);
+
+    setTimeout(() => {
+      messageStart.textContent = 'Press "Restart" to begin game. Good luck!';
+      messageStart.style.display = 'block';
+    }, 2000);
+  }
+
+  if (hasWon) {
+    messageWin.style.opacity = '1';
+  }
+  document.getElementById('score').innerText = score;
 });
+
+// eslint-disable-next-line no-unused-vars
+function toggleRule() {
+  const rule = document.getElementById('rule');
+
+  rule.classList.toggle('rule-visible');
+}
