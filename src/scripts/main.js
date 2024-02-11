dd85235097eb31b1f9de7e457fc5495e847575cc
Kirill <ovoo1339@gmail.com>
2024-02-11 16:46:24 +0200

    2048

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,47 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
 // Write your code here
+const startButton = document.getElementById('start');
+
+startButton.addEventListener('click', () => {
+  game.start();
+});
+SetupInputOnce();
+
+function SetupInputOnce() {
+  window.addEventListener('keydown', handleInput, { once: true });
+};
+
+function handleInput(events) {
+  switch (events.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    default:
+      SetupInputOnce();
+
+      return;
+  }
+
+  SetupInputOnce();
+}
+
+const restart = document.getElementById('restart');
+
+restart.addEventListener('click', () => {
+  game.restart();
+  game.spw();
+});
