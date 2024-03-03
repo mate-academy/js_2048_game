d81cff9dbcee5d18d8485eaa882ace7d3b777529
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-03-03 15:43:28 +0200

    initial commit

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -20,25 +20,220 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]]) {
+    this.initialState = initialState;
+    this.resultOfMowes = [];
+
+    // console.log(initialState);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    for (let i = 0; i < this.initialState.length; i++) {
+      const row = this.initialState[i];
+      const newRow = [];
+
+      for (let j = 0; j < row.length - 1; j++) {
+        let nextIndex = j + 1;
+
+        if (row[j] !== 0) {
+          while (nextIndex < row.length && row[nextIndex] === 0) {
+            nextIndex++;
+          }
+
+          if (nextIndex < row.length && row[nextIndex] === row[j]) {
+            row[j] += row[nextIndex];
+            row[nextIndex] = 0;
+          } else if (row[nextIndex] !== row[j] && row[j] !== 0) {
+            console.log('error');
+          }
+        }
+      }
+
+      for (let j = 0; j < row.length; j++) {
+        if (row[j] !== 0) {
+          newRow.push(row[j]);
+        }
+      }
+
+      while (newRow.length < row.length) {
+        newRow.push(0);
+      }
+      this.initialState[i] = newRow;
+    }
+    this.addNumberAfterStep();
+  }
+
+  moveRight() {
+    for (let i = 0; i < this.initialState.length; i++) {
+      const row = this.initialState[i];
+
+      for (let j = 0; j < row.length - 1; j++) {
+        if (row[j] !== 0) {
+          let nextIndex = j + 1;
+
+          while (nextIndex < row.length && row[nextIndex] === 0) {
+            nextIndex++;
+          }
+
+          if (nextIndex < row.length && row[nextIndex] === row[j]) {
+            row[j] += row[nextIndex];
+            row[nextIndex] = 0;
+          }
+        }
+      }
+
+      const newRow = [];
+
+      for (let j = 0; j < row.length; j++) {
+        if (row[j] !== 0) {
+          newRow.push(row[j]);
+        }
+      }
+
+      while (newRow.length < row.length) {
+        newRow.unshift(0);
+      }
+      this.initialState[i] = newRow;
+    }
+    this.addNumberAfterStep();
+  }
+
+  moveUp() {
+    this.initialState.map((_, i) => {
+      const col = this.initialState.map(c => c[i]);
+
+      for (let j = 0; j < col.length - 1; j++) {
+        if (col[j] !== 0) {
+          let nextIndex = j + 1;
+
+          while (nextIndex < col.length && col[nextIndex] === 0) {
+            nextIndex++;
+          }
+
+          if (nextIndex < col.length && col[nextIndex] === col[j]) {
+            col[j] += col[nextIndex];
+            col[nextIndex] = 0;
+          }
+        }
+      }
+
+      const newColumn = [];
+
+      for (let j = 0; j < col.length; j++) {
+        if (col[j] !== 0) {
+          newColumn.push(col[j]);
+        }
+      }
+
+      while (newColumn.length < col.length) {
+        newColumn.push(0);
+      }
+
+      for (let j = 0; j < this.initialState.length; j++) {
+        this.initialState[j][i] = newColumn[j];
+      }
+    });
+    this.addNumberAfterStep();
+  }
+
+  moveDown() {
+    this.initialState.map((_, i) => {
+      const col = this.initialState.map(c => c[i]);
+
+      for (let j = 0; j < col.length - 1; j++) {
+        if (col[j] !== 0) {
+          let nextIndex = j + 1;
+
+          while (nextIndex < col.length && col[nextIndex] === 0) {
+            nextIndex++;
+          }
+
+          if (nextIndex < col.length && col[nextIndex] === col[j]) {
+            col[j] += col[nextIndex];
+            col[nextIndex] = 0;
+          }
+        }
+      }
+
+      const newColumn = [];
+
+      for (let j = 0; j < col.length; j++) {
+        if (col[j] !== 0) {
+          newColumn.push(col[j]);
+        }
+      }
+
+      while (newColumn.length < col.length) {
+        newColumn.unshift(0);
+      }
+
+      for (let j = 0; j < this.initialState.length; j++) {
+        this.initialState[j][i] = newColumn[j];
+      }
+    });
+    this.addNumberAfterStep();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() { }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getIndexes() {
+    const emptyFields = [];
+
+    this.initialState.map((row, i) => {
+      row.map((col, j) => {
+        if (col === 0) {
+          emptyFields.push([i, j]);
+        }
+      });
+    });
+
+    const randomEnptyValues = Math.floor(Math.random() * emptyFields.length);
+
+    return emptyFields[randomEnptyValues];
+  }
+
+  getNumber() {
+    return Math.random() < 0.1 ? 4 : 2;
+  }
+
+  renderFields(maxNumbers) {
+    const indexes = [];
+
+    while (indexes.length < maxNumbers) {
+      const index = [this.getIndexes()[0], this.getIndexes()[1]];
+
+      if (this.initialState[index[0]][index[1]] !== 0) {
+        break;
+      }
+
+      indexes.push(index);
+    }
+
+    for (const [row, col] of indexes) {
+      this.initialState[row][col] = this.getNumber();
+    }
+  }
+
+  getState() {
+    this.renderFields(2);
+  }
+
+  addNumberAfterStep() {
+    this.renderFields(1);
+    console.log(this.resultOfMowes);
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +245,22 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() { }
 
   /**
    * Starts the game.
    */
-  start() {}
 
+  start() {
+    this.getState();
+
+    console.log(this.initialState);
+  }
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() { }
 
   // Add your own methods here
 }
-
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,69 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
+
+const gameUIFields = [[], [], [], []];
+const startGameButton = document.querySelector('.button.start');
+const gameFieldRows = [...document.querySelectorAll('.field-row')];
+let gameFieldCell = [...document.querySelectorAll('.field-row .field-cell')];
+
+const renderUIFields = () => {
+  for (let i = 0; i < gameFieldRows.length; i++) {
+    gameUIFields[i] = gameFieldRows[i];
+
+    for (let j = 0; j < gameFieldRows.length; j++) {
+      gameUIFields[i][j] = gameFieldCell[j];
+    }
+    gameFieldCell = gameFieldCell.slice(4);
+  }
+};
+
+const renderGameDesc = () => {
+  game.initialState.map((row, i) => {
+    row.map((coll, j) => {
+      gameUIFields[i][j].textContent = '';
+
+      if (gameUIFields[i][j].textContent === '') {
+        gameUIFields[i][j].className = `field-cell`;
+      }
+
+      if (coll > 0) {
+        gameUIFields[i][j].append(coll);
+        gameUIFields[i][j].className = `field-cell field-cell--${coll}`;
+      }
+    });
+  });
+};
+
+startGameButton.addEventListener('click', el => {
+  game.start();
+  renderUIFields();
+  renderGameDesc();
+});
+
+document.addEventListener('keydown', ev => {
+  switch (ev.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      renderGameDesc();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      renderGameDesc();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      renderGameDesc();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      renderGameDesc();
+      break;
+    default:
+      break;
+  }
+});
 
 // Write your code here
