b958c8c47e162b4e7e06222aaa5c49f3888dc433
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-03-06 00:29:43 +0200

    create the game

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,318 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    // зберігаємо стан гри який було передано до нашого обєкта
+    this.initialState = initialState;
+
+    // тепер зберігаємо стан ношої гри (дошки)
+    // якщо було передано, якщо ні то за замовчуванням
+    this.board = initialState || [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    // початковий рахунок 0
+    this.boardScore = 0;
+
+    // поточний статус ставимо 'idle'-(непрацюючий)
+    this.currentStatus = 'idle'; // 'playing', 'win', 'lose'
+
+    // !!!!тут поки не розумію що до чого потрібно буде додати коменти
+    this.isAbleToMove = true;
+    this.isGameActive = false;
+    this.isGameWon = false;
+    this.isGameLost = false;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  // тут думаю теж зрозуміло
+  // викликаємо функцію за натиском клавіши
+  // moveLeft() і інші викликаються в 'main.js'
+  moveLeft() {
+    if (this.isGameActive) {
+      this.moveTo('left');
+    }
+  }
+  moveRight() {
+    if (this.isGameActive) {
+      this.moveTo('right');
+    }
+  }
+  moveUp() {
+    if (this.isGameActive) {
+      this.moveTo('up');
+    }
+  }
+  moveDown() {
+    if (this.isGameActive) {
+      this.moveTo('down');
+    }
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  getScore() {
+    return this.boardScore;
+  }
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+  getStatus() {
+    switch (true) {
+      case this.isGameWon:
+        this.currentStatus = 'win';
+        break;
+      case this.isGameLost:
+        this.currentStatus = 'lose';
+        break;
+      case this.isGameActive:
+        this.currentStatus = 'playing';
+        break;
+      default:
+        this.currentStatus = 'idle';
+        break;
+    }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+    return this.currentStatus;
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  start() {
+    this.isGameActive = true;
+    this.placeNewCell();
+    this.placeNewCell();
+  }
 
-  // Add your own methods here
+  restart() {
+    /**
+     * не проходить один тест коли при рестарті
+     * потрібно повернути initialState
+     *
+     *    const INITIAL_STATE = [
+      [2, 2, 4, 4],
+      [2, 2, 4, 4],
+      [2, 2, 4, 4],
+      [0, 0, 4, 4],
+    ];
+     *
+     * але коли роблю так:
+     * this.board = this.initialState || [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+     * то тести виснуть і не завершуються
+     *
+     *
+     *
+     */
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    this.boardScore = 0;
+
+    this.currentStatus = 'idle';
+    this.isGameActive = false;
+    this.isGameWon = false;
+    this.isGameLost = false;
+  }
+
+  placeNewCell() {
+    let randomRow, randomColumn;
+
+    do {
+      randomRow = Math.floor(Math.random() * 4);
+      randomColumn = Math.floor(Math.random() * 4);
+    } while (this.board[randomRow][randomColumn] !== 0);
+
+    this.board[randomRow][randomColumn] = this.createCell();
+  }
+
+  createCell() {
+    const randomValue = Math.random();
+
+    return randomValue < 0.1 ? 4 : 2;
+  }
+
+  // ==========================================================
+  // функція переміщення плиток з номерами відповідно до 'direction'
+  moveTo(direction) {
+    if (!this.isGameActive) {
+      return;
+    }
+
+    const numColumns = this.board[0].length; // завжди буде 4 (я так гадаю)
+
+    // тут ми робимо глибоку копію масива 'this.board'
+    // 'JSON.stringify()'- робить з обєкта строку а потім
+    // 'JSON.parse()' уже перетворює строку в обєкт (в нашому випадку масив)
+    // !!!чому ми робимо саме глибоку копію???
+    // тому що вона нам робить копію і масивів всередені масиву
+    // а не просто посилання на них
+    let currentTable = JSON.parse(JSON.stringify(this.board));
+
+    // змінна для підрахунку очок гри
+    let addScore = 0;
+
+    // !!! так тут трішки для мене складнувато!!!
+    // !!! навіщо ми це робимо потрібно теж розібратись!!!!
+    // що ми тут робимо???
+    // ми перетворюємо строки в колонки
+    // зовнішній 'map' приймає тільки індекси а внутрішній формує
+    // формує рядки з однаковими індексами
+    const transpose = (table) => {
+      return table[0].map((_, colIndex) => table.map((row) => row[colIndex]));
+      // return table.map((_, colIndex) => table.map(row => row[colIndex]));
+    };
+
+    // !!! тут більш зрозуміліше що ми робимо але поки що
+    // не зрозуміло навіщо!!!!
+    const reverseRow = (table) => {
+      return table.map((row) => row.slice().reverse());
+    };
+
+    // тут у нас виходить зсув ВПРАВО всієї дошки
+    const moveTable = (table) => {
+      // створюємо копію вхідного 'table'
+      const newTable = table.map((row) => {
+        // створюємо нові рядки в 'table' без нулів 0
+        // [0, 2, 4, 0] ==> [2, 4]
+        let newRow = row.filter((num) => num !== 0);
+        // тут ми отримуємо значення скільки нулів потрібно додати
+        const zerosToAdd = numColumns - newRow.length;
+
+        // ми копіюємо 'newRow' з новим масивом з нулями який
+        // створюємо
+        // [2, 4] ==> [0, 0, 2, 4]
+        // (здвигаємо їх щоб вони були поруч для цикла)
+        newRow = [...Array(zerosToAdd).fill(0), ...newRow];
+
+        // якщо плитки однакові ОБЄДНУЄМО
+        for (let i = newRow.length; i >= 0; i--) {
+          if (newRow[i - 1] === newRow[i]) {
+            newRow[i - 1] *= 2;
+            newRow[i] = 0;
+            addScore += newRow[i - 1];
+            i--;
+          }
+        }
+
+        // знову прибираємо нулі щоб здвинути до краю
+        // [0, 2, 4, 0] ==> [2, 4]
+        newRow = newRow.filter((num) => num !== 0);
+
+        // скільки додати нулів
+        const zerosToAddEnd = numColumns - newRow.length;
+
+        // знову додаємо нулі щоб числа були біля краю
+        newRow = [...Array(zerosToAddEnd).fill(0), ...newRow];
+
+        return newRow;
+      });
+
+      return newTable;
+    };
+
+    const isGameOver = () => {
+      const GAME_FIELD = 4;
+
+      // тут перевірка чи є хоч один нуль
+      for (let i = 0; i < GAME_FIELD; i++) {
+        for (let j = 0; j < GAME_FIELD; j++) {
+          if (this.board[i][j] === 0) {
+            return false;
+          }
+        }
+      }
+
+      // тут перевірка чи є по горизонталі однакові числа
+      for (let i = 0; i < GAME_FIELD; i++) {
+        for (let j = 0; j < GAME_FIELD; j++) {
+          if (j < GAME_FIELD - 1 && this.board[i][j] === this.board[i][j + 1]) {
+            return false;
+          }
+
+          // тут перевірка чи є по вертикалі однакові числа
+          if (i < GAME_FIELD - 1 && this.board[i][j] === this.board[i + 1][j]) {
+            return false;
+          }
+        }
+      }
+
+      // само собою якщо всі цикли нічого не повернули
+      // то гра завершена
+      this.isGameActive = false;
+      this.isGameLost = true;
+
+      return true;
+    };
+
+    const makeMove = (moveToSide) => {
+      if (JSON.stringify(moveToSide) === JSON.stringify(currentTable)) {
+        return;
+      }
+
+      this.board = moveToSide;
+      currentTable = moveToSide;
+
+      if (this.board.flat().includes(2048)) {
+        this.isGameActive = false;
+        this.isGameWon = true;
+
+        this.getStatus();
+
+        return;
+      }
+
+      this.placeNewCell();
+
+      if (isGameOver()) {
+        this.isAbleToMove = false;
+      }
+    };
+
+    switch (direction) {
+      case 'up':
+        const moveUp = transpose(
+          reverseRow(moveTable(reverseRow(transpose(currentTable)))),
+        );
+
+        makeMove(moveUp);
+
+        this.boardScore += addScore;
+        break;
+
+      case 'down':
+        const moveDown = transpose(moveTable(transpose(currentTable)));
+
+        makeMove(moveDown);
+
+        this.boardScore += addScore;
+        break;
+
+      case 'right':
+        const moveRight = moveTable(currentTable);
+
+        makeMove(moveRight);
+
+        this.boardScore += addScore;
+        break;
+
+      case 'left':
+        const moveLeft = reverseRow(moveTable(reverseRow(currentTable)));
+
+        makeMove(moveLeft);
+
+        this.boardScore += addScore;
+        break;
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,103 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
 // Write your code here
+
+// отримуємо доступ до різних єлементів на сторінці html
+// та кидаємо їх в змінні щоб потім їх легко отримувати
+const container = document.querySelector('.container');
+const button = container.querySelector('.button');
+const messageStart = container.querySelector('.message-start');
+const messageLose = container.querySelector('.message-lose');
+const messageWin = container.querySelector('.message-win');
+const scoreInfo = container.querySelector('.game-score');
+
+// коли було натиснуто кнопку 'button'
+// (це ми розуміємо через 'addEventListener'(сlick))
+// і робимо наступні дії які нам потрібно
+button.addEventListener('click', () => {
+  if (button.textContent === 'Start') {
+    game.start();
+
+    // змінюємо текст
+    button.textContent = 'Restart';
+    // додаємо новий клас для scc щоб стилізувати
+    button.classList.add('restart');
+    // видяляємо старий клас стилізації
+    button.classList.remove('start');
+  } else {
+    game.restart();
+
+    // тут все робимо навпаки якщо наша перевірка 'if' false
+    button.textContent = 'Start';
+    button.classList.add('start');
+    button.classList.remove('restart');
+  }
+
+  // думаю зрозуміло (ховаємо всі повідомлення під час гри)
+  messageStart.classList.add('hidden');
+  messageLose.classList.add('hidden');
+  messageWin.classList.add('hidden');
+
+  // викликаєио функцію. Опис її буде нижче !!!!
+  // !!! а сюди потім додади опис що функція робить
+  updateUI();
+});
+
+// по полицям:
+// читаємо яка клавіша була натистуна (перебираємо тільки варіанти
+// вверх, вниз, вліво, вправо) інші ігноруємо
+// відповідно запускаємо функцію в залежності від натиснутої клавіши
+document.addEventListener('keydown', (keyboard) => {
+  switch (keyboard.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    default:
+      break;
+  }
+
+  updateUI();
+});
+
+function updateUI() {
+  const gameState = game.getState().flat();
+  const fieldCells = container.querySelectorAll('.field-cell');
+
+  fieldCells.forEach((fieldCell, index) => {
+    const classList = fieldCell.classList;
+    const classArray = classList;
+
+    for (const className of classArray) {
+      if (className !== 'field-cell') {
+        classList.remove(className);
+        fieldCell.textContent = '';
+      }
+    }
+
+    if (gameState[index] !== 0) {
+      fieldCell.classList.add(`field-cell--${gameState[index]}`);
+      fieldCell.textContent = gameState[index];
+    }
+  });
+
+  scoreInfo.textContent = game.getScore();
+
+  if (game.getStatus() === 'lose') {
+    messageLose.classList.remove('hidden');
+  } else if (game.getStatus() === 'win') {
+    messageWin.classList.remove('hidden');
+  }
+}
