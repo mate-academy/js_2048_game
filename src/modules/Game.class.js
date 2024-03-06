cb61c0d97d0fc7b77efacf7eda7b7af2ec376444
Vlad Kach <kachevl@gmail.com>
2024-03-06 16:43:25 +0200

    2048 game

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,67 @@
-'use strict';
+import { Cell } from './cell';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
-class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+const GAME_FIELD_SIZE = 4;
+const CELLS_COUNT = GAME_FIELD_SIZE * GAME_FIELD_SIZE;
+
+export class Game {
+  constructor(gameElement) {
+    this.cells = [];
+    this.score = Cell.getScore();
+
+    for (let i = 0; i < CELLS_COUNT; i++) {
+      this.cells.push(
+        new Cell(
+          gameElement,
+          i % GAME_FIELD_SIZE,
+          Math.floor(i / GAME_FIELD_SIZE),
+        ),
+      );
+    }
+
+    this.cellsGroupedByColumn = this.groupCellsByColumn();
+
+    this.cellsGroupedByReversedColumn = this.cellsGroupedByColumn.map(
+      (column) => [...column].reverse(),
+    );
+
+    this.cellsGroupedByRow = this.groupCellsByRow();
+
+    // eslint-disable-next-line prettier/prettier
+    this.cellsGroupedByReversedRow = this.cellsGroupedByRow.map((row) =>
+      // eslint-disable-next-line comma-dangle, prettier/prettier
+      [...row].reverse(),);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  getScore() {
+    this.score = Cell.getScore();
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  getRandomEmptyCell() {
+    const emptyCells = this.cells.filter((cell) => cell.isEmpty());
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    return emptyCells[randomIndex];
+  }
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+  groupCellsByColumn() {
+    return this.cells.reduce((groupedCells, cell) => {
+      groupedCells[cell.x] = groupedCells[cell.x] || [];
+      groupedCells[cell.x][cell.y] = cell;
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+      return groupedCells;
+    }, []);
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  groupCellsByRow() {
+    return this.cells.reduce((groupedCells, cell) => {
+      groupedCells[cell.y] = groupedCells[cell.y] || [];
+      groupedCells[cell.y][cell.x] = cell;
 
-  // Add your own methods here
+      return groupedCells;
+    }, []);
+  }
+
+  resetScore() {
+    this.cells[0].resetScore();
+  }
 }
-
-module.exports = Game;
