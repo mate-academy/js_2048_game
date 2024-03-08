c3be55d9ed2d7e00c9a9b0fe199bcec407521d36
Davwidos <d.jurkowski015@gmail.com>
2024-03-08 17:24:32 +0100

    impemented some methods in game class

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -20,25 +20,94 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+
+  static get deafultSize() {
+    return 4;
+  }
+
+  static get deafultInitialState() {
+    return new Array(Game.deafultSize)
+      .fill(null)
+      .map((el) => new Array(Game.deafultSize).fill(0));
+  }
+
+  #status = 'idle';
+  #score = 0;
+
+  constructor(initialState = Game.deafultInitialState) {
+    this.state = initialState.map((arr) => [...arr]);
     // eslint-disable-next-line no-console
     console.log(initialState);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    const mergedCellsIndexes = new Set();
+
+    this.state.forEach((arr) => {
+      const emptyIndexes = [];
+
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+          emptyIndexes.push(i);
+          continue;
+        }
+
+        let cellIndex = i;
+
+        if (emptyIndexes.length) {
+          cellIndex = emptyIndexes.shift();
+
+          arr[cellIndex] = arr[i];
+          arr[i] = 0;
+        }
+
+        const previousCellIndex = cellIndex - 1;
+
+        if (
+          previousCellIndex >= 0 &&
+          arr[previousCellIndex] === arr[cellIndex] &&
+          !mergedCellsIndexes.has(previousCellIndex)
+        ) {
+          mergedCellsIndexes.add(previousCellIndex);
+          arr[previousCellIndex] *= 2;
+          arr[cellIndex] = 0;
+          emptyIndexes.unshift(cellIndex);
+          this.#score += arr[previousCellIndex];
+        }
+      }
+    });
+
+    this.#addNumber();
+  }
+  moveRight() {
+    this.#reverse();
+    this.moveLeft();
+    this.#reverse();
+  }
+  moveUp() {
+    this.#rotate();
+    this.moveLeft();
+    this.#rotate();
+  }
+  moveDown() {
+    this.#rotate();
+    this.moveRight();
+    this.#rotate();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.#score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.state.map((arr) => [...arr]);
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +119,76 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.#status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.#addNumber();
+    this.#status = 'playing';
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.#clearState();
+    this.start();
+  }
 
   // Add your own methods here
+  #addNumber() {
+    const number = Math.random() > 0.1 ? 2 : 4;
+    const emptyFields = this.#findEmptyFields();
+
+    if (!emptyFields.length) {
+      return false;
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyFields.length);
+    const { x, y } = emptyFields[randomIndex];
+
+    this.state[y][x] = number;
+
+    return true;
+  }
+
+  #findEmptyFields() {
+    const result = [];
+
+    this.state.forEach((arr, y) => {
+      arr.forEach((cell, x) => {
+        if (cell === 0) {
+          result.push({ x, y });
+        }
+      });
+    });
+
+    return result;
+  }
+
+  #clearState() {
+    this.state = this.state.map((arr) => new Array(arr.length).fill(0));
+  }
+
+  #reverse() {
+    this.state.forEach((arr) => arr.reverse());
+  }
+
+  #rotate() {
+    this.state = this.state.map((arr, j) => {
+      const newArr = [];
+
+      for (let i = 0; i < arr.length; i++) {
+        newArr.push(this.state[i][j]);
+      }
+
+      return newArr;
+    });
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,43 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+function keyListener(e) {
+  switch (e.key) {
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+  }
+
+  update(game);
+}
+
+function update() {
+  const gameField = document.querySelector('.game-field');
+  const gameCells = Array.from(gameField.querySelectorAll('.field-row')).map(
+    (row) => Array.from(row.children),
+  );
+
+  game.getState().forEach((row, i) => {
+    row.forEach((el, j) => {
+      gameCells[i][j].innerText = el || '';
+    });
+  });
+}
+
+game.start();
+
+update();
+
+document.addEventListener('keydown', keyListener);
