91a12a50c86f8cf97dc8ae3643d0723661f3ddf2
haduigon <haduigon@gmail.com>
2024-03-12 17:28:27 +0200

    commit for big mac

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -40,54 +40,100 @@ class Game {
 
   }
 
-  moveLeft() {}
-  moveRight() {}
+  moveLeft() { }
+  moveRight() { }
 
   moveUp() {
 
   }
-  moveDown() {
-    // let touched = false;
 
-    // for (let row = 0; row <= this.board.length - 2; row++) {
-    //   console.log(this.board[row], 'the row');
-    //   for (let cell = 0; cell <= this.board[row].length - 1 ; cell++) {
-    //     if ((this.board[row][cell] === this.board[row + 1][cell])) {
-    //       this.board[row + 1][cell] = this.board[row][cell] * 2;
-    //       this.board[row][cell] = 0;
-    //       touched = true;
-    //       this.renderBoard();
-    //       console.log(this.board, 'board2');
-    //     }
-    //   }
+
+  compress(col) {
+    // for (let col = 0; col < this.board[0].length; col++) {
+    //   const column = this.board.map(row => row[col]);
+    //   console.log(column, this.board[0],'this is it');
     // }
-    for (let row = this.board.length - 1; row >= 1; row--) {
-      console.log(this.board[row], 'the row');
-      for (let cell = 0; cell <= this.board[row].length - 1 ; cell++) {
-        if ((this.board[row][cell] === this.board[row - 1][cell])) {
-          this.board[row][cell] = this.board[row - 1][cell] * 2;
-          this.board[row - 1][cell] = 0;
-          // touched = true;
-          this.renderBoard();
-          console.log(this.board, 'board2');
-        }
-      }
+    // Remove empty cells
+    const compressedColumn = col.filter(cell => cell.value !== 0);
+    // Fill the column with compressed cells followed by empty cells
+    col.forEach((cell, index) => {
+      return cell.value === index >= col.length - compressedColumn.length
+        ? compressedColumn[index - (col.length - compressedColumn.length)].value
+        : 0;
+    });
+  }
+
+  moveDown() {
+
+    for (let col = 0; col < this.board[0].length; col++) {
+      const column = this.board.map(row => row[col]);
     }
 
 
 
-    this.generateNumbers();
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    // for (let row = 0; row <= this.board.length - 2; row++) {
+    //   // let touch = false;
+
+    //   for (let col = 0; col <= this.board[row].length; col++) {
+
+    //     if (this.board[row + 1][col] === 0) {
+    //       // touch = true;
+    //       this.board[row + 1][col] = this.board[row][col];
+    //       this.board[row][col] = 0;
+    //     }
+    //     if ((this.board[row][col] === this.board[row + 1][col]) && this.board[row + 1][col] > 0) {
+
+    //       this.board[row + 1][col] = this.board[row][col] * 2;
+    //       this.board[row][col] = 0;
+    //     }
+    //     if (this.board[row + 2] !== undefined) {
+    //     }
+    //   }
+    // }
+
+//!!!!!!!!!!!!!!!!!!!!!!!
+
+    // for (let row = this.board.length - 1; row >= 1; row--) {
+    // let touch = false;
+    // console.log(this.board[row], 'the row');
+    // for (let cell = 0; cell <= this.board[row].length - 1 ; cell++) {
+    //   const condition = (this.board[row][cell] === this.board[row - 1][cell]);
+    //   if (condition) {
+    //     this.board[row][cell] = this.board[row - 1][cell] * 2;
+    //     this.board[row - 1][cell] = 0;
+    //     // this.renderBoard();
+    //     touch = true;
+    //     console.log(this.board, 'board2');
+    //   }
+    //   if (this.board[row - 1][cell] === 0) {
+    //     // this.board[row][cell] = this.board[row - 1][cell] * 2;
+    //     this.board[row - 1][cell] = this.board[row][cell];
+    //     // this.renderBoard();
+    //     this.board[row][cell] = 0;
+    //     console.log(this.board, 'board2');
+    //   }
+    // }
+    this.renderBoard();
+
+    // }
+
+
+
+    // this.generateNumbers();
   }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() { }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() { }
 
   /**
    * Returns the current game status.
@@ -99,7 +145,7 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() { }
 
   /**
    * Starts the game.
@@ -110,8 +156,18 @@ class Game {
     // this.board[Math.floor(Math.random() * 4)][Math.floor(Math.random() * 4)] = 2;
     // this.board[Math.floor(Math.random() * 4)][Math.floor(Math.random() * 4)] = 2;
     this.board[0][2] = 2;
+    // this.board[1][2] = 2;
+    // this.board[2][2] = 4;
+    this.board[1][0] = 2;
+    this.board[2][0] = 2;
+    this.board[3][0] = 2;
     this.board[1][2] = 2;
-    this.board[2][2] = 4;
+    this.board[2][2] = 2;
+    this.board[3][2] = 2;
+    this.board[0][3] = 2;
+    this.board[2][3] = 2;
+    this.board[3][3] = 2;
+    // this.board[3][0] = 2;
 
     // console.log(this.board, 'board');
     this.renderBoard()
@@ -121,13 +177,13 @@ class Game {
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() { }
 
   // Add your own methods here
 
   generateNumbers() {
     const empty = [];
-        for (let line = 0; line <= this.board.length - 1; line++) {
+    for (let line = 0; line <= this.board.length - 1; line++) {
       for (let col = 0; col <= this.board[line].length - 1; col++) {
         if (this.board[line][col] === 0) {
           empty.push([line, col]);
@@ -137,7 +193,7 @@ class Game {
     const coords = Math.floor(Math.random() * empty.length);
 
     const test = empty[coords]
-    if (test !== undefined) {this.board[test[0]][test[1]] = 2;}
+    if (test !== undefined) { this.board[test[0]][test[1]] = 2; }
     // console.log(this.board, 'board2');
     this.renderBoard()
 
