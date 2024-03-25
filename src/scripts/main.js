7c381bc82de8645350a4b6706ce259dbd3229b61
Absolution26 <zet2571428@gmail.com>
2024-03-25 19:40:09 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,141 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startButton = document.querySelector('.start');
+const messageStartGame = document.querySelector('.message-start');
+const messageWinGame = document.querySelector('.message-win');
+const messageLoseGame = document.querySelector('.message-lose');
+const gameScore = document.querySelector('.game-score');
+const [...cells] = document.getElementsByClassName('field-cell');
+
+function stylingCells() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].className = 'field-cell';
+
+    if (game.field.flat()[i]) {
+      cells[i].classList.add(`field-cell--${game.field.flat()[i]}`);
+    }
+  }
+}
+
+function renderField() {
+  for (let i = 0; i < cells.length; i++) {
+    [...cells][i].innerText = game.field.flat()[i] || '';
+  }
+  gameScore.innerText = game.score;
+}
+
+function handleUpButtonClickAndUpdateField() {
+  if (!game.checkVictory() && game.gameStatus === 'playing') {
+    game.moveUp();
+    game.addingNumberToField();
+    renderField();
+    game.checkVictory();
+    stylingCells();
+  }
+}
+
+function handleDownButtonClickAndUpdateField() {
+  if (!game.checkVictory() && game.gameStatus === 'playing') {
+    game.moveDown();
+    game.addingNumberToField();
+    renderField();
+    game.checkVictory();
+    stylingCells();
+  }
+}
+
+function handleRightButtonClickAndUpdateField() {
+  if (!game.checkVictory() && game.gameStatus === 'playing') {
+    game.moveRight();
+    game.addingNumberToField();
+    renderField();
+    game.checkVictory();
+    stylingCells();
+  }
+}
+
+function handleLeftButtonClickAndUpdateField() {
+  if (!game.checkVictory() && game.gameStatus === 'playing') {
+    game.moveLeft();
+    game.addingNumberToField();
+    renderField();
+    game.checkVictory();
+    stylingCells();
+  }
+}
+
+startButton.addEventListener('click', () => {
+  if (!startButton.classList.contains('start')) {
+    game.restart();
+    messageLoseGame.classList.add('hidden');
+    messageWinGame.classList.add('hidden');
+    gameScore.innerText = game.score;
+  }
+
+  if (startButton.classList.contains('start')) {
+    game.start();
+    startButton.classList.add('restart');
+    startButton.classList.remove('start');
+    startButton.innerText = 'Restart';
+    messageStartGame.classList.add('hidden');
+  }
+
+  renderField();
+  stylingCells();
+});
+
+document.addEventListener('keyup', (action) => {
+  switch (action.key) {
+    case 'ArrowUp':
+      if (game.canMoveCellsUp()) {
+        handleUpButtonClickAndUpdateField();
+      }
+
+      if (!game.canMoveCellsUp()) {
+        if (game.checkLose()) {
+          messageLoseGame.classList.remove('hidden');
+        }
+      }
+      break;
+    case 'ArrowLeft':
+      if (game.canMoveCellsLeft()) {
+        handleLeftButtonClickAndUpdateField();
+      }
+
+      if (!game.canMoveCellsLeft()) {
+        if (game.checkLose()) {
+          messageLoseGame.classList.remove('hidden');
+        }
+      }
+      break;
+    case 'ArrowDown':
+      if (game.canMoveCellsDown()) {
+        handleDownButtonClickAndUpdateField();
+      }
+
+      if (!game.canMoveCellsDown()) {
+        if (game.checkLose()) {
+          messageLoseGame.classList.remove('hidden');
+        }
+      }
+      break;
+    case 'ArrowRight':
+      if (game.canMoveCellsRight()) {
+        handleRightButtonClickAndUpdateField();
+      }
+
+      if (!game.canMoveCellsRight()) {
+        if (game.checkLose()) {
+          messageLoseGame.classList.remove('hidden');
+        }
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+export { renderField, stylingCells };
