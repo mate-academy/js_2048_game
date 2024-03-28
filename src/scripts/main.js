ec228ec1cfdda9caddea6ce09b382b116dab8cb0
Max Petrashishin <rikogjdefolt@gmail.com>
2024-03-28 12:44:47 +0200

    creat change button Start - Restart

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const start = document.querySelector('.button');
+
+start.addEventListener('click', () => {
+  if (start.classList.contains('start')) {
+    start.classList.remove('start');
+    start.classList.add('restart');
+    start.textContent = 'Restart';
+  } else {
+    start.classList.add('start');
+    start.classList.remove('restart');
+    start.textContent = 'Start';
+  }
+});
+
+const rows = Array.from(document.querySelectorAll('.field-row')).map((row) => {
+  return Array.from(row.querySelectorAll('.field-cell'));
+});
