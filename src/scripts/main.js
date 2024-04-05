3a9735648a227ec99cc59916753fb9339040e193
Andrew Rich <andy.rich3a@gmail.com>
2024-04-05 16:28:47 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,97 @@
 'use strict';
 
 const Game = require('../modules/Game.class');
+const game = new Game();
+const container = document.querySelector('.container');
+const gameField = document.querySelector('.game-field');
+const button = container.querySelector('.button');
+const gameScore = container.querySelector('.game-score');
 
-const game = new Game([
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-]);
+const messages = {
+  start: container.querySelector('.message-start'),
+  restart: container.querySelector('.message-restart'),
+  lose: container.querySelector('.message-lose'),
+  win: container.querySelector('.message-win'),
+};
 
-const startButton = document.getElementById('start-button');
+const fieldRows = [...gameField.querySelectorAll('.field-row')];
+const fieldCells = fieldRows.map((row) => [...row.children]);
 
-startButton.addEventListener('click', () => {
-  if (game.getStatus() === 'idle') {
-    game.start();
-  } else {
-    game.restart();
-  }
-});
+function displayGameField(state, cellElements) {
+  state.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      const currentCell = cellElements[i][j];
 
-window.addEventListener('keyup', handleKey);
+      currentCell.className = !cell
+        ? 'field-cell'
+        : `field-cell field-cell--${cell}`;
+      currentCell.innerHTML = !cell ? '' : cell;
+    });
+  });
+}
 
-function handleKey(action) {
-  if (action.code === 'ArrowUp') {
-    game.moveUp();
-  }
+function updateScore(score, elem) {
+  elem.innerHTML = score;
+}
 
-  if (action.code === 'ArrowDown') {
-    game.moveDown();
-  }
+function showMessage() {
+  const gameStatus = game.getStatus();
 
-  if (action.code === 'ArrowLeft') {
-    game.moveLeft();
-  }
+  for (const key in messages) {
+    if (Object.hasOwnProperty.call(messages, key)) {
+      const message = messages[key];
 
-  if (action.code === 'ArrowRight') {
-    game.moveRight();
+      if (message) {
+        message.classList.toggle('hidden', key !== gameStatus);
+      }
+    }
   }
 }
+
+button.addEventListener('click', () => {
+  const buttonText = button.textContent;
+
+  if (buttonText === 'Start') {
+    game.start();
+    button.textContent = 'Restart';
+    button.classList.replace('start', 'restart');
+  } else {
+    game.restart();
+    updateScore(game.getScore(), gameScore);
+    button.textContent = 'Start';
+    button.classList.replace('restart', 'start');
+  }
+
+  const state = game.getState();
+
+  displayGameField(state, fieldCells);
+  showMessage();
+});
+
+document.addEventListener('keydown', (keyEvent) => {
+  keyEvent.preventDefault();
+
+  if (game.getStatus() !== 'playing') {
+    return;
+  }
+
+  const moveActions = {
+    ArrowUp: game.moveUp,
+    ArrowDown: game.moveDown,
+    ArrowLeft: game.moveLeft,
+    ArrowRight: game.moveRight,
+  };
+
+  const action = moveActions[keyEvent.key];
+
+  if (action) {
+    action.call(game);
+  }
+
+  const currentScore = game.getScore();
+  const currentGameState = game.getState();
+
+  displayGameField(currentGameState, fieldCells);
+  updateScore(currentScore, gameScore);
+  showMessage();
+});
