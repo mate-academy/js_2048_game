9d256547acae24f8ac28e9799b130d77dc6760c2
Andrew Rich <andy.rich3a@gmail.com>
2024-04-05 16:23:07 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,40 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
 
-// Write your code here
+const game = new Game([
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+]);
+
+const startButton = document.getElementById('start-button');
+
+startButton.addEventListener('click', () => {
+  if (game.getStatus() === 'idle') {
+    game.start();
+  } else {
+    game.restart();
+  }
+});
+
+window.addEventListener('keyup', handleKey);
+
+function handleKey(action) {
+  if (action.code === 'ArrowUp') {
+    game.moveUp();
+  }
+
+  if (action.code === 'ArrowDown') {
+    game.moveDown();
+  }
+
+  if (action.code === 'ArrowLeft') {
+    game.moveLeft();
+  }
+
+  if (action.code === 'ArrowRight') {
+    game.moveRight();
+  }
+}
