1598848d674a8032c7fe8ea7bd6f70cbad558e80
Olaf <olaf.chuszno@gmail.com>
2024-04-14 15:30:58 +0200

    Display current score

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -6,6 +6,13 @@
  * Feel free to add more props and methods if needed.
  */
 class Game {
+  static Status = {
+    idle: 'idle',
+    playing: 'playing',
+    win: 'win',
+    lose: 'lose',
+  };
+
   /**
    * Creates a new game instance.
    *
@@ -32,6 +39,8 @@ class Game {
 
     this.state = initialState ?? emptyBoard.concat();
     this.emptyBoard = emptyBoard.concat();
+    this.status = Game.Status.idle;
+    this.score = 0;
   }
 
   // Game has status 'idle' by default
@@ -46,10 +55,7 @@ class Game {
    * @returns {number}
    */
   getScore() {
-    return this.state.reduce(
-      (prev, row) => prev + row.reduce((acc, cell) => acc + cell),
-      0,
-    );
+    return this.score;
   }
 
   /**
@@ -97,6 +103,212 @@ class Game {
     this.status = 'idle';
 
     // LATER --- CHECK WHETHER YOU NEED TO CALCULATE THE SCORE AGAIN
+  }
+
+  moveTilesRight() {
+    let didTilesMove = false;
+
+    for (let row = 0; row < 4; row++) {
+      for (let tile = 2; tile >= 0; tile--) {
+        // Check whether the tile we want to move isn't empty
+        if (this.state[row][tile] > 0) {
+          let moveTileBy = 0;
+          let merge = false;
+
+          // Check how far can we move the tile
+          while (this.state[row][tile + moveTileBy + 1] === 0) {
+            moveTileBy++;
+          }
+
+          // Check whether if we go one tile further we can merge
+          if (
+            this.state[row][tile + moveTileBy + 1] === this.state[row][tile]
+          ) {
+            moveTileBy++;
+            merge = true;
+          }
+
+          if (moveTileBy > 0) {
+            if (merge) {
+              const newNumber = this.state[row][tile + moveTileBy] * 2;
+
+              // Merge current tile to the left
+              this.state[row][tile + moveTileBy] = newNumber;
+
+              this.score += newNumber;
+            } else {
+              // Move the tile to the right
+              this.state[row][tile + moveTileBy] = this.state[row][tile];
+            }
+            // Reset the tile we just moved
+            this.state[row][tile] = 0;
+
+            didTilesMove = true;
+          }
+        }
+      }
+    }
+
+    return didTilesMove;
+  }
+
+  moveTilesLeft() {
+    let didTilesMove = false;
+
+    for (let row = 0; row < 4; row++) {
+      for (let tile = 1; tile < 4; tile++) {
+        // Check whether the tile we want to move isn't empty
+        if (this.state[row][tile] > 0) {
+          let moveTileBy = 0;
+          let merge = false;
+
+          // Check how far can we move the tile
+          while (this.state[row][tile - moveTileBy - 1] === 0) {
+            moveTileBy++;
+          }
+
+          // Check whether if we go one tile further we can merge
+          if (
+            this.state[row][tile - moveTileBy - 1] === this.state[row][tile]
+          ) {
+            moveTileBy++;
+            merge = true;
+          }
+
+          if (moveTileBy > 0) {
+            if (merge) {
+              const newNumber = this.state[row][tile - moveTileBy] * 2;
+
+              // Merge current tile to the left
+              this.state[row][tile - moveTileBy] = newNumber;
+
+              this.score += newNumber;
+            } else {
+              // Move the tile to the [CHECK DIRECTION] left
+              this.state[row][tile - moveTileBy] = this.state[row][tile];
+            }
+            // Reset the tile we just moved
+            this.state[row][tile] = 0;
+
+            didTilesMove = true;
+          }
+        }
+      }
+    }
+
+    return didTilesMove;
+  }
+
+  moveTilesUp() {
+    let didTilesMove = false;
+
+    // Iterate through every column
+    for (let column = 0; column < 4; column++) {
+      // Iterate through the current column
+      for (let tile = 1; tile < 4; tile++) {
+        // Iterate through each tile in the current column
+
+        // Check whether the tile we want to move isn't empty
+        if (this.state[tile][column] > 0) {
+          let moveTileBy = 0;
+          let merge = false;
+
+          // Check how far can we move the tile
+          while (
+            tile - moveTileBy - 1 >= 0 &&
+            this.state[tile - moveTileBy - 1][column] === 0
+          ) {
+            moveTileBy++;
+          }
+
+          // Check whether if we go one tile further we can merge
+          if (
+            tile - moveTileBy - 1 >= 0 &&
+            this.state[tile - moveTileBy - 1][column] ===
+              this.state[tile][column]
+          ) {
+            moveTileBy++;
+            merge = true;
+          }
+
+          if (moveTileBy > 0) {
+            if (merge) {
+              const newNumber = this.state[tile - moveTileBy][column] * 2;
+
+              // Merge current tile to the right
+              this.state[tile - moveTileBy][column] = newNumber;
+
+              this.score += newNumber;
+            } else {
+              // Move the tile [CHECK DIRECTION] down
+              this.state[tile - moveTileBy][column] = this.state[tile][column];
+            }
+            // Reset the tile we just moved
+            this.state[tile][column] = 0;
+
+            didTilesMove = true;
+          }
+        }
+      }
+    }
+
+    return didTilesMove;
+  }
+
+  moveTilesDown() {
+    let didTilesMove = false;
+
+    // Iterate through every column
+    for (let column = 0; column < 4; column++) {
+      // Iterate through the current column
+      for (let tile = 2; tile >= 0; tile--) {
+        // Iterate through each tile in the current column
+
+        // Check whether the tile we want to move isn't empty
+        if (this.state[tile][column] > 0) {
+          let moveTileBy = 0;
+          let merge = false;
+
+          // Check how far can we move the tile
+          while (
+            tile + moveTileBy + 1 < this.state.length &&
+            this.state[tile + moveTileBy + 1][column] === 0
+          ) {
+            moveTileBy++;
+          }
+
+          // Check whether if we go one tile further we can merge
+          if (
+            tile + moveTileBy + 1 < this.state.length &&
+            this.state[tile + moveTileBy + 1][column] ===
+              this.state[tile][column]
+          ) {
+            moveTileBy++;
+            merge = true;
+          }
+
+          if (moveTileBy > 0) {
+            if (merge) {
+              const newNumber = this.state[tile + moveTileBy][column] * 2;
+
+              // Merge current tile to the right
+              this.state[tile + moveTileBy][column] = newNumber;
+
+              this.score += newNumber;
+            } else {
+              // Move the tile [CHECK DIRECTION] down
+              this.state[tile + moveTileBy][column] = this.state[tile][column];
+            }
+            // Reset the tile we just moved
+            this.state[tile][column] = 0;
+
+            didTilesMove = true;
+          }
+        }
+      }
+    }
+
+    return didTilesMove;
   }
 
   // Add your own methods here
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 // Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
 const game = new Game();
+const button = document.querySelector('.start');
+const score = document.querySelector('.game-score');
 
 // Write your code here
 function initialize() {
-  const button = document.querySelector('.start');
-
   button.addEventListener('click', () => {
     if (button.classList.contains('start')) {
       // Change the button to Reset
@@ -40,165 +40,39 @@ function handleKeyDown(keyEvent) {
 
   switch (keyEvent.key) {
     case 'ArrowUp':
-      if (moveTilesUp(game) === true) {
+      if (game.moveTilesUp(game) === true) {
         game.createRandomTile();
         game.printTiles();
+        updateScore(game.getScore());
       }
       break;
 
     case 'ArrowDown':
-      if (moveTilesDown(game) === true) {
+      if (game.moveTilesDown(game) === true) {
         game.createRandomTile();
         game.printTiles();
+        updateScore(game.getScore());
       }
       break;
 
     case 'ArrowRight':
-      if (moveTilesRight(game) === true) {
+      if (game.moveTilesRight(game) === true) {
         game.createRandomTile();
         game.printTiles();
+        updateScore(game.getScore());
       }
       break;
 
     case 'ArrowLeft':
-      if (moveTilesLeft(game) === true) {
+      if (game.moveTilesLeft(game) === true) {
         game.createRandomTile();
         game.printTiles();
+        updateScore(game.getScore());
       }
       break;
   }
 }
 
-function moveTilesRight(gameInstance) {
-  let didTilesMove = false;
-
-  for (let row = 0; row < 4; row++) {
-    for (let tile = 2; tile >= 0; tile--) {
-      // Check whether the tile we want to move isn't empty
-      if (gameInstance.state[row][tile] > 0) {
-        let moveTileBy = 0;
-
-        // Check how far can we move the tile
-        while (gameInstance.state[row][tile + moveTileBy + 1] === 0) {
-          moveTileBy++;
-        }
-
-        if (moveTileBy > 0) {
-          // Move the tile to the right
-          gameInstance.state[row][tile + moveTileBy] =
-            gameInstance.state[row][tile];
-          // Reset the tile we just moved
-          gameInstance.state[row][tile] = 0;
-
-          didTilesMove = true;
-        }
-      }
-    }
-  }
-
-  return didTilesMove;
-}
-
-function moveTilesLeft(gameInstance) {
-  let didTilesMove = false;
-
-  for (let row = 0; row < 4; row++) {
-    for (let tile = 1; tile < 4; tile++) {
-      // Check whether the tile we want to move isn't empty
-      if (gameInstance.state[row][tile] > 0) {
-        let moveTileBy = 0;
-
-        // Check how far can we move the tile
-        while (gameInstance.state[row][tile - moveTileBy - 1] === 0) {
-          moveTileBy++;
-        }
-
-        if (moveTileBy > 0) {
-          // Move the tile to the [CHECK DIRECTION] left
-          gameInstance.state[row][tile - moveTileBy] =
-            gameInstance.state[row][tile];
-          // Reset the tile we just moved
-          gameInstance.state[row][tile] = 0;
-
-          didTilesMove = true;
-        }
-      }
-    }
-  }
-
-  return didTilesMove;
-}
-
-function moveTilesDown(gameInstance) {
-  let didTilesMove = false;
-
-  // Iterate through every column
-  for (let column = 0; column < 4; column++) {
-    // Iterate through the current column
-    for (let tile = 2; tile >= 0; tile--) {
-      // Iterate through each tile in the current column
-
-      // Check whether the tile we want to move isn't empty
-      if (gameInstance.state[tile][column] > 0) {
-        let moveTileBy = 0;
-
-        // Check how far can we move the tile
-        while (
-          tile + moveTileBy + 1 < gameInstance.state.length &&
-          gameInstance.state[tile + moveTileBy + 1][column] === 0
-        ) {
-          moveTileBy++;
-        }
-
-        if (moveTileBy > 0) {
-          // Move the tile [CHECK DIRECTION] down
-          gameInstance.state[tile + moveTileBy][column] =
-            gameInstance.state[tile][column];
-          // Reset the tile we just moved
-          gameInstance.state[tile][column] = 0;
-
-          didTilesMove = true;
-        }
-      }
-    }
-  }
-
-  return didTilesMove;
-}
-
-function moveTilesUp(gameInstance) {
-  let didTilesMove = false;
-
-  // Iterate through every column
-  for (let column = 0; column < 4; column++) {
-    // Iterate through the current column
-    for (let tile = 1; tile < 4; tile++) {
-      // Iterate through each tile in the current column
-
-      // Check whether the tile we want to move isn't empty
-      if (gameInstance.state[tile][column] > 0) {
-        let moveTileBy = 0;
-
-        // Check how far can we move the tile
-        while (
-          tile - moveTileBy - 1 >= 0 &&
-          gameInstance.state[tile - moveTileBy - 1][column] === 0
-        ) {
-          moveTileBy++;
-        }
-
-        if (moveTileBy > 0) {
-          // Move the tile [CHECK DIRECTION] down
-          gameInstance.state[tile - moveTileBy][column] =
-            gameInstance.state[tile][column];
-          // Reset the tile we just moved
-          gameInstance.state[tile][column] = 0;
-
-          didTilesMove = true;
-        }
-      }
-    }
-  }
-
-  return didTilesMove;
+function updateScore(newScore) {
+  score.textContent = newScore;
 }
