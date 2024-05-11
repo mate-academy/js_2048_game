db2c41bee83eefd9982bbf27f43bcb029d1d574f
Max Petrashishin <rikogjdefolt@gmail.com>
2024-05-11 21:38:55 +0300

    solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,6 +5,28 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
+
+const randomizer = (state) => {
+  const stateRow = Math.floor(Math.random() * 4);
+  const stateColumn = Math.floor(Math.random() * 4);
+
+  if (state[stateRow][stateColumn] === 0) {
+    return [stateRow, stateColumn];
+  } else {
+    return randomizer(state);
+  }
+};
+
+const randomNumber = () => {
+  const random = Math.floor(Math.random() * 10) + 1;
+
+  if (random <= 1) {
+    return 4;
+  } else {
+    return 2;
+  }
+};
+
 class Game {
   /**
    * Creates a new game instance.
@@ -20,25 +42,241 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialState = initialState;
+    this.score = 0;
+    this.status = 'none';
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  async gameAdderItem() {
+    const startGameAdder = async () => {
+      const [stateRow, stateColumn] = randomizer(this.initialState);
+
+      this.initialState[stateRow][stateColumn] = randomNumber();
+    };
+
+    const isItemsZero = this.initialState.reduce((prevItem, currentItem) => {
+      return (
+        prevItem +
+        currentItem.reduce((prevItem1, currentItem1) => {
+          return prevItem1 + (currentItem1 === 0 ? 1 : 0);
+        }, 0)
+      );
+    }, 0);
+
+    if (isItemsZero >= 2) {
+      await startGameAdder();
+      await startGameAdder();
+    } else if (isItemsZero === 1) {
+      await startGameAdder();
+    }
+  }
+
+  async moveLeft() {
+    const goToLeft = async () => {
+      this.initialState = this.initialState.map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        const result = [...numberItem, ...zeroItem];
+
+        for (let i = 0; i < 3; i++) {
+          if (result[i] === result[i + 1] && result[i] !== 0) {
+            result[i] = result[i] + result[i + 1];
+            this.score += result[i];
+            result[i + 1] = 0;
+            i = i + 1;
+          }
+        }
+
+        return result;
+      });
+    };
+
+    const goToLefttLast = async () => {
+      this.initialState = this.initialState.map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        return [...numberItem, ...zeroItem];
+      });
+    };
+
+    await goToLeft();
+    await goToLefttLast();
+    await this.gameAdderItem();
+  }
+  async moveRight() {
+    const goToRight = async () => {
+      this.initialState = this.initialState.map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        const result = [...zeroItem, ...numberItem];
+
+        for (let i = 3; i > 0; i--) {
+          if (result[i] === result[i - 1] && result[i] !== 0) {
+            result[i] = result[i] + result[i - 1];
+            this.score += result[i];
+            result[i - 1] = 0;
+            i = i - 1;
+          }
+        }
+
+        return result;
+      });
+    };
+
+    const goToRightLast = async () => {
+      this.initialState = this.initialState.map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        return [...zeroItem, ...numberItem];
+      });
+    };
+
+    await goToRight();
+    await goToRightLast();
+    await this.gameAdderItem();
+  }
+  async moveUp() {
+    const goToTop = async () => {
+      const columnsItems = [
+        this.initialState.map((item) => item[0]),
+        this.initialState.map((item) => item[1]),
+        this.initialState.map((item) => item[2]),
+        this.initialState.map((item) => item[3]),
+      ].map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        const result = [...numberItem, ...zeroItem];
+
+        for (let i = 0; i < 3; i++) {
+          if (result[i] === result[i + 1] && result[i] !== 0) {
+            result[i] = result[i] + result[i + 1];
+            this.score += result[i];
+            result[i + 1] = 0;
+            i = i + 1;
+          }
+        }
+
+        return result;
+      });
+
+      this.initialState = [
+        columnsItems.map((item) => item[0]),
+        columnsItems.map((item) => item[1]),
+        columnsItems.map((item) => item[2]),
+        columnsItems.map((item) => item[3]),
+      ];
+    };
+
+    const goToTopLast = async () => {
+      const columnsItems = [
+        this.initialState.map((item) => item[0]),
+        this.initialState.map((item) => item[1]),
+        this.initialState.map((item) => item[2]),
+        this.initialState.map((item) => item[3]),
+      ].map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        return [...numberItem, ...zeroItem];
+      });
+
+      this.initialState = [
+        columnsItems.map((item) => item[0]),
+        columnsItems.map((item) => item[1]),
+        columnsItems.map((item) => item[2]),
+        columnsItems.map((item) => item[3]),
+      ];
+    };
+
+    await goToTop();
+    await goToTopLast();
+    await this.gameAdderItem();
+  }
+  async moveDown() {
+    const goToDown = async () => {
+      const columnsItems = [
+        this.initialState.map((item) => item[0]),
+        this.initialState.map((item) => item[1]),
+        this.initialState.map((item) => item[2]),
+        this.initialState.map((item) => item[3]),
+      ].map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        const result = [...zeroItem, ...numberItem];
+
+        for (let i = 3; i > 0; i--) {
+          if (result[i] === result[i - 1] && result[i] !== 0) {
+            result[i] = result[i] + result[i - 1];
+            this.score += result[i];
+            result[i - 1] = 0;
+            i = i - 1;
+          }
+        }
+
+        return result;
+      });
+
+      this.initialState = [
+        columnsItems.map((item) => item[0]),
+        columnsItems.map((item) => item[1]),
+        columnsItems.map((item) => item[2]),
+        columnsItems.map((item) => item[3]),
+      ];
+    };
+
+    const goToDownLast = async () => {
+      const columnsItems = [
+        this.initialState.map((item) => item[0]),
+        this.initialState.map((item) => item[1]),
+        this.initialState.map((item) => item[2]),
+        this.initialState.map((item) => item[3]),
+      ].map((item, index) => {
+        const zeroItem = item.filter((num) => num === 0);
+        const numberItem = item.filter((num) => num !== 0);
+
+        return [...zeroItem, ...numberItem];
+      });
+
+      this.initialState = [
+        columnsItems.map((item) => item[0]),
+        columnsItems.map((item) => item[1]),
+        columnsItems.map((item) => item[2]),
+        columnsItems.map((item) => item[3]),
+      ];
+    };
+
+    await goToDown();
+    await goToDownLast();
+    await this.gameAdderItem();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.initialState;
+  }
 
   /**
    * Returns the current game status.
@@ -50,17 +288,99 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  setStatus() {
+    if (this.initialState.some((items) => items.includes(2048))) {
+      if (this.status !== 'win') {
+        this.status = 'win';
+      }
+
+      return this.status;
+    }
+
+    let isGamingTime = false;
+
+    this.initialState.forEach((item, index) => {
+      const zeroItem = item.filter((num) => num === 0);
+      const numberItem = item.filter((num) => num !== 0);
+
+      const result = [...numberItem, ...zeroItem];
+
+      for (let i = 0; i < 3; i++) {
+        if (result[i] === result[i + 1] && result[i] !== 0) {
+          isGamingTime = true;
+        }
+      }
+
+      for (let i = 3; i > 0; i--) {
+        if (result[i] === result[i - 1] && result[i] !== 0) {
+          isGamingTime = true;
+        }
+      }
+    });
+
+    [
+      this.initialState.map((item) => item[0]),
+      this.initialState.map((item) => item[1]),
+      this.initialState.map((item) => item[2]),
+      this.initialState.map((item) => item[3]),
+    ].forEach((item, index) => {
+      const zeroItem = item.filter((num) => num === 0);
+      const numberItem = item.filter((num) => num !== 0);
+
+      const result = [...numberItem, ...zeroItem];
+
+      for (let i = 0; i < 3; i++) {
+        if (result[i] === result[i + 1] && result[i] !== 0) {
+          isGamingTime = true;
+        }
+      }
+
+      for (let i = 3; i > 0; i--) {
+        if (result[i] === result[i - 1] && result[i] !== 0) {
+          isGamingTime = true;
+        }
+      }
+    });
+
+    this.initialState.forEach((item) => {
+      if (item.includes(0)) {
+        isGamingTime = true;
+      }
+    });
+
+    if (isGamingTime) {
+      return this.status;
+    }
+
+    this.status = 'lose';
+
+    return this.status;
+  }
+
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  async start() {
+    await this.gameAdderItem();
+    this.status = 'start';
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  async restart() {
+    this.initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.status = 'none';
+  }
 
   // Add your own methods here
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,171 @@
 
 // Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
-const game = new Game();
+const game = new Game([
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+]);
+
+const rows = Array.from(document.querySelectorAll('.field-row')).map((row) => {
+  return Array.from(row.querySelectorAll('.field-cell'));
+});
+
+const score = document.querySelector('.game-score');
+const isLoser = document.querySelector('.message-lose');
+const isWiner = document.querySelector('.message-win');
+const isStarter = document.querySelector('.message-start');
+
+const setAllGameBoard = async () => {
+  const itemsFromGame = game.getState();
+  const statusGame = game.setStatus();
+
+  score.textContent = game.getScore();
+
+  rows.map((row, indexRow) => {
+    return row.map((itemField, intexItemField) => {
+      itemField.textContent = itemsFromGame[indexRow][intexItemField]
+        ? itemsFromGame[indexRow][intexItemField]
+        : '';
+
+      itemField.className = itemsFromGame[indexRow][intexItemField]
+        ? `field-cell field-cell--${itemsFromGame[indexRow][intexItemField]}`
+        : 'field-cell';
+    });
+  });
+
+  switch (statusGame) {
+    case 'none': {
+      if (isLoser.className !== 'message message-lose hidden') {
+        isLoser.className = 'message message-lose hidden';
+      }
+
+      if (isWiner.className !== 'message message-win hidden') {
+        isWiner.className = 'message message-win hidden';
+      }
+
+      if (isStarter.className !== 'message message-start') {
+        isStarter.className = 'message message-start';
+      }
+
+      break;
+    }
+
+    case 'start': {
+      if (isLoser.className !== 'message message-lose hidden') {
+        isLoser.className = 'message message-lose hidden';
+      }
+
+      if (isWiner.className !== 'message message-win hidden') {
+        isWiner.className = 'message message-win hidden';
+      }
+
+      if (isStarter.className !== 'message message-start hidden') {
+        isStarter.className = 'message message-start hidden';
+      }
+
+      break;
+    }
+
+    case 'lose': {
+      if (isLoser.className !== 'message message-lose') {
+        isLoser.className = 'message message-lose';
+      }
+
+      if (isWiner.className !== 'message message-win hidden') {
+        isWiner.className = 'message message-win hidden';
+      }
+
+      if (isStarter.className !== 'message message-start hidden') {
+        isStarter.className = 'message message-start hidden';
+      }
+
+      break;
+    }
+
+    case 'win': {
+      if (isLoser.className !== 'message message-lose hidden') {
+        isLoser.className = 'message message-lose hidden';
+      }
+
+      if (isWiner.className !== 'message message-win') {
+        isWiner.className = 'message message-win';
+      }
+
+      if (isStarter.className !== 'message message-start hidden') {
+        isStarter.className = 'message message-start hidden';
+      }
+
+      break;
+    }
+
+    default:
+      break;
+  }
+};
 
 const start = document.querySelector('.button');
 
-start.addEventListener('click', () => {
+start.addEventListener('click', async () => {
   if (start.classList.contains('start')) {
     start.classList.remove('start');
     start.classList.add('restart');
     start.textContent = 'Restart';
+    await game.start();
+    setAllGameBoard();
   } else {
     start.classList.add('start');
     start.classList.remove('restart');
     start.textContent = 'Start';
+    await game.restart();
+    setAllGameBoard();
   }
 });
 
-const rows = Array.from(document.querySelectorAll('.field-row')).map((row) => {
-  return Array.from(row.querySelectorAll('.field-cell'));
+let isUseEventListener = true;
+
+document.documentElement.addEventListener('keydown', async function (evenet) {
+  if (game.getStatus() === 'lose') {
+    return;
+  }
+
+  if (isUseEventListener) {
+    switch (evenet.key) {
+      case 'ArrowRight': {
+        isUseEventListener = false;
+        await game.moveRight();
+        await setAllGameBoard();
+        isUseEventListener = true;
+        break;
+      }
+
+      case 'ArrowLeft': {
+        isUseEventListener = false;
+        await game.moveLeft();
+        await setAllGameBoard();
+        isUseEventListener = true;
+        break;
+      }
+
+      case 'ArrowUp': {
+        isUseEventListener = false;
+        await game.moveUp();
+        await setAllGameBoard();
+        isUseEventListener = true;
+        break;
+      }
+
+      case 'ArrowDown': {
+        isUseEventListener = false;
+        await game.moveDown();
+        await setAllGameBoard();
+        isUseEventListener = true;
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
 });
