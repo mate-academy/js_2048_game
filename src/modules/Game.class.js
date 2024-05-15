4a0274eae002e779506a098a1e2d40829ff32527
Ihor Prodan <prodanigor0@gmail.com>
2024-05-15 23:02:33 +0200

    2048

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,68 @@
-'use strict';
+// 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
-class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
-  }
+// /**
+//  * This class represents the game.
+//  * Now it has a basic structure, that is needed for testing.
+//  * Feel free to add more props and methods if needed.
+//  */
+// class Game {
+//   /**
+//    * Creates a new game instance.
+//    *
+//    * @param {number[][]} initialState
+//    * The initial state of the board.
+//    * @default
+//    * [[0, 0, 0, 0],
+//    *  [0, 0, 0, 0],
+//    *  [0, 0, 0, 0],
+//    *  [0, 0, 0, 0]]
+//    *
+//    * If passed, the board will be initialized with the provided
+//    * initial state.
+//    */
+//   constructor(initialState) {
+//     // eslint-disable-next-line no-console
+//     console.log(initialState);
+//   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+//   moveLeft() {}
+//   moveRight() {}
+//   moveUp() {}
+//   moveDown() {}
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+//   /**
+//    * @returns {number}
+//    */
+//   getScore() {}
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+//   /**
+//    * @returns {number[][]}
+//    */
+//   getState() {}
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+//   /**
+//    * Returns the current game status.
+//    *
+//    * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
+//    *
+//    * `idle` - the game has not started yet (the initial state);
+//    * `playing` - the game is in progress;
+//    * `win` - the game is won;
+//    * `lose` - the game is lost
+//    */
+//   getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+//   /**
+//    * Starts the game.
+//    */
+//   start() {}
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+//   /**
+//    * Resets the game.
+//    */
+//   restart() {}
 
-  // Add your own methods here
-}
+//   // Add your own methods here
+// }
 
-module.exports = Game;
+// module.exports = Game;
