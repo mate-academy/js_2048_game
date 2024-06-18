e165eb4f30f4d2274dfa4a4b679b65ff4dd55308
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-06-18 16:21:24 +0300

    problem with moves

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -6,39 +6,46 @@
  * Feel free to add more props and methods if needed.
  */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  #DEFAULT_STATE = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  #copyState(state) {
+    return state.map((arr) => [...arr]);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  constructor(initialState = this.#copyState(this.#DEFAULT_STATE)) {
+    this.matrix = initialState;
+  }
+
+  moveLeft() {
+    this.createRandomTile();
+  }
+  moveRight() {
+    this.createRandomTile();
+  }
+  moveUp() {
+    this.slideTilesUp(this.getMatrixGroupedByCols());
+
+    this.createRandomTile();
+    console.log(this.matrix);
+  }
+  moveDown() {
+    this.createRandomTile();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() { }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() { }
 
   /**
    * Returns the current game status.
@@ -50,19 +57,93 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() { }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.createRandomTile();
+    this.createRandomTile();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.matrix = this.#copyState(this.#DEFAULT_STATE);
+
+    this.clearMatrix();
+  }
 
   // Add your own methods here
+
+  createRandomTile() {
+    const randomDigit = Math.random() < 0.9 ? 2 : 4;
+    const getEmptyTiles = [];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.matrix[i][j] === 0) {
+          getEmptyTiles.push([i, j]);
+        }
+      }
+    }
+
+    const randomEmptyTile = Math.floor(Math.random() * getEmptyTiles.length);
+    const coordinateEmptyTile = getEmptyTiles[randomEmptyTile];
+
+    this.matrix[coordinateEmptyTile[0]][coordinateEmptyTile[1]] = randomDigit;
+
+    // edit HTML element
+    const getRowWithEmptyTile = document.querySelector(
+      `.field-row:nth-child(${coordinateEmptyTile[0] + 1})`,
+    );
+    const emptyTile = getRowWithEmptyTile.querySelector(
+      `.field-cell:nth-child(${coordinateEmptyTile[1] + 1})`,
+    );
+
+    emptyTile.textContent = randomDigit;
+    emptyTile.classList.add(`field-cell--${randomDigit}`);
+  }
+
+  getMatrixGroupedByCols() {
+    const groupedMatrix = this.#copyState(this.#DEFAULT_STATE);
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        groupedMatrix[j][i] = this.matrix[i][j];
+      }
+    }
+
+    return groupedMatrix;
+  }
+
+  clearMatrix() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        const getRow = document.querySelector(`.field-row:nth-child(${i + 1})`);
+        const tile = getRow.querySelector(`.field-cell:nth-child(${j + 1})`);
+
+        tile.textContent = '';
+        tile.className = 'field-cell';
+      }
+    }
+  }
+
+  slideTilesUp(groupedMatrix) {
+    groupedMatrix.map((group) => this.slideUp(group));
+  }
+
+  slideUp(group) {
+    for (let i = 1; i < 4; i++) {
+      while (group[i - 1] === 0 && group[i] !== 0) {
+        group[i - 1] = group[i];
+        group[i] = 0;
+        i--;
+      }
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
 // Write your code here
+const startBtn = document.querySelector('.start');
+const restartBtn = document.querySelector('.restart');
+const arrowsToggle = (event) => {
+  if (event.key === 'ArrowUp') {
+    game.moveUp();
+  } else if (event.key === 'ArrowLeft') {
+    game.moveLeft();
+  } else if (event.key === 'ArrowRight') {
+    game.moveRight();
+  } else if (event.key === 'ArrowDown') {
+    game.moveDown();
+  }
+};;
+
+startBtn.addEventListener('click', () => {
+  startBtn.classList.add('hidden');
+  restartBtn.classList.remove('hidden');
+
+  game.start();
+
+  document.addEventListener('keydown', arrowsToggle);
+});
+
+restartBtn.addEventListener('click', () => {
+  restartBtn.classList.add('hidden');
+  startBtn.classList.remove('hidden');
+
+  game.restart();
+
+  document.removeEventListener('keydown', arrowsToggle);
+});
