b49e5724141dcafb63f99419f8eb1200bddf4e94
Misha Grinko <glukym@gmail.com>
2024-07-20 17:04:15 +0300

    add latest deps

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+    specPattern: 'cypress/integration/**/*.spec.{js,ts,jsx,tsx}',
+  },
+  video: true,
+  viewportHeight: 1920,
+  viewportWidth: 1080,
+  screenshotOnRunFailure: true,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'raw_reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+});
