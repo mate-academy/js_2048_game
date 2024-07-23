dc1443e56e302a627f7cda5e5daca01a8e2bbdb4
Galers <galosaandrew@gmail.com>
2024-07-23 18:24:12 +0300

    Add move up, merge, getScore

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,44 +1,103 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
-class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+const GRID_SIZE = 4;
+
+export default class Game {
+  constructor() {
+    this.board = this.getState();
+
+    this.score = 0;
+    this.gameActive = false;
+    this.cells = document.querySelectorAll('.field-cell');
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveUp() {
+    if (!this.gameActive) {
+      return;
+    }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+    for (let col = 0; col < GRID_SIZE; col++) {
+      this.moveColumnUp(col);
+    }
+    this.getRandomTile();
+    this.addCell();
+  }
+  moveDown() {
+    if (!this.gameActive) {
+      return;
+    }
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    for (let col = 0; col < GRID_SIZE; col++) {
+      this.moveColumnDown(col);
+    }
+    this.getRandomTile();
+    this.addCell();
+  }
+  moveLeft() {
+    if (!this.gameActive) {
+      return;
+    }
+
+    console.log('moveLeft');
+  }
+  moveRight() {
+    if (!this.gameActive) {
+      return;
+    }
+
+    console.log('moveRight');
+  }
+
+  getScore() {
+    return this.board.flat().reduce((acc, value) => acc + value, 0);
+  }
+
+  getState() {
+    return [...Array(GRID_SIZE)].map(() => Array(GRID_SIZE).fill(0));
+  }
+
+  getRandomTile() {
+    const emptyTiles = [];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.board[i][j] === 0) {
+          emptyTiles.push({ i, j });
+        }
+      }
+    }
+
+    if (emptyTiles.length) {
+      const { i, j } =
+        emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+
+      this.board[i][j] = Math.random() < 0.9 ? 2 : 4;
+
+      return { i, j, value: this.board[i][j] };
+    }
+
+    return null;
+  }
+
+  addCell() {
+    this.cells.forEach((cell, index) => {
+      const row = Math.floor(index / GRID_SIZE);
+      const col = index % GRID_SIZE;
+      const value = this.board[row][col];
+      const hasValue = value ? ` field-cell--${value}` : '';
+
+      cell.className = 'field-cell' + hasValue;
+      cell.textContent = value || '';
+    });
+  }
+
+  deleteCell() {
+    this.cells.forEach((cell) => {
+      cell.className = 'field-cell';
+      cell.textContent = '';
+    });
+  }
 
   /**
    * Returns the current game status.
@@ -52,17 +111,70 @@ class Game {
    */
   getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start(element) {
+    this.gameActive = true;
+    this.score = 0;
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+    element.classList.replace('start', 'restart');
+    element.textContent = 'Restart';
 
-  // Add your own methods here
+    this.getRandomTile();
+    this.getRandomTile();
+    this.addCell();
+  }
+
+  restart(element) {
+    this.gameActive = false;
+    this.score = 0;
+
+    element.classList.replace('restart', 'start');
+    element.textContent = 'Start';
+
+    this.board = this.getState();
+    this.deleteCell();
+  }
+
+  moveColumnUp(col) {
+    const compressedColumn = this.board
+      .map((row) => row[col])
+      .filter((value) => value !== 0);
+
+    for (let i = 0; i < compressedColumn.length - 1; i++) {
+      if (compressedColumn[i] === compressedColumn[i + 1]) {
+        compressedColumn[i] *= 2;
+        compressedColumn.splice(i + 1, 1);
+        this.score += compressedColumn[i];
+      }
+    }
+
+    while (compressedColumn.length < 4) {
+      compressedColumn.push(0);
+    }
+
+    for (let row = 0; row < 4; row++) {
+      this.board[row][col] = compressedColumn[row];
+    }
+  }
+
+  moveColumnDown(col) {
+    const compressedColumn = this.board
+      .map((row) => row[col])
+      .filter((value) => value !== 0);
+
+    for (let i = 0; i < compressedColumn.length - 1; i++) {
+      if (compressedColumn[i] === compressedColumn[i + 1]) {
+        compressedColumn[i] *= 2;
+        compressedColumn.splice(i + 1, 1);
+        this.score += compressedColumn[i];
+      }
+    }
+
+    while (compressedColumn.length < 4) {
+      compressedColumn.push(0);
+    }
+
+    for (let row = 0; row < 4; row++) {
+      this.board[row][col] = compressedColumn[row];
+    }
+  }
 }
-
-module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,68 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class.js';
 
-// Write your code here
+// #region constants
+
+const game = new Game();
+const gameStart = document.querySelector('.button');
+// const gameField = document.querySelector('.game-field');
+const score = document.querySelector('.game-score');
+
+// const messageWin = document.querySelector('.message-win');
+// const messageLose = document.querySelector('.message-lose');
+
+// #endregion
+
+// #region func
+
+const chekStatus = () => {
+  if (gameStart.classList.contains('start')) {
+    game.start(gameStart);
+  } else {
+    game.restart(gameStart);
+  }
+
+  setUpScore();
+  setUpInputOnce();
+};
+
+const setUpInputOnce = () =>
+  window.addEventListener('keydown', handleInput, { once: true });
+
+const setUpScore = () => {
+  score.textContent = game.getScore();
+};
+
+const handleInput = (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    default:
+      setUpInputOnce();
+
+      return;
+  }
+  setUpScore();
+  setUpInputOnce();
+};
+
+setUpInputOnce();
+
+// #endregion
+
+// #region addEventListeners
+
+gameStart.addEventListener('click', chekStatus);
+// #endregion
