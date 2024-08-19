9ce3e6bbd27244cfda78e9f9096050917cbf391d
Nikita Kalina <kalinanikita001@gmail.com>
2024-08-19 21:03:35 +0300

    test

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,13 +1,8 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
   /**
-   * Creates a new game instance.
    *
    * @param {number[][]} initialState
    * The initial state of the board.
@@ -16,13 +11,47 @@ class Game {
    *  [0, 0, 0, 0],
    *  [0, 0, 0, 0],
    *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialState = initialState;
+    this.emptyfields = [];
+    this.startValue = 2;
+    this.status = 'idle';
+    this.score = 0;
+    this.rows = document.querySelectorAll('.field-row');
+    this.setEmptyfields();
+  }
+
+  setEmptyfields() {
+    for (let row = 0; row < this.initialState.length; row++) {
+      for (let col = 0; col < this.initialState[row].length; col++) {
+        if (this.initialState[row][col] === 0) {
+          this.emptyfields.push({ row, col });
+        }
+      }
+    }
+  }
+
+  setRandomField() {
+    if (this.emptyfields.length === 0) {
+      return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * this.emptyfields.length);
+    const field = this.emptyfields[randomIndex];
+
+    this.initialState[field.row][field.col] = this.startValue;
+
+    this.emptyfields.splice(randomIndex, 1);
+
+    return field;
   }
 
   moveLeft() {}
@@ -52,14 +81,46 @@ class Game {
    */
   getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    if (this.status === 'idle') {
+      this.status = 'playing';
 
-  /**
-   * Resets the game.
-   */
+      const tableArray = Array.from(this.rows).map((row) =>
+        Array.from(row.children).map((cell) => ({
+          content: cell.textContent.trim(),
+          classes: Array.from(cell.classList),
+        })));
+
+      this.createPlate(tableArray);
+      this.createPlate(tableArray);
+    }
+  }
+
+  createPlate(tableArray) {
+    const newPlate = this.setRandomField();
+
+    if (newPlate !== null) {
+      tableArray[newPlate.row][newPlate.col].content = this.startValue;
+
+      tableArray[newPlate.row][newPlate.col].classes.push(
+        `field-cell--${tableArray[newPlate.row][newPlate.col].content}`,
+      );
+
+      this.updateTable(tableArray);
+    }
+  }
+
+  updateTable(tableArray) {
+    this.rows.forEach((row, rowIndex) => {
+      Array.from(row.children).forEach((cell, colIndex) => {
+        const cellData = tableArray[rowIndex][colIndex];
+
+        cell.textContent = cellData.content;
+        cell.className = 'field-cell';
+        cellData.classes.forEach((cls) => cell.classList.add(cls));
+      });
+    });
+  }
   restart() {}
 
   // Add your own methods here
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startButton = document.getElementsByClassName('button start')[0];
+
+startButton.addEventListener('click', (e) => {
+  game.start();
+});
+
+document.addEventListener('keydown', (e) => {
+  switch (e.code) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+  }
+});
