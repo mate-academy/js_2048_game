6dd4b3df2b62e241e00fca2949ad9bb2d048192e
Nikita Kalina <kalinanikita001@gmail.com>
2024-08-22 15:52:09 +0300

    test 3

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -22,121 +22,87 @@ class Game {
   ) {
     this.initialState = initialState;
     this.emptyfields = [];
-    this.startValue = 2;
     this.status = 'idle';
     this.score = 0;
     this.rows = document.querySelectorAll('.field-row');
     this.setEmptyfields();
   }
 
-  setEmptyfields() {
-    this.emptyfields = [];
-
-    for (let row = 0; row < this.initialState.length; row++) {
-      for (let col = 0; col < this.initialState[row].length; col++) {
-        if (this.initialState[row][col] === 0) {
-          this.emptyfields.push({ row, col });
-        }
-      }
-    }
+  stateChanged(prevState) {
+    return this.initialState.some((row, i) =>
+      row.some((cell, j) => cell !== prevState[i][j]));
   }
 
-  setRandomField( secondVal = 0) {
-    if (this.emptyfields.length === 0) {
-      return null;
-    }
-
-    const randomIndex = Math.floor(Math.random() * this.emptyfields.length);
-    const field = this.emptyfields[randomIndex];
-
-    this.initialState[field.row][field.col] = this.startValue + secondVal;
-
-    this.emptyfields.splice(randomIndex, 1);
-
-    return field;
-  }
-
-  createTeableArray() {
-    const tableArray = this.initialState.map((row) =>
-      row.map((cell) => ({
-        content: cell,
-        classes: cell > 0 ? [`field-cell--${cell}`] : [],
-      })));
-
-    return tableArray;
-  }
-
-  transpose(matrix) {
-    return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
-  }
   moveLeft() {
-    this.initialState.forEach((row, index) => {
-      const lal = this.shiftLeft(row);
+    const prevState = this.initialState.map(row => [...row]);
 
-      this.initialState[index] = lal;
+    this.initialState.forEach((row, index) => {
+      this.initialState[index] = this.merge(row);
     });
 
-    const tableArray = this.createTeableArray();
-
-    this.createPlate(tableArray);
-  }
-
-  shiftLeft(row) {
-    const filteredRow = row.filter(num => num !== 0);
-
-    while (filteredRow.length < row.length) {
-      filteredRow.push(0);
+    if (this.stateChanged(prevState)) {
+      this.createPlate();
     }
-
-    return filteredRow;
   }
+
   moveRight() {
+    const prevState = this.initialState.map(row => [...row]);
+
     this.initialState.forEach((row, index) => {
-      const lal = this.shiftLeft(row.reverse());
+      const lal = this.merge(row.reverse());
 
       this.initialState[index] = lal.reverse();
     });
 
-    const tableArray = this.createTeableArray();
+    if (this.stateChanged(prevState)) {
 
-    this.createPlate(tableArray);
+      this.createPlate();
+    }
   }
   moveUp() {
+    const prevState = this.initialState.map(row => [...row]);
+
     this.initialState = this.transpose(this.initialState);
 
     this.initialState.forEach((row, index) => {
-      const lal = this.shiftLeft(row);
+      const lal = this.merge(row);
 
       this.initialState[index] = lal;
     });
 
     this.initialState = this.transpose(this.initialState);
 
-    const tableArray = this.createTeableArray();
+    if (this.stateChanged(prevState)) {
 
-    this.createPlate(tableArray);
+      this.createPlate();
+    }
   }
 
   moveDown() {
+    const prevState = this.initialState.map(row => [...row]);
+
     this.initialState = this.transpose(this.initialState);
 
     this.initialState.forEach((row, index) => {
-      const lal = this.shiftLeft(row.reverse());
+      const lal = this.merge(row.reverse());
 
       this.initialState[index] = lal.reverse();
     });
 
     this.initialState = this.transpose(this.initialState);
 
-    const tableArray = this.createTeableArray();
+    if (this.stateChanged(prevState)) {
+      this.createPlate();
 
-    this.createPlate(tableArray);
+    }
   }
 
   /**
    * @returns {number}
    */
-  getScore() { }
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
@@ -159,24 +125,85 @@ class Game {
     if (this.status === 'idle') {
       this.status = 'playing';
 
-      const tableArray = this.createTeableArray();
-
-
-      this.createPlate(tableArray);
-      this.createPlate(tableArray, 2);
+      this.createPlate();
+      this.createPlate(2);
     }
   }
 
-  createPlate(tableArray, secondWalue = 0) {
+  setEmptyfields() {
+    this.emptyfields = [];
+
+    for (let row = 0; row < this.initialState.length; row++) {
+      for (let col = 0; col < this.initialState[row].length; col++) {
+        if (this.initialState[row][col] === 0) {
+          this.emptyfields.push({ row, col });
+        }
+      }
+    }
+  }
+
+  setRandomField() {
+    if (this.emptyfields.length === 0) {
+      return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * this.emptyfields.length);
+    const field = this.emptyfields[randomIndex];
+
+    this.initialState[field.row][field.col] = Math.random() < 0.1 ? 4 : 2;
+
+    this.emptyfields.splice(randomIndex, 1);
+
+    return field;
+  }
+
+  createTeableArray() {
+    const tableArray = this.initialState.map((row) =>
+      row.map((cell) => ({
+        content: cell,
+        classes: cell > 0 ? [`field-cell--${cell}`] : [],
+      })));
+
+    return tableArray;
+  }
+
+  transpose(matrix) {
+    return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+  }
+
+  merge(row) {
+    const rowForMerge = row.filter(x => x !== 0);
+
+    for (let i = 0; i < rowForMerge.length - 1; i++) {
+      if (rowForMerge[i] === rowForMerge[i + 1]) {
+        rowForMerge[i] *= 2;
+        rowForMerge[i + 1] = 0;
+        this.score += rowForMerge[i];
+        i++;
+      }
+    }
+
+    const mergedRow = rowForMerge.filter(x => x !== 0);
+
+    while (mergedRow.length < row.length) {
+      mergedRow.push(0);
+    }
+
+    return mergedRow;
+  }
+
+  createPlate() {
+
+    const tableArray = this.createTeableArray();
+
     this.updateTable(tableArray);
 
-    const newPlate = this.setRandomField(secondWalue);
+    const newPlate = this.setRandomField();
 
 
     if (newPlate !== null) {
       tableArray[newPlate.row][newPlate.col].content =
-        this.startValue + secondWalue;
-
+        this.initialState[newPlate.row][newPlate.col];
 
       tableArray[newPlate.row][newPlate.col].classes.push(
         `field-cell--${tableArray[newPlate.row][newPlate.col].content}`,
@@ -187,6 +214,7 @@ class Game {
   }
 
   updateTable(tableArray) {
+
     this.rows.forEach((row, rowIndex) => {
       Array.from(row.children).forEach((cell, colIndex) => {
         const cellData = tableArray[rowIndex][colIndex];
@@ -203,11 +231,27 @@ class Game {
 
       });
     });
+
     this.setEmptyfields();
 
   }
-  restart() { }
 
+  restart() {
+    this.initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.emptyfields = [];
+    this.status = 'idle';
+    this.score = 0;
+    this.setEmptyfields();
+
+    const tableArray = this.createTeableArray();
+
+    this.updateTable(tableArray);
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,21 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
 const game = new Game();
 
 const startButton = document.getElementsByClassName('button start')[0];
+const score = document.getElementsByClassName('game-score')[0];
+const controls = document.getElementsByClassName('controls')[0];
+const startMessage = document.getElementsByClassName(
+  'message message-start',
+)[0];
+const restart = document.createElement('button');
+
+restart.className = 'button restart';
+restart.style.display = 'none';
+restart.innerText = 'Restart';
+
+controls.appendChild(restart);
 
 startButton.addEventListener('click', (e) => {
   game.start();
@@ -13,16 +24,31 @@ startButton.addEventListener('click', (e) => {
     switch (y.code) {
       case 'ArrowLeft':
         game.moveLeft();
+        score.innerText = game.getScore();
         break;
       case 'ArrowRight':
         game.moveRight();
+        score.innerText = game.getScore();
         break;
       case 'ArrowUp':
         game.moveUp();
+        score.innerText = game.getScore();
         break;
       case 'ArrowDown':
         game.moveDown();
+        score.innerText = game.getScore();
         break;
     }
   });
+  startButton.style.display = 'none';
+  restart.style.display = '';
+  startMessage.className += ' hidden';
+});
+
+restart.addEventListener('click', () => {
+  game.restart();
+  startButton.style.display = '';
+  restart.style.display = 'none';
+  score.innerText = game.getScore();
+
 });
