66aded4553a23746891f0f280bfbc29ce15ad2f5
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-08-23 12:19:37 +0300

    create-project

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -6,63 +6,205 @@
  * Feel free to add more props and methods if needed.
  */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.board = initialState || this._generateEmptyBoard();
+    this.score = 0;
+    this.status = 'idle'; // 'idle', 'playing', 'win', 'lose'
   }
-
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
-
-  /**
-   * @returns {number}
-   */
-  getScore() {}
-
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
-
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
+  /*
    * `idle` - the game has not started yet (the initial state);
    * `playing` - the game is in progress;
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  // генерація первинної пустої дошки для гри
+  _generateEmptyBoard() {
+    return [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  // основний метод для переміщення
+  moveLeft() {
+    let moved = false;
+
+    for (let i = 0; i < 4; i++) {
+      const line = this.board[i];
+      const newLine = this._slideAndMerge(line);
+
+      // перевірка чи змінилась дошка
+      if (newLine.join('') !== line.join('')) {
+        moved = true;
+      }
+      this.board[i] = newLine;
+    }
+
+    // якщо була зміщена лінія, то додається
+    // рандомна плитка та перевіряється статус гри
+    if (moved) {
+      this._addRandomTile();
+      this._checkGameStatus();
+    }
+  }
+
+  moveRight() {
+    this.board = this.board.map((row) => row.reverse()); // обертаємо дошку
+    this.moveLeft();
+    this.board = this.board.map((row) => row.reverse());
+  }
+
+  moveUp() {
+    // eslint-disable-next-line max-len
+    this._transposeBoard(); // транспонуємо матрицю, щоб використати метод moveLeft
+    this.moveLeft();
+    this._transposeBoard();
+  }
+
+  moveDown() {
+    this._transposeBoard();
+    this.board = this.board.map((row) => row.reverse());
+    this.moveLeft();
+    this.board = this.board.map((row) => row.reverse());
+    this._transposeBoard();
+  }
+
+  // метод для зсуву і злиття комірок
+  _slideAndMerge(line) {
+    let newLine = line.filter((val) => val); // видаляємо нулі
+
+    for (let i = 0; i < newLine.length - 1; i++) {
+      if (newLine[i] === newLine[i + 1]) {
+        newLine[i] *= 2;
+        this.score += newLine[i]; // додаємо очки
+        newLine[i + 1] = 0;
+      }
+    }
+
+    newLine = newLine.filter((val) => val); // видаляємо злиті комірки
+
+    while (newLine.length < 4) {
+      newLine.push(0); // заповнємо нулями нулями
+    }
+
+    return newLine;
+  }
+
+  // транспонування
+  _transposeBoard() {
+    const newBoard = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        newBoard[i][j] = this.board[j][i];
+      }
+    }
+
+    this.board = newBoard;
+  }
+
+  // отримання кількості очок
+  getScore() {
+    return this.score;
+  }
+
+  // отримання дошки для гри
+  getState() {
+    return this.board;
+  }
+
+  // отримання статусу гру
+  getStatus() {
+    return this.status;
+  }
+
+  // початок гри
+  start() {
+    if (
+      this.status === 'idle' ||
+      this.status === 'lose' ||
+      this.status === 'win'
+    ) {
+      this._addRandomTile(); // добавляє одну дощечку на екран
+      this._addRandomTile();
+      this.status = 'playing';
+    }
+  }
+
+  // перезапуск гри
+  restart() {
+    this.board = this._generateEmptyBoard();
+    this.score = 0;
+    this.status = 'idle';
+    this.start();
+  }
 
   // Add your own methods here
+
+  // створення рандомної дощечки
+  _addRandomTile() {
+    const emptyCells = [];
+
+    // пошук усіх пустих дощечок
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (this.board[row][col] === 0) {
+          emptyCells.push({ row, col });
+        }
+      }
+    }
+
+    // добавлення рандомного числа 2/4 в одну з пустих комірок
+    if (emptyCells.length > 0) {
+      const randomCell =
+        emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+      this.board[randomCell.row][randomCell.col] = Math.random() < 0.9 ? 2 : 4;
+    }
+  }
+
+  // зміна статусу гри win/lose
+  _checkGameStatus() {
+    if (this.board.some((row) => row.includes(2048))) {
+      this.status = 'win';
+    }
+
+    if (this._noMoreMoves()) {
+      this.status = 'lose';
+    }
+  }
+
+  // перевірка чи є ще можливі кроки у грі. повертає boolean
+  _noMoreMoves() {
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        // перевіряє наявність порожніх клітинок
+        if (this.board[row][col] === 0) {
+          return false;
+        }
+
+        // перевіряє чи можливо об'єднати дві сусідні дощечки по колонках
+        if (col < 3 && this.board[row][col] === this.board[row][col + 1]) {
+          return false;
+        }
+
+        // перевіряє чи можливо об'єднати дві сусідні дощечки по рядках
+        if (row < 3 && this.board[row][col] === this.board[row + 1][col]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,78 @@
+/* eslint-disable max-len */
+/* eslint-disable no-shadow */
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
 // Write your code here
+
+document.addEventListener('keydown', (event) => {
+  if (game.status === 'playing') {
+    switch (event.key) {
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+    }
+  }
+
+  workingWithHTMLFile();
+});
+
+function workingWithHTMLFile() {
+  // заповнення комірок html
+  const board = game.getState();
+  const cells = document.querySelectorAll('.field-cell');
+
+  cells.forEach((cell, i) => {
+    const value = board[Math.floor(i / 4)][i % 4];
+
+    cell.textContent = value || '';
+    cell.className = `field-cell ${value ? `field-cell--${value}` : ''}`;
+  });
+  // //////////
+
+  // вивід тексту на екран в залежності від результату win/lose
+  const status = game.getStatus();
+
+  if (status === 'win') {
+    document.querySelector('.message-win').classList.remove('hidden');
+  } else if (status === 'lose') {
+    document.querySelector('.message-lose').classList.remove('hidden');
+  }
+  // ////////
+
+  // початок/перезапуск гри
+  document.querySelector('.button').addEventListener('click', () => {
+    if (game.getStatus() === 'playing') {
+      game.restart();
+    } else {
+      game.start();
+    }
+
+    document.querySelector('.message-start').classList.add('hidden');
+    document.querySelector('.button').classList.remove('start');
+    document.querySelector('.button').classList.add('restart'); // заміняємо клас для button
+    document.querySelector('.button').textContent = 'Restart'; // заміняємо текст button
+
+    workingWithHTMLFile();
+  });
+  // ///////
+
+  // добавлення score на екран
+  document.querySelector('.game-score').textContent = game.getScore();
+  // ////////
+}
+
+// запускаємо дошку вперше
+workingWithHTMLFile();
