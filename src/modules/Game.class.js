f7eeec66e374e409f5aa12215bbba79f040c26ec
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-30 16:20:50 +0300

    add movement functions, constructor, getters, and setters to Game class

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,160 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  static GAME_STATUS = {
+    idle: 'idle',
+    playing: 'playing',
+    win: 'win',
+    lose: 'lose',
+  };
+
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.status = Game.GAME_STATUS.idle;
+    this.initialState = initialState;
+    this.state = initialState.map((row) => [...row]);
+    this.score = 0;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    const newState = [];
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+    for (const row of this.state) {
+      let filteredRow = row.filter((num) => num !== 0);
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+      for (let i = 0; i < filteredRow.length - 1; i++) {
+        if (filteredRow[i] === filteredRow[i + 1]) {
+          filteredRow[i] *= 2;
+          filteredRow[i + 1] = 0;
+        }
+      }
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+      filteredRow = filteredRow.filter((num) => num !== 0);
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+      const newRow = [
+        ...filteredRow,
+        ...Array(row.length - filteredRow.length).fill(0),
+      ];
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+      newState.push(newRow);
+    }
 
-  // Add your own methods here
+    this.setState(newState);
+  }
+  moveRight() {
+    const newState = [];
+
+    for (const row of this.state) {
+      let filteredRow = row.filter((num) => num !== 0);
+
+      for (let i = filteredRow.length - 1; i > 0; i--) {
+        if (filteredRow[i] === filteredRow[i - 1]) {
+          filteredRow[i] *= 2;
+          filteredRow[i - 1] = 0;
+        }
+      }
+
+      filteredRow = filteredRow.filter((num) => num !== 0);
+
+      const newRow = [
+        ...Array(row.length - filteredRow.length).fill(0),
+        ...filteredRow,
+      ];
+
+      newState.push(newRow);
+    }
+
+    this.setState(newState);
+  }
+  moveUp() {
+    const newState = [];
+    const transposedState = this.#transposeState(this.state);
+
+    for (const row of transposedState) {
+      let filteredRow = row.filter((num) => num !== 0);
+
+      for (let i = 0; i < filteredRow.length - 1; i++) {
+        if (filteredRow[i] === filteredRow[i + 1]) {
+          filteredRow[i] *= 2;
+          filteredRow[i + 1] = 0;
+        }
+      }
+
+      filteredRow = filteredRow.filter((num) => num !== 0);
+
+      const newRow = [
+        ...filteredRow,
+        ...Array(row.length - filteredRow.length).fill(0),
+      ];
+
+      newState.push(newRow);
+    }
+
+    this.setState(this.#transposeState(newState));
+  }
+  moveDown() {
+    const newState = [];
+    const transposedState = this.#transposeState(this.state);
+
+    for (const row of transposedState) {
+      let filteredRow = row.filter((num) => num !== 0);
+
+      for (let i = filteredRow.length - 1; i > 0; i--) {
+        if (filteredRow[i] === filteredRow[i - 1]) {
+          filteredRow[i] *= 2;
+          filteredRow[i - 1] = 0;
+        }
+      }
+
+      filteredRow = filteredRow.filter((num) => num !== 0);
+
+      const newRow = [
+        ...Array(row.length - filteredRow.length).fill(0),
+        ...filteredRow,
+      ];
+
+      newState.push(newRow);
+    }
+
+    this.setState(this.#transposeState(newState));
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  setState(newState) {
+    this.state = newState.map((row) => [...row]);
+  }
+
+  #transposeState(matrix) {
+    return matrix[0].map((_, colIndex) => {
+      return matrix.map((row) => row[colIndex]);
+    });
+  }
+
+  start() {
+    this.status = Game.GAME_STATUS.playing;
+  }
+
+  restart() {
+    this.state = this.initialState.map((row) => [...row]);
+    this.score = 0;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,19 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
 // const Game = require('../modules/Game.class');
-// const game = new Game();
+// const game = new Game([
+//   [0, 2, 2, 4],
+//   [0, 2, 0, 0],
+//   [2, 0, 0, 2],
+//   [16, 0, 0, 2],
+// ]);
 
-// Write your code here
+// console.log(game.getState());
+// game.moveLeft();
+// console.log(game.getState());
+// game.moveRight();
+// console.log(game.getState());
+// game.moveUp();
+// console.log(game.getState());
+// game.moveDown();
+// console.log(game.getState());
