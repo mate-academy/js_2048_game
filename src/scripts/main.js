bc18aa58cc5dffbcdddfc5a712ec50150bad05f9
Mikhailo Koshilka <mkosilka@gmail.com>
2024-09-15 21:28:31 +0100

    Add fix code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,45 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
 const game = new Game();
 
-
-// Отримуємо HTML-елементи
 const startButton = document.querySelector('.start');
 
 startButton.addEventListener('click', () => {
   if (startButton.classList.contains('start')) {
-    game.start(); // Якщо це "Start", починаємо гру
+    game.start();
   } else if (startButton.classList.contains('restart')) {
-    game.restart(); // Якщо це "Restart", перезапускаємо гру
+    game.restart();
   }
 });
 
 document.addEventListener('keyup', (e) => {
   if (game.getStatus() === 'lose') {
-    game.loseMessage();
+      game.loseMessage();
+      return;
   }
 
-  if (e.key === 'ArrowLeft') {
-    game.moveLeft();
-    game.updateBoard();
+  switch (e.key) {
+      case 'ArrowLeft':
+          game.moveLeft();
+          break;
+      case 'ArrowRight':
+          game.moveRight();
+          break;
+      case 'ArrowUp':
+          game.moveUp();
+          break;
+      case 'ArrowDown':
+          game.moveDown();
+          break;
+      default:
+          return;
+  }
 
-    if (game.getStatus() === 'win') {
-      game.winMessage();
-    }
-  } else if (e.key === 'ArrowRight') {
-    game.moveRight();
-    game.updateBoard();
+  game.updateBoard();
 
-    if (game.getStatus() === 'win') {
+  if (game.getStatus() === 'win') {
       game.winMessage();
-    }
-  } else if (e.key === 'ArrowUp') {
-    game.moveUp();
-    game.updateBoard();
-
-    if (game.getStatus() === 'win') {
-      game.winMessage();
-    }
-  } else if (e.key === 'ArrowDown') {
-    game.moveDown();
-    game.updateBoard();
-
-    if (game.getStatus() === 'win') {
-      game.winMessage();
-    }
   }
 });
+
