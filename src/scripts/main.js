1f70858538b306263e430c2b7c1e5c8c8847e511
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-27 20:48:27 +0300

    2048

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,61 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startMessage = document.querySelector('.message-start');
+const loseMessage = document.querySelector('.message-lose');
+const winMessage = document.querySelector('.message-win');
+const button = document.querySelector('.button');
+
+button.addEventListener('click', (buttonEvent) => {
+  buttonEvent.preventDefault();
+  button.classList.toggle('start');
+  button.classList.toggle('restart');
+
+  if (game.getStatus() === Game.gameStatus.idle) {
+    game.start();
+    button.textContent = 'Restart';
+  } else {
+    game.restart();
+    button.textContent = 'Start';
+  }
+  score.textContent = game.getScore();
+  startMessage.classList.toggle('hidden');
+  loseMessage.classList.add('hidden');
+  winMessage.classList.add('hidden');
+});
+
+const score = document.querySelector('.game-score');
+
+document.addEventListener('keydown', (keyEvent) => {
+  if (game.getStatus() !== Game.gameStatus.playing) {
+    return;
+  }
+
+  switch (keyEvent.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+  }
+  score.textContent = game.getScore();
+  game.fillFreeCell();
+  game.updateBorder();
+
+  if (game.getStatus() === Game.gameStatus.lose) {
+    loseMessage.classList.remove('hidden');
+  }
+
+  if (game.getStatus() === Game.gameStatus.win) {
+    winMessage.classList.remove('hidden');
+  }
+});
