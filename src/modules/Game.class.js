bd72e04f3d09e52de6cf8882b976175f2482940a
Victor Komara <vitioklss@gmail.com>
2024-09-28 17:09:39 +0300

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,44 +1,126 @@
 'use strict';
+const GRID_SIZE = 4
+const CELL_SIZE = 75
+const CELL_GAP = 10
+let score = 0
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  static Statuses = {
+    IDLE: 'idle',
+    PLAYING: 'playing',
+    WIN: 'win',
+    LOSE: 'lose',
+  }
+  #cells
+
+  constructor(gridElement) {
+    gridElement.style.setProperty("--grid-size", GRID_SIZE)
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}px`)
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}px`)
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      )
+    })
+    score = 0;
+    this.status = Game.Statuses.IDLE;
+    this.board = [
+      [0, 4, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  get cells() {
+    return this.#cells
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || []
+      cellGrid[cell.y][cell.x] = cell
+      return cellGrid
+    }, [])
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || []
+      cellGrid[cell.x][cell.y] = cell
+      return cellGrid
+    }, [])
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter(cell => cell.tile == null)
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
+  }
+
+  moveLeft() {
+    return this.slideTiles(this.cellsByRow);
+  }
+
+  moveRight() {
+    return this.slideTiles(this.cellsByRow.map((row) => [...row].reverse()));
+  }
+
+  moveUp() {
+    return this.slideTiles(this.cellsByColumn);
+  }
+
+  moveDown() {
+    return this.slideTiles(this.cellsByColumn.map((column) => [...column].reverse()));
+  }
+
+  slideTiles(cells) {
+    return Promise.all(
+      cells.flatMap((group) => {
+        const promises = [];
+        for (let i = 1; i < group.length; i++) {
+          const cell = group[i];
+          if (cell.tile == null) continue;
+          let lastValidCell;
+          for (let j = i - 1; j >= 0; j--) {
+            const moveToCell = group[j];
+            if (!moveToCell.canAccept(cell.tile)) break;
+            lastValidCell = moveToCell;
+          }
+
+          if (lastValidCell != null) {
+            promises.push(cell.tile.waitForTransition());
+            if (lastValidCell.tile != null) {
+              lastValidCell.mergeTile = cell.tile;
+            } else {
+              lastValidCell.tile = cell.tile;
+            }
+            cell.tile = null;
+          }
+        }
+        return promises;
+      })
+    );
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +132,114 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  setStatus(status) {
+    this.status = status;
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  start() {
+    this.status = Game.Statuses.PLAYING;
+  }
 
-  // Add your own methods here
+  checkForWin() {
+    let check = false;
+    this.cells.forEach(cell => {
+      if (cell.tile == null) {
+        return
+      };
+
+      if (cell.tile.value === 2048) {
+        check = true;
+        return;
+      }
+    });
+    return check;
+  }
+
+
+  restart() {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((el) => el.remove());
+    score = 0;
+    this.status = Game.Statuses.PLAYING;
+    this.#cells.forEach((cell) => {
+      cell.tile = null;
+      cell.mergeTile = null;
+    });
+  }
+}
+
+class Cell {
+  #cellElement
+  #x
+  #y
+  #tile
+  #mergeTile
+
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement
+    this.#x = x
+    this.#y = y
+  }
+
+  get x() {
+    return this.#x
+  }
+
+  get y() {
+    return this.#y
+  }
+
+  get tile() {
+    return this.#tile
+  }
+
+  set tile(value) {
+    this.#tile = value
+    if (value == null) return
+    this.#tile.x = this.#x
+    this.#tile.y = this.#y
+  }
+
+  get mergeTile() {
+    return this.#mergeTile
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value
+    if (value == null) return
+    this.#mergeTile.x = this.#x
+    this.#mergeTile.y = this.#y
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return
+    score += this.tile.value + this.mergeTile.value
+    this.tile.value = this.tile.value + this.mergeTile.value
+    this.mergeTile.remove()
+    this.mergeTile = null
+  }
+}
+
+function createCellElements(gridElement) {
+  const cells = []
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    cells.push(cell)
+    gridElement.append(cell)
+  }
+  return cells
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,282 @@
 'use strict';
+import Tile from '../modules/Tile.js';
+const Game = require('../modules/Game.class');
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const startBtn = document.querySelector('.start');
+const gameBoard = document.getElementById('game-board');
+const gameScore = document.querySelector('.game-score');
+const messages = document.querySelector('.message-container').children;
+const game = new Game(gameBoard);
 
-// Write your code here
+function updateScore() {
+  gameScore.textContent = game.getScore();
+}
+
+function updateStatus() {
+  console.log(game.getStatus());
+  const [messageLose, messageWin, messageStart] = messages;
+  const gameStatus = game.getStatus();
+  const hiddenClass = 'hidden';
+
+  Array.from(messages).forEach((message) => {
+    message.classList.add(hiddenClass);
+  });
+
+  switch (gameStatus) {
+    case Game.Statuses.IDLE:
+      messageStart.classList.remove(hiddenClass);
+      break;
+    case Game.Statuses.WIN:
+      messageWin.classList.remove(hiddenClass);
+      break;
+    case Game.Statuses.LOSE:
+      messageLose.classList.remove(hiddenClass);
+      break;
+    default:
+      break;
+  }
+}
+
+function setupSwipeInput() {
+  let swiping = false;
+
+  document.addEventListener(
+    'touchstart',
+    (event) => {
+      if (swiping) {
+        return;
+      }
+
+      swiping = true;
+
+      let startX = event.touches[0].clientX;
+      let startY = event.touches[0].clientY;
+      let endSwipe = false;
+
+      document.addEventListener(
+        'touchend',
+        (event) => {
+          if (endSwipe) {
+            return;
+          }
+
+          endSwipe = true;
+
+          const endX = event.changedTouches[0].clientX;
+          const endY = event.changedTouches[0].clientY;
+
+          const deltaX = endX - startX;
+          const deltaY = endY - startY;
+          const absDeltaX = Math.abs(deltaX);
+          const absDeltaY = Math.abs(deltaY);
+
+          const threshold = 100; // Поріг чутливості
+          const obj = { key: '-' };
+
+          if (absDeltaX < threshold && absDeltaY < threshold) {
+            swiping = false;
+            endSwipe = false;
+            setupSwipeInput();
+            return;
+          }
+          // Визначення напрямку свайпа
+          if (absDeltaX > threshold) {
+            if (deltaX > 0) {
+              obj.key = 'ArrowRight';
+            } else {
+              obj.key = 'ArrowLeft';
+            }
+          } else if (Math.abs(deltaY) > threshold) {
+            if (deltaY > 0) {
+              obj.key = 'ArrowDown';
+            } else {
+              obj.key = 'ArrowUp';
+            }
+          }
+
+          swipeInput(obj);
+          swiping = false;
+          endSwipe = false;
+        },
+        { once: true },
+      );
+    },
+    { once: true },
+  );
+}
+
+async function swipeInput(e) {
+  if (game.getStatus() !== Game.Statuses.PLAYING) {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowUp':
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await game.moveUp();
+      break;
+    case 'ArrowDown':
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await game.moveDown();
+      break;
+    case 'ArrowLeft':
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
+      await game.moveLeft();
+      break;
+    case 'ArrowRight':
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
+      await game.moveRight();
+      break;
+    default:
+      setupSwipeInput();
+      return;
+  }
+
+  game.cells.forEach((cell) => cell.mergeTiles());
+
+  if (game.checkForWin()) {
+    game.setStatus(Game.Statuses.WIN);
+    updateStatus();
+    return;
+  }
+
+  const newTile = new Tile(gameBoard);
+  game.randomEmptyCell().tile = newTile;
+
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+    newTile.waitForTransition(true).then(() => {
+      game.setStatus(Game.Statuses.LOSE);
+      updateStatus();
+    });
+    return;
+  }
+
+  updateScore();
+  // updateStatus();
+  setupSwipeInput();
+}
+
+function setupInput() {
+  window.addEventListener('keyup', handleInput, { once: true });
+}
+
+async function handleInput(e) {
+  if (game.getStatus() !== Game.Statuses.PLAYING) {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowUp':
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await game.moveUp();
+      break;
+    case 'ArrowDown':
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await game.moveDown();
+      break;
+    case 'ArrowLeft':
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
+      await game.moveLeft();
+      break;
+    case 'ArrowRight':
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
+      await game.moveRight();
+      break;
+    default:
+      setupInput();
+      return;
+  }
+
+  game.cells.forEach((cell) => cell.mergeTiles());
+
+  if (game.checkForWin()) {
+    game.setStatus(Game.Statuses.WIN);
+    updateStatus();
+    return;
+  }
+
+  const newTile = new Tile(gameBoard);
+  game.randomEmptyCell().tile = newTile;
+
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+    newTile.waitForTransition(true).then(() => {
+      game.setStatus(Game.Statuses.LOSE);
+      updateStatus();
+    });
+    return;
+  }
+
+  updateScore();
+  // updateStatus();
+  setupInput();
+}
+
+startBtn.addEventListener('click', () => {
+  if (startBtn.classList.contains('start')) {
+    startBtn.classList.remove('start');
+    startBtn.classList.add('restart');
+    startBtn.textContent = 'Restart';
+    game.start();
+    game.randomEmptyCell().tile = new Tile(gameBoard);
+    game.randomEmptyCell().tile = new Tile(gameBoard);
+    setupInput();
+    setupSwipeInput();
+  } else if (startBtn.classList.contains('restart')) {
+    game.restart();
+    game.randomEmptyCell().tile = new Tile(gameBoard);
+    game.randomEmptyCell().tile = new Tile(gameBoard);
+    setupInput();
+    setupSwipeInput();
+  }
+});
+
+function canMoveUp() {
+  return canMove(game.cellsByColumn);
+}
+
+function canMoveDown() {
+  return canMove(game.cellsByColumn.map((column) => [...column].reverse()));
+}
+
+function canMoveLeft() {
+  return canMove(game.cellsByRow);
+}
+
+function canMoveRight() {
+  return canMove(game.cellsByRow.map((row) => [...row].reverse()));
+}
+
+function canMove(cells) {
+  return cells.some((group) => {
+    return group.some((cell, index) => {
+      if (index === 0) return false;
+      if (cell.tile == null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    });
+  });
+}
