11f1018302a62dbc0783049df4e87c8c6035a517
Arthur Tolkachov <babanjerelo@gmail.com>
2024-10-07 19:56:33 +0300

    [Feature] created 2048 game

---
+++ src/lib/constants.js
@@ -0,0 +1,33 @@
+export const DEFAULT_NUM_2 = 2;
+export const DEFAULT_NUM_4 = 4;
+
+export const FIELD_ROW_MIN_IDX = 0;
+export const FIELD_ROW_MAX_IDX = 3;
+export const FIELD_ROW_LENGTH = 4;
+export const NUMBER_PROBABILITY_ARRAY_LENGTH = 10;
+
+export const VALUE_2048 = 2048;
+
+export const GAME_STATUSES = {
+  idle: 'idle',
+  playing: 'playing',
+  win: 'win',
+  lose: 'lose',
+};
+
+export const MESSAGES_KEYS = {
+  start: 'msgStart',
+  lose: 'msgLose',
+  win: 'msgWin',
+};
+
+export const KEY_BINDS = {
+  w: 'w',
+  a: 'a',
+  s: 's',
+  d: 'd',
+  ArrowUp: 'ArrowUp',
+  ArrowLeft: 'ArrowLeft',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+};
