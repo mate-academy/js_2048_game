362359e03923e4c3f68ddf9ec704e63a946f2135
Mykhailo Lapchynskyi <mykhailolapchynskyi@mac.local>
2024-10-12 17:26:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,67 @@
+/* eslint-disable padding-line-between-statements */
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
+const game = new Game();
 
-// Write your code here
+const changeStartButton = (button) => {
+  button.classList.remove('start');
+  button.classList.add('restart');
+  button.innerText = 'Restart';
+};
+
+const hideMessageContainer = () => {
+  const messageContainer = document.querySelector('.message-start');
+  messageContainer.classList.add('hidden');
+};
+
+const initializeGame = () => {
+  const startButton = document.querySelector('.button');
+
+  if (startButton) {
+    startButton.addEventListener('click', () => {
+      if (game.getStatus() === 'idle') {
+        game.start();
+
+        changeStartButton(startButton);
+        hideMessageContainer();
+      } else if (game.getStatus() === 'playing') {
+        game.restart();
+      }
+    });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', initializeGame);
+// eslint-disable-next-line no-shadow
+document.addEventListener('keydown', (event) => {
+  if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+    event.preventDefault();
+  }
+
+  if (game.getStatus() !== 'playing') {
+    return;
+  }
+
+  switch (event.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+  }
+
+  if (game.getStatus() === 'win' || game.getStatus() === 'lose') {
+    game.isGameOver();
+  }
+
+  game.updateTheScore();
+  game.updateTheBoard();
+});
