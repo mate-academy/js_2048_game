ea00f5f9d6ffe3285449267334cf5d205a5da95c
Łukasz <1luki9901@gmail.com>
2024-10-15 22:04:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,18 @@
 // const Game = require('../modules/Game.class');
 
-// eslint-disable-next-line no-unused-expressions
-('use strict');
-
 // Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
 const game = new Game();
 
 // Write your code here
 document.addEventListener('DOMContentLoaded', () => {
-  'use strict';
-
   const startButton = document.getElementById('start-button');
   const scoreElement = document.getElementById('score');
   const statusElement = document.getElementById('status');
   const gameBoard = document.getElementById('game-board');
   const cells = gameBoard.getElementsByClassName('field-cell');
+
+  let previousState = [];
 
   function updateUI() {
     const state = game.getState();
@@ -24,15 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
       const cell = cells[i];
       const value = state[Math.floor(i / 4)][i % 4];
 
-      cell.className = 'field-cell';
+      if (previousState[i] !== value) {
+        cell.className = 'field-cell';
 
-      if (value) {
-        cell.classList.add(`field-cell--${value}`);
-        cell.textContent = value;
-      } else {
-        cell.textContent = '';
+        if (value) {
+          cell.classList.add(`field-cell--${value}`);
+          cell.textContent = value;
+        } else {
+          cell.textContent = '';
+        }
       }
     }
+    previousState = state.flat();
     scoreElement.textContent = game.getScore();
     statusElement.textContent = game.getStatus();
   }
@@ -49,26 +49,18 @@ document.addEventListener('DOMContentLoaded', () => {
       game.getStatus() === 'playing' ? 'Restart' : 'Start';
   });
 
+  const keyMap = {
+    ArrowLeft: () => game.moveLeft(),
+    ArrowRight: () => game.moveRight(),
+    ArrowUp: () => game.moveUp(),
+    ArrowDown: () => game.moveDown(),
+  };
+
   // eslint-disable-next-line no-shadow
-  gameBoard.addEventListener('keydown', (event) => {
-    let moved = false;
+  document.addEventListener('keydown', (event) => {
+    const moveFunction = keyMap[event.key];
 
-    switch (event.key) {
-      case 'ArrowLeft':
-        moved = game.moveLeft();
-        break;
-      case 'ArrowRight':
-        moved = game.moveRight();
-        break;
-      case 'ArrowUp':
-        moved = game.moveUp();
-        break;
-      case 'ArrowDown':
-        moved = game.moveDown();
-        break;
-    }
-
-    if (moved) {
+    if (moveFunction && moveFunction()) {
       updateUI();
     }
   });
