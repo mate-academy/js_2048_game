28c847fdcaa7188d0607573d2e8e78f9a08f1bad
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-10-21 10:22:17 +0300

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,21 +1,24 @@
 'use strict';
 
 class Game {
-  constructor(initialState) {
-    this.board = initialState || this.generateEmptyBoard();
+  constructor(initialState = this.generateEmptyBoard()) {
+    this.initialState = initialState;
+    this.state = this.copyState(this.initialState);
+    this.board = this.state;
     this.score = 0;
-    this.status = 'idle'; // 'idle', 'playing', 'win', 'lose'
+    this.status = 'idle';
     this.firstMove = true;
     this.moved = false;
   }
 
+  copyState(state) {
+    return state.map((row) => [...row]);
+  }
+
   generateEmptyBoard() {
-    return [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ];
+    return Array(4)
+      .fill()
+      .map(() => Array(4).fill(0));
   }
 
   getState() {
@@ -31,67 +34,54 @@ class Game {
   }
 
   moveLeft() {
+    if (this.status !== 'playing') {
+      return;
+    }
     this.handleFirstMove();
 
-    const originalBoard = JSON.parse(JSON.stringify(this.board));
-    const newBoard = this.move(originalBoard);
+    const newBoard = this.board.map((row) => this.processRow(row));
 
-    if (this.isBoardChanged(this.board, newBoard)) {
-      this.board = newBoard;
-      this.addRandomCell();
-      this.checkWin();
-      this.checkGameOver();
-      this.moved = true;
-    } else {
-      this.moved = false;
-    }
+    this.handleMove(newBoard);
   }
 
   moveRight() {
+    if (this.status !== 'playing') {
+      return;
+    }
     this.handleFirstMove();
 
-    const reversedBoard = this.board.map((row) => row.reverse());
-    const newBoard = this.move(reversedBoard).map((row) => row.reverse());
+    const newBoard = this.board.map((row) => this.processRowRight(row));
 
-    if (this.isBoardChanged(this.board, newBoard)) {
-      this.board = newBoard;
-      this.addRandomCell();
-      this.checkWin();
-      this.checkGameOver();
-      this.moved = true;
-    } else {
-      this.moved = false;
-    }
+    this.handleMove(newBoard);
   }
 
   moveUp() {
+    if (this.status !== 'playing') {
+      return;
+    }
     this.handleFirstMove();
 
     const transposedBoard = this.transpose(this.board);
-    const newBoard = this.move(transposedBoard);
-    const finalBoard = this.transpose(newBoard);
+    const newBoard = transposedBoard.map((row) => this.processRow(row));
 
-    if (this.isBoardChanged(this.board, finalBoard)) {
-      this.board = finalBoard;
-      this.addRandomCell();
-      this.checkWin();
-      this.checkGameOver();
-      this.moved = true;
-    } else {
-      this.moved = false;
-    }
+    this.handleMove(this.transpose(newBoard));
   }
 
   moveDown() {
+    if (this.status !== 'playing') {
+      return;
+    }
     this.handleFirstMove();
 
     const transposedBoard = this.transpose(this.board);
-    const reversedBoard = transposedBoard.map((row) => row.reverse());
-    const newBoard = this.move(reversedBoard).map((row) => row.reverse());
-    const finalBoard = this.transpose(newBoard);
+    const newBoard = transposedBoard.map((row) => this.processRowRight(row));
 
-    if (this.isBoardChanged(this.board, finalBoard)) {
-      this.board = finalBoard;
+    this.handleMove(this.transpose(newBoard));
+  }
+
+  handleMove(newBoard) {
+    if (this.isBoardChanged(this.board, newBoard)) {
+      this.board = newBoard;
       this.addRandomCell();
       this.checkWin();
       this.checkGameOver();
@@ -102,17 +92,23 @@ class Game {
   }
 
   start() {
-    this.status = 'playing';
-    this.score = 0;
-    this.board = this.generateEmptyBoard();
-    this.firstMove = true;
-    document.querySelector('.message-start').classList.add('hidden');
-    this.addRandomCell();
-    this.addRandomCell();
+    if (this.status === 'idle') {
+      this.status = 'playing';
+      this.addRandomCell();
+      this.addRandomCell();
+
+      const startMessage = document.querySelector('.message-start');
+
+      if (startMessage) {
+        startMessage.classList.add('hidden');
+      }
+    }
   }
 
   restart() {
-    this.start();
+    this.board = this.initialState;
+    this.score = 0;
+    this.status = 'idle';
   }
 
   addRandomCell() {
@@ -129,11 +125,35 @@ class Game {
     if (emptyCells.length > 0) {
       const { row, col } =
         emptyCells[Math.floor(Math.random() * emptyCells.length)];
-      const value = Math.random() < 0.1 ? 4 : 2;
 
-      this.board[row][col] = value;
+      this.board[row][col] = Math.random() < 0.1 ? 4 : 2;
       this.animateCell(row, col);
     }
+  }
+
+  processRow(row) {
+    const filteredRow = row.filter((cell) => cell !== 0);
+
+    for (let i = 0; i < filteredRow.length - 1; i++) {
+      if (filteredRow[i] === filteredRow[i + 1]) {
+        filteredRow[i] *= 2;
+        this.score += filteredRow[i];
+        filteredRow.splice(i + 1, 1);
+      }
+    }
+
+    while (filteredRow.length < 4) {
+      filteredRow.push(0);
+    }
+
+    return filteredRow;
+  }
+
+  processRowRight(row) {
+    const reversedRow = [...row].reverse();
+    const processedRow = this.processRow(reversedRow);
+
+    return processedRow.reverse();
   }
 
   handleFirstMove() {
@@ -142,15 +162,22 @@ class Game {
 
       const startButton = document.querySelector('.button.start');
 
-      startButton.textContent = 'Restart';
-      startButton.classList.add('restart');
+      if (startButton) {
+        startButton.textContent = 'Restart';
+        startButton.classList.add('restart');
+      }
     }
   }
 
   checkGameOver() {
     if (!this.hasAvailableMoves()) {
       this.status = 'lose';
-      document.querySelector('.message-lose').classList.remove('hidden');
+
+      const messageLose = document.querySelector('.message-lose');
+
+      if (messageLose) {
+        messageLose.classList.remove('hidden');
+      }
     }
   }
 
@@ -159,7 +186,12 @@ class Game {
       for (let col = 0; col < 4; col++) {
         if (this.board[row][col] === 2048) {
           this.status = 'win';
-          document.querySelector('.message-win').classList.remove('hidden');
+
+          const messageWin = document.querySelector('.message-win');
+
+          if (messageWin) {
+            messageWin.classList.remove('hidden');
+          }
 
           return;
         }
@@ -187,31 +219,6 @@ class Game {
     return false;
   }
 
-  move(board) {
-    for (let row = 0; row < 4; row++) {
-      const newRow = board[row].filter((cell) => cell !== 0);
-
-      for (let col = 0; col < newRow.length - 1; col++) {
-        if (newRow[col] === newRow[col + 1]) {
-          newRow[col] *= 2;
-          this.score += newRow[col];
-          newRow.splice(col + 1, 1);
-        }
-      }
-
-      while (newRow.length < 4) {
-        newRow.push(0);
-      }
-      board[row] = newRow;
-    }
-
-    return board;
-  }
-
-  transpose(board) {
-    return board[0].map((_, col) => board.map((row) => row[col]));
-  }
-
   isBoardChanged(oldBoard, newBoard) {
     for (let row = 0; row < 4; row++) {
       for (let col = 0; col < 4; col++) {
@@ -224,13 +231,19 @@ class Game {
     return false;
   }
 
+  transpose(board) {
+    return board[0].map((_, colIndex) => board.map((row) => row[colIndex]));
+  }
+
   animateCell(row, col) {
     const cell = document.querySelector(
       `.field-row:nth-child(${row + 1}) .field-cell:nth-child(${col + 1})`,
     );
 
-    cell.classList.add('new-cell');
-    setTimeout(() => cell.classList.remove('new-cell'), 500);
+    if (cell) {
+      cell.classList.add('new-cell');
+      setTimeout(() => cell.classList.remove('new-cell'), 500);
+    }
   }
 }
 
