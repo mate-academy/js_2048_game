b2c4b09e941c58523cf741b6e2890daeb3f68c30
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-30 18:38:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,32 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
 
 // Write your code here
+
+const gameField = document.querySelector('.game-field');
+const gameScore = document.querySelector('.game-score');
+const buttonStart = document.querySelector('.start');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
+
+const game = new Game(
+  gameField,
+  buttonStart,
+  gameScore,
+  messageLose,
+  messageWin,
+  messageStart,
+);
+
+buttonStart.addEventListener('click', () => {
+  if (!game.playing) {
+    game.start();
+  } else {
+    game.restart();
+  }
+});
+
+game.makeMove();
