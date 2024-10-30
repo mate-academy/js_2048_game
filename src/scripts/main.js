791429f681a09f6d5111cba046bf779147d8ba2b
Tomasz Krzywicki <tomaszkrzywicki4@gmail.com>
2024-10-30 21:41:17 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+document.addEventListener('keydown', (e) => {
+  // Renamed 'event' to 'e'
+  if (e.key === 'ArrowLeft') {
+    game.moveLeft();
+  } else if (e.key === 'ArrowRight') {
+    game.moveRight();
+  } else if (e.key === 'ArrowUp') {
+    game.moveUp();
+  } else if (e.key === 'ArrowDown') {
+    game.moveDown();
+  }
+
+  // Update UI based on the game state
+  updateUI();
+});
+
+function updateUI() {
+  // Render the game board and score on the UI
+}
