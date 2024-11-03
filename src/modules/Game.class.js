d5ad72c45b8bc1b8087ccd1582ccfc61633422d4
Zyrak Nur <noorzirak7@gmail.com>
2024-11-03 21:49:56 +0000

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
   /**
    * Creates a new game instance.
@@ -20,25 +15,168 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = Array.from({ length: Game.GRID_SIZE }, () => {
+      return Array(Game.GRID_SIZE).fill(0);
+    }),
+  ) {
+    this.state = initialState;
+
+    this.score = this.state.reduce((sum, row) => {
+      let cellsSum = 0;
+
+      for (const cell of row) {
+        cellsSum += cell;
+      }
+
+      return sum + cellsSum;
+    }, 0);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  static GRID_SIZE = 4;
+  static WIN_NUMBER = 128;
+  static STATUS_IDLE = 'idle';
+  static STATUS_PLAYING = 'playing';
+  static STATUS_WIN = 'win';
+  static STATUS_LOSE = 'lose';
+  static PUSH_METHOD = 'push';
+  static UNSHIFT_METHOD = 'unshift';
+  static MOVE_DIRECTIONS = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    UP: 'up',
+    DOWN: 'down',
+  };
+
+  status = Game.STATUS_IDLE;
+
+  moveCells(matrix, isReverseDirection = false) {
+    let sum = 0;
+
+    const result = matrix.map((row) => {
+      const filteredRow = row.filter((cell) => cell !== 0);
+
+      if (isReverseDirection) {
+        filteredRow.reverse();
+      }
+
+      const [mergedCells, mergedSum] = this.mergeNumbers(filteredRow);
+
+      sum += mergedSum;
+
+      while (mergedCells.length < Game.GRID_SIZE) {
+        mergedCells.push(0);
+      }
+
+      return isReverseDirection ? mergedCells.reverse() : mergedCells;
+    });
+
+    return [result, sum];
+  }
+
+  moveLeft() {
+    const [newState, mergedSum] = this.moveCells(this.state);
+
+    this.updateState(newState, this.score + mergedSum);
+  }
+
+  moveRight() {
+    const [newState, mergedSum] = this.moveCells(this.state, true);
+
+    this.updateState(newState, this.score + mergedSum);
+  }
+
+  moveUp() {
+    const rotatedState = this.rotateMatrix(this.state);
+    const [movedState, mergedSum] = this.moveCells(rotatedState);
+    const newState = this.rotateMatrix(movedState);
+
+    this.updateState(newState, this.score + mergedSum);
+  }
+
+  moveDown() {
+    const rotatedState = this.rotateMatrix(this.state);
+    const [movedState, mergedSum] = this.moveCells(rotatedState, true);
+    const newState = this.rotateMatrix(movedState);
+
+    this.updateState(newState, this.score + mergedSum);
+  }
+
+  isMoveAvailable() {
+    return this.state.some((row, rowIndex) => {
+      return row.some((cell, cellIndex) => {
+        if (cell === 0) {
+          return true;
+        }
+
+        if (
+          cellIndex !== row.length - 1 &&
+          cell === this.state[rowIndex][cellIndex + 1]
+        ) {
+          return true;
+        }
+
+        if (
+          rowIndex !== this.state.length - 1 &&
+          cell === this.state[rowIndex + 1][cellIndex]
+        ) {
+          return true;
+        }
+
+        return false;
+      });
+    });
+  }
+
+  updateState(newState, newScore) {
+    if (this.hasStateChanged(newState) && this.status === Game.STATUS_PLAYING) {
+      this.state = newState;
+      this.score = newScore;
+      this.addRandomCell();
+    }
+
+    if (
+      this.state.some((row) => row.some((cell) => cell === Game.WIN_NUMBER))
+    ) {
+      this.status = Game.STATUS_WIN;
+
+      return;
+    }
+
+    if (!this.isMoveAvailable()) {
+      this.status = Game.STATUS_LOSE;
+    }
+  }
+
+  rotateMatrix(matrix) {
+    const rotatedMatrix = [];
+
+    for (let cellIndex = 0; cellIndex < Game.GRID_SIZE; cellIndex++) {
+      const column = [];
+
+      for (let rowIndex = 0; rowIndex < Game.GRID_SIZE; rowIndex++) {
+        column.push(matrix[rowIndex][cellIndex]);
+      }
+
+      rotatedMatrix.push(column);
+    }
+
+    return rotatedMatrix;
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.state;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +188,82 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    this.status = Game.STATUS_PLAYING;
+    this.addRandomCell();
+    this.addRandomCell();
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  restart() {
+    this.state = Array.from({ length: Game.GRID_SIZE }, () => {
+      return Array(Game.GRID_SIZE).fill(0);
+    });
 
-  // Add your own methods here
+    this.status = Game.STATUS_PLAYING;
+    this.score = 0;
+    this.addRandomCell();
+    this.addRandomCell();
+  }
+
+  addRandomCell() {
+    const availableCells = [];
+
+    this.state.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        if (cell === 0) {
+          availableCells.push({ indexRow: rowIndex, indexCell: cellIndex });
+        }
+      });
+    });
+
+    if (!availableCells.length) {
+      return;
+    }
+
+    const randomIndex = getRandomNumber(0, availableCells.length - 1);
+    const { indexRow, indexCell } = availableCells[randomIndex];
+    const newCellValue = Math.random() < 0.1 ? 4 : 2;
+
+    this.state[indexRow].splice(indexCell, 1, newCellValue);
+  }
+
+  mergeNumbers(array) {
+    const result = [];
+    let mergedSum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      const cell = array[i];
+      const nextCell = array[i + 1];
+
+      if (cell === nextCell) {
+        result.push(cell + nextCell);
+        mergedSum += cell + nextCell;
+        i++;
+      } else {
+        result.push(cell);
+      }
+    }
+
+    return [result, mergedSum];
+  }
+
+  hasStateChanged(newState) {
+    const isChanged = this.state.some((row, rowIndex) => {
+      return row.some((cell, cellIndex) => {
+        return cell !== newState[rowIndex][cellIndex];
+      });
+    });
+
+    return isChanged;
+  }
 }
 
 module.exports = Game;
+
+function getRandomNumber(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,86 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const button = document.querySelector('.button');
+const score = document.querySelector('.game-score');
+const [...cells] = document.querySelectorAll('.field-cell');
+const messageStart = document.querySelector('.message-start');
+const messageWin = document.querySelector('.message-win');
+const messageLose = document.querySelector('.message-lose');
+
+function hideMessages() {
+  messageStart.classList.add('hidden');
+  messageLose.classList.add('hidden');
+  messageWin.classList.add('hidden');
+}
+
+button.addEventListener('click', () => {
+  if (button.classList.contains('restart')) {
+    game.restart();
+  }
+
+  if (button.classList.contains('start')) {
+    game.start();
+
+    document.addEventListener('keydown', (key) => {
+      if (game.status === Game.STATUS_PLAYING) {
+        switch (key.key) {
+          case 'ArrowLeft':
+            game.moveLeft();
+            break;
+
+          case 'ArrowRight':
+            game.moveRight();
+            break;
+
+          case 'ArrowUp':
+            game.moveUp();
+            break;
+
+          case 'ArrowDown':
+            game.moveDown();
+            break;
+        }
+
+        displayOnField(game.state);
+      }
+    });
+
+    button.classList.remove('start');
+    button.textContent = 'Restart';
+    button.classList.add('restart');
+  }
+
+  hideMessages();
+  displayOnField(game.state);
+});
+
+function displayOnField(matrix) {
+  for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < matrix[rowIndex].length; cellIndex++) {
+      const matrixCell = matrix[rowIndex][cellIndex];
+      const cell = cells[Game.GRID_SIZE * rowIndex + cellIndex];
+      // instead of 0 we add empty string to the cell
+      const value = matrixCell === 0 ? '' : matrixCell;
+
+      if (cell.textContent !== value.toString()) {
+        cell.textContent = value;
+        cell.setAttribute('data-value', matrixCell);
+      }
+    }
+  }
+
+  score.textContent = game.score;
+
+  switch (game.status) {
+    case Game.STATUS_WIN:
+      messageWin.classList.remove('hidden');
+      break;
+
+    case Game.STATUS_LOSE:
+      messageLose.classList.remove('hidden');
+      break;
+  }
+}
