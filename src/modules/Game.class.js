39b89dad81ef2447c506dc87d95258a7004af4eb
Iryna Mariiko <IrynaMM497@gmail.com>
2024-11-07 22:21:27 +0200

    started adding event Listener

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -7,8 +7,8 @@
  */
 const IDLE = 'idle';
 const PLAYING = 'playing';
-// const WIN = 'win';
-// const LOSE = 'lose';
+const WIN = 'win';
+const LOSE = 'lose';
 
 class Game {
   /**
@@ -36,9 +36,35 @@ class Game {
     this.score = 0;
 
     this.status = IDLE;
+    this.isStarted = false;
   }
 
-  addRandomTitle() {
+  updateBoard() {
+    const cells = document.querySelectorAll('.field-cell');
+    // всі клітинки дошки
+
+    let cellIndex = 0;
+
+    for (let row = 0; row < this.board.length; row++) {
+      for (let col = 0; col < this.board[row].length; col++) {
+        const cell = cells[cellIndex++];
+        const value = this.board[row][col];
+
+        if (value === 0) {
+          const previousValue = cell.textContent;
+
+          cell.classList.remove(`field-cell--${previousValue}`);
+          cell.textContent = '';
+        } else {
+          cell.classList.remove(`field-cell--${value / 2}`);
+          cell.classList.add(`field-cell--${value}`);
+          cell.textContent = value; // оновлюємо текст
+        }
+      }
+    }
+  }
+
+  addRandomTile() {
     const emptyCells = [];
 
     for (let row = 0; row < 4; row++) {
@@ -53,7 +79,30 @@ class Game {
       const { row, col } =
         emptyCells[Math.floor(Math.random() * emptyCells.length)];
 
-      this.board[row][col] = 2;
+      if (this.score === 0) {
+        this.board[row][col] = 2; // Додаємо плитку 2 лише один раз на старті
+      }
+      this.updateBoard(); // оновлюємо DOM після додавання плитки
+    }
+  }
+
+  addRandomCellAfterMoving() {
+    const emptyCells = [];
+
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (this.board[row][col] === 0) {
+          emptyCells.push({ row, col });
+        }
+      }
+    }
+
+    if (emptyCells.length > 0) {
+      const { row, col } =
+        emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+      this.board[row][col] = Math.random() < 0.9 ? 2 : 4;
+      this.updateBoard(); // оновлюємо DOM після додавання плитки
     }
   }
 
@@ -93,8 +142,23 @@ class Game {
       [0, 0, 0, 0],
     ];
 
-    this.addRandomTitle();
-    this.addRandomTitle();
+    this.addRandomTile();
+    this.addRandomTile();
+  }
+
+  mergeTitles(newRow, canMerge) {
+    const mergedThisMove = []; // масив для відслідковування злитих плиток
+
+    for (let i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i] === newRow[i + 1]) {
+        newRow[i] *= 2;
+        newRow.splice(i + 1, 1);
+        canMerge[i] = false; // вказуємо, що плитка вже злилася
+        mergedThisMove.push(i); // зберігаємо індекс злитої плитки
+      }
+    }
+
+    return mergedThisMove;
   }
 
   moveLeft() {
@@ -104,14 +168,17 @@ class Game {
       const currentRow = this.board[row];
       const newRow = currentRow.filter((val) => val !== 0);
       const canMerge = new Array(newRow.length).fill(true);
+      // [true, true, і тд]
 
-      for (let i = 0; i < newRow.length - 1; i++) {
-        if (newRow[i] === newRow[i + 1]) {
-          newRow[i] *= 2;
-          newRow.splice(i + 1, 1);
-          moved = true;
-          canMerge[i] = false;
-        }
+      const mergedThisMove = this.mergeTitles(newRow, canMerge);
+      // виконується злиття клітинок та передається масив в
+      // якому є індекси злитих клітинок
+
+      if (
+        mergedThisMove.length > 0 ||
+        newRow.length !== currentRow.filter((val) => val !== 0).length
+      ) {
+        moved = true;
       }
 
       while (newRow.length < currentRow.length) {
@@ -119,6 +186,15 @@ class Game {
       }
 
       this.board[row] = newRow;
+
+      // Оновлюємо рахунок, передаючи індекси злитих плиток
+      for (let i = 0; i < mergedThisMove.length; i++) {
+        this.getScore(moved, this.board[mergedThisMove[i]]);
+      }
+    }
+
+    if (moved) {
+      this.addRandomCellAfterMoving();
     }
 
     return moved;
@@ -132,13 +208,13 @@ class Game {
       const newRow = currentRow.filter((val) => val !== 0);
       const canMerge = new Array(newRow.length).fill(true);
 
-      for (let i = newRow.length - 1; i > 0; i--) {
-        if (newRow[i] === newRow[i - 1]) {
-          newRow[i] *= 2;
-          newRow.splice(i - 1, 1);
-          moved = true;
-          canMerge[i] = false;
-        }
+      const mergedThisMove = this.mergeTitles(newRow, canMerge);
+
+      if (
+        mergedThisMove.length > 0 ||
+        newRow.length !== currentRow.filter((val) => val !== 0).length
+      ) {
+        moved = true;
       }
 
       while (newRow.length < currentRow.length) {
@@ -146,6 +222,14 @@ class Game {
       }
 
       this.board[row] = newRow;
+
+      for (let i = 0; i < mergedThisMove.length; i++) {
+        this.getScore(moved, this.board[mergedThisMove[i]]);
+      }
+    }
+
+    if (moved) {
+      this.addRandomCellAfterMoving();
     }
 
     return moved;
@@ -159,11 +243,16 @@ class Game {
       const canMerge = new Array(newColumn.length).fill(true);
 
       for (let i = 0; i < newColumn.length - 1; i++) {
-        if (newColumn[i] === newColumn[i + 1]) {
+        if (
+          newColumn[i] === newColumn[i + 1] &&
+          canMerge[i] &&
+          canMerge[i + 1]
+        ) {
           newColumn[i] *= 2;
           newColumn.splice(i + 1, 1);
           moved = true;
           canMerge[i] = false;
+          this.getScore(moved, newColumn[i]);
         }
       }
 
@@ -172,6 +261,10 @@ class Game {
       }
 
       moved = this.updateColumn(col, newColumn) || moved;
+    }
+
+    if (moved) {
+      this.addRandomCellAfterMoving();
     }
 
     return moved;
@@ -185,11 +278,16 @@ class Game {
       const canMerge = new Array(newColumn.length).fill(true);
 
       for (let i = newColumn.length - 1; i > 0; i--) {
-        if (newColumn[i] === newColumn[i - 1]) {
+        if (
+          newColumn[i] === newColumn[i - 1] &&
+          canMerge[i] &&
+          canMerge[i - 1]
+        ) {
           newColumn[i] *= 2;
           newColumn.splice(i - 1, 1);
           moved = true;
           canMerge[i] = false;
+          this.getScore(moved, newColumn[i]);
         }
       }
 
@@ -200,18 +298,24 @@ class Game {
       moved = this.updateColumn(col, newColumn) || moved;
     }
 
+    if (moved) {
+      this.addRandomCellAfterMoving();
+    }
+
     return moved;
   }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
-
+  getScore(moved, value) {
+    if (moved && value > 0) {
+      this.score += value; // додаємо значення плитки до рахунку
+    }
+  }
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
   /**
    * Returns the current game status.
    *
@@ -222,17 +326,50 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    if (this.score === 0) {
+      this.status = IDLE;
+    } else {
+      let hasEmptyCell = false;
+      let has2048Cell = false;
+
+      for (let row = 0; row < this.board.length; row++) {
+        for (let col = 0; col < this.board[row].length; col++) {
+          if (this.board[row][col] === 0) {
+            hasEmptyCell = true;
+          }
+
+          if (this.board[row][col] === 2048) {
+            has2048Cell = true;
+          }
+        }
+      }
+
+      if (has2048Cell) {
+        this.status = WIN;
+      } else if (hasEmptyCell) {
+        this.status = PLAYING;
+      } else {
+        this.status = LOSE;
+      }
+    }
+
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.resetGameStart();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.resetGameStart();
+  }
 
   // Add your own methods here
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startButton = document.querySelector('.start');
+const cells = document.querySelectorAll('.field-cell');
+
+startButton.addEventListener('click', (e) => {
+  startButton.textContent = 'Restart';
+  startButton.classList.remove('start');
+  startButton.classList.add('restart');
+
+  cells.forEach((cell) => {
+    cell.classList.remove('hidden');
+  });
+
+  game.start();
+});
