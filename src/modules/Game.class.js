dc0798575743319e1be273f7ccf7703a53ac364b
Romanov <freeromanov@gmail.com>
2024-11-07 23:11:16 +0200

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -43,22 +43,6 @@ class Game {
     }
   }
 
-  saveBoardState() {
-    return structuredClone(this.board);
-  }
-
-  hasBoardChanged(oldBoard, newBoard) {
-    for (let i = 0; i < oldBoard.length; i++) {
-      for (let j = 0; j < oldBoard[i].length; j++) {
-        if (oldBoard[i][j] !== newBoard[i][j]) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
   initializeGame(message = '') {
     this.board = this.createEmptyBoard();
     this.score = 0;
