6a669c2a7a22fcb676c004b5f6b109e6c0f1b07e
D0dgerJ <d0dgerj89@gmail.com>
2024-11-26 16:50:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,75 @@
-'use strict';
+const Game = require('../modules/Game.class');
+const game = new Game();
+const cells = document.querySelectorAll('.field-cell');
+const buttonStart = document.querySelector('.button.start');
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+document.addEventListener('keydown', (e) => {
+  if (game.getStatus() === 'playing') {
+    switch (e.key) {
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
 
-// Write your code here
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+    }
+
+    updateView();
+  }
+});
+
+function updateView() {
+  let i = 0;
+  const state = game.getState();
+
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) {
+      const cell = cells[i];
+
+      cell.className = 'field-cell';
+
+      if (state[row][col]) {
+        cell.textContent = state[row][col];
+        cell.classList.add(`field-cell--${state[row][col]}`);
+      } else {
+        cell.textContent = '';
+      }
+      i++;
+    }
+  }
+  document.querySelector('.game-score').textContent = game.getScore();
+
+  const statusGame = game.getStatus();
+
+  if (statusGame === 'win') {
+    document.querySelector('.message-win').classList.remove('hidden');
+  } else if (statusGame === 'lose') {
+    document.querySelector('.message-lose').classList.remove('hidden');
+  }
+}
+
+buttonStart.addEventListener('click', () => {
+  if (buttonStart.className === 'button restart') {
+    game.restart();
+  }
+
+  game.start();
+
+  updateView();
+
+  document.querySelector('.message-win').classList.add('hidden');
+  document.querySelector('.message-lose').classList.add('hidden');
+  document.querySelector('.message-start').classList.add('hidden');
+
+  buttonStart.textContent = 'Restart';
+  buttonStart.className = 'button restart';
+});
