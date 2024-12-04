6f823c8b11076fb1d0382183b2aa794cbc88af19
Ivan <fanmetalheavy@gmail.com>
2024-12-04 20:37:18 +0200

    add basic game functionality without updating the record

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,11 +1,198 @@
+/* eslint-disable max-len */
 'use strict';
+// #region start game
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
+function getRandomIndexEmptyCells(currentIndexEmptyCells, gameStatus) {
+  let firstNum;
+  let secondNum;
+
+  if (currentIndexEmptyCells.length === 0) {
+    return [];
+  }
+
+  if (gameStatus === 'playing') {
+    do {
+      firstNum = Math.floor(Math.random() * 16);
+    } while (!currentIndexEmptyCells.includes(firstNum));
+
+    return [firstNum];
+  }
+
+  do {
+    firstNum = Math.floor(Math.random() * 16);
+    secondNum = Math.floor(Math.random() * 16);
+  } while (
+    !currentIndexEmptyCells.includes(firstNum) ||
+    !currentIndexEmptyCells.includes(secondNum) ||
+    firstNum === secondNum
+  );
+
+  return [firstNum, secondNum];
+}
+
+function getIndexEmptyCells(state) {
+  const indexEmptyCells = [];
+
+  state.flat().forEach((num, i) => {
+    if (num === 0) {
+      indexEmptyCells.push(i);
+    }
+  });
+
+  return indexEmptyCells;
+}
+
+function getNewFlatState(currentState, randomIndexEmptyCells) {
+  const newFlatState = [];
+
+  currentState.flat().forEach((num, i) => {
+    if (randomIndexEmptyCells.includes(i)) {
+      newFlatState.push(getProbabilityFour());
+    } else {
+      newFlatState.push(num);
+    }
+  });
+
+  return newFlatState;
+}
+
+function setCells(flatState) {
+  const cells = [...document.getElementsByClassName('field-cell')];
+
+  cells.forEach((cell, i) => {
+    cell.textContent = '';
+    cell.className = 'field-cell';
+
+    if (flatState[i]) {
+      cell.textContent = flatState[i];
+      cell.classList.add(`field-cell--${flatState[i]}`);
+    }
+  });
+}
+
+function getTwoDimensionalState(flatState) {
+  const twoDimensionalState = [];
+  let start = 0;
+
+  for (let i = 1; i <= flatState.length; i++) {
+    if (i % 4 === 0) {
+      twoDimensionalState.push(flatState.slice(start, i));
+      start = i;
+    }
+  }
+
+  return twoDimensionalState;
+}
+
+function getProbabilityFour() {
+  if (Math.floor(Math.random() * 10) === 0) {
+    return 4;
+  } else {
+    return 2;
+  }
+}
+// #endregion start game
+
+// #region move cells
+
+function moveVertically(state, down = false) {
+  const newState = [[], [], [], []];
+
+  state.forEach((_, i) => {
+    const column = Array.from({ length: 4 }, (__, j) => state[j][i]);
+
+    if (down) {
+      column.reverse();
+    }
+
+    const itemsInColumn = column.filter((cell) => cell);
+
+    if (column.every((cell) => !cell)) {
+      newState.forEach((row) => row.push(0));
+    }
+
+    if (itemsInColumn.length === 1) {
+      column.sort((a, b) => b - a);
+      newState.forEach((row, j) => row.push(column[j]));
+    }
+
+    if (itemsInColumn.length > 1) {
+      const newCol = compareAdjacentCells(itemsInColumn).filter((cell) => cell);
+
+      newState.forEach((row, j) => row.push(newCol[j] || 0));
+    }
+  });
+
+  if (down) {
+    newState.reverse();
+  }
+
+  return newState;
+}
+
+function moveHorizontally(state, right = false) {
+  const newState = [[], [], [], []];
+
+  state.forEach((_, i) => {
+    const row = Array.from({ length: 4 }, (__, j) => state[i][j]);
+
+    if (right) {
+      row.reverse();
+    }
+
+    const itemsInRow = row.filter((cell) => cell);
+
+    if (row.every((cell) => !cell)) {
+      row.forEach(() => newState[i].push(0));
+    }
+
+    if (itemsInRow.length === 1) {
+      newState[i] = row.sort((a, b) => b - a);
+    }
+
+    if (itemsInRow.length > 1) {
+      const newRow = compareAdjacentCells(itemsInRow).filter((cell) => cell);
+
+      for (let j = 0; j < 4; j++) {
+        newState[i].push(newRow[j] || 0);
+      }
+    }
+  });
+
+  if (right) {
+    newState.forEach((row) => row.reverse());
+  }
+
+  return newState;
+}
+
+function compareAdjacentCells(line) {
+  const newLine = Array(4).fill(0);
+
+  for (let i = 0; i < line.length; ) {
+    if (line[i] === line[i + 1]) {
+      newLine[i] = line[i] + line[i + 1];
+      i += 2;
+    } else {
+      newLine[i] = line[i];
+      i++;
+    }
+  }
+
+  return newLine;
+}
+
+// #endregion move cells
+
 class Game {
+  gameStatus = 'idle';
+  currentState = [];
+  startState = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
   /**
    * Creates a new game instance.
    *
@@ -25,10 +212,45 @@ class Game {
     console.log(initialState);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    const newState = moveHorizontally(this.currentState);
+
+    if (JSON.stringify(this.currentState) === JSON.stringify(newState)) {
+      return;
+    }
+
+    this.getState(newState);
+  }
+
+  moveRight() {
+    const newState = moveHorizontally(this.currentState, true);
+
+    if (JSON.stringify(this.currentState) === JSON.stringify(newState)) {
+      return;
+    }
+
+    this.getState(newState);
+  }
+
+  moveUp() {
+    const newState = moveVertically(this.currentState);
+
+    if (JSON.stringify(this.currentState) === JSON.stringify(newState)) {
+      return;
+    }
+
+    this.getState(newState);
+  }
+
+  moveDown() {
+    const newState = moveVertically(this.currentState, true);
+
+    if (JSON.stringify(this.currentState) === JSON.stringify(newState)) {
+      return;
+    }
+
+    this.getState(newState);
+  }
 
   /**
    * @returns {number}
@@ -38,7 +260,21 @@ class Game {
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState(state) {
+    const indexEmptyCells = getIndexEmptyCells(state);
+
+    const randomIndexEmptyCells = getRandomIndexEmptyCells(
+      indexEmptyCells,
+      this.gameStatus,
+    );
+
+    const newFlatState = getNewFlatState(state, randomIndexEmptyCells);
+
+    this.currentState = getTwoDimensionalState(newFlatState);
+    setCells(newFlatState);
+    this.searchMoveOpportunity(newFlatState, this.currentState);
+    this.search2048Cell(newFlatState);
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +286,51 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.gameStatus;
+  }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    this.getState(this.startState);
+    this.gameStatus = 'playing';
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  restart() {
+    this.gameStatus = 'idle';
+    this.getState(this.startState);
+    this.gameStatus = 'playing';
+  }
 
   // Add your own methods here
+  searchMoveOpportunity(flatState, state) {
+    if (flatState.every((cell) => cell)) {
+      let hasEqualCells = false;
+
+      for (let i = 0; i < state.length; i++) {
+        const column = Array.from({ length: 4 }, (__, j) => state[j][i]);
+        const row = Array.from({ length: 4 }, (__, j) => state[i][j]);
+
+        if (
+          column.some((cell, j) => j > 0 && cell === column[j - 1]) ||
+          row.some((cell, j) => j > 0 && cell === row[j - 1])
+        ) {
+          hasEqualCells = true;
+        }
+      }
+
+      if (!hasEqualCells) {
+        this.gameStatus = 'lose';
+      }
+    }
+  }
+
+  search2048Cell(flatState) {
+    flatState.forEach((cell) => {
+      if (cell === 2048) {
+        this.gameStatus = 'win';
+      }
+    });
+  }
 }
 
-module.exports = Game;
+export default Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,72 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
+
+const game = new Game();
 
 // Write your code here
+const startRestart = document.getElementsByClassName('button')[0];
+const message = document.getElementsByClassName('message');
+
+startRestart.onclick = () => {
+  const gameStatus = game.getStatus();
+
+  if (gameStatus === 'idle') {
+    startRestart.classList.remove('start');
+    startRestart.classList.add('restart');
+
+    startRestart.textContent = 'Restart';
+
+    message[2].classList.toggle('hidden');
+
+    game.start();
+  }
+
+  if (gameStatus === 'playing') {
+    game.restart();
+  }
+
+  if (gameStatus === 'lose') {
+    game.restart();
+    message[0].classList.toggle('hidden');
+  }
+
+  if (gameStatus === 'win') {
+    game.restart();
+    message[1].classList.toggle('hidden');
+  }
+};
+
+const keydownHandler = (e) => {
+  if (game.getStatus() === 'idle') {
+    return;
+  }
+
+  if (e.key === 'ArrowUp') {
+    game.moveUp();
+  }
+
+  if (e.key === 'ArrowRight') {
+    game.moveRight();
+  }
+
+  if (e.key === 'ArrowDown') {
+    game.moveDown();
+  }
+
+  if (e.key === 'ArrowLeft') {
+    game.moveLeft();
+  }
+
+  if (game.getStatus() === 'lose') {
+    message[0].classList.toggle('hidden');
+    removeEventListener('keydown', keydownHandler);
+  }
+
+  if (game.getStatus() === 'win') {
+    message[1].classList.toggle('hidden');
+    removeEventListener('keydown', keydownHandler);
+  }
+};
+
+addEventListener('keydown', keydownHandler);
