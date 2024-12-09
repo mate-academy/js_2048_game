7fbc269f73de2ccd95dfec36d123b665768f7921
Andrii Dotsulenko <58427426+Andrew77788@users.noreply.github.com>
2024-12-09 11:47:14 +0200

    Your commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
 
-// Write your code here
+const game = new Game();
+
+// Запуск гри
+game.updateBoard();
+
+// Додавання обробників подій
+document.addEventListener('keydown', (e) => {
+  const allowedKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+
+  if (allowedKeys.includes(event.key)) {
+    game.move(e.key);
+  }
+});
+
+document.querySelector('.start').addEventListener('click', () => {
+  game.start();
+});
