29e94c6567e79cca5630a16eb245a4eb1d6d0265
Igor <igor.olshanskyi@gmail.com>
2024-12-31 14:32:24 +0200

    test_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,62 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
 
-// Write your code here
+import Game from '../modules/Game.class';
+
+const game = new Game();
+const gameScore = document.querySelector('.game-score');
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+  button.classList.toggle('restart');
+
+  if (document.querySelector('.restart')) {
+    document.querySelector('.restart').textContent = 'Restart';
+    game.start();
+  }
+  button.classList.toggle('start');
+
+  if (document.querySelector('.start')) {
+    document.querySelector('.start').textContent = 'Start';
+    game.restart();
+    gameScore.textContent = game.getScore();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+  }
+  gameScore.textContent = game.getScore();
+  game.getState();
+  game.getStatus();
+});
+
+/** button.addEventListener('click', () => {
+    button.classList.toggle('restart');
+    button.classList.toggle('start');
+    button.textContent = button
+    .classList.contains('restart') ? 'Restart' : 'Start';
+});
+
+button.addEventListener('click', (e) => {
+  // e.preventDefault();
+
+  if (button.classList.contains('restart')) {
+    game.start();
+  } else {
+    game.restart();
+  }
+}); */
