70e6dce6debb8e5975b81ab57f5d73683abea5d0
Blervin <blervin121212@gmail.com>
2025-01-07 19:40:17 +0100

    2048 game (pink)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,90 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const scoreResult = document.querySelector('.game-score');
+  const startButton = document.querySelector('.start');
+  const gameRows = document.querySelectorAll('.field-row');
+
+  function removeModificators(item) {
+    const classes = item.className.split(' ');
+
+    classes.forEach((className) => {
+      if (/^field-cell--\d+$/.test(className)) {
+        item.classList.remove(className);
+      }
+    });
+  }
+
+  function checkResult(result) {
+    if (result === 'win' || result === 'lose') {
+      const message = document.querySelector(`.message-${result}`);
+
+      message.classList.remove('hidden');
+    }
+  }
+
+  function replaceItems() {
+    const state = game.getState();
+
+    for (let indexRow = 0; indexRow < state.length; indexRow++) {
+      const row = gameRows[indexRow].querySelectorAll('.field-cell');
+
+      for (let indexCail = 0; indexCail < row.length; indexCail++) {
+        const cailNumber = state[indexRow][indexCail];
+
+        removeModificators(row[indexCail]);
+
+        if (cailNumber !== 0) {
+          row[indexCail].classList.add(`field-cell--${cailNumber}`);
+          row[indexCail].innerText = cailNumber;
+        } else {
+          row[indexCail].innerText = '';
+        }
+      }
+    }
+  }
+
+  startButton.addEventListener('click', () => {
+    const messageStart = document.querySelector('.message-start');
+
+    if (game.getStatus() === 'idle') {
+      game.start();
+    } else {
+      game.restart();
+      location.reload();
+    }
+    messageStart.classList.add('hidden');
+    startButton.classList.remove('start');
+    startButton.classList.add('restart');
+    startButton.innerText = 'Restart';
+    replaceItems();
+  });
+
+  document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+
+    switch (e.key) {
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+    }
+    replaceItems();
+    scoreResult.innerText = game.getScore();
+
+    checkResult(game.getStatus());
+  });
+});
+
+// my github Blervin1 https://github.com/Blervin1
