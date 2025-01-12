f8128d28cf0dabad7841be4f0a2fb7a87e451027
Vitalii <v.hulaievych@gmail.com>
2025-01-12 18:40:46 +0000

    run

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,160 @@
 'use strict';
-
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor() {
+    this.initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.score = 0;
+    this.status = 'idle';
+    this.board = this.initialState.map((item) => this.item.slice());
+    this.size = 4;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  start() {
+    this.board = this.initialState.map((item) => item.slice());
+    this.status = 'playing';
+    this.addRandom();
+    this.addRandom();
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  restart() {
+    this.board = this.initialState.map((item) => item.slice());
+    this.score = 0;
+    this.status = 'idle';
+  }
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+  moveLeft() {
+    let moved = false;
+
+    for (let i = 0; i < this.size; i++) {
+      let row = this.board.filter((item) => item !== 0);
+
+      for (let j = 0; j < row.length - 1; j++) {
+        if (row[j] === row[j + 1]) {
+          row[j] *= 2;
+          row[j + 1] = 0;
+          this.score += row[j];
+        }
+      }
+      row = row.filter((item) => item !== 0);
+
+      while (row.length < this.size) {
+        row.push(0);
+      }
+
+      if (this.board[i].join('') !== row.join('')) {
+        moved = true;
+      }
+
+      this.board = row;
+
+      if (moved) {
+        this.postMoved();
+      }
+    }
+  }
+
+  moveRight() {
+    this.reverseRows();
+    this.moveLeft();
+    this.reverseRows();
+  }
+
+  moveUp() {
+    this.transpose();
+    this.moveLeft();
+    this.transpose();
+  }
+
+  moveDown() {
+    this.transpose();
+    this.moveRight();
+    this.transpose();
+  }
+
+  reverseRows() {
+    this.board.forEach((row) => row.reverse());
+  }
+
+  transpose() {
+    this.board = this.board[0].map((_, index) => {
+      this.board.map((row) => row[index]);
+    });
+  }
+
+  postMoved() {
+    this.addRandom();
+
+    if (this.checkWin()) {
+      this.status = 'win';
+    }
+
+    if (this.checkLose()) {
+      this.status = 'lose';
+    }
+  }
+
+  addRandom() {
+    const emptyCeels = [];
+
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (this.board[i][j] === 0) {
+          emptyCeels.push({ x: i, y: j });
+        }
+      }
+    }
+
+    if (emptyCeels.length === 0) {
+      return null;
+    }
+
+    const { x, y } = emptyCeels[Math.floor(Math.random() * emptyCeels.length)];
+
+    this.board[x][y] = Math.random() < 0.9 ? 2 : 4;
+  }
+
+  checkWin() {
+    return this.board.some((row) => row.includes('2048'));
+  }
+
+  checkLose() {
+    return !this.board.some((row, i) => {
+      row.some(
+        (item, j) =>
+          item === 0 ||
+          (i > 0 && item === this.board[i - 1][j]) ||
+          (i < this.size - 1 && item === this.board[i + 1][j]) ||
+          (j > 0 && item === this.board[i][j - 1]) ||
+          (j < this.size - 1 && item === this.board[i][j + 1]),
+      );
+    });
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
    *
    * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
    *
-   * `idle` - the game has not started yet (the initial state);
+   * `idle` - the game has not started yet (the initias.board);
    * `playing` - the game is in progress;
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
-
-  /**
-   * Starts the game.
-   */
-  start() {}
-
-  /**
-   * Resets the game.
-   */
-  restart() {}
-
-  // Add your own methods here
+  getStatus() {
+    return this.status;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,84 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startGame = document.querySelector('.buttun.start');
+const scoreElement = document.querySelector('.game-score');
+const gameField = document.querySelector('.game-field');
+const messageStart = document.querySelector('.message-start');
+const messageWin = document.querySelector('.message-win');
+const messageLose = document.querySelector('.message-lose');
+
+function updateBoard() {
+  const board = game.getState();
+  const rows = gameField.querySelectorAll('.field-row');
+
+  board.forEach((row, i) => {
+    const cells = rows[i].querySelectorAll('.field-cell');
+
+    row.forEach((value, j) => {
+      cells[j].textContent = value === 0 ? '' : value;
+
+      cells[j].className =
+        `field-cell--${value ? 'field-cell--' + 'value' : ''}`;
+    });
+
+    scoreElement.textContent = game.getScore();
+  });
+}
+
+function updateMessage() {
+  const statusGame = game.getStatus();
+
+  messageStart.classList.add('hidden');
+  messageLose.classList.add('hidden');
+  messageWin.classList.add('hidden');
+
+  if (statusGame === 'idle') {
+    messageStart.classList.remove('hidden');
+  } else if (statusGame === 'win') {
+    messageWin.classList.remove('hidden');
+  } else if (statusGame === 'lose') {
+    messageLose.classList.remove('hidden');
+  }
+}
+
+// Слухач для клавіш стрілок
+document.addEventListener('keydown', handleKeyPress);
+
+function handleKeyPress() {
+  if (game.getStatus !== 'playing') {
+    return null;
+  }
+
+  switch (event.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    default:
+      return null;
+  }
+
+  updateBoard();
+  updateMessage();
+}
+
+startGame.addEventListener('click', () => {
+  game.start();
+  updateBoard();
+  updateMessage();
+});
+
+// Початковий стан
+updateBoard();
+updateMessage();
