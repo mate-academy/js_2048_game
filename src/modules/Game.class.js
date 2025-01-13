b4ef8967d3ca252a246d583df060f3e9fd9e49a5
Iryna <irakogut23190@gmail.com>
2025-01-13 21:47:27 +0200

    solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,3 +1,4 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
 /**
@@ -20,25 +21,81 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+  constructor(initialState = null) {
+    if (initialState) {
+      this.board = initialState;
+    } else {
+      this.board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+    }
     // eslint-disable-next-line no-console
     console.log(initialState);
+
+    this.score = 0;
+    this.isStarted = false;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    for (let row = 0; row < 4; row++) {
+      const newRow = this.board[row].filter((cell) => cell !== 0);
+
+      for (let cell = 0; cell < newRow.length - 1; cell++) {
+        if (newRow[cell] === newRow[cell + 1]) {
+          newRow[cell] = newRow[cell] + newRow[cell + 1];
+          newRow[cell + 1] = 0;
+          cell++;
+          this.score += newRow[cell];
+        }
+      }
+
+      while (newRow.length < 4) {
+        newRow.push(0);
+      }
+
+      this.board[row] = newRow;
+    }
+  }
+  moveRight() {
+    for (let row = 0; row < 4; row++) {
+      this.board[row] = this.board[row].reverse();
+
+      this.moveLeft();
+
+      this.board[row] = this.board[row].reverse();
+    }
+  }
+  moveUp() {
+    this.board = this.transpose(this.board);
+
+    this.moveLeft();
+
+    this.board = this.transpose(this.board);
+  }
+  moveDown() {
+    this.board = this.transpose(this.board);
+
+    this.moveRight();
+
+    this.board = this.transpose(this.board);
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +107,97 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    // when you started play
+    if (!this.isStarted) {
+      return 'idle';
+    }
+
+    // check for win
+    if (this.board.some((row) => row.some((cell) => cell === 2048))) {
+      return 'win';
+    }
+
+    // check for lose
+    const emptyCells = this.board.some((row) => row.some((cell) => cell === 0));
+
+    const canMerge = this.board.some((row, rowIndex) =>
+      row.some((cell, cellINdex) => {
+        const right =
+          cellINdex < 3 && this.board[rowIndex][cellINdex + 1] === cell;
+        const down =
+          rowIndex < 3 && this.board[rowIndex + 1][cellINdex] === cell;
+
+        return right || down;
+      }),
+    );
+
+    if (!emptyCells && !canMerge) {
+      return 'lose';
+    }
+
+    // for playing
+    return 'playing';
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.score = 0;
+    this.status = 'playing';
+
+    this.randomPlace();
+    this.randomPlace();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    this.randomPlace();
+    this.randomPlace();
+
+    this.score = 0;
+
+    this.status = 'playing';
+  }
 
   // Add your own methods here
+
+  transpose(matrix) {
+    return matrix[0].map((_, index) => matrix.map((row) => row[index]));
+  }
+
+  randomPlace() {
+    const emptyCells = [];
+
+    for (let row = 0; row < 4; row++) {
+      for (let column = 0; column < 4; column++) {
+        if (this.board[row][column] === 0) {
+          emptyCells.push([row][column]);
+        }
+      }
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const [randomRow, randomColumn] = emptyCells[randomIndex];
+
+    this.board[randomRow][randomColumn] = Math.random() < 0.9 ? 2 : 4;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const buttonStart = document.querySelector('.start');
+
+buttonStart.addEventListener('click', () => {
+  game.start();
+});
