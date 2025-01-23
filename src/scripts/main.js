d8deafe8d82ed9083fa836a0d970265f33f93258
Flávio Miranda <nascimentoflavio588@hotmail.com>
2025-01-23 14:04:47 -0300

    Add fix challenger - 2048 with JavaScript Advanced

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,91 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
 
-// Write your code here
+const game = new Game();
+
+const startButton = document.querySelector('.button.start');
+const scoreElement = document.querySelector('.game-score');
+const gameField = document.querySelector('.game-field tbody');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
+
+startButton.addEventListener('click', () => {
+  if (
+    game.getStatus() === 'idle' ||
+    game.getStatus() === 'lose' ||
+    game.getStatus() === 'win'
+  ) {
+    game.restart();
+    startButton.textContent = 'Restart';
+    messageLose.classList.add('hidden');
+    messageWin.classList.add('hidden');
+    messageStart.classList.add('hidden');
+    updateUI();
+  }
+});
+
+document.addEventListener('keydown', (ev) => {
+  if (game.getStatus() !== 'playing') {
+    return;
+  }
+
+  switch (ev.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    default:
+      return;
+  }
+
+  updateUI();
+});
+
+function updateUI() {
+  const gameState = game.getState();
+  const stt = game.getStatus();
+
+  scoreElement.textContent = game.getScore();
+
+  gameField.innerHTML = '';
+
+  gameState.forEach((row) => {
+    const rowElement = document.createElement('tr');
+
+    rowElement.classList.add('field-row');
+
+    row.forEach((cell) => {
+      const cellElement = document.createElement('td');
+
+      cellElement.classList.add('field-cell');
+
+      if (cell > 0) {
+        cellElement.textContent = cell;
+        cellElement.classList.add(`field-cell--${cell}`);
+      }
+      rowElement.appendChild(cellElement);
+    });
+    gameField.appendChild(rowElement);
+  });
+
+  if (stt === 'win') {
+    messageWin.classList.remove('hidden');
+  } else if (stt === 'lose') {
+    messageLose.classList.remove('hidden');
+  } else if (stt === 'playing') {
+    messageStart.classList.add('hidden');
+  }
+}
+
+game.start();
+updateUI();
