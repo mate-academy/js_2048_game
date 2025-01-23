44af4ad254571ee065c68a1a74ce295b8f08795b
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2025-01-23 18:29:01 +0200

    Task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -20,25 +20,82 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.board = initialState;
+    this.score = 0;
+    this.status = `idle`;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  transposeBoard() {
+    this.board = this.board[0].map(
+      (_, colIndex) => this.board.map((row) => row[colIndex]),
+      // eslint-disable-next-line function-paren-newline
+    );
+  }
+
+  moveLeft() {
+    const newBoard = this.board.map((row) => {
+      const filteredRow = row.filter((cell) => cell > 0);
+
+      for (let i = 0; i < filteredRow.length - 1; i++) {
+        if (filteredRow[i] === filteredRow[i + 1]) {
+          filteredRow[i] *= 2;
+          this.score += filteredRow[i];
+          filteredRow.splice(i + 1, 1);
+        }
+      }
+
+      while (filteredRow.length < 4) {
+        filteredRow.push(0);
+      }
+
+      return filteredRow;
+    });
+
+    if (JSON.stringify(newBoard) !== JSON.stringify(this.board)) {
+      this.board = newBoard;
+      this.generateRandomTile();
+    }
+  }
+
+  moveRight() {
+    this.board = this.board.map((row) => row?.reverse());
+    this.moveLeft();
+    this.board = this.board.map((row) => row?.reverse());
+  }
+
+  moveUp() {
+    this.transposeBoard();
+    this.moveLeft();
+    this.transposeBoard();
+  }
+
+  moveDown() {
+    this.transposeBoard();
+    this.moveRight();
+    this.transposeBoard();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board.map((row) => [...row]);
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +107,93 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    this.status = 'playing';
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.score = 0;
+    this.generateRandomTile();
+    this.generateRandomTile();
+  }
 
-  // Add your own methods here
+  restart() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    this.score = 0;
+
+    this.status = 'idle';
+
+    this.generateRandomTile();
+    this.generateRandomTile();
+
+    this.status = 'playing';
+  }
+
+  generateRandomTile() {
+    const emptyCells = [];
+
+    this.board.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        if (cell === 0) {
+          emptyCells.push([i, j]);
+        }
+      });
+    });
+
+    if (emptyCells.length === 0) {
+      return;
+    }
+
+    const [x, y] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.board[x][y] = Math.random() < 0.1 ? 4 : 2;
+  }
+
+  canMove() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.board[i][j] === 0) {
+          return true;
+        }
+
+        if (j < 3 && this.board[i][j] === this.board[i][j + 1]) {
+          return true;
+        }
+
+        if (i < 3 && this.board[i][j] === this.board[i + 1][j]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  updateBoard() {
+    if (!this.canMove()) {
+      this.status = 'lose';
+    }
+
+    this.board.forEach((row) => {
+      if (row.find((el) => el === 2048)) {
+        this.status = 'win';
+      }
+    });
+  }
 }
 
 module.exports = Game;
