8f35e0e2f0f5a0364b5d4f4cb78ed51527e42836
DavidKuznets <davidkuznets7@gmail.com>
2025-02-12 16:42:10 +0200

    Solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -166,39 +166,4 @@ class Game {
   }
 }
 
-const game2048 = new Game();
-
-document.getElementById('start-restart-btn').addEventListener('click', () => {
-  if (game2048.getStatus() === 'idle') {
-    game2048.start();
-  } else {
-    game2048.restart();
-  }
-});
-
-document.addEventListener('keydown', (e) => {
-  if (game2048.getStatus() !== 'playing') {
-    return;
-  }
-
-  switch (e.key) {
-    case 'ArrowLeft':
-      game2048.moveLeft();
-      break;
-    case 'ArrowRight':
-      game2048.moveRight();
-      break;
-    case 'ArrowUp':
-      game2048.moveUp();
-      break;
-    case 'ArrowDown':
-      game2048.moveDown();
-      break;
-  }
-
-  if (game2048.getStatus() === 'gameover') {
-    alert('Game Over!');
-  }
-});
-
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,44 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
 
-// Write your code here
+// document.getElementById('start-restart-btn').addEventListener(
+// 'click', () => {
+
+import Game from '../modules/Game.class.js';
+
+const game2048 = new Game();
+const button = document.querySelector('.start-restart-btn');
+
+button.addEventListener('click', () => {
+  if (game2048.getStatus() === 'idle') {
+    game2048.start();
+  } else {
+    game2048.restart();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (game2048.getStatus() !== 'playing') {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      game2048.moveLeft();
+      break;
+    case 'ArrowRight':
+      game2048.moveRight();
+      break;
+    case 'ArrowUp':
+      game2048.moveUp();
+      break;
+    case 'ArrowDown':
+      game2048.moveDown();
+      break;
+  }
+
+  if (game2048.getStatus() === 'gameover') {
+    alert('Game Over!');
+  }
+});
