4b3389de616e3540ca9f6baa2e35f059bb28904b
Tomas <tomas7777tomas@gmail.com>
2025-02-22 19:39:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,75 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class.js';
 
-// Write your code here
+const game2048 = new Game();
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', (e) => {
+  if (e.target.classList.contains('start')) {
+    game2048.start();
+    e.target.classList.replace('start', 'restart');
+    e.target.textContent = 'Restart';
+    checkStatus(game2048.status);
+  } else if (e.target.classList.contains('restart')) {
+    game2048.restart();
+    e.target.classList.replace('restart', 'start');
+    e.target.textContent = 'Start';
+    score.textContent = game2048.score;
+    checkStatus(game2048.status);
+  }
+});
+
+const score = document.querySelector('.game-score');
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft') {
+    game2048.moveLeft();
+    score.textContent = game2048.score;
+    checkStatus(game2048.status);
+  }
+
+  if (e.key === 'ArrowRight') {
+    game2048.moveRight();
+    score.textContent = game2048.score;
+    checkStatus(game2048.status);
+  }
+
+  if (e.key === 'ArrowUp') {
+    game2048.moveUp();
+    score.textContent = game2048.score;
+    checkStatus(game2048.status);
+  }
+
+  if (e.key === 'ArrowDown') {
+    game2048.moveDown();
+    score.textContent = game2048.score;
+    checkStatus(game2048.status);
+  }
+});
+
+function checkStatus(st) {
+  const start = document.querySelector('.message.message-start');
+  const win = document.querySelector('.message.message-win');
+  const lose = document.querySelector('.message.message-lose');
+
+  if (st === 'win') {
+    win.classList.remove('hidden');
+    start.classList.add('hidden');
+    lose.classList.add('hidden');
+  } else if (st === 'lose') {
+    lose.classList.remove('hidden');
+    start.classList.add('hidden');
+    win.classList.add('hidden');
+  } else if (st === 'idle') {
+    start.classList.remove('hidden');
+    win.classList.add('hidden');
+    lose.classList.add('hidden');
+  } else if (st === 'playing') {
+    start.classList.add('hidden');
+    win.classList.add('hidden');
+    lose.classList.add('hidden');
+  }
+}
