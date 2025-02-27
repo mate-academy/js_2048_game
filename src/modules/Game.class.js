b442198530353047578cb9f95b406aa882856844
vkrasnovyd <v.krasnovyd@gmail.com>
2025-02-27 14:24:22 +0100

    Add addRandomTile method

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -47,6 +47,19 @@ class Game {
   }
 
   /**
+   * Setter method for this.state. Replaces the value of the cell with the
+   * provided row and cell indexes with 'newValue'.
+   *
+   * @param {number} rowIndex - The index of the row.
+   * @param {number} cellIndex - The index of the cell.
+   * @param {number} newValue - The value to set in the cell.
+   * @returns {void}
+   */
+  updateState(rowIndex, cellIndex, newValue) {
+    this.state[rowIndex][cellIndex] = newValue;
+  }
+
+  /**
    * Returns the current game status.
    *
    * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
@@ -70,7 +83,39 @@ class Game {
    */
   restart() {}
 
-  // Add your own methods here
+  /**
+   * Returns array of coordinates (row index and column index)
+   * of each empty cell.
+   *
+   * @returns {[number, number][]}
+   */
+  getEmptyCellsCoordinates() {
+    const emptyCellsCoordinates = [];
+
+    this.state.forEach((row, rowIndex) => {
+      row.forEach((cellValue, cellIndex) => {
+        if (!cellValue) {
+          emptyCellsCoordinates.push([rowIndex, cellIndex]);
+        }
+      });
+    });
+
+    return emptyCellsCoordinates;
+  }
+
+  /**
+   * Replaces value of random empty cell with 2 or 4. 4 probability is 10%.
+   *
+   * @returns {void}
+   */
+  addRandomTile() {
+    const emptyCells = this.getEmptyCellsCoordinates();
+    const newTileValue = Math.floor(Math.random() * 10) > 8 ? 4 : 2;
+    const newTileIndex = Math.floor(Math.random() * emptyCells.length);
+    const [rowIndex, cellIndex] = emptyCells[newTileIndex];
+
+    this.updateState(rowIndex, cellIndex, newTileValue);
+  }
 }
 
 module.exports = Game;
