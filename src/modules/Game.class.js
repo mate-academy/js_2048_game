9c9f00cca4284cb17f3c451c3e3595a2a0d88bb5
Veronica Shyian <veronica.shyian@gmail.com>
2025-02-28 18:53:34 +0200

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,146 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(initialState = null) {
+    this.board = initialState || this.createEmptyBoard();
+    this.score = 0;
+    this.status = 'idle';
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  createEmptyBoard() {
+    return Array.from({ length: 4 }, () => Array(4).fill(0));
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  appearRandomTile() {
+    const emptyCellsCoord = [];
+    const randomNumber = Math.random() > 0.9 ? 4 : 2;
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    this.board.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        if (cell === 0) {
+          emptyCellsCoord.push([rowIndex, cellIndex]);
+        }
+      });
+    });
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+    if (emptyCellsCoord.length) {
+      const randomIndex = Math.floor(Math.random() * emptyCellsCoord.length);
+      const [rowCoord, cellCoord] = emptyCellsCoord[randomIndex];
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+      this.board[rowCoord][cellCoord] = randomNumber;
+    }
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  shiftAndMerge(row) {
+    const nonEmptyCells = row.filter((cell) => cell !== 0);
+    const shiftRow = [];
 
-  // Add your own methods here
+    while (nonEmptyCells.length) {
+      if (nonEmptyCells[0] === nonEmptyCells[1]) {
+        shiftRow.push(nonEmptyCells[0] * 2);
+        this.score += nonEmptyCells[0] * 2;
+        nonEmptyCells.splice(0, 2);
+      } else {
+        shiftRow.push(nonEmptyCells[0]);
+        nonEmptyCells.splice(0, 1);
+      }
+    }
+
+    while (shiftRow.length < 4) {
+      shiftRow.push(0);
+    }
+
+    return shiftRow;
+  }
+
+  moveLeft() {
+    let hasMoved = false;
+
+    const newBoard = this.board.map((row) => {
+      const newRow = this.shiftAndMerge(row);
+
+      hasMoved = hasMoved || newRow.toString() !== row.toString();
+
+      return newRow;
+    });
+
+    if (hasMoved) {
+      this.board = newBoard;
+      this.appearRandomTile();
+    }
+
+    this.updateStatus();
+
+    return hasMoved;
+  }
+
+  moveRight() {
+    this.board.map((row) => row.reverse());
+
+    const hasMoved = this.moveLeft();
+
+    this.board.map((row) => row.reverse());
+
+    return hasMoved;
+  }
+
+  rotateBoard(board) {
+    return board[0].map((_, colIndex) => board.map((row) => row[colIndex]));
+  }
+
+  moveUp() {
+    this.board = this.rotateBoard(this.board);
+
+    const hasMoved = this.moveLeft();
+
+    this.board = this.rotateBoard(this.board);
+
+    return hasMoved;
+  }
+
+  moveDown() {
+    this.board = this.rotateBoard(this.board);
+
+    const hasMoved = this.moveRight();
+
+    this.board = this.rotateBoard(this.board);
+
+    return hasMoved;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.board;
+  }
+
+  updateStatus() {
+    if (this.board.some((row) => row.includes(2048))) {
+      this.status = 'win';
+    } else if (!this.board.some((row) => row.includes(0))) {
+      this.status = 'lose';
+    } else {
+      this.status = 'playing';
+    }
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  start() {
+    this.board = this.createEmptyBoard();
+    this.score = 0;
+    this.status = 'playing';
+    this.appearRandomTile();
+    this.appearRandomTile();
+  }
+
+  restart() {
+    this.start();
+  }
 }
 
 module.exports = Game;
