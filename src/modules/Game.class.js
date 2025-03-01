0ac62444558e47ac3517c881936d19985b42b8e2
Denys Kovalchuk <denkov414@gmail.com>
2025-03-02 01:01:21 +0200

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,187 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.initialState =
+      initialState || Array.from({ length: 4 }, () => Array(4).fill(0));
+    this.board = JSON.parse(JSON.stringify(this.initialState));
+    this.status = 'idle';
+    this.score = 0;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  start() {
+    this.status = 'playing';
+    this.addRandomTile();
+    this.addRandomTile();
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  moveDown() {
+    const oldBoard = JSON.stringify(this.board);
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    if (this.status !== 'playing') {
+      return;
+    }
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+    for (let col = 0; col < 4; col++) {
+      let column = this.board.map((row) => row[col]).reverse();
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+      column = this.combineTiles(column).reverse();
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+      for (let row = 0; row < 4; row++) {
+        this.board[row][col] = column[row];
+      }
+    }
 
-  // Add your own methods here
+    if (oldBoard !== JSON.stringify(this.board)) {
+      this.addRandomTile();
+    }
+
+    this.endGame();
+  }
+
+  moveUp() {
+    const oldBoard = JSON.stringify(this.board);
+
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    for (let col = 0; col < 4; col++) {
+      let column = this.board.map((row) => row[col]);
+
+      column = this.combineTiles(column);
+
+      for (let row = 0; row < 4; row++) {
+        this.board[row][col] = column[row];
+      }
+    }
+
+    if (oldBoard !== JSON.stringify(this.board)) {
+      this.addRandomTile();
+    }
+
+    this.endGame();
+  }
+
+  moveRight() {
+    const oldBoard = JSON.stringify(this.board);
+
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    for (let row = 0; row < 4; row++) {
+      this.board[row] = this.combineTiles(this.board[row].reverse()).reverse();
+    }
+
+    if (oldBoard !== JSON.stringify(this.board)) {
+      this.addRandomTile();
+    }
+
+    this.endGame();
+  }
+
+  moveLeft() {
+    const oldBoard = JSON.stringify(this.board);
+
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    for (let row = 0; row < 4; row++) {
+      this.board[row] = this.combineTiles(this.board[row]);
+    }
+
+    if (oldBoard !== JSON.stringify(this.board)) {
+      this.addRandomTile();
+    }
+
+    this.endGame();
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.board;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  restart() {
+    this.board = JSON.parse(JSON.stringify(this.initialState));
+    this.status = 'idle';
+    this.score = 0;
+  }
+
+  combineTiles(row) {
+    let newRow = row.filter((val) => val !== 0);
+
+    for (let i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i] === newRow[i + 1]) {
+        newRow[i] *= 2;
+        this.score += newRow[i];
+        newRow[i + 1] = 0;
+      }
+    }
+
+    newRow = newRow.filter((val) => val !== 0);
+
+    return newRow.concat(Array(4 - newRow.length).fill(0));
+  }
+
+  getEmptyCells() {
+    const emptyCells = [];
+
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (this.board[row][col] === 0) {
+          emptyCells.push({ row, col });
+        }
+      }
+    }
+
+    return emptyCells;
+  }
+
+  addRandomTile() {
+    const emptyCells = this.getEmptyCells();
+
+    if (emptyCells.length === 0) {
+      return;
+    }
+
+    const { row, col } =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.board[row][col] = Math.random() < 0.9 ? 2 : 4;
+  }
+
+  endGame() {
+    let gameOver = true;
+
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (this.board[row][col] >= 2048) {
+          this.status = 'win';
+        }
+
+        if (
+          (row < 3 && this.board[row][col] === this.board[row + 1][col]) ||
+          (col < 3 && this.board[row][col] === this.board[row][col + 1])
+        ) {
+          gameOver = false;
+        }
+      }
+    }
+
+    if (this.getEmptyCells().length === 0 && gameOver) {
+      this.status = 'lose';
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,95 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  function updateButton() {
+    const button = document.querySelector('.button');
+
+    if (game.status === 'idle') {
+      button.classList.remove('restart');
+      button.classList.add('start');
+      button.textContent = 'Start';
+    } else {
+      button.classList.remove('start');
+      button.classList.add('restart');
+      button.textContent = 'Restart';
+    }
+  }
+
+  function updateMessage() {
+    switch (game.status) {
+      case 'idle':
+        document.querySelector('.message-start').classList.remove('hidden');
+        document.querySelector('.message-win').classList.add('hidden');
+        document.querySelector('.message-lose').classList.add('hidden');
+        break;
+      case 'playing':
+        document.querySelector('.message-start').classList.add('hidden');
+        break;
+      case 'win':
+        document.querySelector('.message-win').classList.remove('hidden');
+        break;
+      case 'lose':
+        document.querySelector('.message-lose').classList.remove('hidden');
+        break;
+      default:
+        break;
+    }
+
+    updateButton();
+  }
+
+  function getScore() {
+    document.querySelector('.game-score').textContent = game.score;
+  }
+
+  function updateBoard() {
+    const cells = document.querySelectorAll('.field-cell');
+    let index = 0;
+
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        const cell = cells[index];
+
+        cell.textContent = game.board[row][col] || '';
+        cell.className = `field-cell field-cell--${game.board[row][col]}`;
+        index++;
+      }
+    }
+  }
+
+  document.querySelector('.button').addEventListener('click', () => {
+    if (game.status === 'idle') {
+      game.start();
+    } else {
+      game.restart();
+    }
+
+    updateMessage();
+    getScore();
+    updateBoard();
+  });
+
+  document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+    }
+
+    updateMessage();
+    getScore();
+    updateBoard();
+  });
+});
