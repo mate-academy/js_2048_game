fdd2efce9806dea71020ecbf6625c1d45221e0fd
Dmitrii Damyan <dmitriignc@gmail.com>
2025-03-01 12:29:55 +0200

    solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,7 +5,7 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
-class Game {
+export class Game {
   /**
    * Creates a new game instance.
    *
@@ -20,25 +20,93 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+  gameBoard = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  count = 0;
+
+  name = 'field-cell--';
+
+  num2048 = 2048;
+
+  buttonStart = document.querySelector('button');
+
+  messageLose = document.querySelector('.message-lose');
+
+  messageWin = document.querySelector('.message-win');
+
+  messageStart = document.querySelector('.message-start');
+
+  score = document.querySelector('.game-score');
+
+  board = document.querySelector('tbody');
+
+  constructor(initialState = 'idle') {
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.initialState = initialState;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    for (let k = 1; k < 4; k++) {
+      if (this.initialState !== 'win') {
+        this.shiftLeft();
+      } else {
+        this.messageWin.classList.toggle('hidden');
+        break;
+      }
+    }
+  }
+
+  moveRight() {
+    for (let k = 1; k < 4; k++) {
+      if (this.initialState !== 'win') {
+        this.shiftRight();
+      } else {
+        this.messageWin.classList.toggle('hidden');
+        break;
+      }
+    }
+  }
+
+  moveUp() {
+    for (let k = 1; k < 4; k++) {
+      if (this.initialState !== 'win') {
+        this.shiftUp();
+      } else {
+        this.messageWin.classList.toggle('hidden');
+        break;
+      }
+    }
+  }
+
+  moveDown() {
+    for (let k = 1; k < 4; k++) {
+      if (this.initialState !== 'win') {
+        this.shiftDown();
+      } else {
+        this.messageWin.classList.toggle('hidden');
+        break;
+      }
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    this.score.textContent = this.count;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return `One of: ${this.initialState}`;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +118,257 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    this.buttonStart.textContent = 'Start';
+    this.buttonStart.classList.toggle('start');
+    this.buttonStart.classList.toggle('restart');
+    this.initialState = 'idle';
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.initialState = 'playing';
+
+    if (this.messageStart.className === 'message message-start') {
+      this.messageStart.classList.toggle('hidden');
+    }
+
+    if (this.messageLose.className === 'message message-lose') {
+      this.messageLose.classList.toggle('hidden');
+    }
+
+    if (this.messageWin.className === 'message message-win') {
+      this.messageWin.classList.toggle('hidden');
+    }
+
+    this.score = 0;
+    this.fieldClearing();
+
+    this.randomChip();
+    this.randomChip();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.fieldClearing();
+    this.count = 0;
+    this.start();
+  }
 
   // Add your own methods here
+  randomChip() {
+    const index = Math.floor(Math.random() * 16);
+    const num = Math.random() > 0.2 ? 2 : 4;
+    const x = index % 4;
+    const y = Math.floor(index / 4);
+
+    if (this.gameBoard[x][y] === 0) {
+      this.gameBoard[x][y] = num;
+
+      this.board.rows[x].cells[y].classList.toggle(this.name + num);
+      this.board.rows[x].cells[y].textContent = num;
+    } else if (this.gameBoard.includes(0)) {
+      this.randomChip();
+    }
+  }
+
+  fieldClearing() {
+    for (let i = 0; i < this.gameBoard.length; i++) {
+      for (let j = 0; j < this.gameBoard[i].length; j++) {
+        this.gameBoard[i][j] = 0;
+
+        if (this.board.rows[i].cells[j].textContent === 0) {
+          continue;
+        } else {
+          this.board.rows[i].cells[j].textContent = null;
+          this.board.rows[i].cells[j].classList = 'field-cell';
+        }
+      }
+    }
+  }
+
+  shiftRight() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.gameBoard[i][j] === 0) {
+          continue;
+        } else {
+          const n = this.gameBoard[i][j];
+
+          if (j < 3 && this.gameBoard[i][j + 1] === 0) {
+            this.gameBoard[i][j + 1] = n;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i].cells[j + 1].textContent = n;
+            this.board.rows[i].cells[j + 1].classList.toggle(this.name + n);
+          } else if (j < 3 && this.gameBoard[i][j + 1] === n) {
+            this.gameBoard[i][j + 1] = n * 2;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i].cells[j + 1].textContent = n * 2;
+            this.board.rows[i].cells[j + 1].classList.toggle(this.name + n * 2);
+
+            this.count += n * 2;
+            this.getScore();
+
+            this.checking2048();
+          }
+        }
+      }
+    }
+  }
+
+  shiftLeft() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 1; j <= 3; j++) {
+        if (this.gameBoard[i][j] === 0) {
+          continue;
+        } else {
+          const n = this.gameBoard[i][j];
+
+          if (this.gameBoard[i][j - 1] === 0) {
+            this.gameBoard[i][j - 1] = n;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i].cells[j - 1].textContent = n;
+            this.board.rows[i].cells[j - 1].classList.toggle(this.name + n);
+          } else if (this.gameBoard[i][j - 1] === n) {
+            this.gameBoard[i][j - 1] = n * 2;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i].cells[j - 1].textContent = n * 2;
+            this.board.rows[i].cells[j - 1].classList.toggle(this.name + n * 2);
+
+            this.count += n * 2;
+            this.getScore();
+
+            this.checking2048();
+          }
+        }
+      }
+    }
+  }
+
+  shiftUp() {
+    for (let i = 1; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.gameBoard[i][j] === 0) {
+          continue;
+        } else {
+          const n = this.gameBoard[i][j];
+
+          if (i > 0 && this.gameBoard[i - 1][j] === 0) {
+            this.gameBoard[i - 1][j] = n;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i - 1].cells[j].textContent = n;
+            this.board.rows[i - 1].cells[j].classList.toggle(this.name + n);
+          } else if (i > 0 && this.gameBoard[i - 1][j] === n) {
+            this.gameBoard[i - 1][j] = n * 2;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i - 1].cells[j].textContent = n * 2;
+            this.board.rows[i - 1].cells[j].classList.toggle(this.name + n * 2);
+
+            this.count += n * 2;
+            this.getScore();
+
+            this.checking2048();
+          }
+        }
+      }
+    }
+  }
+
+  shiftDown() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.gameBoard[i][j] === 0) {
+          continue;
+        } else {
+          const n = this.gameBoard[i][j];
+
+          if (i < 3 && this.gameBoard[i + 1][j] === 0) {
+            this.gameBoard[i + 1][j] = n;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i + 1].cells[j].textContent = n;
+            this.board.rows[i + 1].cells[j].classList.toggle(this.name + n);
+          } else if (i < 3 && this.gameBoard[i + 1][j] === n) {
+            this.gameBoard[i + 1][j] = n * 2;
+            this.gameBoard[i][j] = 0;
+
+            this.board.rows[i].cells[j].textContent = null;
+            this.board.rows[i].cells[j].classList = 'field-cell';
+
+            this.board.rows[i + 1].cells[j].textContent = n * 2;
+            this.board.rows[i + 1].cells[j].classList.toggle(this.name + n * 2);
+
+            this.count += n * 2;
+            this.getScore();
+
+            this.checking2048();
+          }
+        }
+      }
+    }
+  }
+
+  isEmpty() {
+    let emptyCell = 0;
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.gameBoard[i][j] === 0) {
+          emptyCell++;
+        }
+      }
+    }
+
+    if (emptyCell === 0) {
+      this.messageLose.classList.toggle('hidden');
+      this.messageStart.classList.toggle('hidden');
+
+      this.initialState = 'lose';
+    }
+  }
+
+  checking2048() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.gameBoard[i][j] < this.num2048) {
+          continue;
+        } else {
+          this.initialState = 'win';
+        }
+      }
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,84 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+// import { Game } from '../modules/Game.class';
 
-// Write your code here
+const game = new Game();
+
+function setupInputOnce() {
+  window.addEventListener('keydown', handleInput, { once: true });
+}
+
+function handleInput(e) {
+  switch (e.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      game.randomChip();
+      game.isEmpty();
+
+      if (game.initialState === 'lose') {
+        game.getStatus();
+        break;
+      } else {
+        setupInputOnce();
+        break;
+      }
+
+    case 'ArrowDown':
+      game.moveDown();
+      game.randomChip();
+      game.isEmpty();
+
+      if (game.initialState === 'lose') {
+        game.getStatus();
+        break;
+      } else {
+        setupInputOnce();
+        break;
+      }
+
+    case 'ArrowLeft':
+      game.moveLeft();
+      game.randomChip();
+      game.isEmpty();
+
+      if (game.initialState === 'lose') {
+        game.getStatus();
+        break;
+      } else {
+        setupInputOnce();
+        break;
+      }
+
+    case 'ArrowRight':
+      game.moveRight();
+      game.randomChip();
+      game.isEmpty();
+
+      if (game.initialState === 'lose') {
+        game.getStatus();
+        break;
+      } else {
+        setupInputOnce();
+        break;
+      }
+
+    default:
+      setupInputOnce();
+  }
+}
+
+game.buttonStart.onclick = () => {
+  if (game.buttonStart.textContent === 'Start') {
+    game.buttonStart.textContent = 'Restart';
+    game.buttonStart.classList.toggle('start');
+    game.buttonStart.classList.toggle('restart');
+    game.initialState = 'playing';
+    game.start();
+    setupInputOnce();
+  } else if (game.buttonStart.textContent === 'Restart') {
+    game.restart();
+    setupInputOnce();
+  }
+};
