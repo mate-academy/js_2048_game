4ac07e2a7b9e896cddcf4bea4e0b26490a31351c
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-03-06 12:23:56 +0200

    edit import

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-import Game from '../modules/Game.class.js';
+const Game = require('../modules/Game.class');
 
 const game = new Game();
 
