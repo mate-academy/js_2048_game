9e41826895e49a7005ba49c3f5817c8bc88f331f
André Leão <andrefelipesl123@gmail.com>
2025-03-12 21:21:28 -0300

    Game class implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+  rules: {
+    'no-proto': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
